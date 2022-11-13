69e98ef7a5e9657abeace239c85d7a7b692e7f42
Angel Fernando Campoverde Quezada <angel.campoverde@cern.ch>
2022-11-13 20:33:36 +0800

    settings_.txt prevents different jobs from overwritting settings file

--- bash/submit_q2sys.sh
+++ bash/submit_q2sys.sh
@@ -6,12 +6,13 @@ conda activate py3p10
 
 IJOB=$1
 VERS=$2
+CACHE_NAME=settings_$IJOB.txt
 
-python python/print_settings.py -i $IJOB > .settings.log
+python python/print_settings.py -i $IJOB > $CACHE_NAME 
 
-TRIG=$(cat .settings.log | grep TRIG | awk -F '=' '{print$2}')
-YEAR=$(cat .settings.log | grep YEAR | awk -F '=' '{print$2}')
-BREM=$(cat .settings.log | grep BREM | awk -F '=' '{print$2}')
+TRIG=$(cat $CACHE_NAME | grep TRIG | awk -F '=' '{print$2}')
+YEAR=$(cat $CACHE_NAME | grep YEAR | awk -F '=' '{print$2}')
+BREM=$(cat $CACHE_NAME | grep BREM | awk -F '=' '{print$2}')
 
 echo "$VERS, $YEAR, $TRIG, $BREM"
 python python/get_tables.py -v $VERS -y $YEAR -t $TRIG -b $BREM
