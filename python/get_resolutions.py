324bb0cfe7851a2d5718991c43785b5a2463f89b
Angel Fernando Campoverde Quezada <angel.campoverde@cern.ch>
2023-01-09 22:00:30 +0800

    Can do data fits

--- python/get_resolutions.py
+++ python/get_resolutions.py
@@ -6,6 +6,7 @@ import os
 
 from rk.dilep_reso import calculator as calc_reso
 from rk.dilep_reso import plot_reso
+from rk.boundaries import boundaries
 
 #---------------------------------------------
 class data:
@@ -23,16 +24,35 @@ def get_data(mc=None, trig='ETOS', year='2018'):
         file_path = f'cached/{year}_{trig}_data_ee.root'
 
     rdf = ROOT.RDataFrame('tree', file_path)
+    rdf.is_mc = mc
 
     return rdf
+#---------------------------------------------
+def get_pars(brem):
+    out_dir   = 'output/resolution/mc'
+    json_path = f'{out_dir}/json/par_brem_{brem}.json'
+
+    d_data = utnr.load_json(json_path)
+    for sbin, d_par in d_data.items():
+        if 'inf' in sbin:
+            continue
+
+        del(d_par['mu'])
+        del(d_par['sg'])
+        del(d_par['nsg'])
+
+    d_data_ref = { boundaries(bounds) : d_par for bounds, d_par in d_data.items()}
+
+    return d_data_ref
 #---------------------------------------------
 def get_resolution(rdf, brem):
     arr_bin       = numpy.array([10**4.0, 10**4.2, 10**4.3, 10**4.4])#, 10**4.4, 10**4.5, 10**4.6, 10**4.7, 10**5 ])
     d_bin         = {}
     d_bin['p1']   = arr_bin.tolist()
     d_bin['p2']   = arr_bin.tolist()
-    
-    obj           = calc_reso(rdf, binning=d_bin, fit=True, d_par={})
+
+    d_par         = get_pars(brem) if not rdf.is_mc else {}
+    obj           = calc_reso(rdf, binning=d_bin, fit=True, d_par=d_par)
     obj.plot_dir  = f'{data.out_dir}/plots'
     d_res, d_par  = obj.get_resolution(brem=brem)
 
@@ -46,7 +66,7 @@ def dump_to_json(d_res, d_par, brem):
     utnr.dump_json(d_par, f'{data.out_dir}/json/par_brem_{brem}.json')
 #---------------------------------------------
 def main():
-    rdf = get_data(mc=True)
+    rdf = get_data(mc=False)
     get_resolution(rdf, 0)
 #---------------------------------------------
 if __name__ == '__main__':
