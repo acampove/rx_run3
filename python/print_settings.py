68ce0dc4f733fe0591010db411b3097c66297238
Angel Fernando Campoverde Quezada <angel.campoverde@cern.ch>
2023-01-16 21:39:05 +0800

    Adding -d flag to use datasets instead of years

--- python/print_settings.py
+++ python/print_settings.py
@@ -7,21 +7,27 @@ class data:
     ijob   = None
 
     l_trig = ['ETOS', 'GTIS']
-    l_year = ['2011', '2012', '2015', '2016', '2017', '2018'] + ['r1', 'r2p1']
+    l_year = ['2011', '2012', '2015', '2016', '2017', '2018'] 
+    l_dset = ['r1', 'r2p1', '2017', '2018']
     l_brem = ['0', '1', '2']
 #-----------------------------
 def get_args():
     parser = argparse.ArgumentParser(description='Used to print settings used to run q2 systematics jobs')
     parser.add_argument('-i', '--index', type=int, help='Job index', required=True)
+    parser.add_argument('-d', '--dset' ,           help='Use datasets (merged years) instead of years', action='store_true')
     args = parser.parse_args()
 
     data.ijob = args.index
+    data.dset = args.dset
 #-----------------------------
 def main():
     index = 0
-    njob  = len(data.l_trig) * len(data.l_year) * len(data.l_brem)
+
+    l_sample = data.l_dset if data.dset else data.l_year
+
+    njob  = len(data.l_trig) * len(l_sample) * len(data.l_brem)
     for trig in data.l_trig:
-        for year in data.l_year:
+        for year in l_sample:
             for brem in data.l_brem:
                 if index != data.ijob:
                     index+= 1
