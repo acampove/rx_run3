43d7c91bc3ecec72a11cc67753a3900de38c5f75
Angel Fernando Campoverde Quezada <angel.campoverde@cern.ch>
2023-01-18 18:40:13 +0800

    Not hardcoding year and trigger

--- python/dump_ratios.py
+++ python/dump_ratios.py
@@ -2,30 +2,50 @@ import utils_noroot as utnr
 import argparse
 import ROOT
 import numpy
+import math
 
 from rk.boundaries import boundaries
 
 #-------------------------------------
 class data:
     out_dir = None
-    version = None
-    dset    = None
-    trig    = None
+    version = None 
+    l_kind  = None
+    l_dset  = None
+    l_trig  = None
+    l_brem  = None
 
-    l_kind  = ['par', 'res']
-    l_brem  = [ 0, 1, 2 ]
+    l_all_kind = ['par', 'res']
+    l_all_brem = [ 0, 1, 2 ]
+    l_all_dset = [ 'r1', 'r2p1', '2017', '2018' ]
+    l_all_trig = [ 'ETOS', 'GTIS' ]
+
+    log = utnr.getLogger(__name__)
 #----------------------------
 def par_to_res(d_par):
     d_res = {}
     for sbin, d_fit_par in d_par.items():
         if 'inf' in sbin: 
             continue
-
-        [sg, _] = d_fit_par['sg']
+        if d_fit_par == {}:
+            sg = numpy.nan
+        else:
+            [sg, _] = d_fit_par['sg']
 
         d_res[sbin] = sg
 
     return d_res
+#-------------------------------------
+def divide(num, den):
+    bad_num = isinstance(num, dict) or math.isnan(num)
+    bad_den = isinstance(den, dict) or math.isnan(den)
+
+    if bad_num and bad_den:
+        return numpy.nan
+    elif not bad_num and not bad_den:
+        return num/den
+    else:
+        raise ValueError
 #-------------------------------------
 def calculate_ratio(d_dt_str, d_mc_str):
     d_rat_str = {}
@@ -33,13 +53,19 @@ def calculate_ratio(d_dt_str, d_mc_str):
         val_dt = d_dt_str[key]
         val_mc = d_mc_str[key]
 
-        d_rat_str[key] = val_dt / val_mc
+        try:
+            d_rat_str[key] = divide(val_dt, val_mc)
+        except ValueError:
+            data.log.error(f'Cannot divide resolutions for {key}')
+            data.log.error(f'Data: {val_dt}')
+            data.log.error(f'MC  : {val_mc}')
+            raise ValueError
 
     return d_rat_str
 #-------------------------------------
-def get_ratio(kind, brem):
-    mc_path  = f'output/resolution/mc/json/{data.version}/{data.trig}_{data.dset}/{kind}_brem_{brem}.json'
-    dt_path  = f'output/resolution/data/json/{data.version}/{data.trig}_{data.dset}/{kind}_brem_{brem}.json'
+def get_ratio(kind, brem, trig, dset):
+    mc_path  = f'output/resolution/mc/json/{data.version}/{trig}_{dset}/{kind}_brem_{brem}.json'
+    dt_path  = f'output/resolution/data/json/{data.version}/{trig}_{dset}/{kind}_brem_{brem}.json'
 
     d_mc_str = utnr.load_json(mc_path)
     d_dt_str = utnr.load_json(dt_path)
@@ -56,15 +82,20 @@ def get_ratio(kind, brem):
     return d_rat_str
 #-------------------------------------
 def get_args():
-    parser = argparse.ArgumentParser(description='Used to perform several operations on TCKs')
-    parser.add_argument('-v', '--vers', help='Version of fits', type=str, required=True) 
-    parser.add_argument('-d', '--dset', help='Dataset'        , type=str, required=True, choices=['r1', 'r2p1', '2017', '2018']) 
-    parser.add_argument('-t', '--trig', help='Trigger'        , type=str, required=True, choices=['ETOS', 'GTIS']) 
+    parser = argparse.ArgumentParser(description='Used to create ratio histograms and JSON files of data and MC resolutions')
+    parser.add_argument('-v', '--vers', type=str , help='Version'        , required=True) 
+    parser.add_argument('-b', '--brem', nargs='+', help='Brem category'  , default=data.l_all_brem) 
+    parser.add_argument('-d', '--dset', nargs='+', help='Dataset'        , default=data.l_all_dset) 
+    parser.add_argument('-t', '--trig', nargs='+', help='Trigger'        , default=data.l_all_trig) 
+    parser.add_argument('-k', '--kind', nargs='+', help='From fit or RMS', default=data.l_all_kind) 
     args = parser.parse_args()
 
     data.version = args.vers
-    data.dset    = args.dset
-    data.trig    = args.trig
+    data.l_brem  = args.brem
+    data.l_dset  = args.dset
+    data.l_trig  = args.trig
+    data.l_kind  = args.kind
+
     data.out_dir = utnr.make_dir_path(f'output/resolution/ratio/{data.version}')
 #-------------------------------------
 def convert_to_hist(d_rat_str):
@@ -95,17 +126,18 @@ def convert_to_hist(d_rat_str):
 #-------------------------------------
 def main():
     get_args()
-    ofile = ROOT.TFile(f'{data.out_dir}/{data.dset}_{data.trig}.root', 'recreate')
-    for kind in data.l_kind:
-        for brem in data.l_brem:
-            d_rat_str = get_ratio(kind, brem)
-            utnr.dump_json(d_rat_str, f'{data.out_dir}/{data.trig}_{data.dset}/{kind}_brem_{brem}.json')
+    for trig in data.l_trig:
+        for dset in data.l_dset:
+            ofile = ROOT.TFile(f'{data.out_dir}/{dset}_{trig}.root', 'recreate')
+            for kind in data.l_kind:
+                for brem in data.l_brem:
+                    d_rat_str = get_ratio(kind, brem, trig, dset)
+                    utnr.dump_json(d_rat_str, f'{data.out_dir}/{trig}_{dset}/{kind}_brem_{brem}.json')
 
-            h_rat = convert_to_hist(d_rat_str)
-            h_rat.SetName(f'h_{kind}_brem_{brem}')
-            h_rat.Write()
-
-    ofile.Close()
+                    h_rat = convert_to_hist(d_rat_str)
+                    h_rat.SetName(f'h_{kind}_brem_{brem}')
+                    h_rat.Write()
+            ofile.Close()
 #-------------------------------------
 if __name__ == '__main__':
     main()
