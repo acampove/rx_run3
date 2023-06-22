48a000f5cd490ae44b21415665c5115208156d48
Angel Campoverde <acampove@cern.ch>
2023-06-22 16:09:52 +0800

    Reorganizing

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import glob
 
 setup(
     name                ='q2_syst',
-    version             ='0.0.3',
+    version             ='0.0.4',
     description         ='Project used to calculate q2 smearing factors and the systematics',
     long_description    ='',
-    scripts             = glob.glob('jobs/*') + glob.glob('src/q2_syst/*'),
+    scripts             = glob.glob('jobs/*') + glob.glob('scripts/*'),
     package_dir         = {'' : 'src'},
     install_requires    = open('requirements.txt').read().splitlines(),
 )
