86686544380a3fbafabe69650561548d82d5d70b
Angel Campoverde <acampove@cern.ch>
2023-06-22 16:31:44 +0800

    Working

--- tests/model.py
+++ tests/model.py
@@ -1,13 +1,31 @@
-from q2_syst.model import model as q2model
+import os
+import matplotlib.pyplot as plt
 
-from logzero import logger as log
+from logzero       import logger as log
+from zutils.plot   import plot   as zfp
+from q2_syst.model import model  as q2model
+from fitter        import zfitter
 
+
+#-------------------------------
+def plot_model(pdf, name):
+    dir_name = 'tests/model'
+    os.makedirs(dir_name, exist_ok=True)
+
+    dat= pdf.create_sampler(n=10000)
+    ftr=zfitter(pdf, dat)
+    res=ftr.fit()
+
+    obj= zfp(data=dat, model=pdf, result=res)
+    obj.plot()
+    plt.savefig(f'{dir_name}/{name}.png')
+    plt.close('all')
 #-------------------------------
 def get_sim_par():
     d_sim_par = {}
-    d_sim_par['mu_1'] = [100, 0]
-    d_sim_par['mu_2'] = [100, 0]
-    d_sim_par['mu_3'] = [100, 0]
+    d_sim_par['mu_1'] = [3100, 0]
+    d_sim_par['mu_2'] = [3100, 0]
+    d_sim_par['mu_3'] = [3100, 0]
 
     d_sim_par['sg_1'] = [10, 0]
     d_sim_par['sg_2'] = [10, 0]
@@ -19,13 +37,17 @@ def test_1():
     obj=q2model()
     pdf=obj.get_pdf(is_signal=False, split_by_nspd=False)
 
+    plot_model(pdf, 'test_1')
     log.info(f'Passed test_1')
+    obj.__del__()
 #-------------------------------
 def test_2():
     obj=q2model()
     pdf=obj.get_pdf(is_signal=True, split_by_nspd=False)
 
+    plot_model(pdf, 'test_2')
     log.info(f'Passed test_2')
+    obj.__del__()
 #-------------------------------
 def test_3():
     d_sim_par = get_sim_par()
@@ -33,10 +55,15 @@ def test_3():
     obj=q2model(d_sim_par=d_sim_par)
     pdf=obj.get_pdf(is_signal=False, split_by_nspd=True)
 
+    plot_model(pdf, 'test_3')
     log.info(f'Passed test_3')
+    obj.__del__()
 #-------------------------------
-if __name__ == '__main__':
+def main():
     test_1()
     test_2()
     test_3()
+#-------------------------------
+if __name__ == '__main__':
+    main()
 
