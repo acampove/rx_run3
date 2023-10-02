2490ddb556d8c9051f76463a2dc50bcc0fed7ea2
Angel Fernando Campoverde Quezada <angel.campoverde@cern.ch>
2023-10-02 19:43:09 +0800

    Updating

--- tests/data_set.py
+++ tests/data_set.py
@@ -1,9 +1,13 @@
-from q2_syst.data_set import dset
+import logging
 
+from q2_syst.data_set import data_set 
 
 #-----------------------------------
 def test_simple():
-    obj = dset(is_mc=True, trigger='ETOS', dset='r1')
+    obj = data_set(is_mc=True, trigger='ETOS', dset='r1')
+    obj.log.setLevel(logging.DEBUG)
+    #obj.nentries = 1000
+    obj.plt_dir  = 'tests/data_set/simple'
     rdf = obj.get_rdf()
 #-----------------------------------
 if __name__ == '__main__':
