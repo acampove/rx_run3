80595aa3474589e6c2adaca8a5c3cc7a3a25cbfb
Angel Campoverde <angel.campoverde@cern.ch>
2024-08-28 08:46:10 +0800

    Usinng xroot to read config

--- src/data_checks/utilities.py
+++ src/data_checks/utilities.py
@@ -1,10 +1,16 @@
 import toml
 import os
+import utils_noroot as utnr
 
+from XRootD              import client
+from functools           import cache
 from importlib.resources import files
 from log_store           import log_store
 
 log=log_store.add_logger('data_checks:utilities')
+
+@utnr.timeit
+@cache
 #--------------------------------------
 def load_config(cfg_nam):
     '''
@@ -16,13 +22,20 @@ def load_config(cfg_nam):
     -----------------
     d_config (dict): Dictionary with configuration
     '''
-    cfg_path = files('data_checks_data').joinpath(f'{cfg_nam}.toml')
-    if not os.path.isfile(cfg_path):
-        log.error(f'Cannot find config: {cfg_path}')
-        raise FileNotFoundError
+    xrd_path = f'root://x509up_u1000@eoslhcb.cern.ch//eos/lhcb/grid/user/lhcb/user/a/acampove/run3/ntupling/config/{cfg_nam}.toml'
+    log.debug(f'Loading: {xrd_path}')
+    with client.File() as ifile:
+        status, into = ifile.open(xrd_path)
+        if not status.ok:
+            log.error(status.message)
+            raise
 
-    log.debug(f'Loading: {cfg_path}')
+        status, file_content = ifile.read()
+        if not status.ok:
+            log.error(status.message)
+            raise
 
-    return toml.load(cfg_path)
+        toml_str  = file_content.decode('utf-8')
+        return toml.loads(toml_str)
 #--------------------------------------
 
