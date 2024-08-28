8fcd249b187d507a60c71467f7d795361d01e2ab
Angel Campoverde <angel.campoverde@cern.ch>
2024-08-28 13:17:22 +0800

    Using input file name for output

--- src/data_checks/filter_file.py
+++ src/data_checks/filter_file.py
@@ -204,12 +204,12 @@ class FilterFile:
         opts.fMode= 'update'
         opts.fCompressionLevel=self._cfg_dat['saving']['compression']
 
-        file_path = self._cfg_dat['saving']['out_path']
+        file_name = os.path.basename(self._file_path)
         for rdf in tqdm.tqdm(l_rdf, ascii=' -'):
             tree_name = rdf.name
             l_branch  = rdf.l_branch
 
-            rdf.Snapshot(tree_name, file_path, l_branch, opts)
+            rdf.Snapshot(tree_name, file_name, l_branch, opts)
     #--------------------------------------
     def _add_lumi_rdf(self, l_rdf):
         if not self._has_lumitree:
