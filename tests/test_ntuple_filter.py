82d9123a9996b6b0964fc1db2e46ab514b82c34a
Angel Campoverde <angel.campoverde@cern.ch>
2024-08-29 08:28:13 +0800

    Updating

--- tests/test_ntuple_filter.py
+++ tests/test_ntuple_filter.py
@@ -6,13 +6,18 @@ def set_log():
     log_store.set_level('data_checks:ntuple_filter', 10)
     log_store.set_level('rx_scripts:atr_mgr:mgr'   , 30)
 #---------------------------------------
-def test_simple():
-    obj=ntuple_filter(dataset='dt_2024_turbo', cfg_ver='comp', index=1, ngroup=1211)
+def test_dt():
+    obj=ntuple_filter(dataset='dt_2024_turbo', cfg_ver='comp', index=1, ngroup=100)
+    obj.filter()
+#---------------------------------------
+def test_mc():
+    obj=ntuple_filter(dataset='mc_2024_turbo', cfg_ver='comp', index=1, ngroup=10)
     obj.filter()
 #---------------------------------------
 def  main():
     set_log()
-    test_simple()
+    test_mc()
+    test_dt()
 #---------------------------------------
 if __name__  == '__main__':
     main()
