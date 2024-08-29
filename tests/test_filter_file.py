b9c02d6069af7c5140e97272e7ac6a333184eae4
Angel Campoverde <angel.campoverde@cern.ch>
2024-08-29 09:38:29 +0800

    Updating

--- tests/test_filter_file.py
+++ tests/test_filter_file.py
@@ -4,21 +4,29 @@ from log_store               import log_store
 import utils_noroot as utnr
 #--------------------------------------
 class data:
-    file_path='/home/acampove/data/aprod/raw/00231366_00000013_1.ftuple.root'
+    dt_path='/home/acampove/data/aprod/raw/dt/00231366_00000013_1.ftuple.root'
+    mc_path='/home/acampove/data/aprod/raw/mc/BdKstmumu.root'
 #--------------------------------------
 def set_log():
-    log_store.set_level('data_checks:FilterFile', 10)
     log_store.set_level('rx_scripts:atr_mgr:mgr', 30)
+    log_store.set_level('data_checks:utilities' , 30)
+    log_store.set_level('data_checks:FilterFile', 10)
+    log_store.set_level('data_checks:selector'  , 10)
 #--------------------------------------
-def test_simple():
-    obj=FilterFile(file_path=data.file_path, cfg_nam='dt_2024_turbo')
+def test_dt():
+    obj=FilterFile(kind='any_kind', file_path=data.dt_path, cfg_nam='dt_2024_turbo_comp')
+    obj.run()
+#--------------------------------------
+def test_mc():
+    obj=FilterFile(kind='any_kind', file_path=data.mc_path, cfg_nam='mc_2024_turbo_comp')
     obj.run()
 #--------------------------------------
 def main():
-    utnr.timer_on=True
+    utnr.timer_on=False
 
     set_log()
-    test_simple()
+    test_mc()
+    test_dt()
 #--------------------------------------
 if __name__ == '__main__':
     main()
