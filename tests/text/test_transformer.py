98d2ff3789b0b2067b4711915c7f320f575ad90c
Angel Campoverde <angel.campoverde@cern.ch>
2024-08-31 13:38:33 +0800

    Updating

--- tests/text/test_transformer.py
+++ tests/text/test_transformer.py
@@ -21,6 +21,14 @@ def test_with_path():
     trf=txt_trf(txt_path=data.txt, cfg_path=data.cfg)
     trf.save_as(out_path=data.out)
 #-------------------------------------------------------------
+def test_settings():
+    data.out = '/tmp/dmu_test/settings' 
+    cfg      = files('dmu_data').joinpath('text/transform_set.toml')
+    txt      = files('dmu_data').joinpath('text/transform_set.txt')
+
+    trf=txt_trf(txt_path=txt, cfg_path=cfg)
+    trf.save_as(out_path=data.out)
+#-------------------------------------------------------------
 def initialize():
     logging.basicConfig()
     log=logging.getLogger('tests:text:transformer')
@@ -37,6 +45,7 @@ def initialize():
 def main():
     initialize()
 
+    test_settings()
     test_with_path()
     test_with_path_ext()
 #-------------------------------------------------------------
