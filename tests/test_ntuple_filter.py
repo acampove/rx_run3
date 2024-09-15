d16092627b72f61bb06ce9f9ff6e766f72b4992a
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-15 13:33:12 +0800

    Using only one file for tests

--- tests/test_ntuple_filter.py
+++ tests/test_ntuple_filter.py
@@ -9,17 +9,17 @@ def set_log():
     log_store.set_level('rx_scripts:atr_mgr:mgr'   , 30)
 # ---------------------------------------
 def test_dt():
-    obj = ntuple_filter(dataset='dt_2024_turbo', cfg_ver='comp', index=1, ngroup=100)
+    obj = ntuple_filter(dataset='dt_2024_turbo', cfg_ver='comp', index=1, ngroup=1211)
     obj.filter()
 # ---------------------------------------
 def test_mc():
-    obj = ntuple_filter(dataset='mc_2024_turbo', cfg_ver='comp', index=1, ngroup=10)
+    obj = ntuple_filter(dataset='mc_2024_turbo', cfg_ver='comp', index=1, ngroup=71)
     obj.filter()
 # ---------------------------------------
 def  main():
     set_log()
-    test_mc()
     test_dt()
+    test_mc()
 # ---------------------------------------
 
 
