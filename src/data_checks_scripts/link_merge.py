4f0a41b34fe53e75abefc733c042acf221b56b1f
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-18 15:22:37 +0800

    Implementing some functions
    Can find paths
    split them into groups
    Find info from paths

--- src/data_checks_scripts/link_merge.py
+++ src/data_checks_scripts/link_merge.py
@@ -3,6 +3,9 @@
 Script used to link ntuples properly and merge them
 '''
 
+import re
+import os
+import glob
 import argparse
 
 from dataclasses import dataclass
@@ -16,7 +19,9 @@ class Data:
     '''
     Class used to hold shared data
     '''
-    job : str
+    job     : str
+    inp_dir : str = '/publicfs/lhcb/user/campoverde/Data/RK/'
+    rgx     : str = r'(dt|mc)_(\d{4}).*ftuple_Hlt2RD_(.*)\.root'
 # ---------------------------------
 def _get_args():
     '''
@@ -32,8 +37,17 @@ def _get_paths():
     '''
     Returns list of paths to ROOT files corresponding to a given job
     '''
+    path_wc = f'{Data.inp_dir}/.run3/{Data.job}/*.root'
+    l_path  = glob.glob(path_wc)
 
-    return []
+    npath   = len(l_path)
+    if npath == 0:
+        log.error(f'No file found in: {path_wc}')
+        raise FileNotFoundError
+
+    log.info(f'Found {npath} paths')
+
+    return l_path
 # ---------------------------------
 def _split_paths(l_path):
     '''
@@ -45,21 +59,93 @@ def _split_paths(l_path):
     npath = len(l_path)
     log.info(f'Splitting {npath} paths into categories')
 
-    return {'none' : []}
+
+    d_info_path = {}
+    for path in l_path:
+        info = _info_from_path(path)
+        if info not in d_info_path:
+            d_info_path[info] = []
+
+        d_info_path[info].append(path)
+
+    return d_info_path
+# ---------------------------------
+def _info_from_path(path):
+    '''
+    Will pick a path to a ROOT file
+    Will return tuple with information associated to file
+    This is needed to name output file and directories
+    '''
+
+    name = os.path.basename(path)
+    mtc  = re.match(Data.rgx, name)
+    if not mtc:
+        log.error(f'Cannot find kind in {name} using {Data.rgx}')
+        raise ValueError
+
+    try:
+        [dtmc, year, decay] = mtc.groups()
+    except ValueError as exc:
+        log.error(f'Expected three elements in: {mtc.groups()}')
+        raise ValueError from exc
+
+    if 'MuMu' in decay:
+        chan = 'mm'
+    elif 'EE' in decay:
+        chan = 'ee'
+    else:
+        log.error(f'Cannot find channel in {decay}')
+        raise ValueError
+
+    dtmc = 'data' if dtmc == 'dt' else dtmc
+
+    kind = _kind_from_decay(decay) 
+
+    return dtmc, chan, kind, year
+# ---------------------------------
+def _kind_from_decay(decay):
+    '''
+    Will take string symbolizing decay
+    Will return kind of sample associated, e.g. analysis, calibration, same sign...
+    '''
+
+    # TODO: This needs a config file 
+    if decay in ['B0ToKpPimEE', 'B0ToKpPimMuMu']:
+        return 'ana_cut_bd'
+
+    if decay in ['BuToKpEE', 'BuToKpMuMu']:
+        return 'ana_cut_bp'
+
+    if decay in ['LbToLEE_LL', 'LbToLMuMu_LL']:
+        return 'ana_cut_lb'
+
+
+    if decay in ['B0ToKpPimEE_MVA', 'B0ToKpPimMuMu_MVA']:
+        return 'ana_mva_bd'
+
+    if decay in ['BuToKpEE_MVA', 'BuToKpMuMu_MVA']:
+        return 'ana_mva_bp'
+
+    if decay in ['LbToLEE_LL_MVA', 'LbToLMuMu_LL_MVA']:
+        return 'ana_mva_lb'
+
+    log.error(f'Unrecognized decay: {decay}')
+    raise ValueError
 # ---------------------------------
 def _link_paths(kind, l_path):
     '''
     Makes symbolic links of list of paths of a specific kind
     '''
     npath = len(l_path)
-    log.info(f'Linking {npath} {kind} paths')
+    log.info(f'Linking {npath} paths {kind}')
 # ---------------------------------
 def _merge_paths(kind, l_path):
     '''
     Merge ROOT files of a specific kind
     '''
     npath = len(l_path)
-    log.info(f'Merging {npath} {kind} paths')
+    log.info(f'Merging {npath} paths {kind}')
+    log.info('')
 # ---------------------------------
 def main():
     '''
