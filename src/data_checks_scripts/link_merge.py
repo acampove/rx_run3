7cd717837e63044e144dba9300f8956b4467f8a5
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-18 14:30:16 +0800

    Adding skeleton code

--- src/data_checks_scripts/link_merge.py
+++ src/data_checks_scripts/link_merge.py
@@ -7,6 +7,9 @@ import argparse
 
 from dataclasses import dataclass
 
+from log_store import log_store
+
+log = log_store.add_logger('rx:data_checks:link_merge')
 # ---------------------------------
 @dataclass
 class Data:
@@ -25,11 +28,49 @@ def _get_args():
 
     Data.job = args.job
 # ---------------------------------
+def _get_paths():
+    '''
+    Returns list of paths to ROOT files corresponding to a given job
+    '''
+
+    return []
+# ---------------------------------
+def _split_paths(l_path):
+    '''
+    Takes list of paths to ROOT files
+    Splits them into categories and returns a dictionary:
+
+    category : [path_1, path_2, ...]
+    '''
+    npath = len(l_path)
+    log.info(f'Splitting {npath} paths into categories')
+
+    return {'none' : []}
+# ---------------------------------
+def _link_paths(kind, l_path):
+    '''
+    Makes symbolic links of list of paths of a specific kind
+    '''
+    npath = len(l_path)
+    log.info(f'Linking {npath} {kind} paths')
+# ---------------------------------
+def _merge_paths(kind, l_path):
+    '''
+    Merge ROOT files of a specific kind
+    '''
+    npath = len(l_path)
+    log.info(f'Merging {npath} {kind} paths')
+# ---------------------------------
 def main():
     '''
     Script starts here
     '''
     _get_args()
+    l_path = _get_paths()
+    d_path = _split_paths(l_path)
+    for kind, l_path in d_path.items():
+        _link_paths(kind, l_path)
+        _merge_paths(kind, l_path)
 # ---------------------------------
 if __name__ == '__main__':
     main()
