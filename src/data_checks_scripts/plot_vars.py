77f05bf4e490fc60e4efcebfc56f8dfabbef8734
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-20 16:44:16 +0800

    Updated to use plotter from dmu

--- src/data_checks_scripts/plot_vars.py
+++ src/data_checks_scripts/plot_vars.py
@@ -5,148 +5,88 @@ Script used to make diagnostic plots from filtered ntuples
 #!/usr/bin/env python3
 
 import os
-import glob
 import argparse
 
-from functools   import cache
 from dataclasses import dataclass
 
-import ROOT
 import mplhep
 import matplotlib.pyplot as plt
 
+from ROOT                 import RDataFrame
 from log_store            import log_store
+from dmu.plotting.plotter import Plotter
 
 import data_checks.utilities as ut
 
-from data_checks.selector import selector
 
-log=log_store.add_logger('data_checks:yield_vs_lumi')
-#-------------------------------------
+log=log_store.add_logger('data_checks:plot_vars')
+# -------------------------------------
 @dataclass
 class Data:
     '''
     Class meant to store shared data
     '''
-    l_trig = [
-            'Hlt2RD_BuToKpEE',
-            'Hlt2RD_BuToKpMuMu',
-            'Hlt2RD_B0ToKpPimEE',
-            'Hlt2RD_B0ToKpPimMuMu',
-            'Hlt2RD_LbToLEE_LL',
-            'Hlt2RD_LbToLMuMu_LL',
-            ]
-
+    l_dst   : list
     log_lvl : int
-    dat_dir : str
-    plt_dir : str
     cfg_nam : str
     cfg_dat : dict
-#-------------------------------------
+
+    year = 2024
+    vers = 'v1'
+# -------------------------------------
 def _get_args():
     parser = argparse.ArgumentParser(description='Used to plot yields of cut based vs MVA based lines vs luminosity')
-    parser.add_argument('-t', '--trig', nargs='+', help='HLT2 trigger', choices=Data.l_trig, default=Data.l_trig)
-    parser.add_argument('-d', '--dir' , type =str, help='Path to ROOT files', required=True)
+    parser.add_argument('-d', '--dst' , nargs='+', help='Type of dataset to plot, e.g. data_ana_cut_bp_ee', required=True)
+    parser.add_argument('-c', '--cfg' , type =str, help='Name of config file specifying what to plot and how', required=True)
     parser.add_argument('-l', '--log' , type =int, help='Log level', default=20)
     args = parser.parse_args()
 
-    Data.l_trig = args.trig
-    Data.dat_dir= args.dir
-    Data.log_lvl= args.log
-#-------------------------------------
-def _set_logs():
-    log_store.set_level('data_checks:yield_vs_lumi', Data.log_lvl)
-    if Data.log_lvl == 10:
-        log_store.set_level('data_checks:selector', 10)
-#-------------------------------------
-@cache
-def _get_paths():
-    root_wc = f'{Data.dat_dir}/*.root'
-    l_path  = glob.glob(root_wc)
-
-    npath = len(l_path)
-    if npath == 0:
-        log.error(f'No ROOT files found in: {root_wc}')
-        raise
-
-    log.debug(f'Found {npath} files')
-
-    return l_path
-#-------------------------------------
+    Data.l_dst   = args.dst
+    Data.cfg_nam = args.cfg
+    Data.log_lvl = args.log
+# -------------------------------------
 def _define_vars(rdf):
     d_def = Data.cfg_dat['define_vars']
+    log.info('Defining variables')
     for name, expr in d_def.items():
+        log.debug(f'{name} <- {expr}')
         rdf = rdf.Define(name, expr)
 
     return rdf
-#-------------------------------------
-def _get_rdf(trig):
-    l_path = _get_paths()
-    rdf = ROOT.RDataFrame(trig, l_path)
-    rdf = _define_vars(rdf)
-    nev = rdf.Count().GetValue()
-    log.debug(f'Found {nev} entries in: {trig}')
+# -------------------------------------
+def _get_rdf(dset):
+    '''
+    Will take label of dataset, e.g. ctrl_BuToKpEE_ana_ee
+    Will return ROOT dataframe
+    '''
+    tree_name = 'KMM' if 'MuMu' in dset else 'KEE'
+    pfs_dir   = os.environ['PFSDIR']
+    fpath     = f'{pfs_dir}/Data/RK/{dset}/{Data.vers}/{Data.year}.root'
 
-    obj   = selector(rdf=rdf, cfg_nam=Data.cfg_nam, is_mc=True)
-    d_rdf = obj.run(as_cutflow=True)
+    log.debug(f'Loading: {fpath}/{tree_name}')
 
-    return d_rdf
-#-------------------------------------
-def _plot_var(d_rdf, var):
-    plt.figure(var)
-    for name, rdf in d_rdf.items():
-        minx, maxx, bins = Data.cfg_dat['plots'][var]['binning']
-        yscale           = Data.cfg_dat['plots'][var]['yscale' ]
-        [xname, yname]   = Data.cfg_dat['plots'][var]['labels' ]
+    rdf    = RDataFrame(tree_name, fpath)
+    rdf    = _define_vars(rdf)
+    nev    = rdf.Count().GetValue()
+    log.debug(f'Found {nev} entries in: {fpath}')
 
-        arr_mass = rdf.AsNumpy([var])[var]
-        plt.hist(arr_mass, bins=bins, range=[minx, maxx], histtype='step', label=name)
-        plt.yscale(yscale)
-        plt.xlabel(xname)
-        plt.ylabel(yname)
-
-    if var in ['B_const_mass_M', 'B_M']:
-        plt.axvline(x=5280, color='r', label='$B^+$', linestyle=':')
-    elif var == 'Jpsi_M':
-        plt.axvline(x=3096, color='r', label='$J/\psi$', linestyle=':')
-
-    plt.legend()
-
-    plot_path = f'{Data.plt_dir}/{var}.png'
-    log.info(f'Saving to: {plot_path}')
-    plt.tight_layout()
-    plt.savefig(plot_path)
-    plt.close()
-#-------------------------------------
-def _initialize(trig):
-    Data.cfg_nam = {
-            'Hlt2RD_BuToKpEE'      : 'bukee_opt',
-            'Hlt2RD_B0ToKpPimMuMu' : 'bdkstmm_opt',
-            }[trig]
-
-    ut.local_config=True
-    Data.cfg_dat = ut.load_config(Data.cfg_nam)
-
-    plt_dir = Data.cfg_dat['saving']['plt_dir']
-    os.makedirs(plt_dir, exist_ok=True)
-    ut.local_config=True
-
-    Data.plt_dir = plt_dir
-
-    plt.style.use(mplhep.style.LHCb2)
-#-------------------------------------
+    return rdf
+# -------------------------------------
 def main():
     '''
     Script starts here
     '''
     _get_args()
-    _set_logs()
+    plt.style.use(mplhep.style.LHCb2)
 
-    for trig in Data.l_trig:
-        _initialize(trig)
-        d_rdf = _get_rdf(trig)
-        for var in Data.cfg_dat['plots']:
-            _plot_var(d_rdf, var)
-#-------------------------------------
+    ut.local_config=True
+    Data.cfg_dat = ut.load_config(Data.cfg_nam)
+    log_store.set_level('data_checks:plot_vars', Data.log_lvl)
+
+    d_rdf = { dset : _get_rdf(dset) for dset in Data.l_dst }
+
+    ptr   = Plotter(d_rdf=d_rdf, cfg=Data.cfg_dat)
+    ptr.run()
+# -------------------------------------
 if __name__ == '__main__':
     main()
