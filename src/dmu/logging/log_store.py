f4e4abdbcaec34ee5882d8f242c977273d7806ba
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-21 10:23:22 +0800

    Adding files

---
+++ src/dmu/logging/log_store.py
@@ -0,0 +1,108 @@
+'''
+Module holding LogStore 
+'''
+
+import logging
+
+#------------------------------------------------------------
+class StoreFormater(logging.Formatter):
+    '''
+    Custom formatter
+    '''
+    LOG_COLORS = {
+        logging.DEBUG   : '\033[94m',     # Gray
+        logging.INFO    : '\033[37m',     # White
+        logging.WARNING : '\033[93m',     # Yellow
+        logging.ERROR   : '\033[91m',     # Red
+        logging.CRITICAL: '\033[1;91m'    # Bold Red
+    }
+
+    RESET_COLOR = '\033[0m'  # Reset color to default
+
+    def format(self, record):
+        log_color = self.LOG_COLORS.get(record.levelno, self.RESET_COLOR)
+        message   = super().format(record)
+
+        return f'{log_color}{message}{self.RESET_COLOR}'
+#------------------------------------------------------------
+class LogStore:
+    '''
+    Class used to make loggers, set log levels, print loggers, e.g. interface to logging/logzero, etc.
+    '''
+    #pylint: disable = invalid-name
+    d_logger      = {}
+    d_levels      = {}
+    log_level     = logging.INFO
+    is_configured = False
+    #--------------------------
+    @staticmethod
+    def add_logger(name=None):
+        '''
+        Will use underlying logging library logzero/logging, etc to make logger
+
+        name (str): Name of logger
+        '''
+
+        if   name is None:
+            raise ValueError('Logger name missing')
+
+        if name in log_store.d_logger:
+            raise ValueError(f'Logger name {name} already found')
+
+        level  = log_store.log_level if name not in log_store.d_levels else log_store.d_levels[name]
+
+        logger = logging.getLogger(name=name)
+        logger.setLevel(level)
+
+        hnd= logging.StreamHandler()
+        hnd.setLevel(level)
+
+        fmt= StoreFormater('%(asctime)s - %(filename)s:%(lineno)d - %(message)s', datefmt='%H:%M:%S')
+        hnd.setFormatter(fmt)
+
+        if logger.hasHandlers():
+            logger.handlers.clear()
+
+        logger.addHandler(hnd)
+
+        log_store.d_logger[name] = logger
+
+        return logger
+    #--------------------------
+    @staticmethod
+    def set_level(name, value):
+        '''
+        Will set the level of a logger, it not present yet, it will store the level and set it when created.
+        Parameters:
+        -----------------
+        name (str): Name of logger
+        value (int): 10 debug, 20 info, 30 warning
+        '''
+
+        if name in log_store.d_logger:
+            lgr=log_store.d_logger[name]
+            lgr.handlers[0].setLevel(value)
+            lgr.setLevel(value)
+        else:
+            log_store.d_levels[name] = value
+    #--------------------------
+    @staticmethod
+    def show_loggers():
+        '''
+        Will print loggers and log levels in two columns
+        '''
+        print(80 * '-')
+        print(f'{"Name":<60}{"Level":<20}')
+        print(80 * '-')
+        for name, logger in log_store.d_logger.items():
+            print(f'{name:<60}{logger.level:<20}')
+    #--------------------------
+    @staticmethod
+    def set_all_levels(level):
+        '''
+        Will set all loggers to this level (int)
+        '''
+        for name, logger in log_store.d_logger.items():
+            logger.setLevel(level)
+            print(f'{name:<60}{"->":20}{logger.level:<20}')
+#------------------------------------------------------------
---
+++ tests/logging/test_log_store.py
@@ -0,0 +1,59 @@
+'''
+Unit test for LogStore class
+'''
+
+from dmu.logging.LogStore import LogStore 
+
+# --------------------------------
+def test_show():
+    '''
+    Test for show_loggers
+    '''
+    LogStore.set_level('sho_1', logzero.WARNING)
+
+    LogStore.add_logger('sho_1')
+    LogStore.add_logger('sho_2')
+
+    LogStore.show_loggers()
+# --------------------------------
+def test_messages():
+    '''
+    Tests each level
+    '''
+    log = LogStore.add_logger('msg')
+    LogStore.set_level('msg', 10)
+
+    log.debug('debug')
+    log.info('info')
+    log.warning('warning')
+    log.error('error')
+    log.critical('critical')
+# --------------------------------
+def test_level():
+    '''
+    Test for level setting
+    '''
+    LogStore.add_logger('lvl_10')
+    LogStore.add_logger('lvl_20')
+
+    LogStore.set_level('lvl_10', 10)
+    LogStore.set_level('lvl_20', 20)
+
+    LogStore.add_logger('lvl_30')
+    LogStore.add_logger('lvl_40')
+
+    LogStore.set_level('lvl_30', 30)
+    LogStore.set_level('lvl_40', 40)
+
+    LogStore.show_loggers()
+# --------------------------------
+def main():
+    '''
+    Tests start here
+    '''
+    test_messages()
+    test_level()
+    test_show()
+# --------------------------------
+if __name__ == '__main__':
+    main()
