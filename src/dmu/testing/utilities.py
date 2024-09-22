8eb7ab92fdcfbf95c7ec5705bc63a31854c4a1b4
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-22 10:55:10 +0800

    Adding module
    Will put unit tests shared functions here

---
+++ src/dmu/testing/utilities.py
@@ -0,0 +1,24 @@
+'''
+Module containing utility functions needed by unit tests
+'''
+
+from importlib.resources import files
+
+import yaml
+
+# -------------------------------
+def get_config(name : str | None = None):
+    '''
+    Takes path to the YAML config file, after `dmu_data`
+    Returns dictionary with config
+    '''
+    if name is None:
+        raise ValueError('Name not pased')
+
+    cfg_path = files('dmu_data').joinpath(name)
+    cfg_path = str(cfg_path)
+    with open(cfg_path, encoding='utf-8') as ifile:
+        d_cfg = yaml.safe_load(ifile)
+
+    return d_cfg
+# -------------------------------
