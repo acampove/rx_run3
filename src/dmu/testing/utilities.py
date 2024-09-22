306c0e1d10d2f0a153a78d4e2d5a78ff2d5b7aed
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-22 13:05:56 +0800

    Adding toy data making function
    Multiple tests need input toy data, will be shared

--- src/dmu/testing/utilities.py
+++ src/dmu/testing/utilities.py
@@ -2,10 +2,46 @@
 Module containing utility functions needed by unit tests
 '''
 
+from dataclasses         import dataclass
 from importlib.resources import files
 
+import numpy
 import yaml
+from ROOT import RDF
 
+from dmu.logging.log_store import LogStore
+
+log = LogStore.add_logger('dmu:ml:utilities')
+# -------------------------------
+@dataclass
+class Data:
+    '''
+    Class storing shared data
+    '''
+    nentries = 3000
+# -------------------------------
+def get_rdf(kind : str | None = None):
+    '''
+    Return ROOT dataframe with toy data
+    '''
+    d_data = {}
+    if   kind == 'sig':
+        d_data['w'] = numpy.random.normal(0, 1, size=Data.nentries)
+        d_data['x'] = numpy.random.normal(0, 1, size=Data.nentries)
+        d_data['y'] = numpy.random.normal(0, 1, size=Data.nentries)
+        d_data['z'] = numpy.random.normal(0, 1, size=Data.nentries)
+    elif kind == 'bkg':
+        d_data['w'] = numpy.random.normal(1, 1, size=Data.nentries)
+        d_data['x'] = numpy.random.normal(1, 1, size=Data.nentries)
+        d_data['y'] = numpy.random.normal(1, 1, size=Data.nentries)
+        d_data['z'] = numpy.random.normal(1, 1, size=Data.nentries)
+    else:
+        log.error(f'Invalid kind: {kind}')
+        raise ValueError
+
+    rdf = RDF.FromNumpy(d_data)
+
+    return rdf
 # -------------------------------
 def get_config(name : str | None = None):
     '''
