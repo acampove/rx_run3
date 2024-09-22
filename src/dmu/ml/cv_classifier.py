2aa57855931ce8df251d2322186f8e5fa606aaa7
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-22 14:51:57 +0800

    Checking hashes before predicting
    Making sure training data is not used when predicting from model

--- src/dmu/ml/cv_classifier.py
+++ src/dmu/ml/cv_classifier.py
@@ -1,12 +1,22 @@
 '''
 Module holding cv_classifier class
 '''
+import hashlib
+
+import pandas as pnd
 
 from sklearn.ensemble        import GradientBoostingClassifier
 
 from dmu.logging.log_store import LogStore
 
 log = LogStore.add_logger('dmu:ml:CVClassifier')
+
+# ---------------------------------------
+class CVSameData(Exception):
+    '''
+    Will be raised if a model is been evaluated with a dataset such that at least one of the
+    samples was also used for the training
+    '''
 # ---------------------------------------
 class CVClassifier(GradientBoostingClassifier):
     '''
@@ -17,42 +27,90 @@ class CVClassifier(GradientBoostingClassifier):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self._l_allowed_attrs = ['dset_hash', 'sset_hash']
+        self._s_hash = set()
 
         self._data = {}
     # ----------------------------------
-    def __setitem__(self, key, value):
-        '''
-        Used to set values of dataset and subdataset (from cross-validation splitting) hashes
-        '''
-        if key not in self._l_allowed_attrs:
-            raise ValueError(f'Invalid item: {key}, choose from {self._l_allowed_attrs}')
-
-        self._data[key] = value
-
-    # ----------------------------------
-    def __getitem__(self, key):
-        '''
-        Used to get values of dataset and subdataset (from cross-validation splitting) hashes
-        '''
-        if key not in self._l_allowed_attrs:
-            raise ValueError(f'Invalid item: {key}, choose from {self._l_allowed_attrs}')
-
-        value = self._data[key]
-
-        return value
-    # ----------------------------------
     def __str__(self):
+        nhash = len(self._s_hash)
+
         msg = 40 * '-' + '\n'
         msg+= f'{"Attribute":<20}{"Value":<20}\n'
         msg+= 40 * '-' + '\n'
-        for attr, value in self._data.items():
-            msg += f'{attr:<20}{value:<20}\n'
+        msg += f'{"Hashes":<20}{nhash:<20}\n'
         msg+= 40 * '-'
 
         return msg
     # ----------------------------------
+    def _get_hashes(self, df_ft):
+        '''
+        Will return hashes for each row in the feature dataframe
+        '''
+        if not isinstance(df_ft, pnd.DataFrame):
+            log.error('Features need to be in a pandas dataframe')
+            raise ValueError
+
+        s_hash = { self._hash_from_row(row) for _, row in df_ft.iterrows() }
+
+        return s_hash
+    # ----------------------------------
+    def _hash_from_row(self, row):
+        '''
+        Will return a hash from a pandas dataframe row
+        corresponding to an event
+        '''
+        l_val   = [ str(val) for val in row ]
+        row_str = ','.join(l_val)
+        row_str = row_str.encode('utf-8')
+
+        hsh = hashlib.sha256()
+        hsh.update(row_str)
+
+        hsh_val = hsh.digest()
+
+        return hsh_val
+    # ----------------------------------
     def fit(self, *args, **kwargs):
+        '''
+        Runs the training of the model
+        '''
         log.debug('Fitting')
+
+        df_ft        = args[0]
+        self._s_hash = self._get_hashes(df_ft)
+        log.debug(f'Saving {len(self._s_hash)} hashes')
+
         super().fit(*args, **kwargs)
+
+        return self
+    # ----------------------------------
+    def _check_hashes(self, df_ft):
+        '''
+        Will check that the hashes of the passed features do not intersect with the
+        hashes of the features used for the training.
+        Else it will raise CVSameData exception
+        '''
+
+        if len(self._s_hash) == 0:
+            raise ValueError('Found no hashes in model')
+
+        s_hash  = self._get_hashes(df_ft)
+        s_inter = self._s_hash.intersection(s_hash)
+
+        nh1 = len(self._s_hash)
+        nh2 = len(      s_hash)
+        nh3 = len(s_inter)
+
+        if nh3 > 0:
+            raise CVSameData(f'Found non empty intersection of size: {nh1} ^ {nh2} = {nh3}')
+    # ----------------------------------
+    def predict_proba(self, X):
+        '''
+        Takes pandas dataframe with features
+        Will first check hashes to make sure none of the events/samples
+        used for the training of this model are in the prediction
+        '''
+        self._check_hashes(X)
+
+        return super().predict_proba(X)
 # ---------------------------------------
