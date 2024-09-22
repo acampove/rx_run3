9a50d9d4c21423b34f4edbaa56de9625cf7311d6
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-22 13:24:09 +0800

    Implementing fitting test

--- tests/ml/test_cv_classifier.py
+++ tests/ml/test_cv_classifier.py
@@ -4,15 +4,43 @@ Module containing unit tests for CVClassifier
 
 import os
 
+import numpy
 import joblib
+import pandas  as pnd
 
-from dmu.ml.cv_classifier import CVClassifier as cls
+from dmu.logging.log_store import LogStore
+from dmu.ml.cv_classifier  import CVClassifier as cls
 
 import dmu.testing.utilities as ut
 
-#rdf_sig = _get_rdf(kind='sig')
-#rdf_bkg = _get_rdf(kind='bkg')
 
+# -------------------------------------------------
+def _get_train_input():
+    '''
+    Will return pandas dataframe with features and list of labels
+    made from toy data
+    '''
+    rdf_sig = ut.get_rdf(kind='sig')
+    rdf_bkg = ut.get_rdf(kind='bkg')
+
+    cfg         = ut.get_config('ml/tests/train_mva.yaml')
+    l_feat_name = cfg['training']['features']
+
+    d_sig = rdf_sig.AsNumpy(l_feat_name)
+    d_bkg = rdf_bkg.AsNumpy(l_feat_name)
+
+    df_sig = pnd.DataFrame(d_sig)
+    df_bkg = pnd.DataFrame(d_bkg)
+
+    df_ft  = pnd.concat([df_bkg, df_sig], axis=0)
+
+    nbkg    = len(df_bkg)
+    nsig    = len(df_sig)
+
+    l_lab   = [0] * nbkg + [1] * nsig
+    arr_lab = numpy.array(l_lab)
+
+    return df_ft, arr_lab
 # -------------------------------------------------
 def test_save_load():
     '''
@@ -39,11 +67,27 @@ def test_save_load():
     assert dh == '123'
     assert sh == '321'
 # -------------------------------------------------
+def test_fit():
+    '''
+    Test model fitting
+    '''
+
+    cfg   = ut.get_config('ml/tests/train_mva.yaml')
+    hyper = cfg['training']['hyper']
+
+    df_ft, l_lab = _get_train_input()
+
+    model              = cls(**hyper)
+    model.fit(df_ft, l_lab)
+# -------------------------------------------------
 def main():
     '''
     Tests start here
     '''
+    LogStore.set_level('dmu:ml:CVClassifier', 10)
+
     test_save_load()
+    test_fit()
 # -------------------------------------------------
 if __name__ == '__main__':
     main()
