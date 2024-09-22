38b3e4c54ba98654a18047226a74a7c91c359ed0
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-22 11:00:12 +0800

    Adding main, etc

--- tests/ml/test_cv_classifier.py
+++ tests/ml/test_cv_classifier.py
@@ -1,12 +1,27 @@
 from dmu.ml.cv_classifier import cls
 
+import dmu.testing.utilities as ut
 
-rdf_sig = _get_rdf(kind='sig')
-rdf_bkg = _get_rdf(kind='bkg')
+#rdf_sig = _get_rdf(kind='sig')
+#rdf_bkg = _get_rdf(kind='bkg')
 
+# -------------------------------------------------
 def test_simple():
-    hyper = _get_conf
-model              = cls(**hyper)
-model['dset_hash'] = '123'
-model['sset_hash'] = '123'
-model['random_sd'] = '123'
+    '''
+    Used to do simplest test 
+    '''
+    hyper = ut.get_config('ml/tests/train_mva.yaml')
+
+    model              = cls(**hyper)
+    model['dset_hash'] = '123'
+    model['sset_hash'] = '123'
+    model['random_sd'] = '123'
+# -------------------------------------------------
+def main():
+    '''
+    Tests start here
+    '''
+    test_simple()
+# -------------------------------------------------
+if __name__ == '__main__':
+    main()
