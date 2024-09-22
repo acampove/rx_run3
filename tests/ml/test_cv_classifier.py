2373524a995bb2a812d44fd72e00d7e86f41dc60
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-22 12:49:13 +0800

    Implementing save/load test

--- tests/ml/test_cv_classifier.py
+++ tests/ml/test_cv_classifier.py
@@ -1,4 +1,12 @@
-from dmu.ml.cv_classifier import cls
+'''
+Module containing unit tests for CVClassifier
+'''
+
+import os
+
+import joblib
+
+from dmu.ml.cv_classifier import CVClassifier as cls
 
 import dmu.testing.utilities as ut
 
@@ -6,21 +14,36 @@ import dmu.testing.utilities as ut
 #rdf_bkg = _get_rdf(kind='bkg')
 
 # -------------------------------------------------
-def test_simple():
+def test_save_load():
     '''
-    Used to do simplest test 
+    Used to save and load class
     '''
-    hyper = ut.get_config('ml/tests/train_mva.yaml')
+    cfg   = ut.get_config('ml/tests/train_mva.yaml')
+    hyper = cfg['training']['hyper']
 
     model              = cls(**hyper)
     model['dset_hash'] = '123'
-    model['sset_hash'] = '123'
+    model['sset_hash'] = '321'
+
+    model_path = 'tests/ml/CVClassifier/save/model.pkl'
+    model_dir  = os.path.dirname(model_path)
+    os.makedirs(model_dir, exist_ok=True)
+
+    joblib.dump(model, model_path)
+
+    model = joblib.load(model_path)
+
+    dh = model['dset_hash']
+    sh = model['sset_hash']
+
+    assert dh == '123'
+    assert sh == '321'
 # -------------------------------------------------
 def main():
     '''
     Tests start here
     '''
-    test_simple()
+    test_save_load()
 # -------------------------------------------------
 if __name__ == '__main__':
     main()
