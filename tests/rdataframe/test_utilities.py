4be8fdcd904e6698a2e95bc5957d16ddd624b1c2
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-23 10:16:23 +0800

    Adding test

---
+++ tests/rdataframe/test_utilities.py
@@ -0,0 +1,34 @@
+'''
+Module will hold unit tests for functions in rdataframe/utilities.py
+'''
+
+from ROOT import RDF
+
+import numpy
+
+import dmu.rdataframe.utilities as ut
+
+# -------------------------------------------------
+def test_add_column():
+    '''
+    Will test adding a numpy array to a ROOT dataframe 
+    '''
+    d_data = {
+            'x' : numpy.array([1, 2, 3]),
+            'y' : numpy.array([4, 5, 6]),
+            }
+
+    rdf = RDF.FromNumpy(d_data)
+
+    arr_val = [10, 20, 30]
+
+    rdf = ut.add_column(rdf, arr_val, 'values')
+# -------------------------------------------------
+def main():
+    '''
+    Tests start here
+    '''
+    test_add_column()
+# -------------------------------------------------
+if __name__ == '__main__':
+    main()
