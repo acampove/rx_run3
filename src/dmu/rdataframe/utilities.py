6ed12300d1d732a849c30b91fb49f608784890fd
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-23 10:16:35 +0800

    Adding skeleton code

---
+++ src/dmu/rdataframe/utilities.py
@@ -0,0 +1,12 @@
+'''
+Module containing utility functions to be used with ROOT dataframes
+'''
+
+import numpy
+
+from ROOT import RDataFrame
+
+# ---------------------------------------------------------------------
+def add_column(rdf : RDataFrame, arr_val : numpy.ndarray, name : str):
+    return rdf
+# ---------------------------------------------------------------------
