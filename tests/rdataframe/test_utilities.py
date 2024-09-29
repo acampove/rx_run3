214a8b266397b44bf0aa648d4598ba19b34f84a9
Angel Campoverde <angel.campoverde@cern.ch>
2024-09-29 16:58:40 +0800

    Adding logger + removing extra functions
    Will use pytest for testing from now on, no main is needed

--- tests/rdataframe/test_utilities.py
+++ tests/rdataframe/test_utilities.py
@@ -5,14 +5,21 @@ Module will hold unit tests for functions in rdataframe/utilities.py
 from ROOT import RDF
 
 import numpy
+import pytest
 
 import dmu.rdataframe.utilities as ut
 
+from dmu.logging.log_store import LogStore
+
+log=LogStore.add_logger('dmu:test:rdataframe:utilities')
+
+@pytest.mark.parametrize('itry', [1, 2])
 # -------------------------------------------------
-def test_add_column():
+def test_add_column(itry):
     '''
     Will test adding a numpy array to a ROOT dataframe 
     '''
+    log.info(f'This is try: {itry}')
     d_data = {
             'x' : numpy.array([1, 2, 3]),
             'y' : numpy.array([4, 5, 6]),
@@ -26,11 +33,3 @@ def test_add_column():
 
     rdf.Display().Print()
 # -------------------------------------------------
-def main():
-    '''
-    Tests start here
-    '''
-    test_add_column()
-# -------------------------------------------------
-if __name__ == '__main__':
-    main()
