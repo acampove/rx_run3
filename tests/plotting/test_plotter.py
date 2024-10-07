cc51c8718a0d1982aad0a4509c0881aef846a061
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-07 10:29:31 +0800

    Adding high_stat test and using pytest

--- tests/plotting/test_plotter.py
+++ tests/plotting/test_plotter.py
@@ -5,6 +5,7 @@ Unit test for plotter class in dmu.plotting
 from importlib.resources import files
 from dataclasses         import dataclass
 
+import pytest
 import matplotlib.pyplot as plt
 import yaml
 import numpy
@@ -19,20 +20,32 @@ class Data:
     '''
     Class used to store shared data
     '''
-    nentries = 10000
 #---------------------------------------
-def _get_rdf(kind):
+@pytest.fixture
+def _initialize():
     '''
-    Will return ROOT dataframe with test data
+    This needs to be ran before any test
     '''
+    plt.style.use(mplhep.style.LHCb2)
+#---------------------------------------
+def _get_rdf(kind : str, test : str):
+    '''
+    kind (str): "class A" or "class B", equivalent to data or mc, but general
+    test (str): Identifies specific test
+    '''
+
+    if test == 'high_stat':
+        nentries = 1_000_000
+    else:
+        nentries =    10_000
 
     d_data = {}
     if   kind == 'class A':
-        d_data['x'] = numpy.random.normal(0, 1, size=Data.nentries)
-        d_data['y'] = numpy.random.normal(0, 2, size=Data.nentries)
+        d_data['x'] = numpy.random.normal(0, 1, size=nentries)
+        d_data['y'] = numpy.random.normal(0, 2, size=nentries)
     elif kind == 'class B':
-        d_data['x'] = numpy.random.normal(1, 1, size=Data.nentries)
-        d_data['y'] = numpy.random.normal(1, 2, size=Data.nentries)
+        d_data['x'] = numpy.random.normal(1, 1, size=nentries)
+        d_data['y'] = numpy.random.normal(1, 2, size=nentries)
     else:
         raise ValueError(f'Invalid class: {kind}')
 
@@ -40,36 +53,36 @@ def _get_rdf(kind):
 
     return rdf
 #---------------------------------------
-def _load_config(kind=None):
+def _load_config(test : str):
     '''
-    Will load config file from dmu_data and return dictionary with config
+    test (str): Identifies specific test
     '''
 
-    cfg_path = files('dmu_data').joinpath(f'plotting/tests/{kind}.yaml')
+    cfg_path = files('dmu_data').joinpath(f'plotting/tests/{test}.yaml')
+    cfg_path = str(cfg_path)
     with open(cfg_path, encoding='utf-8') as ifile:
         cfg = yaml.safe_load(ifile)
 
     return cfg
 #---------------------------------------
-def _test_simple():
+def test_simple():
     '''
     Minimal test
     '''
-    d_rdf =  { kind : _get_rdf(kind) for kind in ['class A', 'class B'] }
+    d_rdf =  { kind : _get_rdf(kind=kind, test='simple') for kind in ['class A', 'class B'] }
 
-    cfg_dat = _load_config(kind='simple')
+    cfg_dat = _load_config(test='simple')
 
     ptr=Plotter(d_rdf=d_rdf, cfg=cfg_dat)
     ptr.run()
 #---------------------------------------
-def main():
+def test_high_stat():
     '''
-    Tests start here
+    Test for large datasets
     '''
-    plt.style.use(mplhep.style.LHCb2)
+    d_rdf =  { kind : _get_rdf(kind=kind, test='high_stat') for kind in ['class A', 'class B'] }
 
-    _test_simple()
-#---------------------------------------
+    cfg_dat = _load_config(test='high_stat')
 
-if __name__ == '__main__':
-    main()
+    ptr=Plotter(d_rdf=d_rdf, cfg=cfg_dat)
+    ptr.run()
