89b6930115bde80d14e0cb520ef2c20c9d8cf1ee
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-07 09:52:49 +0800

    Merge branch 'main' of github.com:acampove/dmu

--- src/dmu/plotting/plotter.py
+++ src/dmu/plotting/plotter.py
@@ -4,6 +4,7 @@ Module containing plotter class
 
 import os
 
+import numpy
 import matplotlib.pyplot as plt
 
 from log_store import log_store
@@ -33,10 +34,7 @@ class Plotter:
         --------------------
         var   (str)  : name of column
         '''
-        plt.figure(var)
-
-        plt_dir = self._d_cfg['saving']['plt_dir']
-        os.makedirs(plt_dir, exist_ok=True)
+        # pylint: disable=too-many-locals
 
         d_cfg = self._d_cfg['plots'][var]
 
@@ -50,26 +48,30 @@ class Plotter:
 
         l_bc_all = []
         for name, rdf in self._d_rdf.items():
-            arr_mass = rdf.AsNumpy([var])[var]
-            l_bc, _, _ = plt.hist(arr_mass, bins=bins, range=(minx, maxx), density=normalized, histtype='step', label=name)
-            l_bc_all  += l_bc.tolist()
+            arr_val    = rdf.AsNumpy([var])[var]
+            l_bc, _, _ = plt.hist(arr_val, bins=bins, range=(minx, maxx), density=normalized, histtype='step', label=name)
+            l_bc_all  += numpy.array(l_bc).tolist()
 
             plt.yscale(yscale)
             plt.xlabel(xname)
             plt.ylabel(yname)
-
-        if var in ['B_const_mass_M', 'B_M']:
-            plt.axvline(x=5280, color='r', label=r'$B^+$'   , linestyle=':')
-        elif var == 'Jpsi_M':
-            plt.axvline(x=3096, color='r', label=r'$J/\psi$', linestyle=':')
 
         if yscale == 'linear':
             plt.ylim(bottom=0)
 
         max_y = max(l_bc_all)
         plt.ylim(top=1.2 * max_y)
+    # --------------------------------------------
+    def _save_plot(self, var):
+        '''
+        Will save to PNG:
 
+        var (str) : Name of variable, needed for plot name
+        '''
         plt.legend()
+
+        plt_dir = self._d_cfg['saving']['plt_dir']
+        os.makedirs(plt_dir, exist_ok=True)
 
         plot_path = f'{plt_dir}/{var}.png'
         log.info(f'Saving to: {plot_path}')
@@ -77,11 +79,25 @@ class Plotter:
         plt.savefig(plot_path)
         plt.close(var)
     # --------------------------------------------
+    def _plot_lines(self, var : str):
+        '''
+        Will plot vertical lines for some variables
+
+        var (str) : name of variable
+        '''
+        if var in ['B_const_mass_M', 'B_M']:
+            plt.axvline(x=5280, color='r', label=r'$B^+$'   , linestyle=':')
+        elif var == 'Jpsi_M':
+            plt.axvline(x=3096, color='r', label=r'$J/\psi$', linestyle=':')
+    # --------------------------------------------
     def run(self):
         '''
         Will run plotting
         '''
         for var in self._d_cfg['plots']:
             log.debug(f'Plotting: {var}')
+            plt.figure(var)
             self._plot_var(var)
+            self._plot_lines(var)
+            self._save_plot(var)
 # --------------------------------------------
