76f7dadcfa101d4f04b27dc0f8ddc61690bf7a50
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-08 10:08:26 +0800

    Can load both toml and yaml

--- src/data_checks/utilities.py
+++ src/data_checks/utilities.py
@@ -1,37 +1,44 @@
+'''
+Module with utility functions
+'''
+
 import os
+from importlib.resources import files
+
 import toml
+import yaml
 import utils_noroot as utnr
 
 from XRootD              import client
-from importlib.resources import files
 from log_store           import log_store
 
 log = log_store.add_logger('data_checks:utilities')
 local_config = False
 
 # --------------------------------------
-def load_config(cfg_nam):
+def load_config(cfg_nam : str, kind :str ='toml'):
     '''
     Parameters
     -----------------
     cfg_nam (str): Name of config file, without extension
+    kind    (str): Type of file, e.g. toml (default), yaml, etc
 
     Returns
     -----------------
     d_config (dict): Dictionary with configuration
     '''
     if not local_config:
-        val = _load_grid_config(cfg_nam)
+        val = _load_grid_config(cfg_nam, kind)
     else:
-        val = _load_local_config(cfg_nam)
+        val = _load_local_config(cfg_nam, kind)
 
     return val
 # --------------------------------------
-def _load_local_config(cfg_nam):
+def _load_local_config(cfg_nam : str, kind : str):
     '''
     Will pick up config file from installed project
     '''
-    cfg_path = files('data_checks_data').joinpath(f'{cfg_nam}.toml')
+    cfg_path = files('data_checks_data').joinpath(f'{cfg_nam}.{kind}')
     cfg_path = str(cfg_path)
     if not os.path.isfile(cfg_path):
         log.error(f'Config path not found: {cfg_path}')
@@ -39,14 +46,22 @@ def _load_local_config(cfg_nam):
 
     log.warning(f'Loading local config file: {cfg_path}')
 
-    return toml.load(cfg_path)
+    if   kind == 'toml':
+        data = toml.load(cfg_path)
+    elif kind == 'yaml':
+        with open(cfg_path, encoding='utf-8') as ifile:
+            data = yaml.safe_load(ifile)
+    else:
+        raise ValueError(f'Invalid config type: {kind}')
+
+    return data
 # --------------------------------------
 @utnr.timeit
-def _load_grid_config(cfg_nam):
+def _load_grid_config(cfg_nam :str, kind : str):
     '''
     Will use XROOTD to pick up file from grid
     '''
-    xrd_path = f'root://x509up_u1000@eoslhcb.cern.ch//eos/lhcb/grid/user/lhcb/user/a/acampove/run3/ntupling/config/{cfg_nam}.toml'
+    xrd_path = f'root://x509up_u1000@eoslhcb.cern.ch//eos/lhcb/grid/user/lhcb/user/a/acampove/run3/ntupling/config/{cfg_nam}.{kind}'
     log.info(f'Loading: {xrd_path}')
     with client.File() as ifile:
         status, _ = ifile.open(xrd_path)
@@ -57,8 +72,16 @@ def _load_grid_config(cfg_nam):
         status, file_content = ifile.read()
         if not status.ok:
             log.error(status.message)
-            raise
+            raise FileNotFoundError
 
-        toml_str  = file_content.decode('utf-8')
-        return toml.loads(toml_str)
+        content = file_content.decode('utf-8')
+
+        if   kind == 'toml':
+            data = toml.loads(content)
+        elif kind == 'yaml':
+            data = yaml.safe_load(content)
+        else:
+            raise ValueError(f'Invalid config type: {kind}')
+
+        return data
 # --------------------------------------
