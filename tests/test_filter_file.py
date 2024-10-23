52f0ba86745babf82ac13a61409759357263ccbf
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-23 15:11:01 +0800

    Cleaning up:
    - Will use pytest, no main function
    - Will pick up test files from cernbox
    - Will run with local config to speed things up
    - Adding logger

--- tests/test_filter_file.py
+++ tests/test_filter_file.py
@@ -1,38 +1,49 @@
-from data_checks.filter_file import FilterFile
+'''
+File containing tests for FilterFile class
+'''
+
 from log_store               import log_store
 
-import utils_noroot          as utnr
+
 import data_checks.utilities as ut
+from data_checks.filter_file import FilterFile
 
+log = log_store.add_logger('data_checks:test_filter_file')
+# --------------------------------------
+class Data:
+    '''
+    Data class with shared attributes
+    '''
+    dt_path = '/home/acampove/cernbox/Run3/analysis_productions/Data/mag_down/c1.root'
+    mc_path = '/home/acampove/cernbox/Run3/analysis_productions/MC/BdKsmumu.root'
+# --------------------------------------
+def initialize():
+    '''
+    Will set loggers, etc
+    '''
+    log.info('Initializing')
+    ut.local_config = True
 
-# --------------------------------------
-class data:
-    dt_path = '/home/acampove/data/aprod/raw/dt/00231366_00000013_1.ftuple.root'
-    mc_path = '/home/acampove/data/aprod/raw/mc/BdKstmumu.root'
-# --------------------------------------
-def set_log():
     log_store.set_level('rx_scripts:atr_mgr:mgr', 30)
     log_store.set_level('data_checks:selector'  , 10)
     log_store.set_level('data_checks:utilities' , 30)
     log_store.set_level('data_checks:FilterFile', 10)
 # --------------------------------------
 def test_dt():
-    obj = FilterFile(kind='any_kind', file_path=data.dt_path, cfg_nam='dt_2024_turbo_comp')
+    '''
+    Run test on data
+    '''
+    initialize()
+
+    obj = FilterFile(kind='any_kind', file_path=Data.dt_path, cfg_nam='dt_2024_turbo_comp')
     obj.run()
 # --------------------------------------
 def test_mc():
-    obj = FilterFile(kind='any_kind', file_path=data.mc_path, cfg_nam='mc_2024_turbo_comp')
+    '''
+    Run test on MC
+    '''
+    initialize()
+
+    obj = FilterFile(kind='any_kind', file_path=Data.mc_path, cfg_nam='mc_2024_turbo_comp')
     obj.run()
 # --------------------------------------
-def main():
-    utnr.timer_on   = False
-    ut.local_config = True
-
-    set_log()
-    test_dt()
-    test_mc()
-# --------------------------------------
-
-
-if __name__ == '__main__':
-    main()
