ee3136a4248262cb67c4c3c8defbadac82341ae7
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-23 09:31:06 +0800

    Adding functions for modifying config

--- src/dmu_scripts/ssh/coned.py
+++ src/dmu_scripts/ssh/coned.py
@@ -14,10 +14,12 @@ class Data:
     '''
     Class used to store shared data
     '''
-    prnt : bool
-    srvr : str
     logl : int
+    dry  : bool
+    prnt : bool
     cfg  : dict
+    l_ad : list[str]
+    l_rm : list[str]
 #----------------------------
 def _print_configs():
     '''
@@ -31,18 +33,26 @@ def _initialize():
     _load_config()
 
     LogStore.set_level('dmu:scripts:coned', Data.logl)
+
+    log.debug(f'Running at {Data.logl} logging level')
 #----------------------------
 def _get_args():
     '''
     Will parse arguments
     '''
     parser = argparse.ArgumentParser(description='Used to edit and print server list specified by ~/.config/connect/servers.yaml')
-    parser.add_argument('-l', '--log_lvl', type=int, help='Logging level', default=20, choices=[10,20,30])
-    parser.add_argument('-p', '--print'  , help='Prints config settings and exits', action='store_true')
+    parser.add_argument('-p', '--print'  , help ='Prints config settings and exits', action='store_true')
+    parser.add_argument('-l', '--log_lvl', type =int, help='Logging level', default=20, choices=[10,20,30])
+    parser.add_argument('-a', '--add'    , nargs=3  , help='Adds task to given server, e.g. task 123 server'     , default=[])
+    parser.add_argument('-r', '--rem'    , nargs=3  , help='Removes task from given server, e.g. task 123 server', default=[])
+    parser.add_argument('-d', '--dry'    , help='Run dry run, for adding and removing entries', action='store_true')
     args   = parser.parse_args()
 
     Data.prnt = args.print
     Data.logl = args.log_lvl
+    Data.l_ad = args.add
+    Data.l_rm = args.rem
+    Data.dry  = args.dry
 #---------------------------------------
 def _load_config():
     home_dir    = os.environ['HOME']
@@ -60,9 +70,13 @@ def main():
     _get_args()
     _initialize()
 
-    if Data.prnt is not None:
+    if Data.prnt:
+        log.debug('Printing and returning')
         _print_configs()
         return
+
+    cfg = _get_updated_config()
+    _dump_config(cfg)
 #---------------------------------------
 if __name__ == '__main__':
     main()
