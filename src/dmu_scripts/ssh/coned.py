a467495d87800b1a46226741312536554ff70f2a
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-23 09:32:59 +0800

    Adding implementation of add/remove/etc config

--- src/dmu_scripts/ssh/coned.py
+++ src/dmu_scripts/ssh/coned.py
@@ -63,6 +63,84 @@ def _load_config():
     with open(config_path, encoding='utf-8') as ifile:
         Data.cfg = yaml.safe_load(ifile)
 #---------------------------------------
+def _dump_config(cfg : dict):
+    if cfg == Data.cfg:
+        log.debug(f'Config was not modified, will not save it')
+        return
+
+    home_dir    = os.environ['HOME']
+    config_path = f'{home_dir}/.config/dmu/ssh/servers.yaml'
+    if not os.path.isfile(config_path):
+        raise FileNotFoundError(f'Config not found: {config_path}')
+
+    if Data.dry:
+        content = yaml.dump(cfg, default_flow_style=False)
+        print(content)
+        return
+
+    with open(config_path, 'w', encoding='utf-8') as ofile:
+        yaml.dump(cfg, ofile, default_flow_style=False)
+#---------------------------------------
+def _get_updated_config() -> dict:
+    log.debug('Getting updated config')
+
+    cfg = dict(Data.cfg)
+    cfg = _add_task(cfg)
+    cfg = _remove_task(cfg)
+
+    return cfg
+#---------------------------------------
+def _add_task(cfg : dict) -> dict:
+    if len(Data.l_ad) == 0:
+        log.debug('No task added')
+        return cfg
+
+    [task, machine, server] = Data.l_ad
+    if server not in cfg:
+        cfg[server] = {}
+
+    if machine not in cfg[server]:
+        cfg[server][machine] = []
+
+    cfg[server][machine].append(task)
+
+    log.info(f'{"Added":<10}{server:<20}{machine:<10}{task:<20}')
+
+    return cfg
+#---------------------------------------
+def _remove_task(cfg : dict) -> dict:
+    if len(Data.l_rm) == 0:
+        log.debug('No task removed')
+        return cfg
+
+    [task, machine, server] = Data.l_rm
+    if server not in cfg:
+        log.warning(f'Server {server} not found')
+        return cfg
+
+    if machine not in cfg[server]:
+        log.warning(f'Machine {machine} not found in server {server}')
+        return cfg
+
+    l_task = cfg[server][machine]
+    if task not in l_task:
+        log.warning(f'Task {task} not found in {server}:{machine}')
+        return cfg
+
+    index = l_task.index(task)
+    del l_task[index]
+    cfg[server][machine] = l_task
+
+    log.info(f'{"Removed":<10}{server:<20}{machine:<10}{task:<20}')
+
+    cfg = _trim_config(cfg, machine, server)
+
+    return cfg
+#---------------------------------------
+def _trim_config(cfg : dict, machine : str, server : str) -> dict:
+    log.debug(f'Trimming {machine}:{server}')
+    return cfg
+#---------------------------------------
 def main():
     '''
     Starts here
