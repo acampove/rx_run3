16e5ae87d20d2ef9b048353459baa0daa1eb6723
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-23 14:39:01 +0800

    Not using main function, adding logger, etc
    Will use pytests

--- tests/test_ntuple_filter.py
+++ tests/test_ntuple_filter.py
@@ -1,27 +1,37 @@
-from data_checks.ntuple_filter import ntuple_filter
+'''
+This script containts tests for the ntuple_filter class
+'''
+
 from log_store                 import log_store
 
+import data_checks.utilities as ut
+from data_checks.ntuple_filter import ntuple_filter
+
+log = log_store.add_logger('data_checks:test_ntuple_filter')
 # ---------------------------------------
-def set_log():
+def initialize():
+    '''
+    Will set loggers, etc
+    '''
+    log.info('Initializing')
+    ut.local_config = True
     log_store.set_level('data_checks:ntuple_filter', 10)
     log_store.set_level('data_checks:FilterFile'   , 10)
     log_store.set_level('data_checks:selector'     , 10)
     log_store.set_level('rx_scripts:atr_mgr:mgr'   , 30)
 # ---------------------------------------
 def test_dt():
+    '''
+    Will test filtering of data
+    '''
+    initialize()
+
     obj = ntuple_filter(dataset='dt_2024_turbo', cfg_ver='comp', index=1, ngroup=1211)
     obj.filter()
 # ---------------------------------------
 def test_mc():
+    '''
+    Will test filtering of MC 
+    '''
     obj = ntuple_filter(dataset='mc_2024_turbo', cfg_ver='comp', index=1, ngroup=71)
     obj.filter()
-# ---------------------------------------
-def  main():
-    set_log()
-    test_dt()
-    test_mc()
-# ---------------------------------------
-
-
-if __name__  == '__main__':
-    main()
