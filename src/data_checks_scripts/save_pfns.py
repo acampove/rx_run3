a21392bbe3fd76ef27217cb4ff1e95f873ac8c91
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-24 16:07:02 +0800

    Fix linting, update argparser

--- src/data_checks_scripts/save_pfns.py
+++ src/data_checks_scripts/save_pfns.py
@@ -1,75 +1,84 @@
 #!/usr/bin/env python3
+'''
+Script used to save PFNs
+'''
+
+import re
+import logging
+import argparse
 
 from importlib.resources import files
 from log_store           import log_store
 
-import re
 import apd
-import logging
-import argparse
 import utils_noroot          as utnr
 import data_checks.utilities as utdc
 
 log=log_store.add_logger('data_checks:save_pfns')
 #------------------------------------
-class data:
-    config      = None
-    log_lvl     = None
-    local_config=False
+class Data:
+    '''
+    Class used to store shared attributes
+    '''
+    config      :  str
+    log_lvl     :  int
+    local_config: bool
 #------------------------------------
-def get_args():
+def _get_args():
     parser = argparse.ArgumentParser(description='Will use apd to save a list of paths to ROOT files in EOS')
     parser.add_argument('-c', '--config' , type=str, help='Name of config file, without TOML extension')
     parser.add_argument('-l', '--log_lvl', type=int, help='Logging level', default=20, choices=[10, 20, 30, 40])
-    parser.add_argument('-L', '--loc_cfg', type=int, help='Will pick up local (1) or remote (0, default) version of TOML config', default=0, choices=[0, 1])
+    parser.add_argument('-L', '--loc_cfg', help='Will pick up local version of config, if not use, will pick up grid version', action='store_true')
     args = parser.parse_args()
 
-    data.config       = args.config
-    data.log_lvl      = args.log_lvl
-    data.local_config = args.loc_cfg
+    Data.config       = args.config
+    Data.log_lvl      = args.log_lvl
+    Data.local_config = args.loc_cfg
 #------------------------------------
-def get_pfns():
-    utdc.local_config = data.local_config
+def _get_pfns() -> dict:
+    '''
+    Returns dictionary of PFS
+    '''
+    utdc.local_config = Data.local_config
 
-    cfg_dat = utdc.load_config(data.config)
+    cfg_dat = utdc.load_config(Data.config)
     d_prod  = cfg_dat['production']
 
     log.debug('Reading paths from APD')
     obj     = apd.get_analysis_data(**d_prod)
 
-    if   data.config.startswith('dt_'):
-        d_pfn = get_dt_pfns(obj)
-    elif data.config.startswith('mc_'):
-        d_pfn = get_mc_pfns(obj)
+    if   Data.config.startswith('dt_'):
+        d_pfn = _get_dt_pfns(obj)
+    elif Data.config.startswith('mc_'):
+        d_pfn = _get_mc_pfns(obj)
     else:
-        log.error(f'Unrecognized start of config: {data.config}')
-        raise
+        raise ValueError(f'Unrecognized start of config: {Data.config}')
 
-    print_pfn_info(d_pfn)
+    _print_pfn_info(d_pfn)
 
     return d_pfn
 #------------------------------------
-def print_pfn_info(d_pfn):
+def _print_pfn_info(d_pfn):
     nsample=0
     for l_sam in d_pfn.values():
         nsample += len(l_sam)
 
     log.info(f'Found {nsample} PFNs')
 #------------------------------------
-def get_dt_pfns(ap_obj):
-    cfg_dat = utdc.load_config(data.config)
+def _get_dt_pfns(ap_obj):
+    cfg_dat = utdc.load_config(Data.config)
     l_samp  = cfg_dat['sample']['names']
     l_path  = []
     for samp in l_samp:
         l_path += ap_obj(name=samp)
 
-    return {data.config : l_path}
+    return {Data.config : l_path}
 #------------------------------------
-def get_mc_pfns(ap_obj):
+def _get_mc_pfns(ap_obj):
     '''
     Reads from AP object sample info and returns samplename -> PFNs dictionary
     '''
-    cfg_dat   = utdc.load_config(data.config)
+    cfg_dat   = utdc.load_config(Data.config)
     regex     = cfg_dat['sample']['name_rx']
     version   = cfg_dat['sample']['version']
     pattern   = re.compile(regex)
