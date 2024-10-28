1ad6ed33e82e2bcd3ba718cce571df096455f24f
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-28 16:28:02 +0800

    Better code
    - Use pytest
    - New logger

--- tests/test_ntuple_filter.py
+++ tests/test_ntuple_filter.py
@@ -2,30 +2,30 @@
 This script containts tests for the ntuple_filter class
 '''
 
-from log_store                 import log_store
+from dmu.logging.log_store import LogStore
+import pytest
 
 import data_checks.utilities as ut
 from data_checks.ntuple_filter import ntuple_filter
 
-log = log_store.add_logger('data_checks:test_ntuple_filter')
+log = LogStore.add_logger('data_checks:test_ntuple_filter')
 # ---------------------------------------
+@pytest.fixture(scope='session', autouse=True)
 def initialize():
     '''
     Will set loggers, etc
     '''
     log.info('Initializing')
     ut.local_config = True
-    log_store.set_level('data_checks:ntuple_filter', 10)
-    log_store.set_level('data_checks:FilterFile'   , 10)
-    log_store.set_level('data_checks:selector'     , 10)
-    log_store.set_level('rx_scripts:atr_mgr:mgr'   , 30)
+    LogStore.set_level('data_checks:ntuple_filter', 10)
+    LogStore.set_level('data_checks:FilterFile'   , 10)
+    LogStore.set_level('data_checks:selector'     , 10)
+    LogStore.set_level('rx_scripts:atr_mgr:mgr'   , 30)
 # ---------------------------------------
 def test_dt():
     '''
     Will test filtering of data
     '''
-    initialize()
-
     obj = ntuple_filter(dataset='dt_2024_turbo', cfg_ver='comp', index=1, ngroup=1211)
     obj.filter()
 # ---------------------------------------
