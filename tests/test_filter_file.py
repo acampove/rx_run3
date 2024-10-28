f929822927afe3b7d20c4f8fcc6cfbcba5abbc77
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-28 10:25:54 +0800

    Updating with pytest used properly

--- tests/test_filter_file.py
+++ tests/test_filter_file.py
@@ -2,13 +2,13 @@
 File containing tests for FilterFile class
 '''
 
-from log_store               import log_store
-
+import pytest
+from dmu.logging.log_store import LogStore
 
 import data_checks.utilities as ut
 from data_checks.filter_file import FilterFile
 
-log = log_store.add_logger('data_checks:test_filter_file')
+log = LogStore.add_logger('data_checks:test_filter_file')
 # --------------------------------------
 class Data:
     '''
@@ -16,26 +16,30 @@ class Data:
     '''
     dt_path = '/home/acampove/cernbox/Run3/analysis_productions/Data/mag_down/c2.root'
     mc_path = '/home/acampove/cernbox/Run3/analysis_productions/MC/BdKsmumu.root'
+
+    l_args_config = [True, False]
 # --------------------------------------
-def initialize():
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
     '''
     Will set loggers, etc
     '''
     log.info('Initializing')
-    ut.local_config = False
 
-    log_store.set_level('rx_scripts:atr_mgr:mgr', 30)
-    log_store.set_level('data_checks:selector'  , 20)
-    log_store.set_level('data_checks:utilities' , 30)
-    log_store.set_level('data_checks:FilterFile', 20)
+    LogStore.set_level('rx_scripts:atr_mgr:mgr', 30)
+    LogStore.set_level('data_checks:selector'  , 20)
+    LogStore.set_level('data_checks:utilities' , 30)
+    LogStore.set_level('data_checks:FilterFile', 20)
 # --------------------------------------
-def test_dt():
+@pytest.mark.parametrize('local_config', Data.l_args_config)
+def test_dt(local_config : bool):
     '''
     Run test on data
     '''
-    initialize()
+    ut.local_config = local_config
 
     obj = FilterFile(kind='any_kind', file_path=Data.dt_path, cfg_nam='dt_2024_turbo_comp')
+    obj.dump_contents = True
     obj.run()
 # --------------------------------------
 def test_mc():
