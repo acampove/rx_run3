ba633855a1eb9f45b11d0d042008e1ffac01c921
Angel Campoverde <angel.campoverde@cern.ch>
2024-10-30 19:51:26 +0800

    Adding tests

--- tests/ml/test_utilities.py
+++ tests/ml/test_utilities.py
@@ -2,6 +2,7 @@
 Module containing unit tests for ml/utilities.py
 '''
 
+import numpy
 import pandas as pnd
 
 import dmu.ml.utilities as ut
@@ -18,11 +19,50 @@ def test_index_with_hashes():
 
     print(df)
 # ----------------------------------------
-def main():
+def test_cleanup():
     '''
-    Tests start here
+    Function used to test cleanup of dataframes
     '''
-    test_index_with_hashes()
+    d_data_in = {
+            'a' : [1., 2.,  numpy.nan, 4.],
+            'b' : [5., 6.,         7., 8.],
+            }
+
+    d_data_ou = {
+            'a' : [1., 2.,            4.],
+            'b' : [5., 6.,            8.],
+            }
+
+    df_in = pnd.DataFrame(d_data_in)
+
+    df_ts = ut.cleanup(df_in)
+    df_ts = df_ts.reset_index(drop=True)
+
+    df_ou = pnd.DataFrame(d_data_ou)
+
+    assert df_ou.equals(df_ts)
 # ----------------------------------------
-if __name__ == '__main__':
-    main()
+def test_patch_and_tag():
+    '''
+    Will test patch_and_tag
+    '''
+    patch = 123.
+
+    d_data_in = {
+            'a' : [1., 2.,  numpy.nan, 4.],
+            'b' : [5., 6.,         7., 8.],
+            }
+
+    d_data_ot = {
+            'a' : [1., 2.,      patch, 4.],
+            'b' : [5., 6.,         7., 8.],
+            }
+
+    df_in = pnd.DataFrame(d_data_in)
+    df_ot = pnd.DataFrame(d_data_ot)
+    df_pa = ut.patch_and_tag(df_in, value=patch)
+
+    arr_index = df_pa.attrs['patched_indices']
+
+    assert arr_index.tolist() == [2]
+    assert df_pa.equals(df_ot)
