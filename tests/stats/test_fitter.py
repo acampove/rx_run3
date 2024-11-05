88fd17b32be36b26c378e0763b30cca0b54d2f03
Angel Campoverde <angel.campoverde@cern.ch>
2024-11-05 15:42:46 +0800

    Larger statistics, fit fails with 50K

--- tests/stats/test_fitter.py
+++ tests/stats/test_fitter.py
@@ -23,7 +23,7 @@ class Data:
     '''
     Data class used to store share data
     '''
-    nsample = 50000
+    nsample = 100_000
     plt_dir = 'tests/Fitter/plots'
 
     arr_sig = numpy.random.normal(5.0, 0.5, size=10_000)
