7133806d8ff7651d388a5c49a63a226af4e69d4a
Angel Campoverde <angel.campoverde@cern.ch>
2024-11-18 15:10:22 +0800

    data_checks -> post_ap

--- tests/test_ntuple_filter.py
+++ tests/test_ntuple_filter.py
@@ -5,10 +5,10 @@ This script containts tests for the ntuple_filter class
 from dmu.logging.log_store import LogStore
 import pytest
 
-import data_checks.utilities as ut
-from data_checks.ntuple_filter import ntuple_filter
+import post_ap.utilities as ut
+from post_ap.ntuple_filter import ntuple_filter
 
-log = LogStore.add_logger('data_checks:test_ntuple_filter')
+log = LogStore.add_logger('post_ap:test_ntuple_filter')
 # ---------------------------------------
 @pytest.fixture(scope='session', autouse=True)
 def initialize():
@@ -17,9 +17,9 @@ def initialize():
     '''
     log.info('Initializing')
     ut.local_config = True
-    LogStore.set_level('data_checks:ntuple_filter', 10)
-    LogStore.set_level('data_checks:FilterFile'   , 10)
-    LogStore.set_level('data_checks:selector'     , 10)
+    LogStore.set_level('post_ap:ntuple_filter', 10)
+    LogStore.set_level('post_ap:FilterFile'   , 10)
+    LogStore.set_level('post_ap:selector'     , 10)
     LogStore.set_level('rx_scripts:atr_mgr:mgr'   , 30)
 # ---------------------------------------
 def test_dt():
