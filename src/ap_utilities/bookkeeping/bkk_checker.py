994b66ac10061cf820205537f8d7dc9f5af494e0
Angel Campoverde <angel.campoverde@cern.ch>
2024-11-21 10:27:36 +0800

    Implementing methods
    Missing DIRAC specific stuff

--- src/ap_utilities/bookkeeping/bkk_checker.py
+++ src/ap_utilities/bookkeeping/bkk_checker.py
@@ -2,6 +2,7 @@
 Module with BkkChecker class
 '''
 
+import os
 import yaml
 
 from dmu.logging.log_store import LogStore
@@ -13,6 +14,7 @@ class BkkChecker:
     Class meant to check if samples exist in Bookkeeping using multithreading.
     This is useful with large lists of samples, due to low performance of Dirac
     '''
+    # pylint disable=too-few-public-methods
     # -------------------------
     def __init__(self, path : str):
         '''
@@ -22,16 +24,28 @@ class BkkChecker:
         with open(path, encoding='utf-8') as ifile:
             self._l_sample = yaml.safe_load(ifile)
     # -------------------------
+    def _was_found(self, sample : list[str]) -> bool:
+        sample_id, event_type, mc_path, polarity, _, _, nu_path, _, sim_version, generator = sample
+
+        sample_path = f'/MC/{sample_id}/Beam6800GeV-{mc_path}-{polarity}-{nu_path}-25ns-{generator}/{sim_version}/HLT2-2024.W31.34/{event_type}/DST'
+
+        log.debug(f'{"":<4}{sample_path:<100}')
+
+        return True
+    # -------------------------
     def save(self, path : str) -> None:
         '''
         Will save list of found samples to given path
         '''
+
+        log.info('Filtering input')
+        l_sample = [ sample for sample in self._l_sample if self._was_found(sample) ]
+
+        dir_name = os.path.dirname(path)
+        os.makedirs(dir_name, exist_ok=True)
+
+        with open(path, 'w', encoding='utf-8') as ofile:
+            yaml.safe_dump(l_sample, ofile)
+
         log.info(f'Saving to: {path}')
 # ---------------------------------
-
-#for sample in l_sample:
-#    sample_id, event_type, mc_path, polarity, conddb_tag, dddb_tag, nu_path, nuval, sim_version, generator = sample 
-#
-#    bk_query = f'/MC/{sample_id}/Beam6800GeV-{mc_path}-{polarity}-{nu_path}-25ns-{generator}/{sim_version}/HLT2-2024.W31.34/{event_type}/DST'
-#
-#
--- tests/test_bkk_check.py
+++ tests/test_bkk_check.py
@@ -3,8 +3,15 @@ Module with tests for BkkChecker class
 '''
 from importlib.resources import files
 
-from ap_utilities.bookkeeping.bkk_checker import BkkChecker 
+import pytest
 
+from dmu.logging.log_store                import LogStore
+from ap_utilities.bookkeeping.bkk_checker import BkkChecker
+
+# ----------------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    LogStore.set_level('ap_utilities:Bookkeeping.bkk_checker', 20)
 # ----------------------------------------
 def test_simple():
     '''
@@ -13,5 +20,5 @@ def test_simple():
     samples_path = files('ap_utilities_data').joinpath('rd_samples.yaml')
 
     obj=BkkChecker(samples_path)
-    obj.save(path='existing_samples.yaml')
+    obj.save(path='/tmp/ap_utilities/existing_samples.yaml')
 # ----------------------------------------
