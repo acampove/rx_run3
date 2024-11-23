8352bbbc153b4593fadcb9bc369261c28bd28916
Angel Campoverde <angel.campoverde@cern.ch>
2024-11-23 14:53:06 +0800

    Renaming DecProdCut

--- src/dmu/physics/utilities.py
+++ src/dmu/physics/utilities.py
@@ -23,11 +23,12 @@ def _format_nickname(nickname : str, style : str) -> str:
     if style != 'safe_1':
         raise ValueError(f'Invalid style: {style}')
 
-    nickname = nickname.replace('.',   'p')
-    nickname = nickname.replace('-',   'mn')
-    nickname = nickname.replace('+',   'pl')
-    nickname = nickname.replace('=', '_eq_')
-    nickname = nickname.replace(',',    '_')
+    nickname = nickname.replace(         '.',  'p')
+    nickname = nickname.replace(         '-',  'mn')
+    nickname = nickname.replace(         '+',  'pl')
+    nickname = nickname.replace(         '=','_eq_')
+    nickname = nickname.replace(         ',',   '_')
+    nickname = nickname.replace('DecProdCut', 'DPC')
 
     return nickname
 # ---------------------------------
