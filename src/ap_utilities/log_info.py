bf08f02f3440ebdc044b8df5f46efb8a98dcf20d
Angel Campoverde <angel.campoverde@cern.ch>
2024-11-26 16:18:22 +0800

    Adding logger

--- src/ap_utilities/log_info.py
+++ src/ap_utilities/log_info.py
@@ -2,9 +2,14 @@
 Module storing LogInfo class
 '''
 import os
+import re
 import glob
 import zipfile
+import functools
 
+from dmu.logging.log_store import LogStore
+
+log = LogStore.add_logger('ap_utilities:log_info')
 # ---------------------------------------------
 class LogInfo:
     '''
@@ -16,6 +21,9 @@ class LogInfo:
         self._zip_path = zip_path
         self._out_path = '/tmp/log_info'
         self._log_wc   = 'DaVinci_*.log'
+        self._log_path : str
+
+        self._entries_regex : str = r'\s*\|\s*Sum of all Algorithms\s*\|\s*(\d+)\s*\|.*'
 
         os.makedirs(self._out_path, exist_ok=True)
     # ---------------------------------------------
@@ -29,20 +37,39 @@ class LogInfo:
 
         return log_path
     # ---------------------------------------------
-    def _get_lines(self) -> list[str]:
+    @functools.lru_cache()
+    def _get_dv_lines(self) -> list[str]:
         with zipfile.ZipFile(self._zip_path, 'r') as zip_ref:
             zip_ref.extractall(self._out_path)
 
-        log_path = self._get_log_path()
+        self._log_path = self._get_log_path()
 
-        with open(log_path, encoding='utf-8') as ifile:
+        with open(self._log_path, encoding='utf-8') as ifile:
             l_line = ifile.read().splitlines()
 
         return l_line
+    # ---------------------------------------------
+    def _entries_from_line(self, line : str) -> int:
+        mtch = re.match(self._entries_regex, line)
+        if not mtch:
+            raise ValueError(f'Cannot extract number of entries from line \"{line}\" using regex \"{self._entries_regex}\"')
+
+        entries = mtch.group(1)
+
+        return int(entries)
     # ---------------------------------------------
     def get_ran_entries(self) -> int:
         '''
         Returns entries that DaVinci ran over
         '''
-        l_line = self._get_lines()
+        l_line    = self._get_dv_lines()
+        l_entries = [ line for line in l_line if 'Sum of all Algorithms' in line ]
+        if len(l_entries) != 1:
+            raise ValueError(f'Not one and only one line with sum of all algorithms found in {self._log_path}')
+
+        nentries = self._entries_from_line(l_entries[0])
+
+        log.info(f'Found {nentries} entries')
+
+        return nentries
 # ---------------------------------------------
