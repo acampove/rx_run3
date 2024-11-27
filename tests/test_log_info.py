21d78535b5009c27e6bcae8f163a6539664d3447
Angel Campoverde <angel.campoverde@cern.ch>
2024-11-27 12:43:56 +0800

    Adding docstringand testing function with algorigthm as arg

--- tests/test_log_info.py
+++ tests/test_log_info.py
@@ -4,11 +4,18 @@ Script with tests for LogInfo class
 
 from ap_utilities.log_info import LogInfo
 
+# ----------------------------
 class Data:
+    '''
+    Class storing shared data
+    '''
     log_path = '/home/acampove/cernbox/dev/tests/ap_utilities/log_info/00006789.zip'
-
-def test_simple():
+# ----------------------------
+def test_mcdt():
+    '''
+    Tests if the statistics used for MCDecayTree are read correctly
+    '''
     obj = LogInfo(zip_path = Data.log_path)
-    nentries = obj.get_ran_entries()
+    nentries = obj.get_mcdt_entries('Bu_Kee_eq_btosllball05_DPC')
 
-    assert nentries == 13759
+    assert nentries == 13584
