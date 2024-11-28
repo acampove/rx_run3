4f36df91121ee9517b2a0517ccf18e7b502d679e
Angel Campoverde <angel.campoverde@cern.ch>
2024-11-28 16:14:23 +0800

    Adding file

---
+++ src/ap_utilities_scripts/check_production.py
@@ -0,0 +1,8 @@
+'''
+This script will do checks on the rd_ap_2024 before pushing to feature branch 
+
+This problems will be caught before pipelines run
+'''
+
+if __name__ == '__main__':
+    main(
