92982a96e8bbb9b4d8f81a5f3c3c31a4ea4d1a59
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-03 16:17:09 +0800

    Need -u flag
    Used to pass username of user that made the venv

--- src/post_ap_scripts/job_filter.py
+++ src/post_ap_scripts/job_filter.py
@@ -29,14 +29,15 @@ class Data:
     venv        : str
     mode        : str
     epat        : str
+    user        : str
     runner_path : str
 # ---------------------------------------
 def _get_job(jobid : int) -> Job:
     j = Job()
     j.setCPUTime(36000)
     j.setDestination('LCG.CERN.cern')
-    j.setExecutable(Data.runner_path, arguments=f'{Data.dset} {Data.conf} {Data.njob} {jobid} {Data.epat}')
-    j.setInputSandbox([f'LFN:/lhcb/user/a/acampove/run3/venv/{Data.venv}/dcheck.tar'])
+    j.setExecutable(Data.runner_path, arguments=f'{Data.dset} {Data.conf} {Data.njob} {jobid} {Data.epat} {Data.user}')
+    j.setInputSandbox([f'LFN:/lhcb/user/{Data.user[0]}/{Data.user}/run3/venv/{Data.venv}/dcheck.tar'])
     j.setOutputData(['*.root'], outputPath=Data.name)
     j.setName(f'{Data.name}_{jobid:03}')
 
@@ -49,6 +50,7 @@ def _get_args() -> argparse.Namespace:
     parser.add_argument('-d', '--dset' , type =str, help='Dataset, e.g. dt_2024_turbo' , required=True)
     parser.add_argument('-j', '--njob' , type =int, help='Number of grid jobs'         , required=True)
     parser.add_argument('-e', '--venv' , type =str, help='Index of virtual environment', required=True)
+    parser.add_argument('-u', '--user' , type =str, help='User associated to venv'     , required=True)
     parser.add_argument('-L', '--lcl_cfg', help='If used, will pick local config file' , action='store_true')
     parser.add_argument('-m', '--mode' , type =str, help='Run locally or in the grid'  , required=True, choices=['local', 'wms'])
 
