507fab905d43704aab59257caab39eba45571d42
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-03 18:27:41 +0800

    Type hinting

--- src/post_ap/ntuple_filter.py
+++ src/post_ap/ntuple_filter.py
@@ -68,14 +68,14 @@ class ntuple_filter:
 
         self._d_root_path = d_path
     # ---------------------------------------------
-    def _load_json(self, json_path : str) -> dict:
+    def _load_json(self, json_path : str) -> dict[str, list[str]]:
         if not os.path.isfile(json_path):
             raise FileNotFoundError(f'File not found: {json_path}')
 
         with open(json_path, encoding='utf-8') as ifile:
             return json.load(ifile)
     # ---------------------------------------------
-    def _reformat(self, d_path):
+    def _reformat(self, d_path : dict[str, list[str]]) -> dict[str,str]:
         '''
         Takes dictionary:
 
