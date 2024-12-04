233887fa1bd1a5c673259f839ba888664243f66c
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-04 19:02:54 +0800

    Using new naming and arguments for NtupleFilter

--- src/post_ap_scripts/filter_ntuples.py
+++ src/post_ap_scripts/filter_ntuples.py
@@ -5,8 +5,8 @@ Script used to filter ntuples produced by AP
 
 import argparse
 
-from dmu.logging.log_store     import LogStore
-from post_ap.ntuple_filter import ntuple_filter
+from dmu.logging.log_store  import LogStore
+from post_ap.ntuple_filter  import NtupleFilter
 
 log=LogStore.add_logger('dmu:post_ap_scripts:filter_ntuples')
 #----------------------------------------
@@ -14,8 +14,8 @@ class Data:
     '''
     Class used to store shared data
     '''
-    cfg_ver : str
-    dset    : str
+    prod    : str
+    nick    : str
     ngroup  : int
     gindex  : int
     log_lv  : int
@@ -27,15 +27,15 @@ def _set_log():
 #----------------------------------------
 def _get_args():
     parser = argparse.ArgumentParser(description='Will produce a smaller ntuple from a large one, for a given group of files')
-    parser.add_argument('-c', '--cfg_ver', type=str, required=True , help='Type of job, e.g. comp')
-    parser.add_argument('-d', '--dset'   , type=str, required=True , help='Dataset, e.g. dt_2024_turbo')
-    parser.add_argument('-n', '--ngroup' , type=int, required=True , help='Number of groups of files')
+    parser.add_argument('-p', '--prod'   , type=str, required=True , help='Production name, e.g. rd_ap_2024')
+    parser.add_argument('-n', '--name'   , type=str, required=True , help='Sample nickname, e.g. data, simulation')
+    parser.add_argument('-g', '--ngroup' , type=int, required=True , help='Number of groups of files')
     parser.add_argument('-i', '--gindex' , type=int, required=True , help='Index of the current group been processed')
     parser.add_argument('-l', '--loglvl' , type=int, required=False, help='Loglevel', default=20, choices=[10, 20, 30, 40])
     args = parser.parse_args()
 
-    Data.cfg_ver= args.cfg_ver
-    Data.dset   = args.dset
+    Data.prod   = args.prod
+    Data.nick   = args.name
     Data.ngroup = args.ngroup
     Data.gindex = args.gindex
     Data.log_lv = args.loglvl
@@ -47,7 +47,7 @@ def main():
     _get_args()
     _set_log()
 
-    obj=ntuple_filter(dataset=Data.dset, cfg_ver=Data.cfg_ver, index=Data.gindex, ngroup=Data.ngroup)
+    obj=NtupleFilter(production=Data.prod, nickname=Data.nick, index=Data.gindex, ngroup=Data.ngroup)
     obj.filter()
 #----------------------------------------
 if __name__ == '__main__':
