5dc79f69c5766b97b6e09b93d7007d4a517fd370
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-06 18:16:35 +0800

    Adding skeleton of code

--- src/post_ap/data_vars_adder.py
+++ src/post_ap/data_vars_adder.py
@@ -1,3 +1,20 @@
 '''
 Module containing DataVarsAdder class
 '''
+
+from ROOT import RDataFrame
+
+class DataVarsAdder:
+    '''
+    Class used to add variables to dataframes that only make sense for data
+    '''
+    # -------------------------------------
+    def __init__(self, rdf : RDataFrame):
+        self._rdf = rdf
+    # -------------------------------------
+    def get_rdf(self) -> RDataFrame:
+        '''
+        Returns dataframe with all variables added (or booked in this case)
+        '''
+        return self._rdf
+# -------------------------------------
