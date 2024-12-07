417832e3f671a3adc449a78c8a482573c4a0d031
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-07 12:23:04 +0800

    Adding implementation of get_block

--- src/post_ap/data_vars_adder.py
+++ src/post_ap/data_vars_adder.py
@@ -7,18 +7,57 @@ from ROOT import RDataFrame, Numba
 from dmu.logging.log_store import LogStore
 
 log = LogStore.add_logger('post_ap:data_vars_adder')
-
 # -------------------------------------
 @Numba.Declare(['int', 'int'], 'int')
 def get_block(run_number : int, fill_number : int) -> int:
-    return 1
+    block_run = 298626 <=  run_number <= 301278
+    block_fil = 9808   <= fill_number <= 9910
+    if block_run and block_fil:
+        return 4
+
+    block_run = 301325 <=  run_number <= 302403
+    block_fil = 9911   <= fill_number <= 9943
+    if block_run and block_fil:
+        return 3
+
+    block_run = 302429 <=  run_number <= 303010
+    block_fil = 9945   <= fill_number <= 9978
+    if block_run and block_fil:
+        return 2
+
+    block_run = 303092 <=  run_number <= 304604
+    block_fil = 9982   <= fill_number <= 10056
+    if block_run and block_fil:
+        return 1
+
+    block_run = 304648 <=  run_number <= 305739
+    block_fil = 10059  <= fill_number <= 10102
+    if block_run and block_fil:
+        return 5
+
+    block_run = 305802 <= run_number  <= 307544
+    block_fil = 10104  <= fill_number <= 10190
+    if block_run and block_fil:
+        return 6
+
+    block_run = 307576 <= run_number  <= 308098
+    block_fil = 10197  <= fill_number <= 10213
+    if block_run and block_fil:
+        return 7
+
+    block_run = 308104 <= run_number  <= 308540
+    block_fil = 10214  <= fill_number <= 10232
+    if block_run and block_fil:
+        return 8
+
+    return -1
 # -------------------------------------
 class DataVarsAdder:
     '''
     Class used to add variables to dataframes that only make sense for data
     It adds:
 
-    - block      : Block number 
+    - block      : Block number
     - is_good_run: Check for run data quality
     '''
     # -------------------------------------
