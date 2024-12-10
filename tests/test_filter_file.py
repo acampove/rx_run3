00a1ba4e93368feffc5f5237af8532f341a19477
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-10 19:22:08 +0800

    Testing with turbo and spruce inputs

--- tests/test_filter_file.py
+++ tests/test_filter_file.py
@@ -26,30 +26,34 @@ def _initialize():
     Will set loggers, etc
     '''
     log.info('Initializing')
-    os.environ['CONFIG_PATH'] = '/home/acampove/Packages/config_files/post_ap/v2.yaml'
+    os.environ['CONFIG_PATH'] = '/home/acampove/Packages/config_files/post_ap/v3.yaml'
 
     LogStore.set_level('dmu:rdataframe:atr_mgr', 30)
     LogStore.set_level('post_ap:selector'      , 20)
     LogStore.set_level('post_ap:utilities'     , 30)
     LogStore.set_level('post_ap:FilterFile'    , 10)
 # --------------------------------------
-def test_dt():
+@pytest.mark.parametrize('is_turbo' , [True, False])
+def test_dt(is_turbo : bool):
     '''
     Run test on data
     '''
-    sample_name = 'data_24_magdown_turbo_24c3'
+    sample_name = 'data_xxx'
+    path        = Data.dt_turbo if is_turbo else Data.dt_spruce
 
-    obj = FilterFile(sample_name=sample_name, file_path=Data.dt_path)
+    obj = FilterFile(sample_name=sample_name, file_path=path)
     obj.dump_contents = True
     obj.run()
 # --------------------------------------
-def test_mc():
+@pytest.mark.parametrize('is_turbo' , [True, False])
+def test_mc(is_turbo : bool):
     '''
     Run test on MC
     '''
-    sample_name = 'mc_24_w31_34_magup_nu6p3_sim10d_pythia8_12143010_bu_jpsipi_mm_tuple'
+    sample_name = 'mc_xxx'
+    path        = Data.mc_turbo if is_turbo else Data.mc_spruce
 
-    obj = FilterFile(sample_name=sample_name, file_path=Data.mc_path)
+    obj = FilterFile(sample_name=sample_name, file_path=path)
     obj.dump_contents = True
     obj.run()
 # --------------------------------------
