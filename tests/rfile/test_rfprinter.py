c4724dddfb246317988873d88e03c8fe7899e2c1
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-15 14:23:09 +0800

    Using pytest and add test for raise_on_fail
    Test files taken from CERNBOX

--- tests/rfile/test_rfprinter.py
+++ tests/rfile/test_rfprinter.py
@@ -2,6 +2,9 @@
 Tests for RFPrinter class
 '''
 
+import os
+import pytest
+
 from dmu.rfile.rfprinter import RFPrinter
 
 # -------------------------------------------------
@@ -9,13 +12,31 @@ class Data:
     '''
     Class used to store shared data
     '''
-    file_path : str = '/publicfs/lhcb/user/campoverde/Data/RK/.run3/flt_003/dt_2024_turbo_comp_00244201_00000002_1_tuple_Hlt2RD_LbTopKMuMu_MVA.root'
+    file_path_simple : str
+    file_path_fail   : str
+# -------------------------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize() -> None:
+    cernbox = os.environ['CERNBOX']
+
+    Data.file_path_simple = f'{cernbox}/Run3/analysis_productions/for_local_tests/mc_turbo.root'
+    Data.file_path_fail   = f'{cernbox}/tests/dmu/rfprinter/fail.root'
 # -------------------------------------------------
 def test_simple():
     '''
     Test basic printing
     '''
 
-    obj = RFPrinter(path=Data.file_path)
+    obj = RFPrinter(path=Data.file_path_simple)
     obj.save(to_screen=True)
 # -------------------------------------------------
+def test_raise_on_fail():
+    '''
+    Test raise_on_fail flag 
+    '''
+
+    obj = RFPrinter(path=Data.file_path_fail)
+    obj.save(raise_on_fail=False)
+    with pytest.raises(OSError) as exc_info:
+        obj.save(raise_on_fail= True)
+        assert exc_info.value.startswith('Cannot open:')
