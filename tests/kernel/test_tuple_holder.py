69ab2f6777ab898556a42ebfed0f93d7666d6e6f
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-21 17:26:02 +0800

    Adding tests with args

--- tests/kernel/test_tuple_holder.py
+++ tests/kernel/test_tuple_holder.py
@@ -2,11 +2,37 @@
 Module containing tests for TupleHolder
 '''
 
-from rx_kernel.tuple_holder import TupleHolder
+from rx_kernel.tuple_holder  import TupleHolder
+from rx_kernel.config_holder import ConfigHolder
 
+# -------------------------
+def _get_config_holder():
+    cfg = {
+            'project' : 'RK',
+            'analysis': 'EE',
+            'sample'  : 'LPT',
+            'q2bin'   : 'central',
+            'year'    : '18',
+            'polarity': 'MD',
+            'trigger' : 'L0L',
+            'trg_cfg' : 'exclusive',
+            'brem'    : '0G',
+            'track'   : 'LL'}
+
+    return ConfigHolder(cfg=cfg)
+# -------------------------
 def test_default():
     '''
     Test for default constructor
     '''
     obj = TupleHolder()
-    obj.PrintInline() 
+    obj.PrintInline()
+# -------------------------
+def test_2018():
+    '''
+    Will test with 2018 configuration
+    '''
+    ch  = _get_config_holder()
+    obj = TupleHolder(ch,'')
+    obj.PrintInline()
+# -------------------------
