db48a7f1dbe9be3409c099caa829eac31a1f3ba3
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-21 16:24:50 +0800

    Adding test for string args

--- tests/kernel/test_config_holder.py
+++ tests/kernel/test_config_holder.py
@@ -2,13 +2,33 @@
 Module where config holder is tested
 '''
 
-from rx_kernel import ConfigHolder
+from rx_kernel.config_holder import ConfigHolder
 
 # -----------------------------------
-def test_simple():
+def test_default():
     '''
-    Simplest test of ConfigHolder
+    Simplest test of default constructor
     '''
     ch = ConfigHolder()
+    ch.Print()
+# -----------------------------------
+# -----------------------------------
+def test_string():
+    '''
+    Simplest test of default constructor
+    '''
+    cfg = {
+            'project' : 'RK',
+            'analysis': 'EE',
+            'sample'  : 'LPT',
+            'q2bin'   : 'central',
+            'year'    : '24',
+            'polarity': 'MD',
+            'trigger' : 'L0L',
+            'trg_cfg' : 'exclusive',
+            'brem'    : '0G',
+            'track'   : 'LL'}
+
+    ch = ConfigHolder(cfg=cfg)
     ch.Print()
 # -----------------------------------
