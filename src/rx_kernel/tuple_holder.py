d54d9bd3b057a22eb603d37e737db189a3f78a14
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-21 17:25:39 +0800

    Changes to run with arguments

--- src/rx_kernel/tuple_holder.py
+++ src/rx_kernel/tuple_holder.py
@@ -1,13 +1,21 @@
 '''
-Module containing python interface to C++ TupleHolder 
+Module containing python interface to C++ TupleHolder
 '''
+from typing import Union
 
-from ROOT import TupleHolder as TupleHolder_cpp
+from ROOT import ConfigHolder as ConfigHolder_cpp
+from ROOT import TupleHolder  as TupleHolder_cpp
 
-def TupleHolder() -> TupleHolder_cpp:
+# pylint: disable=invalid-name
+def TupleHolder(
+        cfg     : Union[ConfigHolder_cpp,None] = None,
+        options : Union[str,None]              = None) -> TupleHolder_cpp:
     '''
     Function returning TupleHolder c++ implementation's instance
     '''
-    obj = TupleHolder_cpp()
+    if cfg is None and options is None:
+        return TupleHolder_cpp()
+
+    obj = TupleHolder_cpp(cfg, options)
 
     return obj
