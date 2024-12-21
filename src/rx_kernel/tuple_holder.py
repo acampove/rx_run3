37eda201f007f42c9c6ed1beeceef7e2dbba92cc
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-21 18:21:09 +0800

    Need to pass actual TString, not python string

--- src/rx_kernel/tuple_holder.py
+++ src/rx_kernel/tuple_holder.py
@@ -2,11 +2,12 @@
 Module containing python interface to C++ TupleHolder
 '''
 from typing import Union
-
-from ROOT import ConfigHolder as ConfigHolder_cpp
-from ROOT import TupleHolder  as TupleHolder_cpp
+from ROOT   import TString
+from ROOT   import ConfigHolder as ConfigHolder_cpp
+from ROOT   import TupleHolder  as TupleHolder_cpp
 
 # pylint: disable=invalid-name
+# -----------------------------------------------------------
 def TupleHolder(
         cfg     : Union[ConfigHolder_cpp,None] = None,
         options : Union[str,None]              = None) -> TupleHolder_cpp:
@@ -16,6 +17,8 @@ def TupleHolder(
     if cfg is None and options is None:
         return TupleHolder_cpp()
 
-    obj = TupleHolder_cpp(cfg, options)
+    opt = TString(options)
+    obj = TupleHolder_cpp(cfg, opt)
 
     return obj
+# -----------------------------------------------------------
