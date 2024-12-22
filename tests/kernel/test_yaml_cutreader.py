045443eade82fa7a6ac3304e0820acd366613a9f
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-22 13:07:34 +0800

    Adding reading test

--- tests/kernel/test_yaml_cutreader.py
+++ tests/kernel/test_yaml_cutreader.py
@@ -2,13 +2,24 @@
 Module with functions needed to test YamlCutReader python interface
 '''
 
-from rx_kernel import YamlCutReader
+from importlib.resources import files
 
+from rx_kernel import YamlCutReader
 # -----------------------------
 def test_default():
     '''
     Tests default contructor
     '''
     rdr = YamlCutReader()
+    rdr.PrintData()
+# -----------------------------
+def test_reading():
+    '''
+    Tests reading YAML file
+    '''
+    config_path = files('rx_tests').joinpath('kernel/yaml_cutreader.yaml')
+    config_path = str(config_path)
+
+    rdr = YamlCutReader(config_path)
     rdr.PrintData()
 # -----------------------------
