6b75250b704a6c94bcf6f35514d19ca27dc3eff2
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-22 18:39:40 +0800

    TupleHolder interface takes *args
    Needed because of the presence of multiple constructors with different
    number of arguments in the C++ class

--- src/rx_kernel/tuple_holder.py
+++ src/rx_kernel/tuple_holder.py
@@ -1,24 +1,33 @@
 '''
 Module containing python interface to C++ TupleHolder
 '''
-from typing import Union
 from ROOT   import TString
-from ROOT   import ConfigHolder as ConfigHolder_cpp
 from ROOT   import TupleHolder  as TupleHolder_cpp
 
 # pylint: disable=invalid-name
 # -----------------------------------------------------------
-def TupleHolder(
-        cfg     : Union[ConfigHolder_cpp,None] = None,
-        options : Union[str,None]              = None) -> TupleHolder_cpp:
+def TupleHolder(*args) -> TupleHolder_cpp:
     '''
     Function returning TupleHolder c++ implementation's instance
     '''
-    if cfg is None and options is None:
+
+    if len(args) == 0:
         return TupleHolder_cpp()
 
-    opt = TString(options)
-    obj = TupleHolder_cpp(cfg, opt)
+    if len(args) == 2:
+        [cfg, opt] = args
+        opt = TString(opt)
 
-    return obj
+        return TupleHolder_cpp(cfg, opt)
+
+    if len(args) == 4:
+        [cfg, file_path, tree_path, opt] = args
+
+        file_path = TString(file_path)
+        tree_path = TString(tree_path)
+        opt       = TString(opt)
+
+        return TupleHolder_cpp(cfg, file_path, tree_path, opt)
+
+    raise ValueError('Invalid number of arguments, allowed: 0, 2, 4')
 # -----------------------------------------------------------
