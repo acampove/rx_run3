4e3d795aacd5a595171b8aea4981853949b6ddc6
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-24 17:26:04 +0800

    Adding fallback to allow for def args
    Needed for run3, where trigger is not needed

--- src/rx_kernel/config_holder.py
+++ src/rx_kernel/config_holder.py
@@ -1,15 +1,22 @@
 '''
 Module containing python interface to C++ ConfigHolder
 '''
+# pylint: disable=import-error, invalid-name
+
 from typing import Union
 
 from ROOT import ConfigHolder as ConfigHolder_cpp
 from ROOT import TString
 # ------------------------------------------------------------------
-def _str_to_tstring(key : str, cfg : dict) -> TString:
-    val = cfg[key]
+def _str_to_tstring(key : str, cfg : dict, fall_back : Union[str,None]) -> TString:
+    if key in cfg:
+        val = cfg[key]
+        return TString(val)
 
-    return TString(val)
+    if fall_back is not None:
+        return TString(fall_back)
+
+    raise ValueError(f'Required argument {key} not found')
 # ------------------------------------------------------------------
 def ConfigHolder(cfg : Union[dict,None] = None) -> ConfigHolder_cpp:
     '''
@@ -19,17 +26,16 @@ def ConfigHolder(cfg : Union[dict,None] = None) -> ConfigHolder_cpp:
         return ConfigHolder_cpp()
 
     obj = ConfigHolder_cpp(
-            _str_to_tstring('project' , cfg),
-            _str_to_tstring('analysis', cfg),
-            _str_to_tstring('sample'  , cfg),
-            _str_to_tstring('q2bin'   , cfg),
-            _str_to_tstring('year'    , cfg),
-            _str_to_tstring('polarity', cfg),
-            _str_to_tstring('trigger' , cfg),
-            _str_to_tstring('trg_cfg' , cfg),
-            _str_to_tstring('brem'    , cfg),
-            _str_to_tstring('track'   , cfg))
+            _str_to_tstring('project' , cfg, fall_back =     None),
+            _str_to_tstring('analysis', cfg, fall_back =     None),
+            _str_to_tstring('sample'  , cfg, fall_back =       ''),
+            _str_to_tstring('q2bin'   , cfg, fall_back = 'global'),
+            _str_to_tstring('year'    , cfg, fall_back = 'global'),
+            _str_to_tstring('polarity', cfg, fall_back = 'global'),
+            _str_to_tstring('trigger' , cfg, fall_back = 'global'),
+            _str_to_tstring('trg_cfg' , cfg, fall_back = 'global'),
+            _str_to_tstring('brem'    , cfg, fall_back = 'global'),
+            _str_to_tstring('track'   , cfg, fall_back = 'global'))
 
     return obj
 # ------------------------------------------------------------------
-
