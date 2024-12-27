b86ba79144dd16eaf712f287e3ff75d479ba6242
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-27 10:36:51 +0800

    DataDir is made part of the configHolder
    Won't be passed to the code through an env var

--- src/rx_kernel/config_holder.py
+++ src/rx_kernel/config_holder.py
@@ -33,13 +33,9 @@ def ConfigHolder(cfg : Union[dict,None] = None) -> ConfigHolder_cpp:
 
     cpp_cfg= std.map('TString, TString')()
     for name, value in cfg.items():
-        if name == 'DATADIR':
-            os.environ[name] = value
-            log.debug(f'Exporting variable: {name} -> {value}')
-        else:
-            name = TString(value)
-            value= TString(value)
-            cpp_cfg[name]=value
+        name = TString(value)
+        value= TString(value)
+        cpp_cfg[name]=value
 
     obj = ConfigHolder_cpp(cpp_cfg)
 
