5da2d412a9351734d56787ec86dfba620278140e
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-28 14:49:10 +0800

    Implementing basic test with ConfigHolder constructor

--- tests/kernel/test_event_type.py
+++ tests/kernel/test_event_type.py
@@ -3,18 +3,19 @@ Module with functions used to test EventTuple class
 '''
 # pylint: disable=import-error, wrong-import-order
 
-from dataclasses import dataclass
-
-from ROOT import MessageSvc
-
+from rx_kernel.event_type  import EventType
+from rx_common             import utilities as ut
 from dmu.logging.log_store import LogStore
 
+from ROOT import MessageSvc
 MessageSvc.Initialize(-1)
 
-log=LogStore.add_logger('rx_common:test_tuple_holder')
+log=LogStore.add_logger('rx_common:test_event_type')
 
-def test_event_type():
+def test_config_constructor():
     '''
-    Test of event type constructor with map argument
+    Test of event type constructor with ConfigHolder arg 
     '''
-
+    cpp_cfg = ut.get_config_holder(is_run3=True)
+    evt     = EventType(cpp_cfg)
+    evt.Init()
