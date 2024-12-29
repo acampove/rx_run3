26fc6ff4344c077e166c17fdd9237a36c303a277
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-29 14:21:18 +0800

    Loading YAML here, not in init file

--- src/rx_kernel/__init__.py
+++ src/rx_kernel/__init__.py
@@ -4,9 +4,17 @@ Module used to put RX core utilities, written in c++ in namespace
 # pylint: disable=import-error
 
 from dmu.logging.log_store import LogStore
-
 from rx_common             import utilities as ut
 
 
 log=LogStore.add_logger('rx_common:kernel')
-ut.initialize_project()
+log.debug('Initializing kernel project')
+ut.initialize_project('kernel')
+
+# SettingDef cannot be accessed unti
+from ROOT import SettingDef
+from ROOT import MessageSvc
+
+MessageSvc.Initialize(0)
+
+SettingDef.AllowedConf.Initialize(ut.Data.yaml_config_path)
