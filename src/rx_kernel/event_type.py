a6362e972aba9d2a98564b476f44058d7bad172d
Angel Campoverde <angel.campoverde@cern.ch>
2024-12-29 15:36:06 +0800

    EventType takes three options as before

--- src/rx_kernel/event_type.py
+++ src/rx_kernel/event_type.py
@@ -9,12 +9,16 @@ from dmu.logging.log_store import LogStore
 
 log = LogStore.add_logger('rx_common:event_type')
 
-def EventType(cfg_cpp : ConfigHolder_cpp) -> EventType_cpp:
+def EventType(
+        cfg_cpp : ConfigHolder_cpp,
+        cut_opt : str,
+        wgt_opt : str,
+        tup_opt : str) -> EventType_cpp:
     '''
     Function taking a ConfigHolder instance 
     and returning EventType object implemented in c++ class
     '''
 
-    evt_cpp = EventType_cpp(cfg_cpp)
+    evt_cpp = EventType_cpp(cfg_cpp, cut_opt, wgt_opt, tup_opt)
 
     return evt_cpp
