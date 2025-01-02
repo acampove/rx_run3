a2469ee2a63aa673ae83d63afef429bf15bae384
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-02 14:42:26 +0800

    Updating for new run3 yaml files with selection

--- src/rx_selection/selection.py
+++ src/rx_selection/selection.py
@@ -19,96 +19,46 @@ class Data:
     '''
     Class used to store share attributes
     '''
-    l_ee_trig   = ['ETOS', 'HTOS', 'GTIS']
-    l_mm_trig   = ['MTOS']
-    l_year_r12  = ['2011', '2012', '2015', '2016', '2017', '2018']
-    l_year_run3 = ['2024']
-    l_year      = l_year_r12 + l_year_run3
+    l_project  = ['RK', 'RKst']
+    l_analysis = ['EE', 'MM'  ]
+    l_q2bin    = ['low', 'central', 'jpsi', 'psi2S', 'high']
 #-----------------------
-def selection(kind : str, trigger : str, year : str, proc : str, q2bin: str, decay : str) -> dict[str,str]:
+def selection(analysis : str, project : str, q2bin: str, process : str) -> dict[str,str]:
     '''
     Picks up sample name, trigger, etc, returns dictionary with selection
 
-    Kind   : Kind of selection, e.g. all_gorder (legacy name, meaning all/full selection in Giulia's [former colleague] ordering)
-    trigger: L0 trigger for Run12, ETOS or MTOS for Run3, used to pick channel
-    proc   : Short name for sample, e.g. data, ctrl, etc, used to pick truth matching
-    decay  : Decay targetted, e.g. bukee, bdkstee, etc, used to pick actual cuts
+    analysis : RK or RKst
+    project  : EE or MM
+    q2bin    : low, central, jpsi, psi2S or high
+    event_type : Event type for MC, "data" for data
     '''
-    if not year in Data.l_year:
-        raise ValueError(f'Cannot find {year} among: {Data.l_year}')
-
-    log.info(f'Using q2bin {q2bin} for cut type {kind}, process {proc}, trigger {trigger} and year {year}')
-
     d_cut : dict[str,str] = {}
 
-    d_cut['truth'] = tm.get_truth(proc)
+    d_cut['truth'] = tm.get_truth(process)
 
-    if   decay in [  'bukee',   'bukmm'] and year in Data.l_year_r12:
-        d_tmp = _get_bukll_run12_selection(kind, trigger, q2bin, year, decay)
-    elif decay in [  'bukee',   'bukmm'] and year in Data.l_year_run3:
-        d_tmp = _get_bukll_run3_selection(kind, trigger, q2bin, year, decay)
-    elif decay in ['bdkstee', 'bdkstmm'] and year in Data.l_year_run3:
-        d_tmp = _get_bdkst_run3_selection(kind, trigger, q2bin, year, decay)
-    else:
-        raise ValueError(f'Invalid decay: {decay}')
-
+    d_tmp = _get_selection(analysis, project, q2bin)
     d_cut.update(d_tmp)
 
     return d_cut
 #-----------------------
-def _get_bukll_run12_selection(kind: str, trigger : str, q2bin : str, year : str, decay : str) -> dict[str,str]:
-    d_cut         = {}
-    trigger_lower = trigger.lower()
+def _get_selection(analysis : str, project : str, q2bin : str) -> dict[str,str]:
+    sel_dir = files('rx_selection_data')
+    sel_dir = str(sel_dir)
 
-    if   kind == 'all_gorder' and trigger in Data.l_ee_trig:
-        l_cut = ['nspd', trigger_lower, 'hlt1', 'hlt2', 'q2', 'kinematics', 'cascade', 'ghost', 'calo_rich', 'pid', 'xyecal', 'bdt', 'mass']
-    elif kind == 'all_gorder' and trigger in Data.l_mm_trig:
-        l_cut = ['nspd', trigger_lower, 'hlt1', 'hlt2', 'q2', 'kinematics', 'cascade', 'ghost',      'rich', 'acceptance', 'pid', 'jpsi_misid', 'bdt', 'mass']
-    else:
-        raise ValueError(f'Wrong kind "{kind}" and/or trigger "{trigger}"')
+    yaml_path = vman.get_last_version(
+            dir_path     = sel_dir,
+            extension    = 'yaml',
+            version_only = False ,
+            main_only    = False)
 
-    d_cut = { cut : rs.get(cut, trigger, q2bin, year, decay) for cut in l_cut }
+    with open(yaml_path, encoding='utf-8') as ifile:
+        d_sel = yaml.safe_load(ifile)
 
-    return d_cut
-#-----------------------
-def _get_bukll_run3_selection(kind : str, trigger : str, q2bin : str, year : str, decay : str) -> dict[str,str]:
-    d_cut         = {}
-    if   kind == 'all_gorder' and trigger in Data.l_ee_trig:
-        l_cut = ['hlt1', 'hlt2', 'q2', 'kinematics', 'cascade', 'ghost', 'rich', 'calo', 'pid_l', 'pid_k', 'jpsi_misid', 'bdt', 'mass']
-    elif kind == 'all_gorder' and trigger in Data.l_mm_trig:
-        l_cut = ['hlt1', 'hlt2', 'q2', 'kinematics', 'cascade', 'ghost', 'rich', 'muon', 'pid_l', 'pid_k', 'jpsi_misid', 'bdt', 'mass']
-    else:
-        raise ValueError(f'Wrong kind "{kind}" and/or trigger "{trigger}"')
+    d_cut  = d_sel[project][analysis]
+    q2_cut = d_cut['q2'][q2bin]
+    del d_cut['q2']
 
-    d_cut = { cut : rs.get(cut, trigger, q2bin, year, decay) for cut in l_cut }
+    d_cut['q2'] = q2_cut
 
     return d_cut
 #-----------------------
-def _get_bdkst_run3_selection(kind : str, trigger : str, q2bin : str, year :str , decay : str) -> dict[str,str]:
-    d_cut         = {}
-    if kind == 'all_gorder':
-        l_cut = [
-                'hlt1',
-                'hlt2',
-                'q2',
-                'kinem_lep',
-                'kinem_had',
-                'kst_mass',
-                'kst_pt',
-                'cascade',
-                'ghost',
-                'rich',
-                'acceptance',
-                'pid_l',
-                'pid_k',
-                'pid_pi',
-                'jpsi_misid',
-                'bdt',
-                'mass',
-                ]
-    else:
-        raise ValueError(f'Wrong kind "{kind}"')
-
-    d_cut = { cut : rs.get(cut, trigger, q2bin, year, decay) for cut in l_cut }
-
-    return d_cut
