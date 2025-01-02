0cad7663e83639170ca0b3ac8f5cb150676e9063
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-02 16:37:04 +0800

    Adding back _get_numeric_version

--- src/rx_selection/version_management.py
+++ src/rx_selection/version_management.py
@@ -4,10 +4,32 @@ Module containing functions used to find latest, next version, etc of a path.
 
 import glob
 import os
+import re
 
 from dmu.logging.log_store  import LogStore
 
 log=LogStore.add_logger('rx_selection:version_management')
+
+#---------------------------------------
+def _get_numeric_version(version : str) -> int:
+    '''
+    Takes string with numbers at the end (padded or not)
+    Returns integer version of numbers
+    '''
+    #Skip these directories
+    if version in ['__pycache__']:
+        return -1
+
+    regex=r'[a-z]+(\d+)'
+    mtch =re.match(regex, version)
+    if not mtch:
+        log.debug(f'Cannot extract numeric version from: {version}')
+        return -1
+
+    str_val = mtch.group(1)
+    val     = int(str_val)
+
+    return val
 #---------------------------------------
 def get_last_version(
         dir_path     : str,
@@ -35,7 +57,7 @@ def get_last_version(
         raise ValueError(f'Nothing found in {file_wc}')
 
     d_file_org = { os.path.basename(obj).replace('.', '') : obj       for obj             in l_obj             if os.path.isfile(obj) }
-    d_file_num = { get_numeric_version(name)              : file_path for name, file_path in d_file_org.items() }
+    d_file_num = {_get_numeric_version(name)              : file_path for name, file_path in d_file_org.items() }
 
     c_file = sorted(d_file_num.items())
 
