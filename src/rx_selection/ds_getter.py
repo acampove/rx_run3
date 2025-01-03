2258ab32318ab8cc5ac59d47a5153cfa3fd7617a
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-03 09:40:48 +0800

    Removing unnecessary imports

--- src/rx_selection/ds_getter.py
+++ src/rx_selection/ds_getter.py
@@ -8,8 +8,6 @@ Module containing class that provides ROOT dataframe after a given selection
 
 import os
 import re
-import glob
-import logging
 from importlib.resources import files
 from typing              import Union
 
