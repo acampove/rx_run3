1132f9c7dd0bdd53804202b851950408b7594eac
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-03 17:23:23 +0800

    Adding file

---
+++ src/rx_common_scripts/make_run3_yaml.py
@@ -0,0 +1,13 @@
+'''
+Script that will create list of samples for each project for Run3
+based on file for Run1/2
+'''
+
+def _load_run12():
+    return
+
+def main():
+    d_run12 = _load_run12()
+
+if __name__ == '__main__':
+    main()
