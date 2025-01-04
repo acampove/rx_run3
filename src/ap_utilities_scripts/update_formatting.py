2981059fa624e3846887dbf10b812772c38c057e
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-04 15:44:10 +0800

    Adding data entries

--- src/ap_utilities_scripts/update_formatting.py
+++ src/ap_utilities_scripts/update_formatting.py
@@ -9,6 +9,18 @@ import yaml
 from ap_utilities.decays import utilities as aput
 
 # --------------------------------
+def _add_data_low_org(d_low_org : dict[str,str]) -> dict[str,str]:
+    d_low_org['data_24_magdown_24c1'] = 'DATA_24_MagDown_24c1'
+    d_low_org['data_24_magdown_24c2'] = 'DATA_24_MagDown_24c2'
+    d_low_org['data_24_magdown_24c3'] = 'DATA_24_MagDown_24c3'
+    d_low_org['data_24_magdown_24c4'] = 'DATA_24_MagDown_24c4'
+    d_low_org['data_24_magup_24c1'  ] = 'DATA_24_MagUp_24c1'
+    d_low_org['data_24_magup_24c2'  ] = 'DATA_24_MagUp_24c2'
+    d_low_org['data_24_magup_24c3'  ] = 'DATA_24_MagUp_24c3'
+    d_low_org['data_24_magup_24c4'  ] = 'DATA_24_MagUp_24c4'
+
+    return d_low_org
+# --------------------------------
 def _load_file(file_name : str) -> dict[str,str]:
     file_path = files('ap_utilities_data').joinpath(file_name)
     file_path = str(file_path)
@@ -44,6 +56,7 @@ def main():
     d_form_evt = _reformat(d_name_evt, keys=True)
     d_evt_form = _reformat(d_evt_name, keys=False)
     d_low_org  = { sample.lower() : sample for sample in d_form_evt }
+    d_low_org  = _add_data_low_org(d_low_org)
 
     _save(d_low_org , 'lower_original.yaml')
     _save(d_form_evt,       'form_evt.yaml')
