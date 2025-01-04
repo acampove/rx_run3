bd38ed8897820c1f95a9bcced493dfc820a5ea6a
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-04 15:24:37 +0800

    Will not format but use formatted YAML

--- src/ap_utilities/decays/utilities.py
+++ src/ap_utilities/decays/utilities.py
@@ -56,49 +56,39 @@ def _load_data(file_name : str) -> dict:
 
     return d_data
 # ---------------------------------
-def format_nickname(nickname : str, style : str) -> str:
+def format_nickname(nickname : str) -> str:
     '''
     Function taking decays nickname and returning formatted version
 
     nickaname: Name to be formatted
-    style    : How to format name, supported: literal, safe_1
     '''
     _initialize()
-
-    if style == 'literal':
-        return nickname
-
-    if style != 'safe_1':
-        raise ValueError(f'Invalid style: {style}')
 
     nickname = _apply_format(Data.d_form_long , nickname)
     nickname = _apply_format(Data.d_form_short, nickname)
 
     return nickname
 # ---------------------------------
-def read_decay_name(event_type : str, style : str = 'safe_1') -> str:
+# ---------------------------------
+def read_decay_name(event_type : str, formatted : bool = True) -> str:
     '''
     Takes event type, and style strings, returns nickname of decay as defined in DecFiles package
 
     Styles:
 
-    literal         : No change is made to nickname
-    safe_1 (default): With following replacements:
-        . -> p
-        = -> _eq_
-        - -> mn
-        + -> pl
-        , -> _
+    literal   : No change is made to nickname
+    formatted : If true, will return name after formatting 
     '''
     _initialize()
 
-    d_evt_name = _load_data('evt_name.yaml')
+    yaml_path  = 'evt_form.yaml' if formatted else 'evt_name.yaml'
+
+    d_evt_name = _load_data(yaml_path)
 
     if event_type not in d_evt_name:
         raise ValueError(f'Event type {event_type} not found')
 
     value = d_evt_name[event_type]
-    value = format_nickname(value, style)
 
     return value
 # ---------------------------------
@@ -109,7 +99,7 @@ def read_event_type(nickname : str) -> str:
     '''
     _initialize()
 
-    d_name_evt = _load_data('name_evt.yaml')
+    d_name_evt = _load_data('form_evt.yaml')
 
     if nickname not in d_name_evt:
         raise ValueError(f'Event type {nickname} not found')
@@ -118,7 +108,7 @@ def read_event_type(nickname : str) -> str:
 
     return value
 # ---------------------------------
-def new_from_old_nick(nickname : str, style : str = 'safe_1') -> str:
+def new_from_old_nick(nickname : str) -> str:
     '''
     Function that takes a decay nick name using Run1/2 naming
     and returns nicknames using Run3 naming
@@ -131,23 +121,22 @@ def new_from_old_nick(nickname : str, style : str = 'safe_1') -> str:
 
     evt_type   = d_old_evt[nickname]
 
-    d_evt_name = _load_data('evt_name.yaml')
+    d_evt_name = _load_data('evt_form.yaml')
     if evt_type not in d_evt_name:
         raise ValueError(f'Event type {evt_type} not found in: evt_name.yaml')
 
     new_nick   = d_evt_name[evt_type]
-    new_nick   = format_nickname(new_nick, style)
 
     return new_nick
 # ---------------------------------
 def old_from_new_nick(nickname : str) -> str:
     '''
-    Function that takes a decay nick name using Run3 naming with safe_1 style
+    Function that takes a decay nick name using Run3 naming
     and returns nicknames using Run1/2 naming
     '''
     _initialize()
 
-    d_name_evt = _load_data('name_evt.yaml')
+    d_name_evt = _load_data('form_evt.yaml')
     if nickname not in d_name_evt:
         raise ValueError(f'Nickname {nickname} not found in: name_evt.yaml')
 
