ab8012f49cff4a32c39dbf9d9d462b43f95d19d4
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-04 18:44:43 +0800

    First working version

--- src/rx_common_scripts/lfn_to_yaml.py
+++ src/rx_common_scripts/lfn_to_yaml.py
@@ -3,13 +3,16 @@ Module that will transform lists of LFNs from Ganga post_ap jobs
 into a yaml file ready to be plugged into the RX c++ framework
 '''
 # pylint: disable=line-too-long, import-error
+# pylint: disable=invalid-name
 
+import os
 import glob
 import json
 import argparse
 
 from importlib.resources    import files
 from dataclasses            import dataclass
+from functools              import cache
 
 import yaml
 from dmu.logging.log_store  import LogStore
@@ -22,11 +25,11 @@ class Data:
     '''
     Class used to hold shared data
     '''
-    # pylint: disable = invalid-name
-
+    out_dir   = 'yaml_files'
     max_files : int
     lfn_vers  : str
-    l_project = ['RK', 'RKst']
+    d_project : dict[str,list[str]]
+    d_sample  : dict[str, dict[str, list[str]]]
 # ---------------------------------
 def _get_paths() -> list[str]:
     '''
@@ -51,6 +54,19 @@ def _get_paths() -> list[str]:
 
     return l_path
 # ---------------------------------
+@cache
+def _load_config(name : str) -> dict:
+    '''
+    Loads config file in rx_config, using its name as argument
+    '''
+    cfg_path = files('rx_config').joinpath(name)
+    cfg_path = str(cfg_path)
+
+    with open(cfg_path, encoding='utf-8') as ifile:
+        d_conf = yaml.safe_load(ifile)
+
+    return d_conf
+# ---------------------------------
 def _get_args() -> argparse.Namespace:
     '''
     Parse arguments
@@ -66,16 +82,22 @@ def _get_args() -> argparse.Namespace:
 def _initialize(args : argparse.Namespace) -> None:
     Data.max_files = args.max
     Data.lfn_vers  = args.ver
+    Data.d_project = _load_config('sample_run3.yaml')
+    Data.d_sample  = _get_formatted_lfns()
 
     LogStore.set_level('rx_data:lfn_to_yaml', args.lvl)
 # ---------------------------------
-def _save(d_path : dict) -> None:
-    for key in d_path:
-        print(key)
+def _get_formatted_lfns() -> dict[str, dict[str, list[str]]]:
+    '''
+    Returns dictionary between sample name and 
+    dictionary between HLT trigger and list of LFNs
+    '''
+    l_path   = _get_paths()
+    splt     = PathSplitter(paths=l_path, max_files=Data.max_files)
+    d_path   = splt.split()
+    d_sample = _reformat_paths(d_path)
 
-    return
-    with open('samples.yaml', 'w', encoding='utf-8') as ofile:
-        yaml.safe_dump(d_path, ofile)
+    return d_sample
 # ---------------------------------
 def _reformat_paths(d_path : dict[tuple[str,str],list[str]]) -> dict[str,dict[str,list[str]]]:
     d_sample = {}
@@ -87,7 +109,69 @@ def _reformat_paths(d_path : dict[tuple[str,str],list[str]]) -> dict[str,dict[st
 
     return d_sample
 # ---------------------------------
-def _filter_samples()
+def _get_metadata(project : str) -> dict[str,str]:
+    d_meta = {
+            'DTName'   : 'DecayTree',
+            'MCDTName' : 'MCDecayTree',
+            }
+
+    if not project.startswith('Data'):
+        return d_meta
+
+    d_meta['LumiTreeName'] = 'LumiTree'
+
+    return d_meta
+# ---------------------------------
+def _path_from_list(l_lfn : list[str], sample : str, hlt : str) -> str:
+    '''
+    Takes list of LFNs and names of sample and HLT2 trigger
+    Makes list of LFNs and returns path to it.
+    '''
+
+    json_path = f'{Data.out_dir}/{sample}_{hlt}.json'
+    json_dir  = os.path.dirname(json_path)
+
+    os.makedirs(json_dir, exist_ok=True)
+    with open(json_path, 'w', encoding='utf-8') as ofile:
+        json.dump(l_lfn, ofile)
+
+    return json_path
+# ---------------------------------
+def _lfns_path_from_sample(sample : str) -> dict[str,str]:
+    '''
+    For a sample name, return a dictionary between trigger and path to list of LFNs 
+    '''
+    sample = sample.lower()
+    if sample not in Data.d_sample:
+        log.warning(f'Sample {sample} not found')
+        return {}
+
+    d_hlt_lfn = Data.d_sample[sample]
+    d_hlt_path= {hlt : _path_from_list(l_lfn, sample, hlt) for hlt, l_lfn in d_hlt_lfn.items()}
+
+    return d_hlt_path
+# ---------------------------------
+def _get_samples(project : str) -> dict[str,dict[str,str]]:
+    '''
+    Returns dictionary between sample and
+    dictionary between HLT2 trigger and path to list of LFNs
+    '''
+    l_sample = Data.d_project[project]
+
+    d_sample_lfn = {sample : _lfns_path_from_sample(sample) for sample in l_sample}
+
+    return d_sample_lfn
+# ---------------------------------
+def _get_data_dict() -> dict[str,dict[str,str]]:
+    d_data    = {}
+    for proj in Data.d_project:
+        d_sam={}
+        d_sam.update(_get_samples (proj))
+        d_sam.update(_get_metadata(proj))
+
+        d_data[proj] = d_sam
+
+    return d_data
 # ---------------------------------
 def main():
     '''
@@ -96,14 +180,10 @@ def main():
     args = _get_args()
     _initialize(args)
 
-    l_path = _get_paths()
-    splt   = PathSplitter(paths=l_path, max_files=Data.max_files)
-    d_path = splt.split()
+    d_data = _get_data_dict()
 
-    d_sample = _reformat_paths(d_path)
-    d_sample = _filter_samples(d_sample)
-
-    _save(d_sample)
+    with open('samples.yaml', 'w', encoding='utf-8') as ofile:
+        yaml.safe_dump(d_data, ofile)
 # ---------------------------------
 if __name__ == '__main__':
     main()
