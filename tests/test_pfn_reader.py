99084c3c208a1725081a80f85872822ccc36d174
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-05 12:52:11 +0800

    Config files live in current project

--- tests/test_pfn_reader.py
+++ tests/test_pfn_reader.py
@@ -1,8 +1,11 @@
 '''
 Script with tests for PFNReader class
 '''
+from importlib.resources import files
+
 import yaml
 import pytest
+
 import dmu.generic.utilities as gut
 
 from post_ap.pfn_reader import PFNReader
@@ -18,7 +21,7 @@ class Data:
             ('rd_ap_2024',       'data_turbo',    8)]
 # -----------------------------
 def _get_cfg() -> dict:
-    config_path = '/home/acampove/Packages/config_files/post_ap/v3.yaml' 
+    config_path = files('post_ap_data').joinpath('post_ap/v3.yaml')
     config_path = str(config_path)
 
     with open(config_path, encoding='utf-8') as ifile:
