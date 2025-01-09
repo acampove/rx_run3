9d9f8c8b2294463a37e274675c767a077589b504
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-09 12:28:01 +0800

    Adding basic structure

--- src/rx_selection_scripts/validate_sample.py
+++ src/rx_selection_scripts/validate_sample.py
@@ -1,9 +1,20 @@
 '''
-Script used to make validation plots 
+Script used to make validation plots
 '''
-import argparse
-from dataclasses import dataclass
+# pylint: disable = import-error
 
+import argparse
+from dataclasses            import dataclass
+from importlib.resources    import files
+from typing                 import Union
+
+import yaml
+import pprint
+from dmu.logging.log_store  import LogStore
+from ROOT                   import RDataFrame
+from rx_selection.ds_getter import ds_getter
+
+log = LogStore.add_logger('rx_selection:cache_data')
 # --------------------------
 @dataclass
 class Data:
@@ -11,20 +22,56 @@ class Data:
     Class used to store shared attributes
     '''
     version : str
+    nparts  : int
 # --------------------------
 def _parse_args():
     parser = argparse.ArgumentParser(description='Script used to validate ntuples')
-    parser.add_argument('-v', '--version' , type=str, help='Version of validation configuration', required=True)
+    parser.add_argument('-v', '--version' , type=str, help='Version of validation configuration'   , required=True)
+    parser.add_argument('-n', '--nparts'  , type=int, help='Number of parts to split validation on, default 1', default=1)
     args = parser.parse_args()
 
     Data.version = args.version
+    Data.nparts  = args.nparts
+# --------------------------
+def _load_config() -> dict[str,Union[str,int]]:
+    cfg_path = files('rx_selection_data').joinpath(f'validation/{Data.version}.yaml')
+    cfg_path = str(cfg_path)
+
+    with open(cfg_path, encoding='utf-8') as ifile:
+        cfg = yaml.safe_load(ifile)
+
+    return cfg
+# --------------------------
+def _get_config() -> dict[str,Union[str,int]]:
+    d_cfg : dict[str,int] = {
+            'npart'    : Data.nparts,
+            'ipart'    : 0,
+            'q2bin'    : 'central', # Just to make sure ds_getter does not complain, this cut will be removed later
+            'redefine' : {},
+            }
+
+    d_ext = _load_config()
+
+    d_sam = d_ext['sample']
+    d_cfg.update(d_sam)
+
+    pprint.pprint(d_cfg)
+
+    return d_cfg
+# --------------------------
+def _validate(rdf : RDataFrame) -> None:
+    pass
 # --------------------------
 def main():
     '''
     Script starts here
     '''
     _parse_args()
+    cfg = _get_config()
+    dsg = ds_getter(cfg=cfg)
+    rdf = dsg.get_rdf()
 
+    _validate(rdf)
 # --------------------------
 if __name__ == '__main__':
     main()
