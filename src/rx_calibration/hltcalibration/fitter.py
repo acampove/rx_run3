3f1f255e17b09d69d2dd643c06dda2b736d1a58b
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-11 16:28:52 +0800

    Adding skeleton of fitting code

--- src/rx_calibration/hltcalibration/fitter.py
+++ src/rx_calibration/hltcalibration/fitter.py
@@ -29,23 +29,42 @@ class Fitter:
 
         self._pdf_sig = smod
         self._pdf_bkg = bmod
+        self._pdf_ful : BasePDF
 
-        self._obs      : zfit.Space
+        self._par_nsg = zfit.Parameter('nsig', 10, 0, 100_000)
+        self._par_nbk = zfit.Parameter('nbkg', 10, 0, 100_000)
+
+        self._obs      : ZfitSpace
         self._obs_name : str
     # -------------------------------
     def _initialize(self):
-        self._obs      = self._pdf_sig.obs
-        self._obs_name = self._obs.name
+        self._obs      = self._pdf_sig.space
+        self._obs_name,= self._pdf_sig.obs
+
+        esig           = self._pdf_sig.create_extended(self._par_nsg)
+        ebkg           = self._pdf_bkg.create_extended(self._par_nbk)
+        self._pdf_ful  = zfit.pdf.SumPDF([esig, ebkg])
 
         log.info(f'Using observable: {self._obs_name}')
+    # -------------------------------
+    def _fit_signal(self) -> Parameter:
+        return Parameter()
+    # -------------------------------
+    def _fit_data(self) -> Parameter:
+        return Parameter()
+    # -------------------------------
+    def _fix_tails(self, sig_par : Parameter) -> None:
+        pass
     # -------------------------------
     def fit(self) -> Parameter:
         '''
         Function returning Parameter object holding fitting parameters
         '''
         self._initialize()
+        sig_par = self._fit_signal()
+        self._fix_tails(sig_par)
 
-        par = Parameter()
+        dat_par = self._fit_data()
 
-        return par
+        return dat_par
 # --------------------------------------------------
