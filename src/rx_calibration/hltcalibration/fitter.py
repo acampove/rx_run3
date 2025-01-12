0a324e9f2e8da13452ce223a754dd31a81767c8a
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-12 17:24:00 +0800

    Removing lines that should be done in FitComponent

--- src/rx_calibration/hltcalibration/fitter.py
+++ src/rx_calibration/hltcalibration/fitter.py
@@ -27,102 +27,65 @@ class Fitter:
     Class meant to produce a Parameter object
     from:
 
-    - Simulated and real data stored in ROOT dataframe
-    - Signal and background zfit PDFs
-
-    If any of the dataframes does not contain a weights column, one will be defined with ones.
-    Otherwise, the weights will be used in the fit.
-
-    The fits to the data will be made by "fixing the tails" to the values in simulation, where
-    the tail parameters are the ones whose names do not end with _flt. Usually the mean and
-    widths are not considered tails and thus they would be named like `mu_flt` and `sig_flt`.
+    - data stored in ROOT dataframe
+    - List of FitComponent objects
     '''
     # -------------------------------
     def __init__(self,
-                 data : RDataFrame,
-                 sim  : RDataFrame,
-                 smod : BasePDF,
-                 bmod : BasePDF,
-                 conf : dict):
+                 data       : RDataFrame,
+                 components : list[FitComponent],
+                 conf       : dict):
         '''
         Parameters
         ------------------
-        data : ROOT dataframe with real data
-        sim  : ROOT dataframe with simulation
-        smod : zfit PDF with signal model
-        bmod : zfit PDF with background model
-        conf : Dictionary with configuration for fitting, plotting, etc
+        data       : ROOT dataframe with real data
+        components : List of FitComponent instances
+        conf       : Dictionary with configuration for fitting, plotting, etc
         '''
         self._rdf_dat = data
-        self._rdf_sim = sim
+        self._l_fcomp = components
         self._conf    = conf
 
-        self._pdf_sig = smod
-        self._pdf_bkg = bmod
+        self._l_pdf   : list[BasePDF] = []
         self._pdf_ful : BasePDF
-        self._zdt_sig : zdata
         self._zdt_dat : zdata
-
-        self._par_nsg = zfit.Parameter('nsig', 10, 0, 1000_000)
-        self._par_nbk = zfit.Parameter('nbkg', 10, 0, 1000_000)
 
         self._minimizer= zfit.minimize.Minuit()
         self._obs      : ZfitSpace
         self._obs_name : str
     # -------------------------------
     def _initialize(self) -> None:
-        log.info('Initializing')
-        self._check_extended()
+        log.debug('Setting PDFs from fit components')
+        self._set_pdfs()
 
-        log.debug('Checking ROOT dataframes')
-        self._rdf_sim  = self._check_weights(self._rdf_sim)
-        self._rdf_dat  = self._check_weights(self._rdf_dat)
-
-        self._obs      = self._pdf_sig.space
-        self._obs_name,= self._pdf_sig.obs
-
-        log.debug('Creating full PDF')
-        ebkg           = self._pdf_bkg.create_extended(self._par_nbk)
-        esig           = self._pdf_sig.create_extended(self._par_nsg)
-        self._pdf_ful  = zfit.pdf.SumPDF([ebkg, esig])
+        self._obs      = self._l_pdf[0].space
+        self._obs_name,= self._l_pdf[0].obs
+        self._pdf_ful  = zfit.pdf.SumPDF(self._l_pdf)
 
         log.debug('Creating zfit data')
-        self._zdt_sig  = self._data_from_rdf(self._rdf_sim)
         self._zdt_dat  = self._data_from_rdf(self._rdf_dat)
 
         log.info(f'Using observable: {self._obs_name}')
     # -------------------------------
+    def _set_pdfs(self) -> None:
+        for fcomp in self._l_fcomp:
+            pdf = fcomp.get_pdf()
+            name= fcomp.name
+            if pdf.is_extended:
+                raise ValueError(f'PDF for component {name} is extended')
+
+            nevt = zfit.Parameter(f'n{name}', 10, 0, 1000_000)
+            epdf = pdf.create_extended(nevt)
+
+            log.debug(f'Extracting PDF for component: {name}')
+
+            self._l_pdf.append(epdf)
+    # -------------------------------
     def _data_from_rdf(self, rdf : RDataFrame) -> zdata:
-        weights= self._conf['weights_column']
-        d_data = rdf.AsNumpy([self._obs_name, weights])
-
-        arr_obs = d_data[self._obs_name]
-        arr_wgt = d_data[weights       ]
-
-        data    = zfit.Data.from_numpy(self._obs, array=arr_obs, weights=arr_wgt)
+        arr_obs = rdf.AsNumpy([self._obs_name])[self._obs_name]
+        data    = zfit.Data.from_numpy(self._obs, array=arr_obs)
 
         return data
-    # -------------------------------
-    def _check_weights(self, rdf) -> RDataFrame:
-        v_col  = rdf.GetColumnNames()
-        l_col  = [col.c_str() for col in v_col]
-
-        weights= self._conf['weights_column']
-        if weights in l_col:
-            log.debug('Weights column {weights} found, not defining ones')
-            return rdf
-
-        log.debug('Weights column {weights} not found, defining \"weights\" as ones')
-        rdf = rdf.Define('weights', '1')
-
-        return rdf
-    # -------------------------------
-    def _check_extended(self) -> None:
-        if self._pdf_sig.is_extended:
-            raise ValueError('Signal PDF should not be extended')
-
-        if self._pdf_bkg.is_extended:
-            raise ValueError('Background PDF should not be extended')
     # -------------------------------
     def _res_to_par(self, res : zfit.result.FitResult) -> Parameter:
         error_method = self._conf['error_method']
@@ -139,62 +102,8 @@ class Fitter:
 
         return obj
     # -------------------------------
-    def _fit_signal(self) -> Parameter:
-        log.info('Fitting signal:')
-
-        print_pdf(self._pdf_sig)
-
-        nll = zfit.loss.UnbinnedNLL(model=self._pdf_sig, data=self._zdt_sig)
-        res = self._minimizer.minimize(nll)
-        res.hesse(method=self._conf['error_method'])
-        par = self._res_to_par(res)
-
-        print(res)
-        self._plot_fit(data=self._zdt_sig, model=self._pdf_sig, name = 'fit_sim.png')
-
-        return par
-    # -------------------------------
-    def _fit_data(self) -> Parameter:
-        log.info('Fitting data:')
-
-        print_pdf(self._pdf_ful)
-
-        nll = zfit.loss.ExtendedUnbinnedNLL(model=self._pdf_ful, data=self._zdt_dat)
-        res = self._minimizer.minimize(nll)
-        error_method = self._conf['error_method']
-        res.hesse(method=error_method)
-        par = self._res_to_par(res)
-
-        print(res)
-        self._plot_fit(data=self._zdt_dat, model=self._pdf_ful, name = 'fit_dat.png')
-
-        return par
-    # -------------------------------
-    def _fix_tails(self, sig_par : Parameter) -> None:
-        s_par = self._pdf_ful.get_params()
-
-        log.info(60 * '-')
-        log.info('Fixing tails')
-        log.info(60 * '-')
-        for par in s_par:
-            name = par.name
-            if name not in sig_par:
-                log.debug(f'Skipping non signal parameter: {name}')
-                continue
-
-            if name.endswith('_flt'):
-                log.debug(f'Not fixing {name}')
-                continue
-
-            val, _ = sig_par[name]
-
-            par.set_value(val)
-            par.floating = True
-
-            log.info(f'{name:<20}{"-->":<20}{val:<20.3f}')
-    # -------------------------------
     def _plot_fit(self, data : zdata, model : BasePDF, name : str) -> None:
-        plot_dir = self._conf['plot_dir']
+        plot_dir = self._conf['out_dir' ]
         plot_cfg = self._conf['plotting']
 
         os.makedirs(plot_dir, exist_ok=True)
@@ -211,10 +120,20 @@ class Fitter:
         Function returning Parameter object holding fitting parameters
         '''
         self._initialize()
-        sig_par = self._fit_signal()
-        self._fix_tails(sig_par)
 
-        dat_par = self._fit_data()
+        log.info('Fitting data:')
 
-        return dat_par
+        print_pdf(self._pdf_ful)
+
+        nll = zfit.loss.ExtendedUnbinnedNLL(model=self._pdf_ful, data=self._zdt_dat)
+        res = self._minimizer.minimize(nll)
+
+        error_method = self._conf['error_method']
+        res.hesse(method=error_method)
+        par = self._res_to_par(res)
+
+        print(res)
+        self._plot_fit(data=self._zdt_dat, model=self._pdf_ful, name = 'fit_dat.png')
+
+        return par
 # --------------------------------------------------
