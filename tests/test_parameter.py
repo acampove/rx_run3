a8efd18a65b7d187aee8a67875f4155c0f041a3f
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-12 14:17:33 +0800

    Adding test_from_json

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -20,13 +20,32 @@ def test_simple():
     with pytest.raises(ValueError):
         _ = obj['c']
 
-def test_save():
+def test_to_json():
     '''
-    Simplest test
+    Tests saving to JSON
     '''
 
     obj = Parameter()
-    obj['a'] = 3,3
-    obj['b'] = 2,2
+    obj['a'] = [3,3]
+    obj['b'] = [2,2]
 
-    obj.save('/tmp/rx_calibration/tests/parameter/save/parameter.json')
+    obj.to_json('/tmp/rx_calibration/tests/parameter/save/parameter.json')
+
+def test_from_json():
+    '''
+    Tests making a parameter object from JSON
+    '''
+    path = '/tmp/rx_calibration/tests/parameter/save/parameter.json'
+
+    obj_1      = Parameter()
+    obj_1['b'] = [2,2]
+    obj_1['a'] = [3,3]
+
+    obj_1.to_json(path)
+
+    obj_2=Parameter.from_json(path)
+
+    assert obj_1 == obj_2
+
+
+
