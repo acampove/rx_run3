b14f7261aed0b85dd0f966d59cd6750b75bc7f55
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-13 14:38:21 +0800

    Simple test working

--- tests/stats/test_minimizer.py
+++ tests/stats/test_minimizer.py
@@ -5,9 +5,12 @@ from dataclasses import dataclass
 
 import zfit
 import numpy
+import pytest
 
-from dmu.stats.minimizers import AnealingMinimizer
+from dmu.stats.minimizers  import AnealingMinimizer
+from dmu.logging.log_store import LogStore
 
+log = LogStore.add_logger('dmu:ml:test_minimizer')
 #---------------------------------------------
 @dataclass
 class Data:
@@ -15,6 +18,10 @@ class Data:
     Class used to share attributes
     '''
     obs = zfit.Space('x', limits=(-10, 10))
+#---------------------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    LogStore.set_level('dmu:ml:minimizers', 10)
 #---------------------------------------------
 def _get_model():
     mu  = zfit.Parameter('mu', 2.4, -1, 5)
@@ -42,7 +49,6 @@ def test_simple():
     '''
     nll       = _get_nll()
     minimizer = AnealingMinimizer(ntries=10, pvalue=0.05)
-    #minimizer = zfit.minimize.Minuit()
     res       = minimizer.minimize(nll)
 
     print(res)
