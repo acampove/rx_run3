7e8fe974317a1b382b610079fe6490a3449e14f8
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-17 12:53:22 +0800

    Improvements
    - Adding get_dt_samples
    - Can override nparts before returning config

--- src/rx_selection/tests.py
+++ src/rx_selection/tests.py
@@ -3,6 +3,7 @@ Module containing functions needed by tests
 '''
 import os
 import glob
+from typing        import Union
 from dataclasses   import dataclass
 
 from dmu.logging.log_store   import LogStore
@@ -30,6 +31,44 @@ class Data:
 
     l_rkst_trigger = ['']
 # ---------------------------------------------
+def _override_parts(cfg : dict, sample : str) -> Union[None,dict]:
+    if sample in [
+            'Bs_phieta_eplemng_eq_Dalitz_DPC',
+            'Bs_phipi0_eplemng_eq_Dalitz_DPC',
+            'Bu_KplKplKmn_eq_sqDalitz_DPC',
+            'Bu_KplpiplKmn_eq_sqDalitz_DPC',
+            'Bu_Lambdacbarppi_Lambdabarmunu_eq_HELAMP_TC',
+            'Bu_piplpimnKpl_eq_sqDalitz_DPC',
+            'Bu_Kstgamma_Kst_eq_KSpi_DPC_SS',
+            'Bd_K1gamma_Kpipi0_eq_mK1270_HighPtGamma_DPC',
+            'Bd_Kstpi0_eq_TC_Kst982width100_HighPtPi0',
+            'Bd_Dmnpipl_eq_DPC',
+            'Bs_Phipi0_gg_eq_DPC_SS',
+            'Bs_PhiEta_gg_eq_DPC_SS',
+            ]:
+        log.warning(f'Skipping sample {sample}')
+        return None
+
+    if sample in [
+            'Bd_JpsiKS_ee_eq_CPV_DPC',
+            'Bd_Ksteta_eplemng_eq_Dalitz_DPC',
+            'Bu_D0pi_Kmunu_eq_DPC',
+            'Bu_D0enu_Kpi_eq_DPC_TC',
+            'Bd_Dstplenu_eq_PHSP_TC',
+            'Bd_D0Xenu_D0_eq_cocktail',
+            'Bs_Dsenu_phienu_eq_DPC_HVM_EGDWC',
+            'Bu_phiKee_KK_eq_DPC']:
+        cfg['npart'] = 10
+
+    if sample in [
+            'Bd_Ksteta_gg_eq_DPC_SS',
+            'Bd_Kstgamma_eq_HighPtGamma_DPC',
+            'Bu_D0munu_Kpi_eq_cocktail_D0muInAcc_BRcorr1',
+            ]:
+        cfg['npart'] = 1
+
+    return cfg
+# ---------------------------------------------
 def _has_files(sample_path : str, trigger : str) -> bool:
     file_wc = f'{sample_path}/{trigger}/*.root'
     l_path  = glob.glob(file_wc)
@@ -44,6 +83,37 @@ def _triggers_from_mc_sample(sample_path : str, is_rk : bool) -> list[str]:
     l_trig    = [ trig for trig in l_trigger if os.path.isdir(f'{sample_path}/{trig}') ]
 
     return l_trig
+# ---------------------------------------------
+def get_dt_samples(is_rk : bool) -> list[tuple[str,str]]:
+    '''
+    Will return list of data samples
+    Where a sample is a pair of sample name and trigger name
+    '''
+    if hasattr(Data, 'l_sam_trg_dt'):
+        return Data.l_sam_trg_dt
+
+    if 'DATADIR' not in os.environ:
+        raise ValueError('DATADIR not found in environment')
+
+    l_trigger  = Data.l_rk_trigger if is_rk else Data.l_rkst_trigger
+    data_dir   = os.environ['DATADIR']
+    sample_dir = f'{data_dir}/RX_run3/{Data.data_version}/post_ap'
+    l_sam_trg  = []
+    for sample_path in glob.glob(f'{sample_dir}/DATA_*'):
+        for trigger in l_trigger:
+            sample_name = os.path.basename(sample_path)
+            if not _has_files(sample_path, trigger):
+                log.warning(f'Cannot find any files for: {sample_name}/{trigger}')
+                continue
+
+            l_sam_trg.append((sample_name, trigger))
+
+    nsample = len(l_sam_trg)
+    log.info(f'Found {nsample} samples')
+
+    Data.l_sam_trg_dt = l_sam_trg
+
+    return Data.l_sam_trg_dt
 # ---------------------------------------------
 def get_mc_samples(is_rk : bool, included : str = '') -> list[tuple[str,str]]:
     '''
@@ -82,7 +152,7 @@ def get_mc_samples(is_rk : bool, included : str = '') -> list[tuple[str,str]]:
 
     return l_sam_trg
 # ---------------------------------------------
-def get_config(sample : str, trigger : str, is_rk : bool) -> dict:
+def get_config(sample : str, trigger : str, is_rk : bool) -> Union[dict, None]:
     '''
     Takes name to config file
     Return settings from YAML as dictionary
@@ -100,13 +170,18 @@ def get_config(sample : str, trigger : str, is_rk : bool) -> dict:
     d_conf['hlt2'   ] = trigger
     d_conf['remove' ] = ['q2', 'bdt']
 
+    d_conf = _override_parts(d_conf, sample)
+
     return d_conf
 # ---------------------------------------------
-def get_dsg_config(sample : str, trigger : str, is_rk : bool) -> dict:
+def get_dsg_config(sample : str, trigger : str, is_rk : bool) -> Union[None,dict]:
     '''
     Function will return config file for DsGetter tests
     '''
-    cfg        = get_config(sample, trigger, is_rk)
+    cfg = get_config(sample, trigger, is_rk)
+    if cfg is None:
+        return None
+
     l_remove   = cfg['remove']
     d_redefine = { rem : '(1)' for rem in l_remove}
 
