1520fbe03e8319a6195190b84d1ffc1b69e6ff41
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-17 12:55:49 +0800

    Test runs over samples with nparts overriden

--- tests/test_dsgetter.py
+++ tests/test_dsgetter.py
@@ -11,7 +11,7 @@ from rx_selection.ds_getter import DsGetter
 
 log = LogStore.add_logger('rx_selection:test_dsgetter')
 # -------------------------------------------
-@pytest.mark.parametrize('sample, trigger', tst.get_mc_samples(is_rk=True, included='btosll'))
+@pytest.mark.parametrize('sample, trigger', tst.get_mc_samples(is_rk=True, included=''))
 def test_simple(sample : str, trigger : str) -> None:
     '''
     Simplest test of DsGetter class
@@ -20,6 +20,8 @@ def test_simple(sample : str, trigger : str) -> None:
     log.info(f'Running over: {sample}/{trigger}')
 
     cfg = tst.get_dsg_config(sample, trigger, is_rk=True)
+    if cfg is None:
+        return
 
     obj = DsGetter(cfg=cfg)
     _   = obj.get_rdf()
