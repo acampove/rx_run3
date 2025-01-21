0a6b27044bb5c84d428d5d7a74f956772360141d
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-21 18:55:02 +0800

    Adding tests

--- tests/pdataframe/test_pdataframe_utilities.py
+++ tests/pdataframe/test_pdataframe_utilities.py
@@ -1,5 +1,5 @@
 '''
-Module will hold test for pandas dataframes utilities 
+Module will hold test for pandas dataframes utilities
 '''
 import pandas as pnd
 
@@ -16,9 +16,32 @@ def _get_df() -> pnd.DataFrame:
 
     return pnd.DataFrame(d_data)
 # --------------------------------------
-def test_df_to_tex():
+def test_df_to_tex_simple():
     '''
     Saving dataframe to latex table
     '''
     df = _get_df()
     put.df_to_tex(df, '/tmp/dmu/tests/pdataframe/utilities/df_to_tex/simple.tex')
+# --------------------------------------
+def test_df_to_tex_format():
+    '''
+    Saving dataframe to latex table with formatting for columns
+    '''
+    d_format = {
+            'a' : '{:.0f}',
+            'b' : '{:.3f}'}
+
+    df = _get_df()
+    put.df_to_tex(
+            df,
+            '/tmp/dmu/tests/pdataframe/utilities/df_to_tex/format.tex',
+            d_format = d_format)
+# --------------------------------------
+def test_df_to_tex_caption():
+    '''
+    Saving dataframe to latex table with caption
+    '''
+    df = _get_df()
+    put.df_to_tex(df,
+                  '/tmp/dmu/tests/pdataframe/utilities/df_to_tex/caption.tex',
+                  caption = 'Some caption')
