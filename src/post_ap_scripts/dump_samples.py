d0bf14f38e1e0e90078f784beff044d29bd05f79
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-23 15:13:57 +0800

    Adding samples dumper

---
+++ src/post_ap_scripts/dump_samples.py
@@ -0,0 +1,73 @@
+'''
+Script used to provide blocks of MC samples in a YAML file
+Needed to write YAML config for post_ap
+'''
+
+import re
+import argparse
+
+import yaml
+from apd import AnalysisData
+
+# ----------------------------------------------
+class IndentedDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(IndentedDumper, self).increase_indent(flow, False)
+# ----------------------------------------------
+class Data:
+    '''
+    Data class used to store shared data
+    '''
+    regex = r'mc_\d{2}_(w\d{2}_\d{2})_mag.*'
+    group : str
+    prod  : str
+# ----------------------------------------------
+def _version_from_name(name : str) -> str:
+    mtch = re.match(Data.regex, name)
+    if not mtch:
+        raise ValueError(f'Cannot find version in: {name}')
+
+    return mtch.group(1)
+# ----------------------------------------------
+def _get_samples(samples) -> dict[str,list[str]]:
+    d_data   = {}
+    for sample in samples:
+        if sample['version'] != 'v1r2266':
+            continue
+
+        name = sample['name']
+        if not name.startswith('mc_'):
+            continue
+
+        vers = _version_from_name(name)
+        if vers not in d_data:
+            d_data[vers] = []
+
+        d_data[vers].append(name)
+
+    return d_data
+# ----------------------------------------------
+def _parse_args() -> None:
+    parser = argparse.ArgumentParser(description='Script used to create a list of MC samples in YAML, split by sim production')
+    parser.add_argument('-p', '--prod' , type=str, help='Production, e.g. rd_ap_2024', required=True)
+    parser.add_argument('-g', '--group', type=str, help='Group, e.g. rd'             , required=True)
+    args = parser.parse_args()
+
+    Data.group = args.group
+    Data.prod  = args.prod
+# ----------------------------------------------
+def main():
+    '''
+    Script starts here
+    '''
+    _parse_args()
+
+    datasets = AnalysisData(Data.group, Data.prod)
+    samples  = datasets.all_samples()
+
+    d_data = _get_samples(samples)
+    with open('data.yaml', 'w', encoding='utf-8') as ofile:
+        yaml.dump(d_data, ofile, Dumper=IndentedDumper)
+# ----------------------------------------------
+if __name__ == '__main__':
+    main()
