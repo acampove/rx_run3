3ecfdd7998349416aa1b2287f017be79a3921b97
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-24 15:27:22 +0800

    Annotation plot has to be the last one

--- src/dmu/ml/train_mva.py
+++ src/dmu/ml/train_mva.py
@@ -334,10 +334,10 @@ class TrainMva:
         if 'max' in self._cfg['plotting']['roc']:
             [max_x, max_y] = self._cfg['plotting']['roc']['max']
 
-        self._plot_probabilities(xval_ts, yval_ts, l_prb_ts)
-
         plt.plot(xval_ts, yval_ts, color='b', label=f'Test: {area_ts:.3f}')
         plt.plot(xval_tr, yval_tr, color='r', label=f'Train: {area_tr:.3f}')
+        self._plot_probabilities(xval_ts, yval_ts, l_prb_ts, l_lab_ts)
+
         plt.xlabel('Signal efficiency')
         plt.ylabel('Background rejection')
         plt.title(f'Fold: {ifold}')
