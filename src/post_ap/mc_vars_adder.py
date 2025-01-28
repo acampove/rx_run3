fb0f2fc2a0c2ecf16dc49070d9d9bb9c316a7761
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-28 14:49:20 +0800

    Adding skeleton functions

--- src/post_ap/mc_vars_adder.py
+++ src/post_ap/mc_vars_adder.py
@@ -12,12 +12,33 @@ class MCVarsAdder:
     '''
     # ---------------------------
     def __init__(self, rdf_rec : RDataFrame, rdf_gen : Union[RDataFrame,None] = None):
+        '''
+        rdf_gen: ROOT dataframe with generator level candidates (MCDecayTree), by default None
+        rdf_rec: ROOT dataframe with reconstructed candidates (DecayTree)
+
+        Two modes are implemented:
+
+        - Only `rdf_rec` is passed: Then the class only assigns columns to this dataframe.
+        - Both dataframes are passed: Then the reco tree is used to add columns to the `rdf_gen` dataframe.
+        '''
         self._rdf_rec = rdf_rec
         self._rdf_gen = rdf_gen
+    # ---------------------------
+    def _add_to_rec(self):
+        return self._rdf_rec
+    # ---------------------------
+    def _add_to_gen(self):
+        return self._rdf_gen
     # ---------------------------
     def get_rdf(self) -> RDataFrame:
         '''
         Returns dataframe after adding column
         '''
-        return self._rdf_rec
+
+        if self._rdf_gen is None:
+            rdf = self._add_to_gen()
+        else:
+            rdf = self._add_to_rec()
+
+        return rdf
 # -----------------------------
