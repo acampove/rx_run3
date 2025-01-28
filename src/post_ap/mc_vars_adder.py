9c00ed191c8f2d7c9862e4d649ed65e43e6b9c63
Angel Campoverde <angel.campoverde@cern.ch>
2025-01-28 15:28:56 +0800

    Tool needs sample name and add _get_blocks

--- src/post_ap/mc_vars_adder.py
+++ src/post_ap/mc_vars_adder.py
@@ -11,8 +11,12 @@ class MCVarsAdder:
     Class intended to add columns to ROOT dataframe representing MC
     '''
     # ---------------------------
-    def __init__(self, rdf_rec : RDataFrame, rdf_gen : Union[RDataFrame,None] = None):
+    def __init__(self,
+                 sample_name : str,
+                 rdf_rec     : RDataFrame,
+                 rdf_gen     : Union[RDataFrame,None] = None):
         '''
+        sample_name: The name of the MC sample, needed to assign the block number, e.g. `mc_24_w31_34_magup_sim10d-splitsim02_11102202_bd_kstgamma_eq_highptgamma_dpc_ss_tuple`
         rdf_gen: ROOT dataframe with generator level candidates (MCDecayTree), by default None
         rdf_rec: ROOT dataframe with reconstructed candidates (DecayTree)
 
@@ -21,8 +25,14 @@ class MCVarsAdder:
         - Only `rdf_rec` is passed: Then the class only assigns columns to this dataframe.
         - Both dataframes are passed: Then the reco tree is used to add columns to the `rdf_gen` dataframe.
         '''
-        self._rdf_rec = rdf_rec
-        self._rdf_gen = rdf_gen
+        self._sample_name = sample_name
+        self._rdf_rec     = rdf_rec
+        self._rdf_gen     = rdf_gen
+
+        self._l_block = self._get_blocks()
+    # ---------------------------
+    def _get_blocks(self) -> list[int]:
+        return [1, 2]
     # ---------------------------
     def _add_to_rec(self):
         return self._rdf_rec
