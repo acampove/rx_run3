d04307412eb873e9e8ff56beeff35c8388a4b188
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-03 18:50:23 +0800

    Add test for legend

--- tests/plotting/test_plotter_1d.py
+++ tests/plotting/test_plotter_1d.py
@@ -160,3 +160,14 @@ def test_normalized():
 
     ptr=Plotter(d_rdf=d_rdf, cfg=cfg_dat)
     ptr.run()
+#---------------------------------------
+def test_legend():
+    '''
+    Tests legend options
+    '''
+    d_rdf =  { kind : _get_rdf(kind=kind, test='simple') for kind in ['class A', 'class B'] }
+
+    cfg_dat = _load_config(test='legend')
+
+    ptr=Plotter(d_rdf=d_rdf, cfg=cfg_dat)
+    ptr.run()
