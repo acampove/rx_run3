f3d11f906b2da7a3b855f6edf6292a7e6c91d68e
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-10 15:57:05 +0800

    Working test

--- tests/test_hop_calculator.py
+++ tests/test_hop_calculator.py
@@ -1,15 +1,23 @@
 '''
 Module containing tests for HOPVarAdder
 '''
+import os
 
 import yaml
-from ROOT import RDataFrame
-
-from post_ap.hop_vars_adder import HOPVarAdder as HVA
+import pytest
+import matplotlib.pyplot as plt
+from ROOT                   import RDataFrame
+from rx_data.hop_calculator import HOPCalculator
 
 # ----------------------------
+class Data:
+    '''
+    Class used to share attributes
+    '''
+    out_dir = '/tmp/rx_data/tests/hop_calculator'
+# ----------------------------
 def _get_samples() -> dict:
-    samples_list='/publicfs/ucas/user/campoverde/Data/RX_run3/v4/rx_samples.yaml'
+    samples_list='/publicfs/ucas/user/campoverde/Data/RX_run3/v5/rx_samples.yaml'
     with open(samples_list, encoding='utf-8') as ifile:
         cfg = yaml.safe_load(ifile)
 
@@ -22,14 +30,34 @@ def _get_rdf(sample : str, trigger : str) -> RDataFrame:
 
     return rdf
 # ----------------------------
-def test_simple():
+def _plot_variables(rdf : RDataFrame, rdf_hop : RDataFrame, name : str) -> None:
+    out_dir = f'{Data.out_dir}/{name}'
+    os.makedirs(out_dir, exist_ok=True)
+
+    d_data = rdf_hop.AsNumpy(['alpha', 'mass'])
+    arr_ms = rdf.AsNumpy(['B_M'])['B_M']
+
+    plt.hist(d_data['alpha'], bins=40, range=[0,5])
+    plt.savefig(f'{out_dir}/alpha.png')
+    plt.close()
+
+    plt.hist(d_data['mass'], bins=40, label='HOP')
+    plt.hist(        arr_ms, bins=40, label='Original')
+    plt.legend()
+    plt.savefig(f'{out_dir}/mass.png')
+    plt.close()
+# ----------------------------
+@pytest.mark.parametrize('sample', ['Bd_Kstee_eq_btosllball05_DPC', 'Bu_Kee_eq_btosllball05_DPC'])
+def test_simple(sample : str):
     '''
     Simplest test
     '''
-    rdf = _get_rdf(sample = 'Bd_Kstee_eq_btosllball05_DPC', trigger='Hlt2RD_BuToKpEE_MVA')
+    rdf = _get_rdf(sample = sample, trigger='Hlt2RD_BuToKpEE_MVA')
 
-    obj = HVA(rdf=rdf)
-    rdf = obj.get_rdf()
+    obj     = HOPCalculator(rdf=rdf)
+    rdf_hop = obj.get_rdf()
+
+    _plot_variables(rdf=rdf, rdf_hop=rdf_hop, name=f'simple_{sample}')
 # ----------------------------
 
 # Bd_Kstmumu_eq_btosllball05_DPC                                                                                                                                                                                                        
