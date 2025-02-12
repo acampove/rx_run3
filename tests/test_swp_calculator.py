2331b3f08d0ba3f92c95401723a1e702ce0fdb4e
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-12 18:38:59 +0800

    Updated for no multithreading
    Multithreading crashes due to ROOT issue

--- tests/test_swp_calculator.py
+++ tests/test_swp_calculator.py
@@ -4,10 +4,11 @@ Module with tests for swap calculator class
 import os
 from importlib.resources     import files
 
+import numpy
 import pytest
 import pandas            as pnd
 import matplotlib.pyplot as plt
-from ROOT                   import RDataFrame, EnableImplicitMT
+from ROOT                   import RDataFrame, RDF
 from rx_data.swp_calculator import SWPCalculator
 from rx_data.mis_calculator import MisCalculator
 
@@ -16,27 +17,31 @@ class Data:
     '''
     Class used to share attributes
     '''
-    nthread = 10
-    EnableImplicitMT(nthread)
-
     inp_dir : str = '/home/acampove/external_ssd/Data/main/v5'
     out_dir : str = '/tmp/rx_data/tests/swap_calculator'
 
     l_file_wc = [
-            'data_24_mag*_24c1_Hlt2RD_BuToKpMuMu_MVA_0000000000.root',
+            #'data_24_mag*_24c1_Hlt2RD_BuToKpMuMu_MVA_0000000000.root',
             'data_24_mag*_24c2_Hlt2RD_BuToKpMuMu_MVA_0000000000.root',
-            'data_24_mag*_24c3_Hlt2RD_BuToKpMuMu_MVA_0000000000.root',
-            'data_24_mag*_24c4_Hlt2RD_BuToKpMuMu_MVA_0000000000.root']
+            #'data_24_mag*_24c3_Hlt2RD_BuToKpMuMu_MVA_0000000000.root',
+            #'data_24_mag*_24c4_Hlt2RD_BuToKpMuMu_MVA_0000000000.root',
+            ]
 # ----------------------------------
 @pytest.fixture(scope='session', autouse=True)
 def _initialize():
     os.makedirs(Data.out_dir, exist_ok=True)
 # ----------------------------------
-def _get_df(test : str, file_wc : str) -> pnd.DataFrame:
+def _get_rdf(test : str, file_wc : str) -> RDataFrame:
     if test == 'cascade':
         json_path = files('rx_data_data').joinpath('tests/swap_adder/bpd0kpienu.json')
-        df = pnd.read_json(json_path)
-        return df
+        df        = pnd.read_json(json_path)
+        d_data    = df.to_dict(orient='list')
+        d_numpy   = { name : numpy.array(l_val) for name, l_val in d_data.items() }
+        rdf       = RDF.FromNumpy(d_numpy)
+        rdf       = rdf.Define('EVENTNUMBER', '1')
+        rdf       = rdf.Define('RUNNUMBER'  , '2')
+
+        return rdf
 
     if test == 'jpsi_misid':
         rdf   = RDataFrame('DecayTree', f'{Data.inp_dir}/{file_wc}')
@@ -44,9 +49,7 @@ def _get_df(test : str, file_wc : str) -> pnd.DataFrame:
         msc   = MisCalculator(rdf=rdf, trigger='Hlt2RD_BuToKpMuMu_MVA')
         rdf   = msc.get_rdf()
 
-        d_data= rdf.AsNumpy()
-        df    = pnd.DataFrame(d_data)
-        return df
+        return rdf
 
     raise ValueError(f'Invalid test: {test}')
 # ----------------------------------
@@ -54,37 +57,41 @@ def test_cascade():
     '''
     Tests cascade decay contamination
     '''
-    df  = _get_df(test='cascade')
+    rdf = _get_rdf(test='cascade', file_wc='NA')
+    obj = SWPCalculator(rdf, d_lep={'L1' : 211, 'L2' : 211}, d_had={'H' : 321})
+    rdf = obj.get_rdf(preffix='cascade')
 
-    obj = SWPCalculator(df, d_lep={'L1' : 211, 'L2' : 211}, d_had={'H' : 321})
-    df  = obj.get_df()
-    df.H_swp.hist(bins=40, range=(1800, 1950) , histtype='step', label='Swapped')
-    df.H_org.hist(bins=40, range=(1800, 1950) , histtype='step', label='Original')
-    plt.axvline(x=1864, color='r', label='$D_0$')
-    plt.grid(False)
-    plt.legend()
-
-    plt.savefig(f'{Data.out_dir}/cascade.png')
-    plt.close('all')
+    _plot(rdf, 'cascade', preffix='cascade')
 # ----------------------------------
 @pytest.mark.parametrize('file_wc', Data.l_file_wc)
 def test_jpsi_misid(file_wc : str):
     '''
     Tests jpsi misid contamination
     '''
-    df  = _get_df(test='jpsi_misid', file_wc = file_wc)
+    rdf = _get_rdf(test='jpsi_misid', file_wc = file_wc)
+    obj = SWPCalculator(rdf, d_lep={'L1' : 13, 'L2' : 13}, d_had={'H' : 13})
+    rdf = obj.get_rdf(preffix='jpsi_misid')
 
-    if len(df) == 0:
-        raise ValueError('No entries found in input dataframe')
+    _plot(rdf, file_wc, preffix='jpsi_misid')
+# ----------------------------------
+def _plot(rdf : RDataFrame, file_wc : str, preffix : str):
+    d_data = rdf.AsNumpy([f'{preffix}_mass_swp', f'{preffix}_mass_org'])
+    arr_swp= d_data[f'{preffix}_mass_swp']
+    arr_org= d_data[f'{preffix}_mass_org']
 
-    obj = SWPCalculator(df, d_lep={'L1' : 13, 'L2' : 13}, d_had={'H' : 13})
-    df  = obj.get_df(nthread=Data.nthread)
-    df.H_swp.hist(bins=40, range=(2700, 3300) , histtype='step', label='Swapped')
-    df.H_org.hist(bins=40, range=(2700, 3300) , histtype='step', label='Original')
-    plt.axvline(x=3100, color='r', label=r'$J/\psi$')
+    mass_rng = {'jpsi_misid' : [2700, 3300], 'cascade' : [1800, 1950]}[preffix]
+
+    plt.hist(arr_swp, bins=40, range=mass_rng, histtype='step', label='Swapped')
+    plt.hist(arr_org, bins=40, range=mass_rng, histtype='step', label='Original')
     plt.grid(False)
     plt.legend()
 
+    if preffix == 'jpsi_misid':
+        plt.axvline(x=3100, color='r', label=r'$J/\psi$')
+    else:
+        plt.axvline(x=1864, color='r', label='$D_0$')
+
     suffix = file_wc.replace('*', 'p')
-    plt.savefig(f'{Data.out_dir}/jpsi_misid_{suffix}.png')
+    plt.savefig(f'{Data.out_dir}/{preffix}_{suffix}.png')
     plt.close('all')
+# ----------------------------------
