33db04f3503b810a48f9b252b8c6aaf7c4b1988f
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-12 12:46:37 +0800

    Adding files

---
+++ tests/test_swap_calculator.py
@@ -0,0 +1,45 @@
+'''
+Module with tests for swap calculator class
+'''
+import os
+from importlib.resources     import files
+
+import pytest
+import pandas            as pnd
+import matplotlib.pyplot as plt
+from rx_data.swap_calculator import Calculator
+
+# ----------------------------------
+class Data:
+    '''
+    Class used to share attributes
+    '''
+
+    out_dir : str = '/tmp/rx_data/tests/swap_calculator'
+# ----------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    os.makedirs(Data.out_dir, exist_ok=True)
+# ----------------------------------
+def _get_df() -> pnd.DataFrame:
+    json_path = files('rx_data_data').joinpath('tests/swap_adder/bpd0kpienu.json')
+    df = pnd.read_json(json_path)
+
+    return df
+# ----------------------------------
+def test_simple():
+    '''
+    Simples test
+    '''
+    df  = _get_df()
+
+    obj = Calculator(df, d_lep={'L1' : 211, 'L2' : 211}, d_had={'H' : 321})
+    df  = obj.get_df()
+    df.H_swp.hist(bins=40, range=(1800, 1950) , histtype='step', label='Swapped')
+    df.H_org.hist(bins=40, range=(1800, 1950) , histtype='step', label='Original')
+    plt.axvline(x=1864, color='r', label='$D_0$')
+    plt.grid(False)
+    plt.legend()
+
+    plt.savefig(f'{Data.out_dir}/simple.png')
+    plt.close('all')
