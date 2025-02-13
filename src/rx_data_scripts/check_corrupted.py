24f3ff4c8f22f2fdbc5a106762c04299c3630342
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-13 16:44:01 +0800

    Adding file checker

---
+++ src/rx_data_scripts/check_corrupted.py
@@ -0,0 +1,81 @@
+'''
+Script meant to check if ROOT files are corrupted
+'''
+import glob
+import argparse
+
+import tqdm
+from ROOT                  import TFile
+from dmu.logging.log_store import LogStore
+
+log=LogStore.add_logger('rx_data:check_file')
+# -----------------------------------
+class Data:
+    '''
+    Data class
+    '''
+    inp_dir : str
+# -----------------------------------
+def _get_paths() -> list[str]:
+    files_wc = f'{Data.inp_dir}/*.root'
+    l_path   = glob.glob(files_wc)
+    npath    = len(l_path)
+
+    if npath == 0:
+        raise ValueError(f'No files found in {files_wc}')
+
+    log.info(f'Found {npath} files')
+
+    return l_path
+# -----------------------------------
+def _parse_args() -> None:
+    '''
+    Parse arguments
+    '''
+    parser = argparse.ArgumentParser(description='Script used to check if ROOT files in directory are OK')
+    parser.add_argument('-p', '--path', type=str, help='Path to directory with files', required=True)
+    parser.add_argument('-l', '--lvl' , type=int, help='log level', choices=[10, 20, 30], default=20)
+    args = parser.parse_args()
+
+    Data.inp_dir = args.path
+    Data.lvl     = args.lvl
+
+    LogStore.set_level('rx_data:check_file', Data.lvl)
+# -----------------------------------
+def _is_good_file(path : str) -> bool:
+    ifile = TFile.Open(path)
+    if ifile.IsZombie():
+        ifile.Close()
+        return False
+
+    if ifile.TestBit(TFile.kRecovered) != 0:
+        ifile.Close()
+        return False
+
+    ifile.Close()
+
+    return True
+# -----------------------------------
+def _remove_files(l_path : list[str]) -> None:
+    nfile = len(l_path)
+    log.info(f'Found {nfile} bad files')
+
+# -----------------------------------
+def main():
+    '''
+    Start here
+    '''
+    _parse_args()
+
+    l_all_path = _get_paths()
+    l_bad_path = []
+    for path in tqdm.tqdm(l_all_path, ascii=' -'):
+        if _is_good_file(path):
+            continue
+
+        l_bad_path.append(path)
+
+    _remove_files(l_bad_path)
+# -----------------------------------
+if __name__ == '__main__':
+    main()
