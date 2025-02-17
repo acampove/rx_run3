5c0f8742c5f9e18ce5a17426810fc78390c9eb4b
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-17 20:01:19 +0800

    Test with pytest, add printout of dataframe

--- tests/testing/test_utilities.py
+++ tests/testing/test_utilities.py
@@ -1,7 +1,9 @@
 '''
 Module with unit tests for testing/utilities.py functions
 '''
+# pylint: disable=no-name-in-module
 
+import pytest
 from ROOT import RDataFrame
 
 import dmu.testing.utilities as ut
@@ -10,27 +12,36 @@ from dmu.logging.log_store import LogStore
 
 log = LogStore.add_logger('dmu:tests:test_utilities')
 # ----------------------------------------------
-def _check_rdf(rdf):
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    LogStore.set_level('dmu:testing:utilities', 10)
+# ----------------------------------------------
+def _check_rdf(rdf : RDataFrame) -> None:
     if not isinstance(rdf, RDataFrame):
         kind = str(type(rdf))
         log.error(f'Object is not a dataframe but: {kind}')
         raise ValueError
+
+    log.info('')
+    rdf.Display(nRows=20).Print()
 # ----------------------------------------------
 def test_get_rdf():
     '''
     Test for toy dataframe getter
     '''
-    rdf = ut.get_rdf(kind='sig')
+    rdf = ut.get_rdf(kind='sig', nentries=10)
     _check_rdf(rdf)
 
-    rdf = ut.get_rdf(kind='bkg')
+    rdf = ut.get_rdf(kind='bkg', nentries=10)
     _check_rdf(rdf)
 # ----------------------------------------------
-def main():
+def test_get_rdf_with_nans():
     '''
-    Tests start here
+    Test for toy dataframe getter
     '''
-    test_get_rdf()
+    rdf = ut.get_rdf(kind='sig', nentries=10, add_nans=True)
+    _check_rdf(rdf)
+
+    rdf = ut.get_rdf(kind='bkg', nentries=10, add_nans=True)
+    _check_rdf(rdf)
 # ----------------------------------------------
-if __name__ == '__main__':
-    main()
