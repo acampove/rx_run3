4afb67ded7b8dc308c8b037e845dfbcf50d73d66
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-20 12:08:15 +0800

    Tests will run over all channels and q2bins

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -1,17 +1,21 @@
 '''
 Script with functions needed to test functions in selection.py
 '''
+import pytest
+
 from dmu.logging.log_store  import LogStore
 from rx_selection import selection as sel
 
 log=LogStore.add_logger('rx_selection:test_selection')
 # --------------------------
-def test_simple():
+@pytest.mark.parametrize('analysis', ['EE', 'MM'])
+@pytest.mark.parametrize('q2bin'   , ['low', 'central', 'high'])
+def test_simple(analysis : str, q2bin : str):
     '''
     Simplest test
     '''
     LogStore.set_level('rx_selection:selection', 10)
-    d_sel = sel.selection(project='RK', analysis='EE', q2bin='central', process='DATA')
+    d_sel = sel.selection(project='RK', analysis=analysis, q2bin=q2bin, process='DATA')
     for cut_name, cut_value in d_sel.items():
         log.info(f'{cut_name:<20}{cut_value}')
 # --------------------------
