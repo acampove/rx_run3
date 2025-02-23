6c0852efcbd02e8a4e3cfb3ec910f791466e7521
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-23 15:10:44 +0800

    Updating

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -2,13 +2,25 @@
 Module with testing functions for the Stats class
 '''
 
-from rx_data.stats import Stats
+import pytest
+from dmu.logging.log_store import LogStore
+from rx_data.stats         import Stats
 
+log=LogStore.add_logger('rx_data:test_stats')
 # ----------------------------------------
-def test_simple():
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    LogStore.set_level('rx_data:stats', 10)
+    Stats.d_sample = {
+            'main' : '/home/acampove/external_ssd/Data/samples/main.yaml',
+            }
+# ----------------------------------------
+def test_mcdt():
     '''
-    xxx
+    Tests MCDecayTree statistics retrieval
     '''
-    obj = Stats(sample='Bd_JpsiX_ee_eq_JpsiInAcc')
+    obj = Stats(sample='Bu_JpsiK_ee_eq_DPC', trigger='Hlt2RD_BuToKpEE_MVA')
     val = obj.get_entries(tree='MCDecayTree')
+
+    assert val > 0
 # ----------------------------------------
