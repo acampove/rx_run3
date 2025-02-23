720e0040c90e39323c757001ec603faea6ea7dd5
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-23 14:30:18 +0800

    Adding file

---
+++ src/rx_data/stats.py
@@ -0,0 +1,12 @@
+'''
+Module with Stats class
+'''
+
+class Stats:
+    '''
+    Class meant to provide number of candidates 
+    '''
+    def __init__(self, sample : str):
+        '''
+        sample (str): MC sample identifier
+        '''
