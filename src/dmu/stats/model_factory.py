9b80cea775d04cf1f330416cbd38dc69f7d24003
Angel Campoverde <angel.campoverde@cern.ch>
2025-02-26 10:41:35 +0800

    Adding SUJohnson

--- src/dmu/stats/model_factory.py
+++ src/dmu/stats/model_factory.py
@@ -136,9 +136,20 @@ class ModelFactory:
         mu  = self._get_parameter('mu_cbr', suffix, 5300, 5100, 5350)
         sg  = self._get_parameter('sg_cbr', suffix,   10,    2,  300)
         ar  = self._get_parameter('ac_cbr', suffix,   -2, -14., -0.1)
-        nr  = self._get_parameter('nc_cbr', suffix,    1,  0.5, 10.0)
+        nr  = self._get_parameter('nc_cbr', suffix,    1,  0.5, 50.0)
 
         pdf = zfit.pdf.CrystalBall(mu, sg, ar, nr, self._obs, name=suffix)
+
+        return pdf
+    #-----------------------------------------
+    @MethodRegistry.register('suj')
+    def _get_suj(self, suffix : str = '') -> zpdf:
+        mu  = self._get_parameter('mu_suj', suffix, 5300, 5100, 5350)
+        sg  = self._get_parameter('sg_suj', suffix,   10,    2,  300)
+        gm  = self._get_parameter('gm_suj', suffix,  -10,  10., 0.0)
+        dl  = self._get_parameter('dl_suj', suffix,    0,    0,  50)
+
+        pdf = zfit.pdf.JohnsonSU(mu, sg, gm, dl, self._obs, name=suffix)
 
         return pdf
     #-----------------------------------------
@@ -147,7 +158,7 @@ class ModelFactory:
         mu  = self._get_parameter('mu_cbl', suffix, 5300, 5100, 5350)
         sg  = self._get_parameter('sg_cbl', suffix,   10,    2,  300)
         al  = self._get_parameter('ac_cbl', suffix,    2,  0.1,  14.)
-        nl  = self._get_parameter('nc_cbl', suffix,    1,  0.5,  15.)
+        nl  = self._get_parameter('nc_cbl', suffix,    1,  0.5, 50.0)
 
         pdf = zfit.pdf.CrystalBall(mu, sg, al, nl, self._obs, name=suffix)
 
