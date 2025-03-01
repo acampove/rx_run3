7d166880b5af89f1fb57ae5595db66d1bc882027
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-01 14:45:23 +0800

    Updating with correct layout

---
+++ src/rx_efficiencies/efficiency_scanner.py
@@ -0,0 +1,20 @@
+'''
+Module containing EfficiencyScanner class
+'''
+
+# --------------------------------
+class EfficiencyScanner:
+    '''
+    Class meant to scan efficiencies in MC samples
+    '''
+    # --------------------------------
+    def __init__(self, cfg : dict):
+        self._cfg = cfg
+    # --------------------------------
+    def run(self):
+        '''
+        return dataframe with efficiency and values of variables in scan
+        '''
+
+        return
+# --------------------------------
--- tests/test_efficiency_scanner.py
+++ tests/test_efficiency_scanner.py
@@ -1,6 +1,23 @@
 '''
 Module used to test EfficiencyScanner class
 '''
+import numpy
 
 from rx_efficiencies.efficiency_scanner import EfficiencyScanner as EffSc
 
+def test_scan():
+    '''
+    Test efficiency scanning
+    '''
+    cfg = {
+            'input' : {
+                'sample' : 'Bu_JpsiK_ee_eq_DPC',
+                'trigger': 'Hlt2RD_BuToKpEE_MVA'},
+            'variables' : {
+                'mva.mva_cmb' : numpy.arange(0.5, 1.0, 0.1),
+                'mva.mva_prc' : numpy.arange(0.5, 1.0, 0.1),
+                }
+            }
+
+    obj = EffSc(cfg=cfg)
+    df  = obj.run()
