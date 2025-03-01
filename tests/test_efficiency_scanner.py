298e2e84f273b2a0ce8f756b0d9b72140b1b907d
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-01 15:37:48 +0800

    Improving test

--- tests/test_efficiency_scanner.py
+++ tests/test_efficiency_scanner.py
@@ -2,9 +2,17 @@
 Module used to test EfficiencyScanner class
 '''
 import numpy
+import pytest
 
+from dmu.logging.log_store import LogStore
 from rx_efficiencies.efficiency_scanner import EfficiencyScanner as EffSc
 
+log = LogStore.add_logger('rx_efficiencies:test_efficiency_scanner')
+# -----------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    LogStore.set_level('rx_efficiencies:efficiency_scanner', 10)
+# -----------------------------------
 def test_scan():
     '''
     Test efficiency scanning
@@ -25,10 +33,11 @@ def test_scan():
                 },
             'variables' : 
             {
-                'mva.mva_cmb' : numpy.arange(0.5, 1.0, 0.1),
-                'mva.mva_prc' : numpy.arange(0.5, 1.0, 0.1),
+                'mva_cmb' : numpy.arange(0.5, 1.0, 0.1),
+                'mva_prc' : numpy.arange(0.5, 1.0, 0.1),
                 }
             }
 
     obj = EffSc(cfg=cfg)
     df  = obj.run()
+# -----------------------------------
