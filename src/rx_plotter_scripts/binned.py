e3e875781598b93926467861f4365d1d81887fb3
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-04 18:11:29 +0800

    Not using dmu plotter but matplotlib

--- src/rx_plotter_scripts/binned.py
+++ src/rx_plotter_scripts/binned.py
@@ -5,13 +5,13 @@ import os
 import json
 import argparse
 from importlib.resources import files
+from functools           import lru_cache
 
 import yaml
 import numpy
 import matplotlib.pyplot as plt
 from ROOT                    import RDataFrame, EnableImplicitMT
 from dmu.logging.log_store   import LogStore
-from dmu.plotting.plotter_2d import Plotter2D as Plotter
 from rx_data.rdf_getter      import RDFGetter
 from rx_selection            import selection as sel
 
@@ -23,6 +23,7 @@ class Data:
     '''
     conf_name : str
     cfg       : dict
+    d_bound   : dict[str,list[str]] = {}
     cache_dir = '/tmp/rx_plotter/cache'
 
     os.makedirs(cache_dir, exist_ok=True)
@@ -39,6 +40,7 @@ def _load_config() -> None:
     with open(conf_path, encoding='utf-8') as ifile:
         Data.cfg = yaml.safe_load(ifile)
 # -----------------------------
+@lru_cache
 def _get_rdf() -> RDataFrame:
     q2bin   = Data.cfg['input']['q2bin']
     trigger = Data.cfg['input']['trigger']
@@ -60,77 +62,153 @@ def _get_rdf() -> RDataFrame:
 
     return rdf
 # -----------------------------
+def _get_bounds(var_name : str, d_bin : dict) -> list[float]:
+    if var_name in Data.d_bound:
+        return Data.d_bound[var_name]
+
+    nbin    = d_bin['nbin']
+    minx    = d_bin['min' ]
+    maxx    = d_bin['max' ]
+
+    rdf     = _get_rdf()
+    rdf     = rdf.Filter(f'({var_name} > {minx}) && ({var_name} < {maxx})')
+    arr_val = rdf.AsNumpy([var_name])[var_name]
+
+    arr_qnt = numpy.linspace(0, 1, nbin + 1)
+    arr_bnd = numpy.quantile(arr_val, arr_qnt)
+    l_bound = arr_bnd.tolist()
+
+    Data.d_bound[var_name] = l_bound
+
+    return l_bound
+# -----------------------------
 def _cuts_from_binning(l_name : list[str], l_setting : list, index : int) -> list[str]:
-    var_name            = l_name[index]
-    [nbins, minv, maxv] = l_setting[index]['binning']
-    arr_bound           = numpy.linspace(minv, maxv, nbins)
-    l_bound             = arr_bound.tolist()
-    l_min               = l_bound[:-1]
-    l_max               = l_bound[+1:]
+    var_name = l_name[index]
+    d_bin    = l_setting[index]['binning']
+
+    l_bound  = _get_bounds(var_name, d_bin)
+    l_min    = l_bound[:-1]
+    l_max    = l_bound[+1:]
 
     l_cut = [ f'({var_name} > {minv:>3.0f}) && ({var_name} <={maxv:>3.0f})' for minv, maxv in zip(l_min, l_max)]
 
     return l_cut
 # -----------------------------
-def _get_normalized_value(rdf : RDataFrame, l_cut : list[str]) -> dict[str,float]:
-    path = f'{Data.cache_dir}/normalized_rms.json'
+def _get_full_stats(rdf: RDataFrame, var_name : str) -> tuple[float,float]:
+    arr_val = rdf.AsNumpy([var_name])[var_name]
+    nval    = len(arr_val)
+    val     = numpy.std(arr_val)
+    err     = val / numpy.sqrt(2 * nval)
+
+    return val, err
+# -----------------------------
+def _plot_distribution(rdf: RDataFrame, var_name : str, cut : str, val : float, err : float) -> None:
+    return
+    arr_val = rdf.AsNumpy([var_name])[var_name]
+    plt.hist(arr_val, histtype='step', bins=30, range=[4500, 6000])
+
+    out_dir = Data.cfg['saving']['plt_dir']
+    out_dir = f'{out_dir}/distributions'
+    os.makedirs(out_dir, exist_ok=True)
+
+    name = cut
+    name = name.replace('>', 'gt')
+    name = name.replace('<', 'lt')
+    name = name.replace(' ',  '_')
+    name = name.replace('=', 'eq')
+    name = name.replace('&',  'a')
+    name = name.replace('-',  'm')
+    name = name.replace('(',  '_')
+    name = name.replace(')',  '_')
+
+    std   = numpy.std(arr_val)
+    nent  = len(arr_val)
+    title = f'{std:.0f}; {nent}'
+
+    plt.title(title)
+    plt.axvline(val, color='red')
+    plt.axvline(val - err, color='red', linestyle=':')
+    plt.axvline(val + err, color='red', linestyle=':')
+    plt.savefig(f'{out_dir}/{name}.png')
+    plt.close()
+# -----------------------------
+def _get_resolutions(mat_cut : list[list[str]]) -> list[list[float]]:
+    path = f'{Data.cache_dir}/resolutions.json'
     if os.path.isfile(path):
         log.info(f'Cached values found, loading: {path}')
         with open(path, encoding='utf-8') as ifile:
-            d_nrm = json.load(ifile)
-            return d_nrm
+            mat_res = json.load(ifile)
+            return mat_res
+
+    d_rdf = {}
+    rdf   = _get_rdf()
+    for l_cut in mat_cut:
+        for cut in l_cut:
+            d_rdf[cut] = rdf.Filter(cut)
 
     var_name = Data.cfg['target']['variable']
     quantity = Data.cfg['target']['quantity']
-
-    d_rdf = { cut : rdf.Filter(cut) for cut in l_cut }
+    val, err = _get_full_stats(rdf, var_name)
 
     if quantity == 'RMS':
         d_rms = { cut : rdf.StdDev(var_name) for cut, rdf in d_rdf.items() }
-        d_frq = { cut : rdf.Count()          for cut, rdf in d_rdf.items() }
     else:
         raise NotImplementedError(f'Invalid quantity: {quantity}')
 
-    d_nrm = {}
+    d_frq = { cut : rdf.Count()          for cut, rdf in d_rdf.items() }
+
     log.info('Evaluating nodes')
-    for cut in d_rdf:
-        rms = d_rms[cut]
-        frq = d_frq[cut]
+    mat_rms = []
+    for l_cut in mat_cut:
+        l_res = []
+        for cut in l_cut:
+            rms = d_rms[cut]
+            frq = d_frq[cut]
+            rdf = d_rdf[cut]
 
-        rms_val = rms.GetValue()
-        frq_val = frq.GetValue()
+            frq_val = frq.GetValue()
+            rms_val = rms.GetValue()
 
-        if frq_val == 0:
-            log.warning(f'Zero entries at: {cut}')
-            d_nrm[cut] = 0
-        else:
-            nrm_rms    = rms_val / frq_val
-            log.debug(f'{nrm_rms:.3f}')
-            d_nrm[cut] = nrm_rms
+            within_nsigma = val - 3 * err < rms_val < val + 3 * err
+            if not within_nsigma:
+                _plot_distribution(rdf, var_name, cut, val, err)
+
+            if frq_val == 0:
+                log.warning(f'Zero entries at: {cut}')
+                l_res.append(0)
+            else:
+                l_res.append(rms_val)
+
+        mat_rms.append(l_res)
 
     with open(path, 'w', encoding='utf-8') as ofile:
-        json.dump(d_nrm, ofile, indent=4, sort_keys=True)
+        json.dump(mat_rms, ofile, indent=4)
 
-    return d_nrm
+    return mat_rms
 # -----------------------------
-def _get_cuts(l_name : list[str], l_setting : list) -> list[str]:
+def _get_cuts(l_name : list[str], l_setting : list) -> list[list[str]]:
     l_cutx = _cuts_from_binning(l_name, l_setting, 0)
     l_cuty = _cuts_from_binning(l_name, l_setting, 1)
 
-    l_cutxy = []
+    l_cut_xy = []
     for cutx in l_cutx:
+        l_cut_x = []
         for cuty in l_cuty:
-            cutxy  = f'({cutx}) && ({cuty})'
-            l_cutxy.append(cutxy)
+            l_cut_x.append(f'({cutx}) && ({cuty})')
 
-    return l_cutxy
+        l_cut_xy.append(l_cut_x)
+
+    return l_cut_xy
 # -----------------------------
-def _attach_as_weight(rdf : RDataFrame, d_nval : dict[str,float]) -> RDataFrame:
-    l_part = [ f'(double({cut}) * {val})' for cut, val in d_nval.items() ]
-    cut    = '+'.join(l_part)
-    rdf    = rdf.Define('weight', cut)
+def _plot(mat_res : list[list[str]], variables : list[str]) -> None:
+    [var_x, var_y] = variables
+    l_bound_x      = Data.d_bound[var_x]
+    l_bound_y      = Data.d_bound[var_y]
 
-    return rdf
+    mat_res = numpy.array(mat_res).T
+    plt.pcolormesh(l_bound_x, l_bound_y, mat_res, cmap="viridis", shading="auto")
+    plt.colorbar()
+    plt.show()
 # -----------------------------
 def main():
     '''
@@ -139,25 +217,13 @@ def main():
     _parse_args()
     _load_config()
 
-    root_path = f'{Data.cache_dir}/data.root'
-    if os.path.isfile(root_path):
-        rdf=RDataFrame('tree', root_path)
-        ptr=Plotter(rdf=rdf, cfg=Data.cfg)
-        ptr.run()
-        return
-
     d_axis = Data.cfg['axes']
     l_name = list(d_axis.keys())
     l_sett = list(d_axis.values())
-    l_cutxy= _get_cuts(l_name, l_sett)
+    mat_cut= _get_cuts(l_name, l_sett)
 
-    rdf    = _get_rdf()
-    d_nval = _get_normalized_value(rdf, l_cutxy)
-    rdf    = _attach_as_weight(rdf, d_nval)
-    rdf.Snapshot('tree', root_path, columnList=['B_M', 'weight', 'L2_STATEAT_Ecal_positionX', 'L2_STATEAT_Ecal_positionY'])
-
-    ptr=Plotter(rdf=rdf, cfg=Data.cfg)
-    ptr.run()
+    mat_res= _get_resolutions(mat_cut)
+    _plot(mat_res, l_name)
 # -----------------------------
 if __name__ == '__main__':
     main()
