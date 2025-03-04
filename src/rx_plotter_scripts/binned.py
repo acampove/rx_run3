e69e5accc4be864270123e37751200bc20de7167
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-04 13:15:46 +0800

    Can get normalized RMS

--- src/rx_plotter_scripts/binned.py
+++ src/rx_plotter_scripts/binned.py
@@ -5,10 +5,12 @@ import argparse
 from importlib.resources import files
 
 import yaml
-from ROOT                  import RDataFrame, EnableImplicitMT
-from dmu.logging.log_store import LogStore
-from rx_data.rdf_getter    import RDFGetter
-from rx_selection          import selection as sel
+import numpy
+from ROOT                    import RDataFrame, EnableImplicitMT
+from dmu.logging.log_store   import LogStore
+from dmu.plotting.plotter_2d import Plotter2D as Plotter
+from rx_data.rdf_getter      import RDFGetter
+from rx_selection            import selection as sel
 
 log=LogStore.add_logger('rx_plotter:binned')
 # -----------------------------
@@ -45,20 +47,64 @@ def _get_rdf() -> RDataFrame:
         rdf = rdf.Define(name, expr)
 
     d_sel = sel.selection(project='RK', analysis='EE', q2bin=q2bin, process='DATA')
-    d_cut = Data.cfg['selection']
+    d_cut = Data.cfg['cuts']
     d_sel.update(d_cut)
 
     for cut_name, cut_value in d_sel.items():
         log.info(f'{cut_name:<20}{cut_value}')
         rdf = rdf.Filter(cut_value, cut_name)
 
-    rep = rdf.Report()
-    rep.Print()
+    #rep = rdf.Report()
+    #rep.Print()
 
     return rdf
 # -----------------------------
+def _cuts_from_binning(l_name : list[str], l_setting : list, index : int) -> list[str]:
+    var_name            = l_name[index]
+    [nbins, minv, maxv] = l_setting[index]['binning']
+    arr_bound           = numpy.linspace(minv, maxv, nbins)
+    l_bound             = arr_bound.tolist()
+    l_min               = l_bound[:-1]
+    l_max               = l_bound[+1:]
+
+    l_cut = [ f'({var_name} > {minv:>3.0f}) && ({var_name} <={maxv:>3.0f})' for minv, maxv in zip(l_min, l_max)]
+
+    return l_cut
+# -----------------------------
+def _get_normalized_value(rdf : RDataFrame, l_cut : list[str]) -> dict[str,float]:
+    var_name = Data.cfg['target']['variable']
+    quantity = Data.cfg['target']['quantity']
+
+    d_rdf = { cut : rdf.Filter(cut) for cut in l_cut }
+
+    if quantity == 'RMS':
+        d_rms = { cut : rdf.StdDev(var_name) for cut, rdf in d_rdf.items() }
+        d_frq = { cut : rdf.Count()          for cut, rdf in d_rdf.items() }
+    else:
+        raise NotImplementedError(f'Invalid quantity: {quantity}')
+
+    d_nrm = {}
+    log.info('Evaluating nodes')
+    for cut in d_rdf:
+        rms = d_rms[cut]
+        frq = d_frq[cut]
+
+        rms_val = rms.GetValue()
+        frq_val = frq.GetValue()
+
+        if frq_val == 0:
+            log.warning(f'Zero yield at: {cut}')
+            d_nrm[cut] = numpy.nan
+        else:
+            nrm_rms    = rms_val / frq_val
+            log.info(f'{nrm_rms:.3f}')
+            d_nrm[cut] = nrm_rms
+
+    return d_nrm
+# -----------------------------
 def _plot(rdf : RDataFrame) -> None:
-    pass
+    ptr=Plotter(rdf=rdf, cfg=Data.cfg)
+    ptr.run()
 # -----------------------------
 def main():
     '''
@@ -66,7 +112,26 @@ def main():
     '''
     _parse_args()
     _load_config()
-    rdf = _get_rdf()
+
+    d_axis = Data.cfg['axes']
+
+    l_name = list(d_axis.keys())
+    l_sett = list(d_axis.values())
+
+    l_cutx = _cuts_from_binning(l_name, l_sett, 0)
+    l_cuty = _cuts_from_binning(l_name, l_sett, 1)
+
+    l_cutxy = []
+    for cutx in l_cutx:
+        for cuty in l_cuty:
+            cutxy  = f'({cutx}) && ({cuty})'
+            l_cutxy.append(cutxy)
+
+    rdf     = _get_rdf()
+    d_nvval = _get_normalized_value(rdf, l_cutxy)
+
+    return
+    rdf     = _attach_normalized_value(d_nval)
 
     _plot(rdf)
 # -----------------------------
