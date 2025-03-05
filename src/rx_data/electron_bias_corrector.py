eb19d613a65017ddb4a0ce56a6396cc5876baa21
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-05 15:04:03 +0800

    Adding file

---
+++ src/rx_data/electron_bias_corrector.py
@@ -0,0 +1,20 @@
+'''
+Module with ElectronBiasCorrector class
+'''
+import pandas as pnd
+
+# ---------------------------------
+class ElectronBiasCorrector:
+    '''
+    Class meant to correct electron kinematics
+    '''
+    # ---------------------------------
+    def __init__(self):
+        pass
+    # ---------------------------------
+    def correct(self, row : pnd.Series) -> pnd.Series:
+        '''
+        Corrects kinematics and returns row
+        '''
+        return row
+# ---------------------------------
