fa20b0d524e898fe936340b6c22636fc93f5aba7
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-05 14:13:54 +0800

    Adding all code, before implementing corrections

--- src/rx_data/electron_bias_corrector.py
+++ src/rx_data/electron_bias_corrector.py
@@ -2,7 +2,8 @@
 Module storing ElectronBiasCorrector class
 '''
 
-import numpy
+import math
+import vector
 import pandas as pnd
 
 from ROOT                  import RDataFrame, RDF
@@ -16,9 +17,12 @@ class ElectronBiasCorrector:
     by correcting biases in electrons
     '''
     # ------------------------------------------
-    def __init__(self, rdf : RDataFrame):
-        self._rdf = self._preprocess_rdf(rdf)
+    def __init__(self, rdf : RDataFrame, skip_correction : bool = False):
+        self._rdf             = self._preprocess_rdf(rdf)
+        self._skip_correction = skip_correction
 
+        self._emass   = 0.511
+        self._kmass   = 493.6
         self._to_keep = ['EVENTNUMBER', 'RUNNUMBER', 'B_M']
     # ------------------------------------------
     def _preprocess_rdf(self, rdf: RDataFrame) -> RDataFrame:
@@ -27,11 +31,38 @@ class ElectronBiasCorrector:
 
         return rdf
     # ------------------------------------------
-    def _calculate_correction(self, row : pnd.Series) -> float:
-        if not row.L1_HASBREMADDED and not row.L2_HASBREMADDED:
-            return row.B_M
+    def _correct_electron(self, name : str, row : pnd.Series) -> pnd.Series:
+        if self._skip_correction:
+            return row
 
-        return 1
+        has_brem = row[f'{name}_HASBREMADDED']
+        if not has_brem:
+            return row
+
+        return row
+    # ------------------------------------------
+    def _calculate_mass(self, row : pnd.Series) -> float:
+        l1 = vector.obj(pt=row.L1_PT, phi=row.L1_PHI, eta=row.L1_ETA, m=self._emass)
+        l2 = vector.obj(pt=row.L2_PT, phi=row.L2_PHI, eta=row.L2_ETA, m=self._emass)
+        kp = vector.obj(pt=row.H_PT , phi=row.H_PHI , eta=row.H_ETA , m=self._kmass)
+        bp = l1 + l2 + kp
+
+        mass = float(bp.mass)
+        if math.isnan(mass):
+            log.warning('NaN mass found for:')
+            log.info(f'L1: {l1}')
+            log.info(f'L2: {l2}')
+            log.info(f'Kp: {kp}')
+            log.info(f'Bp: {bp}')
+
+        return mass
+    # ------------------------------------------
+    def _calculate_correction(self, row : pnd.Series) -> float:
+        row  = self._correct_electron('L1', row)
+        row  = self._correct_electron('L2', row)
+        mass = self._calculate_mass(row)
+
+        return mass
     # ------------------------------------------
     def _pick_column(self, name : str) -> bool:
         col_type = self._rdf.GetColumnType(name)
@@ -48,6 +79,10 @@ class ElectronBiasCorrector:
             return False
 
         if name in self._to_keep:
+            return True
+
+        if name.startswith('H_'):
+            #log.info(f'{col_type:<20}{name}')
             return True
 
         if name.startswith('L1_'):
