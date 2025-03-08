ffbbfef28d21b24e630317a4c79a315825147a9a
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-08 16:13:24 +0800

    Updating with plotting tests

--- tests/test_calo_translator.py
+++ tests/test_calo_translator.py
@@ -1,9 +1,10 @@
 '''
 Module with functions meant to test the row,col <-> x,y translation
 '''
-
+import os
 from importlib.resources import files
 
+import pytest
 import pandas           as pnd
 import matplotlib.pyplot as plt
 
@@ -12,14 +13,33 @@ from dmu.logging.log_store import LogStore
 log=LogStore.add_logger('rx_data:test_calo_translator')
 
 # --------------------------------
+class Data:
+    '''
+    Data class
+    '''
+    out_dir = '/tmp/tests/rx_data/calo_translator'
+# --------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    '''
+    This function will execute before the tests
+    '''
+    os.makedirs(Data.out_dir, exist_ok=True)
+# --------------------------------
+def _cast_column(column, ctype) -> pnd.Series:
+    column = pnd.to_numeric(column, errors='coerce')
+    column = column.fillna(-100_000)
+    column = column.astype(ctype)
+
+    return column
+# --------------------------------
 def _get_data() -> pnd.DataFrame:
     data_path = files('rx_data_data').joinpath('brem_correction/coordinates.csv')
     df      = pnd.read_csv(data_path)
-    df['x'] = df.x.astype(float)
-    df['y'] = df.y.astype(float)
-    df['z'] = df.z.astype(float)
-    df['r'] = df.r.astype(int)
-    df['c'] = df.c.astype(int)
+    df['x'] = _cast_column(df.x, float)
+    df['y'] = _cast_column(df.y, float)
+    df['r'] = _cast_column(df.r, int)
+    df['c'] = _cast_column(df.c, int)
 
     df = df[df.x > - 10_000]
     df = df[df.y > - 10_000]
@@ -31,7 +51,9 @@ def test_load():
     Tests loading data to dataframe
     '''
     log.info('')
-    _get_data()
+    df = _get_data()
+    print(df)
+    print(df.dtypes)
 # --------------------------------
 def test_read_xy():
     '''
@@ -53,19 +75,20 @@ def test_plot_row_col():
     Plots row and column histograms
     '''
     df     = _get_data()
-    df.c.hist(bins=50, label='column')
-    df.r.hist(bins=50, label='row')
+    df.c.hist(bins=64, histtype='step', linestyle='-', label='column')
+    df.r.hist(bins=64, histtype='step', linestyle=':', label='row')
     plt.legend()
-    plt.show()
+    plt.savefig(f'{Data.out_dir}/row_col.png')
+    plt.close()
 # --------------------------------
-def test_plot_xyz():
+def test_plot_xy():
     '''
-    Tests plotting X, Y and Z coodinates
+    Tests plotting X and Y coodinates
     '''
     df     = _get_data()
     df.x.hist(bins=50, histtype='step', label='x')
     df.y.hist(bins=50, histtype='step', label='y')
-    df.z.hist(bins=50, histtype='step', label='z')
     plt.legend()
-    plt.show()
+    plt.savefig(f'{Data.out_dir}/xy.png')
+    plt.close()
 # --------------------------------
