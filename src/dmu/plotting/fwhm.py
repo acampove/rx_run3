b2c16a8e516d070abf0c939370527edf0b77e5ff
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-10 15:55:37 +0800

    Better overlay of KDE and normalize properly

--- src/dmu/plotting/fwhm.py
+++ src/dmu/plotting/fwhm.py
@@ -15,10 +15,17 @@ class FWHM:
     as a Plotter1d plugin
     '''
     # -------------------------
-    def __init__(self, cfg : dict, val : numpy.ndarray, wgt : numpy.ndarray):
+    def __init__(self, cfg : dict, val : numpy.ndarray, wgt : numpy.ndarray, maxy : float):
         self._cfg     = cfg
         self._arr_val = val
         self._arr_wgt = wgt
+        self._maxy    = maxy
+    # -------------------------
+    def _normalize_yval(self, arr_pdf_val : numpy.ndarray) -> None:
+        max_pdf_val = numpy.max(arr_pdf_val)
+        arr_pdf_val*= self._maxy / max_pdf_val
+
+        return arr_pdf_val
     # -------------------------
     def run(self):
         '''
@@ -32,7 +39,8 @@ class FWHM:
         pdf= zfit.pdf.KDE1DimExact(obs=obs, data=self._arr_val, weights=self._arr_wgt)
 
         xval = numpy.linspace(minx, maxx, 200)
-        yval = 1000 * pdf.pdf(xval)
+        yval = pdf.pdf(xval)
+        yval = self._normalize_yval(yval)
 
-        plt.plot(xval, yval)
+        plt.plot(xval, yval, linestyle='-', linewidth=2, color='red')
 # --------------------------------------------
