cc1f3a484bae63eef4fdee77316c3c84ca3238fd
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-13 20:10:05 +0800

    Moving get_models to testing/utilties.py

--- tests/ml/test_cv_predict.py
+++ tests/ml/test_cv_predict.py
@@ -2,14 +2,11 @@
 Contains unit tests for CVPredict class
 '''
 
-import glob
 import pytest
 
-import joblib
 import numpy
 
 from dmu.logging.log_store import LogStore
-from dmu.ml.train_mva      import TrainMva
 from dmu.ml.cv_predict     import CVPredict
 
 import dmu.testing.utilities as ut
@@ -46,27 +43,6 @@ def _check_probabilities(arr_prb : numpy, has_negative : bool) -> None:
     else:
         assert n_below == 0
 #--------------------------------------------------------------------
-def _get_models(rdf_sig, rdf_bkg):
-    '''
-    Will train and return models
-    '''
-
-    cfg                   = ut.get_config('ml/tests/train_mva.yaml')
-    pkl_path              = f'{Data.out_dir}/model.pkl'
-    plt_dir               = f'{Data.out_dir}/cv_predict'
-    cfg['saving']['path'] = pkl_path
-    cfg['plotting']['val_dir'] = plt_dir
-    cfg['plotting']['features']['saving']['plt_dir'] = plt_dir
-
-    obj= TrainMva(sig=rdf_sig, bkg=rdf_bkg, cfg=cfg)
-    obj.run()
-
-    pkl_wc     = pkl_path.replace('.pkl', '_*.pkl')
-    l_pkl_path = glob.glob(pkl_wc)
-    l_model    = [ joblib.load(pkl_path) for pkl_path in l_pkl_path ]
-
-    return l_model
-#--------------------------------------------------------------------
 def test_non_overlap():
     '''
     Tests prediction when input dataset is different from training one
@@ -75,7 +51,7 @@ def test_non_overlap():
     LogStore.set_level('dmu:ml:cv_predict', 10)
     rdf_sig = ut.get_rdf(kind='sig')
     rdf_bkg = ut.get_rdf(kind='bkg')
-    l_model = _get_models(rdf_sig, rdf_bkg)
+    l_model = ut.get_models(rdf_sig, rdf_bkg)
 
     rdf     = ut.get_rdf(kind='sig')
     cvp     = CVPredict(models=l_model, rdf=rdf)
