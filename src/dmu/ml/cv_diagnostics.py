30e7ca398f2cc635dda937f2fd45b89e9d5ff9b5
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-14 14:04:55 +0800

    Correlations should be WRT target variable

--- src/dmu/ml/cv_diagnostics.py
+++ src/dmu/ml/cv_diagnostics.py
@@ -1,8 +1,11 @@
 '''
 Module containing CVDiagnostics class
 '''
+import os
+
 import numpy
-import pandas as pnd
+import pandas            as pnd
+import matplotlib.pyplot as plt
 
 from ROOT                  import RDataFrame
 from dmu.ml.cv_classifier  import CVClassifier
@@ -26,15 +29,12 @@ class CVDiagnostics:
         self._l_model = models
         self._cfg     = cfg
         self._rdf     = rdf
-        self._l_var   = self._get_variables()
+        self._target  = cfg['correlations']['target']
+        self._l_feat  = self._get_features()
     # -------------------------
-    def _get_variables(self) -> list[str]:
-        cfg    = self._l_model[0].cfg
-        l_feat = cfg['training']['features']
-        l_var  = l_feat + self._cfg['correlations']['variables']
-        s_var  = set(l_var)
-        l_var  = list(s_var)
-        l_var  = sorted(l_var)
+    def _get_features(self) -> list[str]:
+        cfg   = self._l_model[0].cfg
+        l_var = cfg['training']['features']
 
         return l_var
     # -------------------------
@@ -50,8 +50,9 @@ class CVDiagnostics:
         rdf   = self._add_columns(self._rdf)
         l_col = [ name.c_str() for name in rdf.GetColumnNames() ]
 
-        missing=False
-        for var in self._l_var:
+        missing= False
+        l_var  = self._l_feat + [self._target]
+        for var in l_var:
             if var not in l_col:
                 log.error(f'{"Missing":<20}{var}')
                 missing=True
@@ -59,29 +60,52 @@ class CVDiagnostics:
         if missing:
             raise ValueError('Columns missing')
 
-        d_var = rdf.AsNumpy(self._l_var)
+        d_var          = rdf.AsNumpy(l_var)
+        prd            = CVPredict(models=self._l_model, rdf = self._rdf)
+        d_var['score'] = prd.predict()
 
         return d_var
     # -------------------------
     def _run_correlations(self, method : str):
-        prd      = CVPredict(models=self._l_model, rdf = self._rdf)
-        arr_prob = prd.predict()
+        d_arr      = self._get_arrays()
+        arr_target = d_arr[self._target]
 
-        d_arr = self._get_arrays()
         d_corr= {}
         for name, arr_val in d_arr.items():
-            d_corr[name] = self._calculate_correlations(var=arr_val, prob=arr_prob, method=method)
+            if name == self._target:
+                continue
 
-        self._plot_correlations(d_corr)
+            d_corr[name] = self._calculate_correlations(var=arr_val, target=arr_target, method=method)
+
+        self._plot_correlations(d_corr, method)
     # -------------------------
-    def _plot_correlations(self, d_corr : dict[str,float]) -> None:
+    def _plot_correlations(self, d_corr : dict[str,float], method : str) -> None:
         df = pnd.DataFrame.from_dict(d_corr, orient="index", columns=['Correlation'])
+        df = df.sort_values(by='Correlation')
 
-        print(df)
+        figsize     = self._cfg['correlations']['figure']['size']
+        df.plot(kind='bar', legend=False, figsize=figsize)
+
+        out_dir = self._cfg['output'] + '/correlations'
+        os.makedirs(out_dir, exist_ok=True)
+
+        plot_path = f'{out_dir}/{method}.png'
+        log.info(f'Saving to: {plot_path}')
+
+        title = f'W.R.T. {self._target}'
+
+        plt.ylim(-1, +1)
+        plt.title(title)
+        plt.ylabel('Correlation')
+        plt.grid()
+        plt.xticks(rotation=30)
+        plt.tight_layout()
+        plt.savefig(plot_path)
+        plt.close()
     # -------------------------
-    def _calculate_correlations(self, var : NPA, prob : NPA, method : str) -> float:
+    def _calculate_correlations(self, var : NPA, target : NPA, method : str) -> float:
         if method == 'pearson':
-            mat = numpy.corrcoef(var, prob)
+            mat = numpy.corrcoef(var, target)
             return mat[0,1]
 
         raise NotImplementedError(f'Correlation coefficient {method} not implemented')
