444ea0b4ea32e9bdff0283c981e3a4c3f6f13a0a
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-14 14:05:40 +0800

    Styling

--- tests/ml/test_cv_diagnostics.py
+++ tests/ml/test_cv_diagnostics.py
@@ -1,12 +1,20 @@
 '''
 Module with functions testing CVDiagnostics class
 '''
+import mplhep
+import pytest
+import matplotlib.pyplot as plt
+
 from dmu.logging.log_store import LogStore
 from dmu.ml.cv_diagnostics import CVDiagnostics
 
 import dmu.testing.utilities as ut
 
 log = LogStore.add_logger('dmu:ml:tests:test_cv_diagnostics')
+# -------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    plt.style.use(mplhep.style.LHCb2)
 # -------------------------------
 def test_correlations():
     '''
