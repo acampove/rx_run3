7f99426b90a773bc2443958a5c8d983bf498c66d
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-14 15:58:46 +0800

    Adding test

--- tests/ml/test_cv_diagnostics.py
+++ tests/ml/test_cv_diagnostics.py
@@ -16,16 +16,34 @@ log = LogStore.add_logger('dmu:ml:tests:test_cv_diagnostics')
 def _initialize():
     plt.style.use(mplhep.style.LHCb2)
 # -------------------------------
-def test_correlations():
+def test_score_from_model():
     '''
-    Measures correlations between signal probability and other variables
+    Measures correlations between target and features + score
+    where the score is calculated from the model 
     '''
+
     rdf_sig = ut.get_rdf(kind='sig')
     rdf_bkg = ut.get_rdf(kind='bkg')
 
     l_model = ut.get_models(rdf_sig, rdf_bkg)
     rdf     = ut.get_rdf(kind='sig')
-    cfg     = ut.get_config('ml/tests/cv_diagnostics.yaml')
+    cfg     = ut.get_config('ml/tests/diagnostics_from_model.yaml')
+
+    cvd     = CVDiagnostics(models=l_model, rdf=rdf, cfg=cfg)
+    cvd.run()
+# -------------------------------
+def test_score_from_rdf():
+    '''
+    Measures correlations between target and features + score
+    where the score comes from the dataframe
+    '''
+
+    rdf_sig = ut.get_rdf(kind='sig')
+    rdf_bkg = ut.get_rdf(kind='bkg')
+
+    l_model = ut.get_models(rdf_sig, rdf_bkg)
+    rdf     = ut.get_rdf(kind='sig')
+    cfg     = ut.get_config('ml/tests/diagnostics_from_file.yaml')
 
     cvd     = CVDiagnostics(models=l_model, rdf=rdf, cfg=cfg)
     cvd.run()
