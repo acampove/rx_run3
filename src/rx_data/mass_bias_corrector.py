c61505a8b3c57356e1e9567b5d0a3c26e8eca99a
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-24 21:51:29 +0800

    Utility functions moved to utilities.py

--- src/rx_data/mass_bias_corrector.py
+++ src/rx_data/mass_bias_corrector.py
@@ -8,110 +8,11 @@ import pandas as pnd
 from pandarallel                     import pandarallel
 from ROOT                            import RDataFrame, RDF
 from dmu.logging.log_store           import LogStore
+
+import rx_data.utilities             as ut
 from rx_data.electron_bias_corrector import ElectronBiasCorrector
 
 log=LogStore.add_logger('rx_data:mass_bias_corrector')
-# ------------------------------------------
-def df_from_rdf(rdf : RDataFrame) -> pnd.DataFrame:
-    '''
-    Utility method needed to get pandas dataframe from ROOT dataframe
-    '''
-    rdf    = _preprocess_rdf(rdf)
-    l_col  = [ name.c_str() for name in rdf.GetColumnNames() if _pick_column(name.c_str()) ]
-    d_data = rdf.AsNumpy(l_col)
-    df     = pnd.DataFrame(d_data)
-
-    sr_nan = df.isna().any(axis=1)
-    nnan   = sr_nan.sum()
-    if nnan != 0:
-        log.warning(f'Found {nnan} NaNs in Pandas dataframe')
-        df_nan = df[sr_nan]
-        for name in df_nan.columns:
-            sr_val = df_nan[name]
-            if name in ['EVENTNUMBER', 'RUNNUMBER']:
-                log.info(sr_val)
-
-            if not sr_val.isna().any():
-                continue
-            log.info(sr_val)
-
-        df = df.dropna()
-
-    return df
-# ------------------------------------------
-def _preprocess_rdf(rdf: RDataFrame) -> RDataFrame:
-    rdf = _preprocess_lepton(rdf, 'L1')
-    rdf = _preprocess_lepton(rdf, 'L2')
-    rdf = _preprocess_lepton(rdf,  'H')
-
-    return rdf
-# ------------------------------------------
-def _preprocess_lepton(rdf : RDataFrame, lep : str) -> None:
-    # Make brem flag an int (will make things easier later)
-    rdf = rdf.Redefine(f'{lep}_HASBREMADDED'        , f'int({lep}_HASBREMADDED)')
-    # If there is no brem, make energy zero
-    rdf = rdf.Redefine(f'{lep}_BREMHYPOENERGY'      , f'{lep}_HASBREMADDED == 1 ? {lep}_BREMHYPOENERGY : 0')
-    # If track based energy is NaN, make it zero
-    rdf = rdf.Redefine(f'{lep}_BREMTRACKBASEDENERGY', f'{lep}_BREMTRACKBASEDENERGY == {lep}_BREMTRACKBASEDENERGY ? {lep}_BREMTRACKBASEDENERGY : 0')
-
-    return rdf
-# ------------------------------------------
-def _pick_column(name : str) -> bool:
-    to_keep  = ['EVENTNUMBER', 'RUNNUMBER']
-
-    if '_TRUE' in name:
-        return False
-
-    if name in to_keep:
-        return True
-
-    not_l1 = not name.startswith('L1')
-    not_l2 = not name.startswith('L2')
-    not_kp = not name.startswith('H')
-
-    if not_l1 and not_l2 and not_kp:
-        return False
-
-    if 'BREMTRACKBASEDENERGY' in name:
-        return True
-
-    if 'HASBREMADDED' in name:
-        return True
-
-    if 'NVPHITS' in name:
-        return False
-
-    if 'CHI2' in name:
-        return False
-
-    if 'HYPOID' in name:
-        return False
-
-    if 'HYPODELTA' in name:
-        return False
-
-    if 'PT' in name:
-        return True
-
-    if 'ETA' in name:
-        return True
-
-    if 'PHI' in name:
-        return True
-
-    if 'PX' in name:
-        return True
-
-    if 'PY' in name:
-        return True
-
-    if 'PZ' in name:
-        return True
-
-    if 'BREMHYPO' in name:
-        return True
-
-    return False
 # ------------------------------------------
 class MassBiasCorrector:
     '''
@@ -132,7 +33,7 @@ class MassBiasCorrector:
         brem_energy_threshold: Lowest energy that an ECAL cluster needs to have to be considered a photon, used as argument of ElectronBiasCorrector, default 0 (MeV)
         ecorr_kind : Kind of correction to be added to electrons, [ecalo_bias, brem_track]
         '''
-        self._df              = df_from_rdf(rdf)
+        self._df              = ut.df_from_rdf(rdf)
         self._skip_correction = skip_correction
         self._nthreads        = nthreads
 
