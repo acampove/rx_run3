aa4a4ee900c3899690a419cc8f60572aae2be32d
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-26 17:34:45 +0800

    Better documentation, use kwargs for more flexibility

--- src/dmu/pdataframe/utilities.py
+++ src/dmu/pdataframe/utilities.py
@@ -9,13 +9,21 @@ from dmu.logging.log_store import LogStore
 log=LogStore.add_logger('dmu:pdataframe:utilities')
 
 # -------------------------------------
-def df_to_tex(df : pnd.DataFrame, path : str, hide_index : bool = True, d_format : dict[str,str]=None, caption : str =None) -> None:
+def df_to_tex(df         : pnd.DataFrame,
+              path       : str,
+              hide_index : bool         = True,
+              d_format   : dict[str,str]= None,
+              **kwargs   : str       ) -> None:
     '''
     Saves pandas dataframe to latex
 
     Parameters
     -------------
+    df              : Dataframe with data
+    path     (str)  : Path to latex file
+    hide_index      : If true (default), index of dataframe won't appear in table
     d_format (dict) : Dictionary specifying the formattinng of the table, e.g. `{'col1': '{}', 'col2': '{:.3f}', 'col3' : '{:.3f}'}`
+    kwargs          : Arguments needed in `to_latex`
     '''
 
     if path is not None:
@@ -30,7 +38,7 @@ def df_to_tex(df : pnd.DataFrame, path : str, hide_index : bool = True, d_format
         st=st.format(formatter=d_format)
 
     log.info(f'Saving to: {path}')
-    buf = st.to_latex(buf=path, caption=caption, hrules=True)
+    buf = st.to_latex(buf=path, hrules=True, **kwargs)
 
     return buf
 # -------------------------------------
