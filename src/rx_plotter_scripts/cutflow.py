1a24d9242b32b60cce0ec726683b7d4c123f57ff
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-28 19:14:09 +0800

    Updating documentation and config choices

--- src/rx_plotter_scripts/cutflow.py
+++ src/rx_plotter_scripts/cutflow.py
@@ -109,11 +109,11 @@ def _get_q2cut() -> str:
     return cut
 # ---------------------------------
 def _parse_args() -> None:
-    parser = argparse.ArgumentParser(description='Script used to make generic plots')
+    parser = argparse.ArgumentParser(description='Script used to cutflow plots')
     parser.add_argument('-q', '--q2bin'  , type=str, help='q2 bin' , choices=['low', 'central', 'jpsi', 'psi2', 'high'])
     parser.add_argument('-s', '--sample' , type=str, help='Sample' , required=True)
     parser.add_argument('-t', '--trigger', type=str, help='Trigger' , required=True)
-    parser.add_argument('-c', '--config' , type=str, help='Configuration')
+    parser.add_argument('-c', '--config' , type=str, help='Configuration', choices=['cleanup', 'bdt_q2_mass'])
     parser.add_argument('-x', '--substr' , type=str, help='Substring that must be contained in path, e.g. magup')
     args = parser.parse_args()
 
