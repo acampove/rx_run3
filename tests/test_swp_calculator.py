412421f38c9c6e49cd53cbfdeacf53f9136604e6
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-30 18:11:57 +0800

    Improving way data is obtained

--- tests/test_swp_calculator.py
+++ tests/test_swp_calculator.py
@@ -12,7 +12,6 @@ from ROOT                   import RDataFrame, RDF
 from dmu.logging.log_store  import LogStore
 from rx_data.rdf_getter     import RDFGetter
 from rx_data.swp_calculator import SWPCalculator
-from rx_data.mis_calculator import MisCalculator
 
 log = LogStore.add_logger('rx_data:test_swp_calculator')
 # ----------------------------------
@@ -20,79 +19,55 @@ class Data:
     '''
     Class used to share attributes
     '''
-    inp_dir : str = '/home/acampove/external_ssd/Data/main/v5'
     out_dir : str = '/tmp/tests/rx_data/swap_calculator'
-
-    l_file_wc = [
-            #'data_24_mag*_24c1_Hlt2RD_BuToKpMuMu_MVA_0000000000.root',
-            'data_24_mag*_24c2_Hlt2RD_BuToKpMuMu_MVA_0000000000.root',
-            #'data_24_mag*_24c3_Hlt2RD_BuToKpMuMu_MVA_0000000000.root',
-            #'data_24_mag*_24c4_Hlt2RD_BuToKpMuMu_MVA_0000000000.root',
-            ]
 # ----------------------------------
 @pytest.fixture(scope='session', autouse=True)
 def _initialize():
     os.makedirs(Data.out_dir, exist_ok=True)
     LogStore.set_level('rx_data:swp_calculator', 20)
 # ----------------------------------
-def _get_rdf(test : str, file_wc : str) -> RDataFrame:
-    if test == 'cascade_mc':
-        json_path = files('rx_data_data').joinpath('tests/swap_adder/bpd0kpienu.json')
-        df        = pnd.read_json(json_path)
-        d_data    = df.to_dict(orient='list')
-        d_numpy   = { name : numpy.array(l_val) for name, l_val in d_data.items() }
-        rdf       = RDF.FromNumpy(d_numpy)
-        rdf       = rdf.Define('EVENTNUMBER', '1')
-        rdf       = rdf.Define('RUNNUMBER'  , '2')
-
-        return rdf
-
-    if test == 'cascade_dt_ss':
+def _get_rdf(kind : str) -> RDataFrame:
+    if   kind == 'dt_ss':
         sample = 'DATA_24_MagUp_24c3'
         trigger= 'Hlt2RD_BuToKpEE_SameSign_MVA'
-        RDFGetter.samples = {
-        'main' : '/home/acampove/external_ssd/Data/samples/main.yaml',
-        }
+    elif kind == 'mc':
+        sample = 'Bu_JpsiK_ee_eq_DPC'
+        trigger= 'Hlt2RD_BuToKpEE_MVA'
+    else:
+        raise ValueError(f'Invalid dataset of kind: {kind}')
 
-        gtr = RDFGetter(sample=sample, trigger=trigger)
-        rdf = gtr.get_rdf()
-        rdf = rdf.Range(100_000)
+    data_dir = os.environ['DATADIR']
+    RDFGetter.samples = {'main' : f'{data_dir}/samples/main.yaml'}
 
-        return rdf
+    gtr = RDFGetter(sample=sample, trigger=trigger)
+    rdf = gtr.get_rdf()
+    rdf = rdf.Range(100_000)
 
-    if test == 'jpsi_misid':
-        rdf   = RDataFrame('DecayTree', f'{Data.inp_dir}/{file_wc}')
-        rdf   = rdf.Filter('Jpsi_M * Jpsi_M > 15000000')
-        msc   = MisCalculator(rdf=rdf, trigger='Hlt2RD_BuToKpMuMu_MVA')
-        rdf   = msc.get_rdf()
-
-        return rdf
-
-    raise ValueError(f'Invalid test: {test}')
+    return rdf
 # ----------------------------------
 @pytest.mark.parametrize('kind', ['mc', 'dt_ss'])
 def test_cascade(kind : str):
     '''
     Tests cascade decay contamination
     '''
-    rdf = _get_rdf(test=f'cascade_{kind}', file_wc='NA')
+    rdf = _get_rdf(kind=kind)
     obj = SWPCalculator(rdf, d_lep={'L1' : 211, 'L2' : 211}, d_had={'H' : 321})
     rdf = obj.get_rdf(preffix='cascade', progress_bar=True, use_ss= 'ss' in kind)
 
-    _plot(rdf, 'cascade', preffix='cascade', kind=kind)
+    _plot(rdf, preffix='cascade', kind=kind)
 # ----------------------------------
-@pytest.mark.parametrize('file_wc', Data.l_file_wc)
-def test_jpsi_misid(file_wc : str):
+@pytest.mark.parametrize('kind', ['mc', 'dt_ss'])
+def test_jpsi_misid(kind : str):
     '''
     Tests jpsi misid contamination
     '''
-    rdf = _get_rdf(test='jpsi_misid', file_wc = file_wc)
+    rdf = _get_rdf(kind=kind)
     obj = SWPCalculator(rdf, d_lep={'L1' : 13, 'L2' : 13}, d_had={'H' : 13})
-    rdf = obj.get_rdf(preffix='jpsi_misid')
+    rdf = obj.get_rdf(preffix='jpsi_misid', progress_bar=True, use_ss= 'ss' in kind)
 
-    _plot(rdf, file_wc, preffix='jpsi_misid', kind='none')
+    _plot(rdf, preffix='jpsi_misid', kind=kind)
 # ----------------------------------
-def _plot(rdf : RDataFrame, file_wc : str, preffix : str, kind : str):
+def _plot(rdf : RDataFrame, preffix : str, kind : str):
     d_data = rdf.AsNumpy([f'{preffix}_mass_swp', f'{preffix}_mass_org'])
     arr_swp= d_data[f'{preffix}_mass_swp']
     arr_org= d_data[f'{preffix}_mass_org']
@@ -109,7 +84,6 @@ def _plot(rdf : RDataFrame, file_wc : str, preffix : str, kind : str):
     else:
         plt.axvline(x=1864, color='r', label='$D_0$')
 
-    suffix = file_wc.replace('*', 'p')
-    plt.savefig(f'{Data.out_dir}/{preffix}_{suffix}_{kind}.png')
+    plt.savefig(f'{Data.out_dir}/{preffix}_{kind}.png')
     plt.close('all')
 # ----------------------------------
