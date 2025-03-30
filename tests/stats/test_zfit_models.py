413dfbf6a4ad444905f8db38bde45fab70f3e659
Angel Campoverde <angel.campoverde@cern.ch>
2025-03-30 23:48:43 +0800

    Adding test for hyperexp

--- tests/stats/test_zfit_models.py
+++ tests/stats/test_zfit_models.py
@@ -2,13 +2,15 @@
 Module holding functions meant to test PDFs in zfit_models.py module
 '''
 import os
-import pytest
 import zfit
+import pytest
+import matplotlib.pyplot as plt
+
 from zfit.core.interfaces   import ZfitSpace as zobs
 from zfit.core.basepdf      import BasePDF   as zpdf
 from zfit.core.parameter    import Parameter as zpar
 
-from stats.zfit_models      import HypExp
+from dmu.stats.zfit_models  import HypExp
 from dmu.stats.zfit_plotter import ZFitPlotter
 
 # -------------------------------
@@ -20,25 +22,25 @@ class Data:
 # -------------------------------
 @pytest.fixture(scope='session', autouse=True)
 def _initialize():
-    os.mkdir(Data.out_dir, exist_ok=True)
+    os.makedirs(Data.out_dir, exist_ok=True)
 # -------------------------------
 def _plot_pdf(pdf : zpdf, name : str) -> None:
     data = pdf.create_sampler(n=10000)
     obj  = ZFitPlotter(data=data, model=pdf)
     obj.plot(nbins=50)
 
-    plt.savefig(f'{Data.out_dir}/{name}.pdf')
+    plt.savefig(f'{Data.out_dir}/{name}.png')
 # -------------------------------
 def test_hypexp():
     '''
     Test HypExp PDF
     '''
     obs= zfit.Space('x', limits=(4500, 6000))
-    mu = zfit.Parameter('mu',   5000, 4000, 6000)
-    ap = zfit.Parameter('ap',    200,    0, 1000)
-    bt = zfit.Parameter('bt', 0.0001, 0.00, 0.01)
+    mu = zfit.Parameter('mu',  5000,  4000,  6000)
+    ap = zfit.Parameter('ap', 0.020,     0,   0.1)
+    bt = zfit.Parameter('bt', 0.002, 0.001, 0.005)
 
-    pdf= HypExp(obs=obs, mu=mu, ap=ap, bt=bt)
+    pdf= HypExp(obs=obs, mu=mu, alpha=ap, beta=bt)
 
     _plot_pdf(pdf, name='hypexp')
 # -------------------------------
