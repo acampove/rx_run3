9491fd1e085b47b8dcbece84a2d660506d52b3be
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-02 21:06:36 +0800

    Clean up

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,26 +1,25 @@
-from acceptance_calculator import calculator
+'''
+Module meant to test the AcceptanceCalculator class
+'''
 
-import ROOT
+from ROOT import RDataFrame
+
+from rx_efficiencies.acceptance_calculator import AcceptanceCalculator
 
 #--------------------------
-def get_rdf():
-    #file_path = '/home/acampove/Test/acceptance/B2Kee_tree.root'
+def _get_rdf():
     file_path = '/home/acampove/Test/acceptance/bsphiee_tree.root'
-    #file_path = '/home/acampove/Test/acceptance/bdkskpiee_tree.root'
 
-    rdf = ROOT.RDataFrame('DecayTree', file_path)
+    rdf = RDataFrame('DecayTree', file_path)
 
     return rdf
 #--------------------------
 def test_simple():
-    rdf=get_rdf()
-    obj=calculator(rdf)
+    '''
+    Simplest test of geometric acceptance calculation
+    '''
+    rdf=_get_rdf()
+    obj=AcceptanceCalculator(rdf=rdf)
     obj.plot_dir     = 'tests/calculator/simple'
     acc_phy, acc_lhc = obj.get_acceptances()
 #--------------------------
-def main():
-    test_simple()
-#--------------------------
-if __name__ == '__main__':
-    main()
-
