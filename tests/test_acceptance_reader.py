815b33d570721c7edc8ace98f9fc97b92e6ff7e2
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-02 23:31:46 +0800

    Using pytest, modernizing, etc

--- tests/test_acceptance_reader.py
+++ tests/test_acceptance_reader.py
@@ -1,13 +1,29 @@
-from acceptance_reader import reader as acreader
+'''
+Modules used to test AcceptanceReader class
+'''
 
+import pytest
+from dmu.logging.log_store             import LogStore
+from rx_efficiencies.acceptance_reader import AcceptanceReader
+
+log=LogStore.add_logger('rx_efficiencies:test_acceptance_reader')
 #------------------------------------
-def test_simple():
-    obj=acreader(year='2011', proc='bsph')
+class Data:
+    '''
+    Data class
+    '''
+    l_process = ['bdkskpiee', 'bpk1kpipiee', 'bpk2kpipiee', 'bpkskpiee', 'bsphiee']
+#------------------------------------
+@pytest.mark.parametrize('year'   , ['2018', '2024'])
+@pytest.mark.parametrize('process', Data.l_process)
+def test_simple(year : str, process : str):
+    '''
+    Test reading acceptances from JSON files for multiple processes and years
+    '''
+    obj=AcceptanceReader(year=year, proc=process)
     acc=obj.read()
-#------------------------------------
-def main():
-    test_simple()
-#------------------------------------
-if __name__ == '__main__':
-    main()
 
+    log.info(f'{year:<20}{process:<20}{acc:10.3f}')
+
+    assert 0.1 < acc < 0.2
+#------------------------------------
