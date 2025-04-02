e7f607b7f83da4fd9765ad977dbc1d0e42a36e87
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-02 21:20:50 +0800

    Updating for Run3 inputs

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,24 +2,33 @@
 Module meant to test the AcceptanceCalculator class
 '''
 
-from ROOT import RDataFrame
+import os
 
+import pytest
+from ROOT import RDataFrame
 from rx_efficiencies.acceptance_calculator import AcceptanceCalculator
 
 #--------------------------
-def _get_rdf():
-    file_path = '/home/acampove/Test/acceptance/bsphiee_tree.root'
-
-    rdf = RDataFrame('DecayTree', file_path)
+class Data:
+    '''
+    Data class
+    '''
+    out_dir = '/tmp/tests/rx_efficiencies/acceptance_calculator'
+    rsm_dir = os.environ['RAPIDSIM_NTUPLES']
+#--------------------------
+def _get_rdf(sample : str) -> RDataFrame:
+    file_path = f'{Data.rsm_dir}/{sample}/{sample}_tree.root'
+    rdf       = RDataFrame('DecayTree', file_path)
 
     return rdf
 #--------------------------
-def test_simple():
+@pytest.mark.parametrize('sample', ['bdkskpiee'])
+def test_sample(sample : str):
     '''
     Simplest test of geometric acceptance calculation
     '''
-    rdf=_get_rdf()
+    rdf=_get_rdf(sample=sample)
     obj=AcceptanceCalculator(rdf=rdf)
-    obj.plot_dir     = 'tests/calculator/simple'
+    obj.plot_dir     = f'{Data.out_dir}/{sample}'
     acc_phy, acc_lhc = obj.get_acceptances()
 #--------------------------
