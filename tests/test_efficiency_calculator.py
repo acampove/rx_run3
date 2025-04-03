f390fcf7fbf334ece013572e288caa57c53c5cef
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-03 16:47:25 +0800

    Add code

---
+++ tests/test_efficiency_calculator.py
@@ -0,0 +1,18 @@
+'''
+Module with functions needed to test EfficiencyCalculator class
+'''
+
+from rx_efficiencies.efficiency_calculator import EfficiencyCalculator
+
+#-------------------------------------------------
+def test_simple():
+    '''
+    Tests calculation of total efficiency (acceptance x reco x selection)
+    for multiple MC samples
+    '''
+    obj         = EfficiencyCalculator(proc=None, year=2024)
+    obj.out_dir = '/tmp/tests/rx_efficiencies/efficiency_calculator/simple'
+    df          = obj.get_stats()
+
+    print(df)
+#-------------------------------------------------
