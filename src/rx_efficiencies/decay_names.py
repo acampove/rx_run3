8452505363874d9b82874e5e9f66f00dc915f8e3
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-03 14:06:05 +0800

    Adding code

---
+++ src/rx_efficiencies/decay_names.py
@@ -0,0 +1,17 @@
+'''
+Module used to define variables whose values are decay nicknames
+'''
+
+from dataclasses import dataclass
+
+@dataclass
+class DecayNames:
+    '''
+    Class used to hold names of decays for ease of use in the rest of the code
+    '''
+    bpkpee     = 'bpkpee'
+    bdkskpiee  = 'bdksee'
+    bpkskpiee  = 'bpksee'
+    bsphiee    = 'bpksee'
+    bpk1pipiee = 'bpk1kpipiee'
+    bpk2kpiee  = 'bpk2kpipiee'
