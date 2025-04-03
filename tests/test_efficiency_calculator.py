216270d1c7333d2b15f97b3a662dde8758fb3d6a
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-03 17:39:19 +0800

    Updated to run over q2bins

--- tests/test_efficiency_calculator.py
+++ tests/test_efficiency_calculator.py
@@ -1,17 +1,18 @@
 '''
 Module with functions needed to test EfficiencyCalculator class
 '''
-
+import pytest
 from rx_efficiencies.efficiency_calculator import EfficiencyCalculator
 
 #-------------------------------------------------
-def test_simple():
+@pytest.mark.parametrize('q2bin', ['low', 'central', 'high'])
+def test_simple(q2bin : str):
     '''
     Tests calculation of total efficiency (acceptance x reco x selection)
     for multiple MC samples
     '''
-    obj         = EfficiencyCalculator(proc=None, year='2024')
-    obj.out_dir = '/tmp/tests/rx_efficiencies/efficiency_calculator/simple'
+    obj         = EfficiencyCalculator(proc=None, year='2024', q2bin=q2bin)
+    obj.out_dir = '/tmp/tests/rx_efficiencies/efficiency_calculator/simple/{q2bin}'
     df          = obj.get_stats()
 
     print(df)
