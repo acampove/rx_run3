90bdc9622a51b66fecc73c664661d6b4355c98d4
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-03 14:08:54 +0800

    Fix names

--- src/rx_efficiencies/decay_names.py
+++ src/rx_efficiencies/decay_names.py
@@ -10,8 +10,8 @@ class DecayNames:
     Class used to hold names of decays for ease of use in the rest of the code
     '''
     bpkpee     = 'bpkpee'
-    bdkskpiee  = 'bdksee'
-    bpkskpiee  = 'bpksee'
-    bsphiee    = 'bpksee'
+    bdkskpiee  = 'bdkskpiee'
+    bpkskpiee  = 'bpkskpiee'
+    bsphiee    = 'bsphiee'
     bpk1pipiee = 'bpk1kpipiee'
     bpk2kpiee  = 'bpk2kpipiee'
