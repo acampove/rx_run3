2a02ab9dd59259d036ebddc3d9241b870e3d0211
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-03 17:09:29 +0800

    Year needs to be a string

--- tests/test_efficiency_calculator.py
+++ tests/test_efficiency_calculator.py
@@ -10,7 +10,7 @@ def test_simple():
     Tests calculation of total efficiency (acceptance x reco x selection)
     for multiple MC samples
     '''
-    obj         = EfficiencyCalculator(proc=None, year=2024)
+    obj         = EfficiencyCalculator(proc=None, year='2024')
     obj.out_dir = '/tmp/tests/rx_efficiencies/efficiency_calculator/simple'
     df          = obj.get_stats()
 
