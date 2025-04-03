0968bd6959c420c41032fdc2db501fdfe66e77ea
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-03 20:38:14 +0800

    Update name of class and arguments

--- tests/test_prec_scales.py
+++ tests/test_prec_scales.py
@@ -1,24 +1,27 @@
 '''
-Module used to test ModelScales class
+Module used to test PrecScales class
 '''
 
 import os
 import pytest
 import matplotlib.pyplot as plt
 
-from dmu.logging.log_store           import LogStore
-from rx_efficiencies.model_scales    import ModelScales
-from rx_efficiencies.model_scales    import get_proc_labels
+from dmu.logging.log_store          import LogStore
+from rx_efficiencies.decay_names    import DecayNames as dn
+from rx_efficiencies.prec_scales    import PrecScales
 
-
-log=LogStore.add_logger('rk_extractor:test_scales')
+log=LogStore.add_logger('rk_extractor:test_prec_scales')
+# -----------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    LogStore.set_level('rx_efficiencies:prec_scales', 10)
 #-------------------------------
 def _plot_df(df, trig):
     df = df[df.trig == trig]
     ax = None
-    d_proc_lab = get_proc_labels()
     for proc, df_p in df.groupby('kind'):
-        ax=df_p.plot(x='year', y='val', yerr='err', ax=ax, label=d_proc_lab[proc], linestyle='none', marker='o')
+        label = dn.tex_from_decay(proc)
+        ax=df_p.plot(x='year', y='val', yerr='err', ax=ax, label=label, linestyle='none', marker='o')
 
     os.makedirs('tests/scales/all_datasets', exist_ok=True)
 
@@ -32,15 +35,14 @@ def _plot_df(df, trig):
     plt.savefig(plt_path)
     plt.close('all')
 #-------------------------------
-@pytest.mark.parametrize('year'   , ['2024'])
-@pytest.mark.parametrize('trigger', ['Hlt2RD_BuToKpEE_MVA'])
-@pytest.mark.parametrize('process', ['bdkskpiee', 'bpk1kpipiee', 'bpk2kpipiee', 'bpkskpiee', 'bsphiee'])
-def test_all_datasets(year : str, trigger : str, process : str):
+@pytest.mark.parametrize('q2bin'  , ['low', 'central', 'high'])
+@pytest.mark.parametrize('process', dn.get_decays())
+def test_all_datasets(q2bin : str, process : str):
     '''
     Tests retrieval of scales between signal and PRec yields
     '''
-    obj      = ModelScales(dset=year, trig=trigger, kind=process)
-    val, err = obj.get_scale()
+    obj      = PrecScales(proc=process, q2bin=q2bin)
+    val, err = obj.get_scale(signal='bpkpee')
 
     assert val < 1   # Prec should be smaller than signal
     assert err < val # Error smaller than value
