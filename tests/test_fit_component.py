99e618db0a4cde861a046787d7811d6f01b36823
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-11 22:29:38 +0800

    Update location of tests

--- tests/test_fit_component.py
+++ tests/test_fit_component.py
@@ -36,7 +36,7 @@ def _initialize():
 def _get_conf(name : str) -> dict:
     return {
             'name'   : name,
-            'out_dir': f'/tmp/rx_calibration/tests/fit_component/{name}',
+            'out_dir': f'/tmp/tests/rx_calibration/fit_component/{name}',
             'fitting':
             {
                 'error_method'  : 'minuit_hesse',
