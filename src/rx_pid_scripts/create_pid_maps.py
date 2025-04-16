03fa08a956406c42e6cd0a2a60f270d608dcf165
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-16 16:10:10 +0800

    Remove electron lines
    Need time to figure out how to deal with these maps

--- src/rx_pid_scripts/create_pid_maps.py
+++ src/rx_pid_scripts/create_pid_maps.py
@@ -8,7 +8,6 @@ from importlib.resources      import files
 
 import yaml
 from pidcalib2.make_eff_hists import make_eff_hists
-from pidcalib2.ref_calib      import ref_calib 
 from dmu.logging.log_store    import LogStore
 
 log=LogStore.add_logger('rx_pid:create_pid_maps')
@@ -133,10 +132,7 @@ def main():
     if Data.dry_run:
         return
 
-    if Data.particle == 'e':
-        ref_calib(Data.conf)
-    else:
-        make_eff_hists(Data.conf)
+    make_eff_hists(Data.conf)
 # --------------------------------
 if __name__ == '__main__':
     main()
