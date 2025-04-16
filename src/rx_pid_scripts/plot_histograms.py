102407e90de99c525152a83a236f57b0a438100c
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-16 17:14:30 +0800

    Updated

--- src/rx_pid_scripts/plot_histograms.py
+++ src/rx_pid_scripts/plot_histograms.py
@@ -2,192 +2,119 @@
 Script used to plot histograms made by PIDCalib2
 '''
 import os
+import re
+import glob
 import pickle
 import argparse
+from typing import TypeAlias
 
-import numpy             as np
-import matplotlib
+import numpy
 import matplotlib.pyplot as plt
+import mplhep
+from dmu.logging.log_store import LogStore
 
-matplotlib.use('Agg')
+Npa : TypeAlias = numpy.ndarray
 
-#fancy = True
-fancy = False
-if fancy:
-    figsize= (12, 12)
-    fontsize = 20
-    #plt.style.use('/disk/lhcb_data/matzeni/AmplitudeFit-Bd2Kstll-DeltaWilsonCoeff/AmplitudeFit/work/scripts/matplotlib_LHCb.mplstyle')
-else:
-    figsize = (8, 8)
-    fontsize=15
+log=LogStore.add_logger('rx_pid:plot_histograms')
+# ---------------------------------
+class Data:
+    '''
+    Data class
+    '''
+    dir_path   : str
+    figsize    : tuple[int,int]
+    fontsize   : int
+    fancy      : bool = True
+    skip_values: bool = True
+    regex      : str  = r'effhists-2024_WithUT_block\d(?:_v1)?-(up|down)-([A-Z,a-z,0-9]+)-([A-Z,a-z,0-9,<,>,\.,&,\(,\)]+)-(\w+)\.(\w+)\.pkl'
+# ---------------------------------
+def _initialize() -> None:
+    plt.style.use(mplhep.style.LHCb2)
 
-def _plot_boosthistogram_1d(hist, label=None):
-    # Extract bin edges and values for plotting
-    bin_edges = hist.axes[0].edges
-    bin_values = hist.view()
+    if Data.fancy:
+        Data.figsize  = 12, 12
+        Data.fontsize = 20
+    else:
+        Data.figsize  = 8, 8
+        Data.fontsize = 15
+# ------------------------------------
+def _parse_args():
+    parser = argparse.ArgumentParser(description='Script used to plot histograms in pkl files created by PIDCalib2')
+    parser.add_argument('-d', '--dir_path', type=str, help='Path to directory with PKL files')
+    args   = parser.parse_args()
 
-    values = bin_values['value']
-    variances = bin_values['variance']
-    errors = np.sqrt(variances)
+    Data.dir_path = args.dir_path
+# ------------------------------------
+def _get_pkl_paths() -> list[str]:
+    path_wc = f'{Data.dir_path}/*.pkl'
+    l_path  = glob.glob(path_wc)
+    npath   = len(l_path)
 
-    c = (bin_edges[:-1] + bin_edges[1:])/2
-    w = (-bin_edges[:-1] + bin_edges[1:])/2
+    if npath == 0:
+        raise FileNotFoundError(f'No files found in {path_wc}')
 
-    # Plot the histogram
-    plt.errorbar(c, values, xerr=w, yerr=errors, capsize=4, fmt='o', label=label)
-
-def _plot_fit_histogram_1d(hist, label=None):
-    # Extract bin edges and values for plotting
-    values = hist['efficiency_value']
-    errors = hist['efficiency_error']
-
-    c = hist['bin_centers']
-    w = hist['bin_widths']
-
-    # Plot the histogram
-    plt.errorbar(c, values, xerr=w, yerr=errors, capsize=4, fmt='o', label=label)
-
-def plot_boosthistogram_1d(fn, figure_dict=None):
-
-    if figure_dict is None:
-        figure_dict = {"xlabel": "Value", 'ylabel': "Counts", "legend": False,
-                       "outputname":"test", "outputfold":'./plots', "label":None}
-
-    with open(fn, 'rb') as file:
-        hist = pickle.load(file)
-
-    plt.close('all')
-    _plot_boosthistogram_1d(hist, label=figure_dict['label'])
-
-    plt.xlabel(figure_dict['xlabel'])
-    plt.ylabel(figure_dict['ylabel'])
-    plt.ylim(0.2, 1.1)
-    outputfold = figure_dict['outputfold']
-    os.system(f"mkdir -p {outputfold}")
-    print(f"Saving figure in {outputfold}")
-    plt.savefig(f"{outputfold}/{figure_dict['outputname']}")
-
-def plot_list_of_boosthistogram_1d(fn_list, figure_dict=None, label_list=None):
-    if label_list is None:
-        label_list = [None for _ in fn_list]
-
-    if figure_dict is None:
-        figure_dict = {"xlabel": "Value", 'ylabel': "Counts", "legend": False, "title":None, "outputname":"test", "outputfold":'./plots', "label":None}
-    plt.close('all')
-    for fn, label in zip(fn_list, label_list):
-        with open(fn, 'rb') as file:
-            hist = pickle.load(file)
-
-        _plot_boosthistogram_1d(hist, label=label)
-    plt.title(figure_dict['title'])
-    plt.xlabel(figure_dict['xlabel'])
-    plt.ylabel(figure_dict['ylabel'])
-    if figure_dict['legend']:
-        plt.legend(frameon=False)
-    outputfold = figure_dict['outputfold']
-    os.system(f"mkdir -p {outputfold}")
-    print(f"Saving figure in {outputfold}")
-    plt.savefig(f"{outputfold}/{figure_dict['outputname']}")
-
-def plot_boosthistogram_2d(fn, figure_dict=None, size=2):
-
-    if figure_dict is None:
-        figure_dict = {"xlabel": "Value", 'ylabel': "Counts", "legend": False,"title":None,
-                       "outputname":"test", "outputfold":'./plots', "label":None}
-
-    with open(fn, 'rb') as file:
-        hist = pickle.load(file)
-    plt.close('all')
-    x_edges = hist.axes[0].edges
-    y_edges = hist.axes[1].edges
-    bin_values = hist.view()
-
-    counts  = bin_values['value']
-    variances = bin_values['variance']
-    errors = np.sqrt(variances)
-
-    # Step 4: Plot using matplotlib's pcolormesh
-    X, Y = np.meshgrid(x_edges, y_edges)
-    plt.pcolormesh(X, Y, counts.T, shading='auto', vmin=0.2, vmax=1.)  # Transpose counts to match the axes
-    plt.colorbar(label='Efficiency')
-
-    plt.xlabel(figure_dict['xlabel'])
-    plt.title(figure_dict['title'])
-    plt.ylabel(figure_dict['ylabel'])
-    outputfold = figure_dict['outputfold']
-    os.system(f"mkdir -p {outputfold}")
+    return l_path
+# ------------------------------------
+def _add_values(x_edges : Npa, y_edges : Npa, counts : Npa, errors : Npa) -> None:
+    if Data.skip_values:
+        return
 
     for i in range(len(x_edges)-1):
         for j in range(len(y_edges)-1):
             x_center = (x_edges[i] + x_edges[i+1]) / 2
             y_center = (y_edges[j] + y_edges[j+1]) / 2
-            plt.text(x_center, y_center, f'{counts[i][j]:.2f}\n$\pm$ {errors[i][j]:.2f}', ha='center', va='center', color='white', fontdict={'size': size})
-            plt.text(x_center, y_center*0.9, f'{i}-{j}', ha='center', va='center', color='orange', fontdict={'size': size})
+            val      = counts[i][j]
+            err      = errors[i][j]
+            text     = f'{val:.2f}\n$\pm${err:.2f}'
+            plt.text(x_center, y_center, text, ha='center', va='center', color='white', fontdict={'size': Data.fontsize})
+# ------------------------------------
+def _plot_hist(pkl_path : str) -> None:
+    log.info(f'Plotting histograms in: {pkl_path}')
+    with open(pkl_path, 'rb') as ifile:
+        hist = pickle.load(ifile)
 
-    print(f"Saving figure in {outputfold}")
-    plt.savefig(f"{outputfold}/{figure_dict['outputname']}")
+    x_edges    = hist.axes[0].edges
+    y_edges    = hist.axes[1].edges
+    bin_values = hist.view()
+    counts     = bin_values['value']
+    variances  = bin_values['variance']
+    errors     = numpy.sqrt(variances)
 
-def get_fancy_xlabel(var):
-    fancy_dict = {"P":r"$p(\mu)$ [MeV/c]",
-                  "muprobe_P":r"$p(\mu)$ [MeV/c]",
-                  "PT":r"$p_T(\mu)$ [MeV/c]",
-                  "ETA":r"$\eta(\mu)$",
-                  "PHI":r"$\phi(\mu)$",
-                  "nPVs":r"nPVs",
-                  "nFTClusters":r"nFTClusters",
-                  }
-    return fancy_dict[var]
+    arr_x, arr_y = numpy.meshgrid(x_edges, y_edges)
+    plt.pcolormesh(arr_x, arr_y, counts.T, shading='auto', vmin=0.0, vmax=1.)
+    plt.colorbar(label='Efficiency')
 
+    _add_values(x_edges, y_edges, counts, errors)
+    _add_info(pkl_path)
 
-def _parse_args():
-    parser = argparse.ArgumentParser(description="A simple program that demonstrates argparse usage")
-    parser.add_argument('--pid-cut', required=False, help='Path to the input file', dest="pid_cut", action='append')
-    parser.add_argument('--output-dir', required=False, help='Path to the input file', dest="input_dir")
-    parser.add_argument('--bin-var', required=False, help='Path to the input file', dest="bin_var", action='append')
-    parser.add_argument('--sample', required=False, help='Path to the input file', dest="year")
-    parser.add_argument('--samples-file', required=False, help='Path to the input file', dest="samples_file")
-    parser.add_argument('--magnet', required=False, help='Path to the input file', dest="magnet")
-    parser.add_argument('--particle', required=False, help='Path to the input file', dest="particle")
-    parser.add_argument('--binning-file', required=False, help='Path to the input file')
+    out_path = pkl_path.replace('.pkl', '.png')
+    plt.savefig(out_path)
+    plt.close()
+# ------------------------------------
+def _add_info(pkl_path : str) -> None:
+    file_name = os.path.basename(pkl_path)
+    mtch      = re.match(Data.regex, file_name)
+    if not mtch:
+        raise ValueError(f'Cannot extract information from {file_name}, using {Data.regex}')
 
-    args         = parser.parse_args()
-    pid_cut_list = args.pid_cut
-    input_dir    = args.input_dir
-    bin_var      = args.bin_var
-    year         = args.year
-    samples_file = args.samples_file
-    particle     = args.particle
-    magnet = args.magnet
-    binning_file = args.binning_file
+    [pol, par, cut, xlabel, ylabel] = mtch.groups()
 
+    title = f'{par}; {pol}; {cut}'
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
 # ------------------------------------
 def main():
     '''
     start here
     '''
-    output_dir = os.path.dirname(samples_file)
+    _parse_args()
+    _initialize()
 
-    if len(bin_var)<2:
-        bin_var = bin_var[0]
-        for pid_cut in pid_cut_list:
-            pid_cut = pid_cut.replace(" ","")
-            fn = f'{input_dir}/effhists-{year}-{magnet}-{particle}-{pid_cut}-{bin_var}.pkl'
-            figure_dict = {"xlabel": get_fancy_xlabel(bin_var), 'ylabel': "Efficiency", "legend": False,
-                        "outputname":f"effhists-{year}-{magnet}-{particle}-{pid_cut}-{bin_var}.pdf",
-                        "outputfold":f'{output_dir}/std/{particle}/', "label":None,"title": pid_cut}
-            plot_boosthistogram_1d(fn, figure_dict=figure_dict)
-    elif len(bin_var)==2:
-        joined_bin_var = ".".join(bin_var)
-        for pid_cut in pid_cut_list:
-            pid_cut = pid_cut.replace(" ","")
-            fn = f'{input_dir}/effhists-{year}-{magnet}-{particle}-{pid_cut}-{joined_bin_var}.pkl'
-            figure_dict = {"xlabel": get_fancy_xlabel(bin_var[0]),
-                            'ylabel': get_fancy_xlabel(bin_var[1]),
-                            "legend": False,
-                            "title": pid_cut,
-                        "outputname":f"effhists-{year}-{magnet}-{particle}-{pid_cut}-{joined_bin_var}.pdf",
-                        "outputfold":f'{output_dir}/std/{particle}/', "label":None}
-            plot_boosthistogram_2d(fn, figure_dict=figure_dict)
+    l_path = _get_pkl_paths()
+    for pkl_path in l_path:
+        _plot_hist(pkl_path)
 # ------------------------------------
 if __name__ == "__main__":
     main()
