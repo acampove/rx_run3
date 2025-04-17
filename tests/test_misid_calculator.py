80bfe806a6f1bac4ba3a0a58e7fcc55ece0e0496
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-17 22:20:48 +0800

    Loading config from YAML

--- tests/test_misid_calculator.py
+++ tests/test_misid_calculator.py
@@ -1,12 +1,20 @@
 '''
 Script meant to test MisIDCalculator class
 '''
+from importlib.resources import files
 
+import yaml
+from dmu.logging.log_store     import LogStore
 from rx_misid.misid_calculator import MisIDCalculator
 
+log=LogStore.add_logger('rx_misid:test_misid_calculator')
 # ---------------------------------
 def _get_config() -> dict:
-    return {}
+    config_path = files('rx_misid_data').joinpath('config.yaml')
+    with open(config_path, encoding='utf-8') as ifile:
+        data = yaml.safe_load(ifile)
+
+    return data
 # ---------------------------------
 def test_simple():
     '''
