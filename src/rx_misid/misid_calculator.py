e3e25c6f228761dd104b12e41bc36be04d1b09bc
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-18 20:38:40 +0800

    Update
    Arguments of splitter, split by hadron id
    config should be taken from class

--- src/rx_misid/misid_calculator.py
+++ src/rx_misid/misid_calculator.py
@@ -22,7 +22,7 @@ class MisIDCalculator:
         '''
         self._cfg = cfg
     # -----------------------------
-    def _get_sample(self, kind : str, is_bplus : bool) -> zdata:
+    def _get_sample(self, kind : str, is_bplus : bool, hadron_id : str) -> zdata:
         sample  = self._cfg['input']['samples'][kind]
         trigger = self._cfg['input']['trigger']
         d_cut   = self._cfg['input']['selection']
@@ -36,24 +36,25 @@ class MisIDCalculator:
         for cut_name, cut_expr in d_sel.items():
             rdf = rdf.Filter(cut_expr, cut_name)
 
-        splitter = SampleSplitter(rdf=rdf, is_bplus=is_bplus)
+        splitter = SampleSplitter(rdf=rdf, is_bplus=is_bplus, hadron_id=hadron_id, cfg=self._cfg['splitting'])
         d_sample = splitter.get_samples()
 
-        weighter = SampleWeighter(samples=d_sample, cfg=cfg['maps'])
-        data     = weighter.get_weighted_data()
+        weighter = SampleWeighter(samples=d_sample, cfg=self._cfg['maps'])
+        d_data   = weighter.get_weighted_data()
+        data     = sum(d_data.values())
 
         return data
     # -----------------------------
-    def _get_dataset(self, is_bplus : bool) -> zdata:
+    def _get_dataset(self, is_bplus : bool, hadron_id : str) -> zdata:
         '''
         Returns zfit dataset with weighted events
         representing contamination
 
         is_bplus : Sign of the B meson 
         '''
-        data_msid = self._get_sample(kind=  'data', is_bplus = is_bplus)
-        data_sign = self._get_sample(kind='signal', is_bplus = is_bplus)
-        data_leak = self._get_sample(kind=  'leak', is_bplus = is_bplus)
+        data_msid = self._get_sample(kind=  'data', is_bplus = is_bplus, hadron_id=hadron_id)
+        data_sign = self._get_sample(kind='signal', is_bplus = is_bplus, hadron_id=hadron_id)
+        data_leak = self._get_sample(kind=  'leak', is_bplus = is_bplus, hadron_id=hadron_id)
 
         return data_msid - data_sign - data_leak
     # -----------------------------
@@ -62,8 +63,14 @@ class MisIDCalculator:
         Returns zfit dataset with weighted events
         '''
 
-        bp_misid=self._get_dataset(is_bplus= True)
-        bm_misid=self._get_dataset(is_bplus=False)
+        bp_misid_kaon=self._get_dataset(is_bplus= True, hadron_id='kaon')
+        bm_misid_kaon=self._get_dataset(is_bplus=False, hadron_id='kaon')
 
-        return bp_misid + bm_misid
+        bp_misid_pion=self._get_dataset(is_bplus= True, hadron_id='pion')
+        bm_misid_pion=self._get_dataset(is_bplus=False, hadron_id='pion')
+
+        kaon_misid = bp_misid_kaon + bm_misid_kaon
+        pion_misid = bp_misid_pion + bm_misid_pion
+
+        return kaon_misid + pion_misid
 # -----------------------------
