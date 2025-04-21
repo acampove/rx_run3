cb76f67c888d4b603891ab93ab3bc4028f45b9c7
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-21 19:21:38 +0800

    Add initializer with logger

--- tests/test_misid_calculator.py
+++ tests/test_misid_calculator.py
@@ -4,10 +4,17 @@ Script meant to test MisIDCalculator class
 from importlib.resources import files
 
 import yaml
+import pytest
 from dmu.logging.log_store     import LogStore
 from rx_misid.misid_calculator import MisIDCalculator
 
 log=LogStore.add_logger('rx_misid:test_misid_calculator')
+# -------------------------------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    LogStore.set_level('rx_misid:misid_calculator', 10)
+    LogStore.set_level('rx_misid:splitter'        , 10)
+    LogStore.set_level('rx_misid:weighter'        , 10)
 # ---------------------------------
 def _get_config() -> dict:
     config_path = files('rx_misid_data').joinpath('config.yaml')
