82e4d167548bd96831eeb061872fce792544f173
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-21 18:48:57 +0800

    Updating

--- tests/test_sample_splitter.py
+++ tests/test_sample_splitter.py
@@ -24,6 +24,7 @@ def _initialize():
 # -------------------------------------------------------
 def _get_config() -> dict:
     cfg_path = files('rx_misid_data').joinpath('config.yaml')
+    log.info(f'Picking up config from: {cfg_path}')
     with open(cfg_path, encoding='utf-8') as ifile:
         cfg = yaml.safe_load(ifile)
 
