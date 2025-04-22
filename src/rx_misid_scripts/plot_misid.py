e44aedd88227d2731dd1ab99466b1b4e18b443ff
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-22 17:39:10 +0800

    Basic plotting in place

--- src/rx_misid_scripts/plot_misid.py
+++ src/rx_misid_scripts/plot_misid.py
@@ -4,7 +4,9 @@ used to study fully hadronic mis-ID backgrounds
 '''
 import argparse
 
-import pandas as pnd
+import mplhep
+import pandas            as pnd
+import matplotlib.pyplot as plt
 from dmu.logging.log_store import LogStore
 
 log=LogStore.add_logger('rx_misid:plot_misid')
@@ -14,6 +16,10 @@ class Data:
     Data class
     '''
     file_path : str
+    mass = 'B_M_brem_track_2'
+    plt.style.use(mplhep.style.LHCb2)
+
+    binning = {'bins' : 30, 'range' : (4500, 7000)}
 # ---------------------------------------
 def _parse_args():
     parser = argparse.ArgumentParser(description='Script meant to make plots for the samples used to study fully hadronic misID')
@@ -22,8 +28,22 @@ def _parse_args():
 
     Data.file_path = args.path
 # ---------------------------------------
+def _plot_kind(kind : str, df : pnd.DataFrame) -> None:
+    ax = None
+    ax = df.plot.hist(y=Data.mass, **Data.binning, alpha=0.3      , color='blue', label='Unweighed', ax=ax)
+    ax = df.plot.hist(y=Data.mass, **Data.binning, histtype='step', color='red' , label='Weighted', ax=ax, weights=df['weights'])
+
+    path = Data.file_path.replace('.parquet', f'_{kind}.png')
+
+    plt.title(kind)
+    plt.savefig(path)
+    plt.close()
+# ---------------------------------------
 def _plot(df : pnd.DataFrame) -> None:
-    print(df)
+    for kind, df_kind in df.groupby('kind'):
+        _plot_kind(kind=kind, df=df_kind)
+
+    _plot_kind(kind='PF+FP+FF', df=df)
 # ---------------------------------------
 def main():
     '''
