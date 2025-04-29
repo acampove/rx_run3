a53fdf18da5c3b1995066a04171ff46e077eab58
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-29 21:50:15 +0800

    Updating

--- tests/test_misid_pdf.py
+++ tests/test_misid_pdf.py
@@ -55,7 +55,7 @@ def test_pdf(q2bin : str):
 
     obj = MisIdPdf(obs=Data.obs, q2bin=q2bin)
     pdf = obj.get_pdf()
-    dat = obj.get_data()
+    dat = obj.get_data(kind='zfit')
 
     _plot_pdf(pdf, dat, name='simple')
 # ----------------------------
