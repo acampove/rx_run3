c65a9e7c029bef9ffa82eaa66a378fb3e2b4e2df
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-30 17:43:44 +0800

    Update to loop over every sample

--- tests/test_mc_scaler.py
+++ tests/test_mc_scaler.py
@@ -1,20 +1,31 @@
 '''
 File with functions to test MCScaler
 '''
+import pytest
 
-from rx_misid.mc_scaler import MCScaler
+from dmu.logging.log_store  import LogStore
+from rx_misid.mc_scaler     import MCScaler
 
+log=LogStore.add_logger('rx_misid:test_ms_scaler')
 # -----------------------------------------------
-def test_simple():
+class Data:
     '''
-    Simplest test
+    data class
     '''
-    q2bin = 'central'
-    sample= 'Bu_Kee_eq_btosllball05_DPC'
+    sig_reg = '(L1_PID_E > 3) && (L2_PID_E > 3)'
+# -----------------------------------------------
+@pytest.mark.parametrize('q2bin' , ['low', 'central', 'high'])
+@pytest.mark.parametrize('sample', ['Bu_Kee_eq_btosllball05_DPC', 'Bu_JpsiK_ee_eq_DPC'])
+def test_simple(q2bin : str, sample : str):
+    '''
+    Test over all samples and q2 bins
+    '''
+    scl = MCScaler(
+            q2bin  =q2bin,
+            sample =sample,
+            sig_reg=Data.sig_reg)
 
-    scl = MCScaler(q2bin=q2bin, sample=sample)
     val = scl.get_scale()
 
-    print(val)
+    log.info(f'{sample:<20}{q2bin:<20}{val:.3f}')
 # -----------------------------------------------
-
