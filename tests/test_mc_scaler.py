1321614dae14be096e29bc3ff092a6d2e0177f69
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-30 18:52:34 +0800

    get_signal_cut made into static method of MisIdPdf

--- tests/test_mc_scaler.py
+++ tests/test_mc_scaler.py
@@ -1,13 +1,12 @@
 '''
 File with functions to test MCScaler
 '''
-from importlib.resources import files
 import pytest
-import yaml
 
 from dmu.logging.log_store  import LogStore
 from conftest               import DataCollector
 from rx_misid.mc_scaler     import MCScaler
+from rx_misid.misid_pdf     import MisIdPdf
 
 log=LogStore.add_logger('rx_misid:test_ms_scaler')
 # -----------------------------------------------
@@ -17,27 +16,13 @@ class Data:
     '''
     version = 'v1' # This should be the version of the config actually used in the analysis
 # -----------------------------------------------
-def _get_signal_cut() -> str:
-    config_path = files('rx_misid_data').joinpath(f'misid_{Data.version}.yaml')
-    with open(config_path, encoding='utf-8') as ifile:
-        cfg = yaml.safe_load(ifile)
-
-    cut = cfg['splitting']['lepton_tagging']['pass']
-
-    log.info(f'Using signal cut: {cut}')
-
-    cut_l1 = cut.replace('LEP', 'L1')
-    cut_l2 = cut.replace('LEP', 'L2')
-
-    return f'({cut_l1}) && ({cut_l2})'
-# -----------------------------------------------
 @pytest.mark.parametrize('q2bin' , ['low', 'central', 'high'])
 @pytest.mark.parametrize('sample', ['Bu_Kee_eq_btosllball05_DPC', 'Bu_JpsiK_ee_eq_DPC'])
 def test_simple(q2bin : str, sample : str):
     '''
     Test over all samples and q2 bins
     '''
-    sig_reg = _get_signal_cut()
+    sig_reg = MisIdPdf.get_signal_cut(Data.version)
 
     scl = MCScaler(
             q2bin  =q2bin,
