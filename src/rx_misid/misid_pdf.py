f30894f3c1050c65964e9fded5fad7710a42751d
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-30 16:01:23 +0800

    Scales taken from function
    Cleaner this way

--- src/rx_misid/misid_pdf.py
+++ src/rx_misid/misid_pdf.py
@@ -33,8 +33,11 @@ class MisIdPdf:
 
         self._bandwidth     = 80
         self._nan_threshold = 0.02
-        self._d_scale       = {'signal' : 0.5, 'leakage' : 1.0, 'data' : 1.0}
         self._d_padding     = {'lowermirror' : 1.0, 'uppermirror' : 1.0}
+        self._d_scale       = self._get_scales()
+    # ----------------------------------------
+    def _get_scales(self) -> dict[str,float]:
+        return {'signal' : 0.5, 'leakage' : 1.0, 'data' : 1.0}
     # ----------------------------------------
     def _preprocess_df(self, df : pnd.DataFrame, sample : str) -> pnd.DataFrame:
         log.debug(f'Preprocessing {sample}')
