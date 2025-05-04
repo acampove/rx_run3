d67cc82fbc1a2d69608405348d5c3744149835a0
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-04 22:49:35 +0800

    Use _get_parameter

--- src/dmu/stats/model_factory.py
+++ src/dmu/stats/model_factory.py
@@ -318,9 +318,9 @@ class ModelFactory:
     #-----------------------------------------
     @MethodRegistry.register('voigt')
     def _get_voigt(self, suffix : str = '') -> zpdf:
-        mu  = zfit.Parameter('mu_voigt', 5280,  5040, 5500)
-        sg  = zfit.Parameter('sg_voigt',   20,    10,  400)
-        gm  = zfit.Parameter('gm_voigt',    4,   0.1,  100)
+        mu  = self._get_parameter('mu_voigt', suffix, 5280,  5040, 5500)
+        sg  = self._get_parameter('sg_voigt', suffix,   20,    10,  400)
+        gm  = self._get_parameter('gm_voigt', suffix,    4,   0.1,  100)
 
         pdf = zfit.pdf.Voigt(m=mu, sigma=sg, gamma=gm, obs=self._obs, name=f'voigt{suffix}')
 
@@ -328,9 +328,9 @@ class ModelFactory:
     #-----------------------------------------
     @MethodRegistry.register('qgauss')
     def _get_qgauss(self, suffix : str = '') -> zpdf:
-        mu  = zfit.Parameter('mu_qgauss', 5280,  5040, 5500)
-        sg  = zfit.Parameter('sg_qgauss',   20,    10,  400)
-        q   = zfit.Parameter( 'q_qgauss',    1,     1,    3)
+        mu  = self._get_parameter('mu_qgauss', suffix, 5280,  5040, 5500)
+        sg  = self._get_parameter('sg_qgauss', suffix,   20,    10,  400)
+        q   = self._get_parameter( 'q_qgauss', suffix,    1,     1,    3)
 
         pdf = zfit.pdf.QGauss(q=q, mu=mu, sigma=sg, obs=self._obs, name =f'qgauss{suffix}')
 
@@ -338,8 +338,8 @@ class ModelFactory:
     #-----------------------------------------
     @MethodRegistry.register('cauchy')
     def _get_cauchy(self, suffix : str = '') -> zpdf:
-        mu  = zfit.Parameter('mu', 5280,  5040, 5500)
-        gm  = zfit.Parameter('gm',  150,    50,  500)
+        mu  = self._get_parameter('mu', suffix, 5280,  5040, 5500)
+        gm  = self._get_parameter('gm', suffix,  150,    50,  500)
 
         pdf = zfit.pdf.Cauchy(obs=self._obs, m=mu, gamma=gm, name=f'cauchy{suffix}')
 
