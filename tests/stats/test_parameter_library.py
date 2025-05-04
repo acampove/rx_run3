43d6e76c24c713d15370c4863d12fd0ddf48f8e4
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-04 23:38:15 +0800

    Testing the printing over multiple PDFs

--- tests/stats/test_parameter_library.py
+++ tests/stats/test_parameter_library.py
@@ -2,10 +2,14 @@
 Module with tests for ParameterLibrary class
 '''
 
+import pytest
 from dmu.stats.parameters import ParameterLibrary as PL
 
-def test_print():
+# ------------------------------------
+@pytest.mark.parametrize('kind', ['gauss', 'cbl', 'cbr', 'suj'])
+def test_print(kind : str):
     '''
     Will test printing parameters
     '''
-    PL.print_parameters()
+    PL.print_parameters(kind=kind)
+# ------------------------------------
