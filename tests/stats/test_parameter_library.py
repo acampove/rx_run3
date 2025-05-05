ead8a11bb2f947d9eed35376b2af6fb3234c6f2a
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-05 15:01:00 +0800

    Test set_values

--- tests/stats/test_parameter_library.py
+++ tests/stats/test_parameter_library.py
@@ -26,3 +26,20 @@ def test_get_values(parameter : str):
 
     log.info(f'{parameter:<20}{x:<10.0f}{y:<10.0f}{z:<10.0f}')
 # ------------------------------------
+@pytest.mark.parametrize('parameter', ['mu', 'sg', 'ac', 'nc'])
+def test_set_values(parameter : str):
+    '''
+    Will test retrieving values for different parameters of cbl
+    '''
+    kind = 'cbl'
+    x_in = 1
+    y_in = 2
+    z_in = 3
+
+    PL.set_values(kind=kind, parameter=parameter, val=x_in, low=y_in, high=z_in)
+    x_out, y_out, z_out = PL.get_values(kind=kind, parameter=parameter)
+
+    assert x_in == x_out
+    assert y_in == y_out
+    assert z_in == z_out
+# ------------------------------------
