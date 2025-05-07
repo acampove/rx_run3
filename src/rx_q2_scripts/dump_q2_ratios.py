54ac2cee7e3f8b9cfa95c184f326f202be405a83
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-07 22:35:28 +0800

    Updated
    Simpler

--- src/rx_q2_scripts/dump_q2_ratios.py
+++ src/rx_q2_scripts/dump_q2_ratios.py
@@ -1,153 +1,152 @@
-#!/usr/bin/env python3
-
-import utils_noroot as utnr
-import argparse
-import ROOT
-import numpy
-import math
+'''
+Script used to transform JSON files with fitting parameters to create a dataframe
+with the widths and means of the Jpsi peak, fitted from data and MC samples
+'''
 import os
+import re
+import glob
+import math
+import argparse
+from typing import Callable
 
-from rk.boundaries import boundaries
+import numpy
+import jacobi
+import pandas                as pnd
+import dmu.generic.utilities as gut
+from dmu.logging.log_store import LogStore
 
+log=LogStore.add_logger('rx_q2:dump_q2_ratios')
 #-------------------------------------
-class data:
-    out_dir = None
-    inp_dir = None
-    version = None 
-    l_dset  = None
-    l_trig  = None
-    l_brem  = None
+class Data:
+    '''
+    Data class
+    '''
+    regex   = r'Hlt2RD_BuToKpEE_MVA_2024_(\d)_(\d)_nom'
+    inp_dir : str
+    version : str
     l_par   = ['rsg', 'dmu', 'mMC', 'mDT']
-
-    l_all_brem = [ 0, 1, 2 ]
-    l_all_dset = [ 'r1', 'r2p1', '2017', '2018' ]
-    l_all_trig = [ 'ETOS', 'GTIS' ]
-
-
-    log = utnr.getLogger(__name__)
-#----------------------------
-def par_to_res(par, d_par):
-    name  = {'rsg' : 'sg', 'dmu' : 'mu', 'mMC' : 'mu', 'mDT' : 'mu'}[par]
-    d_res = {}
-    for sbin, d_fit_par in d_par.items():
-        if 'inf' in sbin: 
-            continue
-
-        if d_fit_par == {}:
-            val = numpy.nan
-        else:
-            [val, _] = d_fit_par[name]
-
-        d_res[sbin] = val 
-
-    return d_res
 #-------------------------------------
-def divide(num, den):
-    bad_num = isinstance(num, dict) or math.isnan(num)
-    bad_den = isinstance(den, dict) or math.isnan(den)
-
-    if bad_num and bad_den:
-        return numpy.nan
-    elif not bad_num and not bad_den:
-        return num/den
-    else:
-        raise ValueError
-#-------------------------------------
-def calculate_par(par, d_dt_str, d_mc_str):
-    d_rat_str = {}
-    for key in d_dt_str.keys():
-        val_dt = d_dt_str[key]
-        val_mc = d_mc_str[key]
-
-        if   par == 'mMC':
-            d_rat_str[key] = val_mc
-        elif par == 'mDT':
-            d_rat_str[key] = val_dt
-        elif par == 'dmu':
-            d_rat_str[key] = val_dt - val_mc
-        elif par == 'rsg':
-            d_rat_str[key] = divide(val_dt, val_mc)
-        else:
-            data.log.error(f'Invalid parameter: {par}')
-            raise ValueError
-
-    return d_rat_str
-#-------------------------------------
-def get_par(par, brem, trig, dset):
-    mc_path  = f'{data.inp_dir}/get_q2_resolutions/mc/json/{data.version}/{trig}_{dset}/par_brem_{brem}.json'
-    dt_path  = f'{data.inp_dir}/get_q2_resolutions/data/json/{data.version}/{trig}_{dset}/par_brem_{brem}.json'
-
-    d_mc_str = utnr.load_json(mc_path)
-    d_dt_str = utnr.load_json(dt_path)
-
-    d_mc_str = par_to_res(par, d_mc_str)
-    d_dt_str = par_to_res(par, d_dt_str)
-
-    d_par_str= calculate_par(par, d_dt_str, d_mc_str)
-
-    return d_par_str
-#-------------------------------------
-def get_args():
-    parser = argparse.ArgumentParser(description='Used to create ratio histograms and JSON files of data and MC resolutions')
-    parser.add_argument('-v', '--vers', type=str , help='Version'        , required=True) 
-    parser.add_argument('-b', '--brem', nargs='+', help='Brem category'  , default=data.l_all_brem) 
-    parser.add_argument('-d', '--dset', nargs='+', help='Dataset'        , default=data.l_all_dset) 
-    parser.add_argument('-t', '--trig', nargs='+', help='Trigger'        , default=data.l_all_trig) 
+def _parse_args():
+    parser = argparse.ArgumentParser(description='Used to create pandas dataframe with information from fits needed to smear q2')
+    parser.add_argument('-v', '--vers', type=str, help='Version'        , required=True)
     args = parser.parse_args()
 
-    data.version = args.vers
-    data.l_brem  = args.brem
-    data.l_dset  = args.dset
-    data.l_trig  = args.trig
-
-    data.out_dir = utnr.make_dir_path(f'{os.environ["QSQSYS"]}/get_q2_resolutions/ratio/{data.version}')
-    data.inp_dir = os.environ['QSQSYS']
+    Data.version = args.vers
 #-------------------------------------
-def convert_to_hist(d_rat_str):
-    s_bound = set()
-    for sbound, _ in d_rat_str.items():
-        bnd = boundaries(sbound)
-        (p1, p2), _ = bnd.data
-        s_bound.add(p1)
-        s_bound.add(p2)
+def _initialize():
+    ana_dir      = os.environ['ANADIR']
+    Data.inp_dir = f'{ana_dir}/q2/fits/{Data.version}'
+#-------------------------------------
+def _row_from_path(path : str) -> list[float]:
+    data = gut.load_json(path)
 
-    l_bound = list(s_bound)
-    nbin    = len(l_bound) - 1
-    l_bound.sort()
-    arr_bound = numpy.array(l_bound)
+    [[mu_val, mu_err]] = [ val for name, val in data.items() if name.startswith('mu_')]
+    [[sg_val, sg_err]] = [ val for name, val in data.items() if name.startswith('sg_')]
 
-    hist = ROOT.TH2F('hist', '', nbin, arr_bound, nbin, arr_bound)
+    brem, block = _brem_block_from_path(path=path)
 
-    for sbound, ratio in d_rat_str.items():
-        bnd = boundaries(sbound)
-        (p1, p2), (q1, q2) = bnd.data
+    return [mu_val, mu_err, sg_val, sg_err, brem, block]
+#-------------------------------------
+def _brem_block_from_path(path : str) -> tuple[str,str]:
+    dir_name = os.path.dirname(path)
+    sample   = os.path.basename(dir_name)
 
-        pval = 0.5 * (p1 + p2)
-        qval = 0.5 * (q1 + q2)
+    mtch     = re.match(Data.regex, sample)
+    if not mtch:
+        raise ValueError(f'Cannot extract information from {sample} using {Data.regex}')
 
-        hist.Fill(pval, qval, ratio)
+    [brem, block] = mtch.groups()
 
-    return hist
+    return brem, block
+#-------------------------------------
+def _get_df(sample : str) -> pnd.DataFrame:
+    path_wc = f'{Data.inp_dir}/{sample}/*/parameters.json'
+    l_path  = glob.glob(path_wc)
+    nfiles  = len(l_path)
+
+    df = pnd.DataFrame(columns=['mu_val', 'mu_err', 'sg_val', 'sg_err', 'brem', 'block'])
+    if nfiles == 0:
+        raise ValueError(f'Cannot find any parameters file in: {path_wc}')
+
+
+    log.info(f'Found {nfiles} parameters files')
+    for path in l_path:
+        df.loc[len(df)] = _row_from_path(path=path)
+
+    return df
+#-------------------------------------
+def _process_dataframe(df : pnd.DataFrame) -> None:
+    l_df_scale = []
+    for (block, brem), df_group in df.groupby(['block', 'brem']):
+        df_scale          = _scales_from_df(df=df_group)
+        df_scale['block'] = block
+        df_scale['brem' ] = brem
+
+        l_df_scale.append(df_scale)
+
+    df = pnd.concat(l_df_scale, ignore_index=True)
+
+    print(df)
+#-------------------------------------
+def _scales_from_df(df : pnd.DataFrame) -> pnd.DataFrame:
+    d_mu = _get_scale(df=df, name='mu', fun=lambda x : x[0] - x[1])
+    d_sg = _get_scale(df=df, name='sg', fun=lambda x : x[0] / x[1])
+    df   = pnd.DataFrame({**d_mu, **d_sg})
+
+    return df
+#-------------------------------------
+def _get_scale(df : pnd.DataFrame, name : str, fun : Callable) -> dict[str:float]:
+    df_dat  = df[df['sample'] == 'dat']
+    df_sim  = df[df['sample'] == 'sim']
+
+    dat_val = float(df_dat[f'{name}_val'])
+    dat_err = float(df_dat[f'{name}_err'])
+
+    sim_val = float(df_sim[f'{name}_val'])
+    sim_err = 0# float(df_sim[f'{name}_err'])
+
+    cov     = [
+            [dat_err ** 2,            0],
+            [0           , sim_err ** 2]]
+
+    val, var = jacobi.propagate(fun, [dat_val, sim_val], cov)
+    val      = float(val)
+    err      = math.sqrt(var)
+
+    if name == 'sg' and err > 100:
+        log.warning('-----------------')
+        log.info(f'Error: {err:.0f}')
+        print(df)
+        log.warning('-----------------')
+
+    return {f's{name}_val' : [val], f's{name}_err' : [err]}
 #-------------------------------------
 def main():
-    get_args()
-    for trig in data.l_trig:
-        for dset in data.l_dset:
-            map_path = f'{data.out_dir}/{dset}_{trig}.root'
-            data.log.visible(f'Saving to: {map_path}')
-            ofile = ROOT.TFile(map_path, 'recreate')
-            for par in data.l_par:
-                for brem in data.l_brem:
-                    d_rat_str = get_par(par, brem, trig, dset)
-                    par_path = f'{data.out_dir}/{trig}_{dset}/{par}_brem_{brem}.json'
-                    data.log.visible(f'Saving to: {par_path}')
-                    utnr.dump_json(d_rat_str, par_path)
+    '''
+    Starts here
+    '''
+    _parse_args()
+    _initialize()
+    out_path = f'{Data.inp_dir}/parameters.json'
+    if os.path.isfile(out_path):
+        log.warning(f'Dataframe already found, reusing: {out_path}')
+        df = pnd.read_json(out_path)
+        _process_dataframe(df)
 
-                    h_rat = convert_to_hist(d_rat_str)
-                    h_rat.SetName(f'h_{par}_brem_{brem}')
-                    h_rat.Write()
-            ofile.Close()
+        return
+
+    log.info('Dataframe already not found, making it')
+    l_df = []
+    for sample in ['dat', 'sim']:
+        df           = _get_df(sample=sample)
+        df['sample'] = sample
+        l_df.append(df)
+
+    df = pnd.concat(l_df, axis=0, ignore_index=True)
+    df.fillna(0, inplace=True)
+
+    df.to_json(out_path, indent=2)
+    _process_dataframe(df)
 #-------------------------------------
 if __name__ == '__main__':
     main()
-
