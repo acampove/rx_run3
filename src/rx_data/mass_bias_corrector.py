3327e10c1d69968474941d1a5f6d2364a5fc3daa
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-09 18:49:11 +0800

    Swapping imports

--- src/rx_data/mass_bias_corrector.py
+++ src/rx_data/mass_bias_corrector.py
@@ -11,8 +11,8 @@ from ROOT                            import RDataFrame, RDF
 from dmu.logging.log_store           import LogStore
 
 import rx_data.utilities             as ut
-from rx_data.electron_bias_corrector import ElectronBiasCorrector
 from rx_q2.q2smear_corrector         import Q2SmearCorrector
+from rx_data.electron_bias_corrector import ElectronBiasCorrector
 
 log=LogStore.add_logger('rx_data:mass_bias_corrector')
 # ------------------------------------------
