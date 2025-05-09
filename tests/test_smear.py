5204fdf27f53e1d8a79a98ec51a9937367eb3efe
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-10 00:56:45 +0800

    Updated with better plots and true_mass

--- tests/test_smear.py
+++ tests/test_smear.py
@@ -31,21 +31,23 @@ def _plot_masses(df : pnd.DataFrame, name : str) -> None:
     log.info('Plotting')
 
     ax=None
-    ax=df.plot.hist(y='mass'    , range=[2000, 3500], bins=60, histtype='step', ax=ax)
-    ax=df.plot.hist(y='mass_smr', range=[2000, 3500], bins=60, histtype='step', ax=ax)
+    ax=df.plot.hist(y='reco_mass', range=[2000, 3500], bins=60, alpha=0.3      , ax=ax)
+    ax=df.plot.hist(y='mass_smr' , range=[2000, 3500], bins=60, histtype='step', ax=ax)
 
     plt.savefig(f'{Data.out_dir}/{name}.png')
     plt.close()
 # -------------------------------------------
 def _get_df(uniform : bool = True) -> pnd.DataFrame:
-    df             = pnd.DataFrame(columns=['nbrem', 'block', 'mass'])
+    df             = pnd.DataFrame()
     df['nbrem']    = numpy.random.choice([0, 1, 2], Data.nentries)
     df['block']    = numpy.random.choice(range(9) , Data.nentries)
 
     if uniform:
-        df['mass'] = numpy.random.uniform(1800, 3700, Data.nentries)
+        df['true_mass'] = numpy.random.uniform(1800, 3700, Data.nentries)
+        df['reco_mass'] = numpy.random.uniform(1800, 3700, Data.nentries)
     else:
-        df['mass'] = numpy.random.normal(loc=3000, scale=100, size=Data.nentries)
+        df['true_mass'] = numpy.random.normal(loc=3000, scale=100, size=Data.nentries)
+        df['reco_mass'] = numpy.random.normal(loc=3000, scale=100, size=Data.nentries)
 
     return df
 # -------------------------------------------
@@ -59,8 +61,8 @@ def test_simple(is_uniform : bool):
 
     l_val = []
     itr   = df.itertuples(index=False)
-    for nbrem, block, mass in tqdm.tqdm(itr, total=len(df), ascii=' -'):
-        val    = obj.get_mass(nbrem=nbrem, block=block, jpsi_mass_reco=mass)
+    for nbrem, block, reco_mass, true_mass in tqdm.tqdm(itr, total=len(df), ascii=' -'):
+        val    = obj.get_mass(nbrem=nbrem, block=block, jpsi_mass_reco=reco_mass, jpsi_mass_true=true_mass)
         l_val += [val]
 
     df['mass_smr'] = l_val
