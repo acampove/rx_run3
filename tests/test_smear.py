fb96f047c7ece7ecfaa76871bfab4851e3f5f04b
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-09 18:11:42 +0800

    Updating

--- tests/test_smear.py
+++ tests/test_smear.py
@@ -17,7 +17,6 @@ class Data:
     Data class
     '''
     nentries = 10_000
-    columns  = ['Jpsi_M_brem_track_2', 'nbrem', 'block', 'L1_TRUEID']
     out_dir  = '/tmp/tests/rx_q2/smear'
 
     os.makedirs(out_dir, exist_ok=True)
@@ -30,41 +29,28 @@ def _plot_masses(rdf : RDataFrame, name : str) -> None:
     plt.savefig(f'{Data.out_dir}/{name}.png')
     plt.close()
 # -------------------------------------------
-def _get_rdf(lepton_id : int = 11, uniform : bool = True) -> RDataFrame:
-    df                        = pnd.DataFrame(columns=Data.columns)
-    df['nbrem']               = numpy.random.choice([0, 1, 2], Data.nentries)
-    df['block']               = numpy.random.choice(range(9) , Data.nentries)
-    df['L1_TRUEID']           = lepton_id
-    df['EVENTNUMBER']         = 1
-    df['RUNNUMBER']           = 1
+def _get_df(uniform : bool = True) -> pnd.DataFrame:
+    df             = pnd.DataFrame(columns=['nbrem', 'block', 'mass'])
+    df['nbrem']    = numpy.random.choice([0, 1, 2], Data.nentries)
+    df['block']    = numpy.random.choice(range(9) , Data.nentries)
 
     if uniform:
-        df['Jpsi_M_brem_track_2'] = numpy.random.uniform(2000, 3500, Data.nentries)
+        df['mass'] = numpy.random.uniform(2000, 3500, Data.nentries)
     else:
-        df['Jpsi_M_brem_track_2'] = numpy.random.normal(loc=3000, scale=40, size=Data.nentries)
+        df['mass'] = numpy.random.normal(loc=3000, scale=40, size=Data.nentries)
 
-    rdf = RDF.FromPandas(df)
-
-    return rdf
-# -------------------------------------------
-def test_wrong_input():
-    '''
-    Checks if the input is wrong
-    '''
-    rdf = _get_rdf(lepton_id = 13)
-
-    with pytest.raises(WrongQ2SmearInput):
-        obj = Q2SmearCorrector(rdf=rdf)
-        rdf = obj.get_rdf()
+    return df
 # -------------------------------------------
 @pytest.mark.parametrize('is_uniform', [True, False])
 def test_simple(is_uniform : bool):
     '''
     Checks if the input is wrong
     '''
-    rdf = _get_rdf(lepton_id = 11, uniform = is_uniform)
-    obj = Q2SmearCorrector(rdf=rdf)
-    rdf = obj.get_rdf()
+    obj = Q2SmearCorrector()
+    df  = _get_df(uniform = is_uniform)
 
-    _plot_masses(rdf, name = f'simple_{is_uniform}')
+    for nbrem, block, mass in df.itertuples(index=False):
+        val = obj.get_mass(nbrem=nbrem, block=block, jpsi_mass_reco=mass)
+
+    #_plot_masses(rdf, name = f'simple_{is_uniform}')
 # -------------------------------------------
