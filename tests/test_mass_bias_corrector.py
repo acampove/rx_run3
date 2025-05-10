ab2064609d75db816fd61b8bccab4071432b028c
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-10 17:42:31 +0800

    Test requires 18 variables, added DIRA

--- tests/test_mass_bias_corrector.py
+++ tests/test_mass_bias_corrector.py
@@ -86,7 +86,7 @@ def _check_input_columns(rdf : RDataFrame) -> None:
 def _check_output_columns(rdf : RDataFrame) -> None:
     l_colname = [ name.c_str() for name in rdf.GetColumnNames() ]
     ncol = len(l_colname)
-    if ncol != 16:
+    if ncol != 18:
         for colname in l_colname:
             log.info(f'   {colname}')
 
