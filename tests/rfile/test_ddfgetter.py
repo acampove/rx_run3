00f6f36c87b64797d5c4a7d1ad05dcff3c4b1c71
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-18 19:03:31 +0800

    Improve names

--- tests/rfile/test_ddfgetter.py
+++ tests/rfile/test_ddfgetter.py
@@ -4,7 +4,7 @@ Script used to test the DDataFrameGetter class
 
 from importlib.resources   import files
 
-#from dmu.rfile.ddfgetter   import DDataFrameGetter
+from dmu.rfile.ddfgetter   import DDFGetter
 from dmu.testing.utilities import build_friend_structure
 
 # ------------------------------
@@ -16,6 +16,8 @@ def test_simple():
     build_friend_structure(file_name=file_name)
     cfg_path  = files('dmu_data').joinpath(f'rfile/{file_name}')
 
-    ddfg = DDataFrameGetter(config_path=cfg_path)
+    ddfg = DDFGetter(config_path=cfg_path)
     ddf  = ddfg.get_dataframe()
+
+    print(ddf)
 # ------------------------------
