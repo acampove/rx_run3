dfbc2a9e3f14f3b9dd546650ed1e427436569c65
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-19 14:42:51 +0800

    Add comments and make the call to loader a delayed task

--- src/dmu/rfile/ddfgetter.py
+++ src/dmu/rfile/ddfgetter.py
@@ -67,16 +67,19 @@ class DDFGetter:
         return df
     # ----------------------
     def _get_file_dfs(self, fname : str) -> list[pnd.DataFrame]:
+        log.debug(f'Getting dataframes for: {fname}')
+
         l_fpath = [ f'{sample_dir}/{fname}'          for sample_dir in self._cfg['samples'] ]
         l_df    = [ self._get_file_df(fpath = fpath) for fpath in l_fpath ]
 
         return l_df
     # ----------------------
-    @dask.delayed
-    def _load_root_file(self, fname : str) -> pnd.DataFrame:
+    def _load_root_file(self, fname : str, ifname : int, size : int) -> pnd.DataFrame:
         keys = self._cfg['primary_keys']
         l_df = self._get_file_dfs(fname=fname)
         fun  = lambda df_l, df_r : pnd.merge(df_l, df_r, on=keys)
+
+        log.info(f'Merging dataframes: {ifname}/{size}')
         df   = reduce(fun, l_df)
         df   = df.drop(columns=keys)
 
@@ -87,8 +90,12 @@ class DDFGetter:
         Returns dask dataframe
         '''
         l_fname = self._cfg['files']
-        l_dfs   = [ self._load_root_file(fname = fname) for fname in l_fname  ]
+        nfiles  = len(l_fname)
 
+        log.debug('Building dataframes for single files')
+        l_dfs   = [ dask.delayed(self._load_root_file)(fname = fname, ifname=ifname, size=nfiles) for ifname, fname in enumerate(l_fname)  ]
+
+        log.debug('Bulding full dask dataframe')
         output = ddf.from_delayed(l_dfs)
 
         return output
