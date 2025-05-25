18f32f514dbf9c27d7272dd537bd8eecca3dfe1d
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-25 19:22:38 +0800

    Updating

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,13 +1,17 @@
 '''
 Module with tests for utility functions
 '''
+import matplotlib.pyplot as plt
 
 from ecal_calibration   import utilities as cut
 
+# -----------------------------------------
 def test_get_ddf():
     '''
     Tests getter of dask dataframe
     '''
     ddf = cut.get_ddf()
-    print(ddf)
+    df  = ddf.compute()
 
+    assert len(df) == 10_000
+# -----------------------------------------
