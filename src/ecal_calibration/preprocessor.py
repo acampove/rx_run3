d89fc1dbbae2393ab6d74d9faabe5b5c93a6caa2
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-26 00:01:48 +0800

    Adding selection

--- src/ecal_calibration/preprocessor.py
+++ src/ecal_calibration/preprocessor.py
@@ -10,9 +10,24 @@ class PreProcessor:
     Class used to process input data into features and target columns
     '''
     # ---------------------------------
-    def __init__(self, ddf : DDF):
+    def __init__(self, ddf : DDF, cfg : dict):
         '''
         ddf: Dask dataframe with raw data to preprocess
         '''
         self._ddf = ddf
+        self._cfg = cfg
+    # ---------------------------------
+    def _apply_selection(self, ddf : DDF) -> DDF:
+        for selection in self._cfg['selection']:
+            ddf = ddf.query(selection)
+
+        return ddf
+    # ---------------------------------
+    def get_data(self) -> DDF:
+        '''
+        Returns dask dataframe after preprocessing
+        '''
+        ddf = self._apply_selection(ddf=self._ddf)
+
+        return ddf
 # --------------------------
