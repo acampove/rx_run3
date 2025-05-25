7f5917df9cde655c20d158dde39434765a7a5e17
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-25 21:59:42 +0800

    Plotting brem branches

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -20,6 +20,21 @@ def _initialize():
     os.makedirs(Data.out_dir, exist_ok=True)
 # -----------------------------------------
 def _plot_distributions(df : pnd.DataFrame) -> None:
+    _plot_vtx(df=df)
+    _plot_brem(df=df)
+# -----------------------------------------
+def _plot_brem(df : pnd.DataFrame) -> None:
+    df['nbrem'  ] = df['L1_brem'] + df['L2_brem']
+
+    df['L1_brem'].plot.hist(label='$e^+$', bins=10, alpha=0.3)
+    df['L2_brem'].plot.hist(label='$e^-$', bins=10, alpha=0.3)
+    df['nbrem'  ].plot.hist(label='both' , bins=10, alpha=0.3)
+
+    plt.legend()
+    plt.savefig(f'{Data.out_dir}/brem.png')
+    plt.close()
+# -----------------------------------------
+def _plot_vtx(df : pnd.DataFrame) -> None:
     df['B_END_VX'].plot.hist(label='VX', bins=80, alpha=0.3, range=[-20, +20])
     df['B_END_VY'].plot.hist(label='VY', bins=80, alpha=0.3, range=[-20, +20])
     df['B_END_VY'].plot.hist(label='VY', bins=80, alpha=0.3, range=[-20, +20])
