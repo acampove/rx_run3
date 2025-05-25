7d7ef3b2ff5beafc23fcc1c95eba65ef052bfad3
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-25 16:17:09 +0800

    Adding blueprint code

---
+++ src/ecal_calibration/corrector.py
@@ -0,0 +1,26 @@
+'''
+Module containing the Corrector class
+'''
+from dask.dataframe import DataFrame as DDF
+
+# ---------------------------------------------
+class Corrector:
+    '''
+    Class used to train a regressor to _learn_ energy
+    corrections
+    '''
+    # ---------------------------------------------
+    def __init__(self, ddf : DDF, cfg : dict):
+        '''
+        Parameters
+        -------------------
+        ddf : Dask DataFrame holding the data needed for the regression.
+        cfg : Dictionary holding configuration
+        '''
+    # ---------------------------------------------
+    def get_ddf(self) -> DDF:
+        '''
+        Returns Dask DataFrame with event kinematics
+        after corrections have been applied
+        '''
+# ---------------------------------------------
