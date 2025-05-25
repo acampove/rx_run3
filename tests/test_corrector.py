62aeee74ea028a180b842ff7055813736af92767
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-25 15:49:36 +0800

    Adding files

---
+++ tests/test_corrector.py
@@ -0,0 +1,16 @@
+'''
+Script with code needed to test the Corrector class
+'''
+
+from ecal_calibration.corrector import Corrector
+from ecal_calibration           import utilities as cut
+
+def test_simple():
+    '''
+    Simplest test
+    '''
+    ddf = cut.get_ddf()
+    cfg = cut.load_cfg(name='corrector/simple')
+
+    obj = Corrector(ddf=ddf)
+    ddf = obj.get_ddf()
---
+++ tests/test_regressor.py
@@ -0,0 +1,17 @@
+'''
+Script with code needed to test Calibration class
+'''
+
+from ecal_calibration.regressor import Regressor 
+from ecal_calibration           import utilities as cut
+
+def test_simple():
+    '''
+    Simplest test for calibration
+    '''
+    ddf = cut.get_ddf()
+    cfg = cut.load_cfg(name='regressor/simple')
+
+    obj = Regressor(ddf=ddf, cfg=cfg)
+    obj.train()
+    obj.test()
