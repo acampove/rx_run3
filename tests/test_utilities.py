eceb38ffcf5fda8c40c496da8e041a4879624e6c
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-25 21:42:40 +0800

    Add plot for secondary vertex position

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,10 +1,32 @@
 '''
 Module with tests for utility functions
 '''
+import os
+import pytest
+import pandas as pnd
 import matplotlib.pyplot as plt
 
 from ecal_calibration   import utilities as cut
 
+# -----------------------------------------
+class Data:
+    '''
+    Data class
+    '''
+    out_dir = '/tmp/tests/ecal_calibration/utilities'
+# -----------------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    os.makedirs(Data.out_dir, exist_ok=True)
+# -----------------------------------------
+def _plot_distributions(df : pnd.DataFrame) -> None:
+    df['B_END_VX'].plot.hist(label='VX', bins=80, alpha=0.3, range=[-20, +20])
+    df['B_END_VY'].plot.hist(label='VY', bins=80, alpha=0.3, range=[-20, +20])
+    df['B_END_VY'].plot.hist(label='VY', bins=80, alpha=0.3, range=[-20, +20])
+
+    plt.yscale('log')
+    plt.savefig(f'{Data.out_dir}/end_vtx.png')
+    plt.close()
 # -----------------------------------------
 def test_get_ddf():
     '''
@@ -14,4 +36,6 @@ def test_get_ddf():
     df  = ddf.compute()
 
     assert len(df) == 10_000
+
+    _plot_distributions(df=df)
 # -----------------------------------------
