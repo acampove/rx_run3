06a1de58567c23e878665bf6b1535e46251cb48c
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-27 23:07:03 +0800

    Add logger

--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -10,6 +10,9 @@ from dask.dataframe                import DataFrame    as DDF
 from ecal_calibration.preprocessor import PreProcessor
 from ecal_calibration              import utilities    as cut
 
+from dmu.logging.log_store import LogStore
+
+log = LogStore.add_logger('ecal_calibration:test_preprocessor')
 # -----------------------------------------
 class Data:
     '''
