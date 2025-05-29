70371a5c609ee56ab1e92bcbc3fa4815ccfdcdde
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-30 00:12:46 +0800

    Adding docstrings

--- conftest.py
+++ conftest.py
@@ -1,8 +1,21 @@
+'''
+This module is used by pytest to _inject_ fixtures in the tests
+'''
+
 import pytest
+
 from dask.distributed import Client
 
-@pytest.fixture(scope="session")  # One client for all tests
+# ---------------------------------------
+@pytest.fixture(scope='session')
 def dask_client():
+    '''
+    Create a dask client to:
+
+    - Turn off multithreading. Due to lack of thread safety in vector
+    - Turn on multiprocessing
+    '''
     client = Client(processes=True)
     yield client
     client.close()
+# ---------------------------------------
