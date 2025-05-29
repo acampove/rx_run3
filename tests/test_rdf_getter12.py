4d148c76a500869fdcc1dca591dbf46b1ac2a44d
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-29 19:18:22 +0800

    Implemented code

--- tests/test_rdf_getter12.py
+++ tests/test_rdf_getter12.py
@@ -1,16 +1,33 @@
 '''
 Script used to test RDFGetter12 class
 '''
-import pytest
-from dmu.rx_data.rdf_getter12 import RDFGetter12
+from ROOT import RDataFrame
 
+import pytest
+from dmu.logging.log_store import LogStore
+from rx_data.rdf_getter12 import RDFGetter12
+
+log=LogStore.add_logger('rx_data:test_rdf_getter12')
+# ---------------------------------------------
+class Data:
+    '''
+    Data class
+    '''
+    l_dset = ['2011', '2012', '2015', '2016', '2017', '2018', 'all']
+# ---------------------------------------------
+def _check_rdf(rdf : RDataFrame, name : str) -> None:
+    nentries = rdf.Count().GetValue()
+
+    log.info('')
+    log.info(f'{name:<40}{nentries:<15}')
+# ---------------------------------------------
 @pytest.mark.parametrize('sample', ['Bu_Kee_eq_btosllball05_DPC'])
-def test_simple(sample : str):
+@pytest.mark.parametrize('dset'  , Data.l_dset)
+def test_simple(sample : str, dset : str):
     '''
     Simplest test
     '''
     trigger = 'Hlt2RD_BuToKpEE_MVA'
-    dset    = '2018'
 
     gtr = RDFGetter12(
             sample =sample,
@@ -18,3 +35,6 @@ def test_simple(sample : str):
             dset   =dset)
 
     rdf = gtr.get_rdf()
+
+    _check_rdf(rdf=rdf, name = f'simple_{sample}_{dset}')
+# ---------------------------------------------
