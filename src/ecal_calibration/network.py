a26aafdf9e471f628edb3dc070e922bb57051762
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-30 18:26:06 +0800

    Adding ConstantModel and better description

--- src/ecal_calibration/network.py
+++ src/ecal_calibration/network.py
@@ -1,6 +1,11 @@
 '''
-Module used to hold Network class
+Module used to hold:
+
+Network: Class meant to represent a neural network, used for actual prediction
+ConstantModel: Used for debugging purposes, outputs the same value for any input
 '''
+
+import torch
 from torch import nn
 from torch import Tensor
 
@@ -26,4 +31,21 @@ class Network(nn.Module):
         Evaluates the features through the model
         '''
         return self.model(x)
+# --------------------------------------
+class ConstantModel(nn.Module):
+    '''
+    Model used for debugging purposes, it outputs a given constant
+    '''
+    # ------------------------------
+    def __init__(self, target : float):
+        self._target = target
+
+        super().__init__()
+        self.value = nn.Parameter(torch.tensor(target))
+    # ------------------------------
+    def forward(self, x: Tensor) -> Tensor:
+        '''
+        Returns required target, no training needed
+        '''
+        return self.value.expand(x.shape[0], self._target)
 # --------------------------------------
