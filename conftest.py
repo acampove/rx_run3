7404a638b67ad32cc4519bbc2ab90f109daf1dcc
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-30 17:59:42 +0800

    Silencing matplotlib and PIL messages

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@
 This module is used by pytest to _inject_ fixtures in the tests
 '''
 
+import logging
 import pytest
 
 from dask.distributed      import Client
@@ -13,6 +14,9 @@ def pytest_configure(config : pytest.Config) -> None:
     Runs before all tests, needed to do global initialization. e.g. logging level setting
     '''
     _ = config
+
+    logging.getLogger("PIL.PngImagePlugin").setLevel(logging.WARNING)
+    logging.getLogger("matplotlib.font_manager").setLevel(logging.WARNING)
 
     LogStore.set_level('ecal_calibration:regressor'   , 10)
     LogStore.set_level('ecal_calibration:preprocessor', 10)
