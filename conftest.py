3a59fc1f0d581477ff2a1c2621590a128851a745
Angel Campoverde <angel.campoverde@cern.ch>
2025-05-30 16:31:00 +0800

    Adding pytest_configure to set logging levels

--- conftest.py
+++ conftest.py
@@ -4,8 +4,16 @@ This module is used by pytest to _inject_ fixtures in the tests
 
 import pytest
 
-from dask.distributed import Client
+from dask.distributed      import Client
+from dmu.logging.log_store import LogStore
 
+# ---------------------------------------
+def pytest_configure(_config : pytest.Config) -> None:
+    '''
+    Runs before all tests, needed to do global initialization. e.g. logging level setting
+    '''
+    LogStore.set_level('ecal_calibration:regressor'   , 10)
+    LogStore.set_level('ecal_calibration:preprocessor', 10)
 # ---------------------------------------
 @pytest.fixture(scope='session')
 def _dask_client():
