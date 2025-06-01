17d2fd1be2deeb60998bd9ddec916e7c12c6b6d7
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-01 20:45:09 +0800

    Improvements:
    - Pass config name instead of path. More flexible and configs will be
      saved in project anyway
    - Change arg from -i to -c

--- src/ap_utilities_scripts/check_samples.py
+++ src/ap_utilities_scripts/check_samples.py
@@ -3,6 +3,7 @@ Script used to check which MC samples are found in grid
 '''
 import argparse
 
+from importlib.resources                  import files
 from dataclasses                          import dataclass
 
 import yaml
@@ -16,10 +17,17 @@ class Data:
     '''
     Class storing shared attributes
     '''
+    config  : str
+    nthread : int
+    log_lvl : int
 
-    input_path  : str
-    nthread     : int
-    log_lvl     : int
+    config_path : str
+# ----------------------------------------
+def _initialize() -> None:
+    config_path      = files('ap_utilities_data').joinpath(f'{Data.config}.yaml')
+    Data.config_path = str(config_path)
+
+    _set_logs()
 # ----------------------------------------
 def _sections_from_path(path : str) -> dict[str, dict]:
     with open(path, encoding='utf-8') as ifile:
@@ -29,14 +37,14 @@ def _sections_from_path(path : str) -> dict[str, dict]:
 # --------------------------------
 def _parse_args() -> None:
     parser = argparse.ArgumentParser(description='Used to filter samples based on what exists in the GRID')
-    parser.add_argument('-i', '--input'  , type=str, help='Path to input YAML file')
+    parser.add_argument('-c', '--config' , type=str, help='Config file specifying samples to check')
     parser.add_argument('-n', '--nthread', type=int, help='Number of threads', default=1)
     parser.add_argument('-l', '--log_lvl', type=int, help='Logging level', default=20, choices=[10,20,30,40])
     args = parser.parse_args()
 
-    Data.input_path  = args.input
-    Data.nthread     = args.nthread
-    Data.log_lvl     = args.log_lvl
+    Data.config   = args.config
+    Data.nthread  = args.nthread
+    Data.log_lvl  = args.log_lvl
 # --------------------------------
 def _set_logs() -> None:
     log.debug(f'Running with log level: {Data.log_lvl}')
@@ -49,9 +57,9 @@ def main():
     Script starts here
     '''
     _parse_args()
-    _set_logs()
+    _initialize()
 
-    d_cfg      = _sections_from_path(Data.input_path)
+    d_cfg      = _sections_from_path(Data.config_path)
     d_sections = d_cfg['sections']
     for name, d_section in d_sections.items():
         log.info(f'Processing section: {name}')
