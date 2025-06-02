4d7bac45bc0a079b05066b7d15431709075a4dc6
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-02 18:24:20 +0800

    Reuse instance

--- tests/test_pizerodb.py
+++ tests/test_pizerodb.py
@@ -3,18 +3,25 @@ Module holding tests for PiZeroDb class
 '''
 import pytest
 from dmu.logging.log_store            import LogStore
+from conftest                         import ConfData
 from rx_calibration.electron.pizerodb import PiZeroDb
 
 log=LogStore.add_logger('rx_calibration:test_pizerodb')
 # -----------------------------------------------------
-@pytest.mark.parametrize('run_number', [1, 2, 3, 4])
-def test_simple(run_number : int):
+class Data:
+    '''
+    Data class
+    '''
+    db  = PiZeroDb()
+# -----------------------------------------------------
+@pytest.mark.parametrize('run', ConfData.l_run)
+def test_simple(run : int):
     '''
     Simplest test
     '''
+    log.info('Simple')
 
-    db  = PiZeroDb()
-    val = db.get_period(run = run_number)
+    val = Data.db.get_period(run = run)
 
     log.info(val)
 # -----------------------------------------------------
