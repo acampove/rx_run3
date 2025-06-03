0ac4ed7bad90870ea87c07612d02b8deac80f90a
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-03 20:37:29 +0800

    Adding implementation

--- src/ecal_calibration/corrector.py
+++ src/ecal_calibration/corrector.py
@@ -1,28 +1,66 @@
 '''
 Module containing the Corrector class
 '''
-from dask.dataframe import DataFrame as DDF
 
-# ---------------------------------------------
-class Corrector:
+from dask.dataframe             import DataFrame        as DDF
+from vector                     import MomentumObject4D as v4d
+from dmu.logging.log_store      import LogStore
+from ecal_calibration.network   import Network
+from ecal_calibration.regressor import Regressor
+
+log=LogStore.add_logger('ecal_calibration:calibrator')
+# ---------------------------------------
+class Calibrator:
     '''
-    Class used to train a regressor to _learn_ energy
-    corrections
+    Class intended to calibrate electrons
     '''
-    # ---------------------------------------------
-    def __init__(self, ddf : DDF, cfg : dict):
+    # ---------------------------------------
+    def __init__(self, cfg : dict):
         '''
-        Parameters
-        -------------------
-        ddf : Dask DataFrame holding the data needed for the regression.
-        cfg : Dictionary holding configuration
+        Parameters:
+
+        cfg: Dictionary with configuration
         '''
-        self._ddf = ddf
         self._cfg = cfg
-    # ---------------------------------------------
-    def get_ddf(self) -> DDF:
+        self._net = self._get_network()
+    # ---------------------------------------
+    def _get_network(self) -> Network:
+        model_dir = Regressor.get_out_dir(cfg=self._cfg)
+        net       = Regressor.load(model_dir=model_dir)
+        if net is None:
+            raise FileNotFoundError(f'Model could not be found in: {model_dir}')
+
+        net       = Regressor.move_to_gpu(net)
+
+        return net
+    # ---------------------------------------
+    def run(self, electron : v4d, ddf : DDF) -> v4d:
         '''
-        Returns Dask DataFrame with event kinematics
-        after corrections have been applied
+        Calibrates electron
+
+        Parameters
+        -----------------
+        electron: Lorentz vector before calibration
+        ddf     : Dask dataframe, after the `PreProcessor` class has been ran.
+
+        Returns
+        -----------------
+        Lorentz vector representing calibrated electron
         '''
-# ---------------------------------------------
+
+        features, _ = Regressor.get_tensors(cfg = self._cfg, ddf = ddf)
+        features    = Regressor.move_to_gpu(features)
+
+        targets     = self._net(features)
+        targets     = targets.cpu()
+        val         = targets.detach().numpy()
+        # The target was scaled by 1000 for training
+        # Scale back the correction
+        val         = float(val) / 1000.
+
+        log.debug(f'Original : {electron}')
+        electron    = val * electron
+        log.debug(f'Corrected: {electron}')
+
+        return electron
+# ---------------------------------------
