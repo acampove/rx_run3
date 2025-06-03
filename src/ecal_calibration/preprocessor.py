8490bb12c672aab42157d147010e3fe6bb6e9d46
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-03 23:16:24 +0800

    Make methods static
    Need to be reused

--- src/ecal_calibration/preprocessor.py
+++ src/ecal_calibration/preprocessor.py
@@ -47,67 +47,6 @@ class PreProcessor:
 
         return ddf
     # ---------------------------------
-    def _get_normal(self, row : pnd.Series) -> v3d:
-        pvx = row['B_BPVX']
-        pvy = row['B_BPVY']
-        pvz = row['B_BPVZ']
-
-        svx = row['B_END_VX']
-        svy = row['B_END_VY']
-        svz = row['B_END_VZ']
-
-        dr  = v3d(x=svx - pvx, y=svy - pvy, z=svz - pvz)
-
-        return dr / dr.mag
-    # ---------------------------------
-    def _get_momentum(self, row : pnd.Series, name : str) -> v3d:
-        pt = row[f'{name}_PT' ]
-        et = row[f'{name}_ETA']
-        ph = row[f'{name}_PHI']
-
-        return v3d(pt=pt, eta=et, phi=ph)
-    # ---------------------------------
-    def _get_correction(self, row : pnd.Series, lepton : str) -> float:
-        norm = self._get_normal(row=row)
-        l1_p = self._get_momentum(row=row, name='L1')
-        l2_p = self._get_momentum(row=row, name='L2')
-        kp_p = self._get_momentum(row=row, name= 'H')
-
-        # Remove the component alongside normal. i.e. vectors lie on plane
-        l1_p = l1_p - norm * norm.dot(l1_p)
-        l2_p = l2_p - norm * norm.dot(l2_p)
-        kp_p = kp_p - norm * norm.dot(kp_p)
-
-        if   lepton == 'L1':
-            lep = l1_p
-            oth = l2_p + kp_p
-        elif lepton == 'L2':
-            lep = l2_p
-            oth = l1_p + kp_p
-        else:
-            raise ValueError(f'Invalid lepton: {lepton}')
-
-        a = lep.dot(oth)
-        b = lep.mag ** 2
-
-        return - a/b
-    # ---------------------------------
-    def _build_features(self, row_sr : pnd.Series) -> pnd.Series:
-        lep = 'L1' if row_sr['L1_brem'] == 1 else 'L2'
-
-        data        = {}
-        data['row'] = row_sr[f'{lep}_BREMHYPOROW']
-        data['col'] = row_sr[f'{lep}_BREMHYPOCOL']
-        data['are'] = row_sr[f'{lep}_BREMHYPOAREA']
-        data['eng'] = row_sr[f'{lep}_BREMTRACKBASEDENERGY'] / 1000 # Very large numbers seem to break down training
-        data['npv'] = row_sr['nPVs']
-        data['blk'] = row_sr['block']
-        data['mu' ] = 1000 * self._get_correction(row=row_sr, lepton=lep)
-
-        row_sr = pnd.Series(data)
-
-        return row_sr
-    # ---------------------------------
     def _values(self, kind : str) -> tensor:
         ddf = self.get_data()
         df  = ddf.compute()
@@ -161,4 +100,76 @@ class PreProcessor:
         '''
 
         return self._values(kind='targets')
+    # ---------------------------------
+    @staticmethod
+    def _get_normal(row : pnd.Series) -> v3d:
+        pvx = row['B_BPVX']
+        pvy = row['B_BPVY']
+        pvz = row['B_BPVZ']
+
+        svx = row['B_END_VX']
+        svy = row['B_END_VY']
+        svz = row['B_END_VZ']
+
+        dr  = v3d(x=svx - pvx, y=svy - pvy, z=svz - pvz)
+
+        return dr / dr.mag
+    # ---------------------------------
+    @staticmethod
+    def _get_momentum(row : pnd.Series, name : str) -> v3d:
+        pt = row[f'{name}_PT' ]
+        et = row[f'{name}_ETA']
+        ph = row[f'{name}_PHI']
+
+        return v3d(pt=pt, eta=et, phi=ph)
+    # ---------------------------------
+    @staticmethod
+    def _get_correction(row : pnd.Series, lepton : str) -> float:
+        norm = PreProcessor._get_normal(row=row)
+        l1_p = PreProcessor._get_momentum(row=row, name='L1')
+        l2_p = PreProcessor._get_momentum(row=row, name='L2')
+        kp_p = PreProcessor._get_momentum(row=row, name= 'H')
+
+        # Remove the component alongside normal. i.e. vectors lie on plane
+        l1_p = l1_p - norm * norm.dot(l1_p)
+        l2_p = l2_p - norm * norm.dot(l2_p)
+        kp_p = kp_p - norm * norm.dot(kp_p)
+
+        if   lepton == 'L1':
+            lep = l1_p
+            oth = l2_p + kp_p
+        elif lepton == 'L2':
+            lep = l2_p
+            oth = l1_p + kp_p
+        else:
+            raise ValueError(f'Invalid lepton: {lepton}')
+
+        a = lep.dot(oth)
+        b = lep.mag ** 2
+
+        return - a/b
+    # ---------------------------------
+    @staticmethod
+    def build_features(row_sr : pnd.Series, skip_target : bool = False) -> pnd.Series:
+        '''
+        Takes a pandas series with information from tree branches, one entry.
+
+        Returns series with features and target needed for training or prediction
+        '''
+        lep = 'L1' if row_sr['L1_brem'] == 1 else 'L2'
+
+        data        = {}
+        data['row'] = row_sr[f'{lep}_BREMHYPOROW']
+        data['col'] = row_sr[f'{lep}_BREMHYPOCOL']
+        data['are'] = row_sr[f'{lep}_BREMHYPOAREA']
+        data['eng'] = row_sr[f'{lep}_BREMTRACKBASEDENERGY'] / 1000 # Very large numbers seem to break down training
+        data['npv'] = row_sr['nPVs']
+        data['blk'] = row_sr['block']
+
+        if not skip_target:
+            data['mu' ] = 1000 * PreProcessor._get_correction(row=row_sr, lepton=lep)
+
+        row_sr = pnd.Series(data)
+
+        return row_sr
 # --------------------------
