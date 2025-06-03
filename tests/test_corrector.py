2cbb3c7bf613e2b50744c0b16b3806f9432c7314
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-03 20:44:10 +0800

    Adding implementation for first version of class

--- tests/test_corrector.py
+++ tests/test_corrector.py
@@ -1,18 +1,46 @@
 '''
-Script with code needed to test the Corrector class
+Script with code needed to test Corrector class
 '''
 
-from ecal_calibration.corrector import Corrector
-from ecal_calibration           import utilities as cut
+import dask
+import pandas as pnd
 
-# -----------------------------------------
-def test_simple():
-    '''
-    Simplest test
-    '''
-    ddf = cut.get_ddf()
-    cfg = cut.load_cfg(name='tests/corrector/simple')
+from vector                        import MomentumObject4D as v4d
+from ecal_calibration              import utilities        as cut
+from ecal_calibration.preprocessor import PreProcessor
+from ecal_calibration.corrector    import Corrector
 
-    obj = Corrector(ddf=ddf, cfg=cfg)
-    ddf = obj.get_ddf()
-# -----------------------------------------
+# -----------------------------------------------------------
+def _load_data(name : str) -> dict:
+    fpath = files('ecal_calibration_data').joinpath(f'tests/regressor/{name}.json')
+    fpath = str(fpath)
+    data  = gut.load_json(fpath)
+
+    return data
+# -----------------------------------------------------------
+def test_calibrate():
+    '''
+    Tests `calibrate_electron` from the Corrector class
+    '''
+    kind     = 'row_col_are_eng'
+
+    electron = v4d(px=2250, py=-3287, pz=43253, e=43437)
+    data     = _load_data(name='row')
+    sr       = pnd.Series(data)
+    df       = sr.to_frame()
+    df       = df.T
+    df['L1_brem'] = df['L1_HASBREMADDED']
+    df['L2_brem'] = df['L2_HASBREMADDED']
+
+    ddf_in = dask.dataframe.from_pandas(df)
+
+    cfg = cut.load_cfg(name='tests/preprocessor/simple')
+    pre = PreProcessor(ddf=ddf_in, cfg=cfg)
+    ddf = pre.get_data()
+
+    cfg      = cut.load_cfg(name='tests/regressor/simple')
+    cfg['saving']['out_dir'] =f'regressor/predict_{kind}'
+
+    cal      = Corrector(cfg=cfg)
+    electron = cal.run(electron, ddf=ddf)
+# -----------------------------------------------------------
