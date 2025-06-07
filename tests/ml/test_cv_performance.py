fd81ab85d8ed1adeb3e7c9fda51878f5cca9aabd
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-08 00:56:10 +0800

    Add basic test

--- tests/ml/test_cv_performance.py
+++ tests/ml/test_cv_performance.py
@@ -2,9 +2,11 @@
 Script with functions meant to test CVPerformance
 '''
 
-import dmu.testing.utilities as tut
-from dmu.stats.cv_performance import CVPerformance
+from dmu.testing           import utilities as tut
+from dmu.logging.log_store import LogStore
+from dmu.ml.cv_performance import CVPerformance
 
+log=LogStore.add_logger('dmu:ml:test_cv_performance')
 # -------------------------------
 class Data:
     '''
@@ -16,15 +18,15 @@ def test_simple():
     '''
     Simplest test
     '''
-    rdf_sig_1 = tut.get_rdf(kind='sig_1')
-    rdf_bkg_1 = tut.get_rdf(kind='bkg_1')
-    rdf_bkg_2 = tut.get_rdf(kind='bkg_2')
+    rdf_sig_1 = tut.get_rdf(kind='sig'    )
+    rdf_bkg_1 = tut.get_rdf(kind='bkg'    )
+    rdf_bkg_2 = tut.get_rdf(kind='bkg_alt')
 
-    l_model_1 = tut.get_models(rdf_sig_1, rdf_bkg_1)
-    l_model_2 = tut.get_models(rdf_sig_1, rdf_bkg_2)
+    l_model_1 = tut.get_models(rdf_sig_1, rdf_bkg_1, name='def')
+    l_model_2 = tut.get_models(rdf_sig_1, rdf_bkg_2, name='alt')
 
     cvp = CVPerformance()
-    cvp.add_data(sig=rdf_sig_1, bkg=rdf_bkg_1, model=l_model_1, name='def')
-    cvp.add_data(sig=rdf_sig_1, bkg=rdf_bkg_2, model=l_model_2, name='alt')
-    cvp.save(name=f'{Data.out_dir}/simple')
+    cvp.load(sig=rdf_sig_1, bkg=rdf_bkg_1, model=l_model_1, name='def')
+    cvp.load(sig=rdf_sig_1, bkg=rdf_bkg_2, model=l_model_2, name='alt')
+    cvp.save(path=f'{Data.out_dir}/simple')
 # -------------------------------
