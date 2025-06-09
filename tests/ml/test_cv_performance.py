5e314d3e406974062a79e66145b0594f0be4206e
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-09 22:12:38 +0800

    Testing model saved to custom directory
    To compare training and performance results

--- tests/ml/test_cv_performance.py
+++ tests/ml/test_cv_performance.py
@@ -3,7 +3,6 @@ Script with functions meant to test CVPerformance
 '''
 import os
 
-import pytest
 import matplotlib.pyplot as plt
 
 from dmu.testing           import utilities as tut
@@ -18,20 +17,19 @@ class Data:
     '''
     out_dir = '/tmp/tests/dmu/stats/cv_performance'
 # -------------------------------
-@pytest.fixture(scope='session', autouse=True)
-def _initialize():
-    os.makedirs(Data.out_dir, exist_ok=True)
-# -------------------------------
 def test_simple():
     '''
     Simplest test
     '''
+    out_dir   = f'{Data.out_dir}/simple'
+    os.makedirs(out_dir, exist_ok=True)
+
     rdf_sig_1 = tut.get_rdf(kind='sig'    )
     rdf_bkg_1 = tut.get_rdf(kind='bkg'    )
     rdf_bkg_2 = tut.get_rdf(kind='bkg_alt')
 
-    l_model_1 = tut.get_models(rdf_sig_1, rdf_bkg_1, name='def')
-    l_model_2 = tut.get_models(rdf_sig_1, rdf_bkg_2, name='alt')
+    l_model_1 = tut.get_models(rdf_sig_1, rdf_bkg_1, out_dir=f'{out_dir}/def')
+    l_model_2 = tut.get_models(rdf_sig_1, rdf_bkg_2, out_dir=f'{out_dir}/alt')
 
     cvp = CVPerformance()
     cvp.plot_roc(
