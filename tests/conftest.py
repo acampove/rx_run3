2b85709ac88b9a78c0453d574501cd27a9e5c8ba
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-09 21:21:38 +0800

    Turn off matplotlib and PIL noise through pytest_configure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,20 @@
 '''
 File needed by pytest
 '''
+import logging
 
+from _pytest.config import Config
+# Needed to make sure zfit gets imported properly
+# before any test runs
 from dmu.stats.zfit import zfit
+
+# ------------------------------
+def pytest_configure(config : Config):
+    '''
+    This will run before any test by pytest
+    '''
+    _config = config
+
+    logging.getLogger('PIL').setLevel(logging.WARNING)
+    logging.getLogger('matplotlib').setLevel(logging.WARNING)
+# ------------------------------
