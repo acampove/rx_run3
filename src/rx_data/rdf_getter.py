cf21b0217a670b77cae2562fa5f56bbd894419a7
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-14 23:07:16 +0800

    Better indentation

--- src/rx_data/rdf_getter.py
+++ src/rx_data/rdf_getter.py
@@ -69,11 +69,12 @@ class RDFGetter:
         self._l_electron_only = self._cfg['trees']['electron_only']
         self._ext_weight      = '(L1_PID_E > 1 && L2_PID_E > 1) ? 1 : 10'
 
-        self._l_ee_trigger    = ['Hlt2RD_BuToKpEE_MVA',
-                                 'Hlt2RD_BuToKpEE_MVA_cal',
-                                 'Hlt2RD_BuToKpEE_MVA_misid',
-                                 'Hlt2RD_BuToKpEE_MVA_ext',
-                                 'Hlt2RD_BuToKpEE_SameSign_MVA']
+        self._l_ee_trigger    = [
+                'Hlt2RD_BuToKpEE_MVA',
+                'Hlt2RD_BuToKpEE_MVA_cal',
+                'Hlt2RD_BuToKpEE_MVA_misid',
+                'Hlt2RD_BuToKpEE_MVA_ext',
+                'Hlt2RD_BuToKpEE_SameSign_MVA']
 
         self._l_mm_trigger    = ['Hlt2RD_BuToKpMuMu_MVA',
                                  'Hlt2RD_BuToKpMuMu_SameSign_MVA']
