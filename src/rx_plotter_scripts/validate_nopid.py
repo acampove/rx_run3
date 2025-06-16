4ee916c20bfbc753e23b6eed9c2e7298f9db8597
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-16 20:06:52 +0800

    Adding partial implementation

--- src/rx_plotter_scripts/validate_nopid.py
+++ src/rx_plotter_scripts/validate_nopid.py
@@ -5,14 +5,22 @@ Script used to plot PID distributions from:
     - Check that the distributions agree when the former ones have had the PID added
 '''
 
+import os
 import argparse
 
+from ROOT                  import RDataFrame
+from dmu.logging.log_store import LogStore
+
+log = LogStore.add_logger('rx_plots:validate_nopid')
 # ----------------------------
 class Data:
     '''
     Data class
     '''
     channel : str
+    ana_dir = os.environ['ANADIR']
+    fname   = '00012345_00006789_2.tuple.root'
+    tname   = 'Hlt2RD_BuToKpEE_MVA'
 # ----------------------------
 def _parse_args():
     parser = argparse.ArgumentParser(description='')
@@ -21,11 +29,34 @@ def _parse_args():
 
     Data.channel = args.channel
 # ----------------------------
+def _get_rdf(has_pid : bool) -> RDataFrame:
+    root_path = f'{Data.ana_dir}/ana_prod/ntuples/no_pid/{Data.channel}/{Data.fname}'
+
+    if has_pid:
+        tname = f'{Data.tname}_noPID'
+    else:
+        tname = f'{Data.tname}'
+
+    rdf = RDataFrame(f'{tname}/DecayTree', root_path)
+
+    return rdf
+# ----------------------------
+def _compare(rdf_nopid : RDataFrame,
+             rdf_yspid : RDataFrame) -> None:
+    pass
+# ----------------------------
 def main():
     '''
     Start here
     '''
     _parse_args()
+
+    rdf_nopid = _get_rdf(has_pid=False)
+    rdf_yspid = _get_rdf(has_pid=True )
+
+    _compare(
+            rdf_nopid=rdf_nopid,
+            rdf_yspid=rdf_yspid)
 # ----------------------------
 if __name__ == '__main__':
     main()
