241629841936293a1fe3dbcea2c742e8a426893c
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-19 18:17:27 +0800

    Log message before doing the thing

--- src/dmu/plotting/plotter_1d.py
+++ src/dmu/plotting/plotter_1d.py
@@ -178,8 +178,8 @@ class Plotter1D(Plotter):
         d_data = {}
         for name, rdf in self._d_rdf.items():
             try:
-                d_data[name] = rdf.AsNumpy([var])[var]
                 log.debug(f'Plotting: {var}/{name}')
+                d_data[name] = rdf.AsNumpy([var])[var]
             except cppyy.gbl.std.runtime_error as exc:
                 raise ValueError(f'Cannot find variable {var} in category {name}') from exc
 
