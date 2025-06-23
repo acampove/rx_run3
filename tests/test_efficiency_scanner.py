05668d5eeef27784537bc241d79410415c026eaa
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-23 23:22:25 +0800

    Add plots

--- tests/test_efficiency_scanner.py
+++ tests/test_efficiency_scanner.py
@@ -1,17 +1,58 @@
 '''
 Module used to test EfficiencyScanner class
 '''
+
+import os
 import numpy
 import pytest
+import matplotlib.pyplot as plt
+import pandas            as pnd
 
 from dmu.logging.log_store import LogStore
 from rx_efficiencies.efficiency_scanner import EfficiencyScanner as EffSc
 
 log = LogStore.add_logger('rx_efficiencies:test_efficiency_scanner')
 # -----------------------------------
+class Data:
+    '''
+    Class needed to store attributes
+    '''
+    out_dir = '/tmp/tests/rx_efficiencies/efficiency_scanner'
+# -----------------------------------
 @pytest.fixture(scope='session', autouse=True)
 def _initialize():
     LogStore.set_level('rx_efficiencies:efficiency_scanner', 10)
+
+    os.makedirs(Data.out_dir, exist_ok=True)
+# -----------------------------------
+def _plot_values(
+        df  : pnd.DataFrame,
+        name:  str) -> None:
+    '''
+    Plot dataframe as an interpolated mesh
+    '''
+    pivoted = df.pivot(index='y', columns='x', values='z')
+    plt.imshow(
+        pivoted.values,
+        origin='lower',
+        extent=[
+            pivoted.columns.min(), pivoted.columns.max(),
+            pivoted.index.min()  , pivoted.index.max()],
+        cmap='viridis',
+        interpolation='bilinear',
+    )
+
+    plt.colorbar(label='z')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Heatmap of z vs (x, y)')
+    plt.xticks(pivoted.columns)
+    plt.yticks(pivoted.index)
+    plt.tight_layout()
+
+    out_path = f'{Data.out_dir}/{name}.png'
+    plt.savefig(out_path)
+    plt.close()
 # -----------------------------------
 def test_scan():
     '''
@@ -33,4 +74,6 @@ def test_scan():
 
     obj = EffSc(cfg=cfg)
     df  = obj.run()
+
+    _plot_values(df=df, name='scan')
 # -----------------------------------
