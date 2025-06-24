6983c2dce31436f7e8a277c1d1bdfaf3861171ee
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-24 23:35:10 +0800

    Add config and module with calculator of signal yields

---
+++ src/rx_classifier/signal_calculator.py
@@ -0,0 +1,23 @@
+'''
+Module with SignalCalculator class
+'''
+import pandas as pnd
+
+# -----------------------------------
+class SignalCalculator:
+    '''
+    Class meant to calculate expected value of
+    signal yield for different working points
+    '''
+    def __init__(self, cfg : dict):
+        '''
+        Picks configuration
+        '''
+        self._cfg = cfg
+    # -----------------------------------
+    def get_signal(self) -> pnd.DataFrame:
+        '''
+        Reuturns pandas dataframe with signal yields
+        '''
+        return
+# -----------------------------------
