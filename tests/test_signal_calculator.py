ad7ac4af9242242d904062d1771116699a23843f
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-25 00:24:49 +0800

    Use pytest for parametrization, add q2 bin testing

--- tests/test_signal_calculator.py
+++ tests/test_signal_calculator.py
@@ -1,16 +1,18 @@
 '''
 This module contains tests for SignalCalculator class
 '''
+import pytest
 
 from dmu.generic                     import utilities as gut
 from rx_classifier.signal_calculator import SignalCalculator
 
 # --------------------------------
-def test_simple():
+@pytest.mark.parametrize('q2bin', ['low', 'central', 'high'])
+def test_simple(q2bin : str):
     '''
     Simplest test
     '''
     cfg = gut.load_data(package='rx_classifier_data', fpath='optimization/scanning.yaml')
-    cal = SignalCalculator(cfg=cfg)
+    cal = SignalCalculator(cfg=cfg, q2bin=q2bin)
     df  = cal.get_signal()
 # --------------------------------
