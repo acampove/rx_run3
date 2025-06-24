c0c9f5e11ab3f1aa7a65db05186d03c588d1b1e4
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-25 00:21:55 +0800

    Add q2 to initializer
    Needed to calculate signal efficiencies, control mode always uses Jpsi

--- src/rx_classifier/signal_calculator.py
+++ src/rx_classifier/signal_calculator.py
@@ -9,11 +9,13 @@ class SignalCalculator:
     Class meant to calculate expected value of
     signal yield for different working points
     '''
-    def __init__(self, cfg : dict):
+    # -----------------------------------
+    def __init__(self, cfg : dict, q2bin : str):
         '''
         Picks configuration
         '''
         self._cfg = cfg
+        self._q2bin = q2bin
     # -----------------------------------
     def get_signal(self) -> pnd.DataFrame:
         '''
