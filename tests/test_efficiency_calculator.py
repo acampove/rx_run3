f47b49a1a876ea668c628e81015afcf8292b8ce9
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-26 00:04:37 +0800

    Test efficiency calculator

--- tests/test_efficiency_calculator.py
+++ tests/test_efficiency_calculator.py
@@ -23,3 +23,15 @@ def test_stats(q2bin : str):
 
     assert len(df) > 0
 #-------------------------------------------------
+@pytest.mark.parametrize('sample', ['Bu_JpsiK_ee_eq_DPC'])
+@pytest.mark.parametrize('q2bin' , ['low', 'central', 'high'])
+def test_efficiency(q2bin : str, sample : str):
+    '''
+    Tests retrieval of total efficiency (acceptance x reco x selection)
+    for given sample
+    '''
+    obj         = EfficiencyCalculator(q2bin=q2bin)
+    eff         = obj.get_efficiency(sample=sample)
+
+    assert 0 <= eff < 1
+#-------------------------------------------------
