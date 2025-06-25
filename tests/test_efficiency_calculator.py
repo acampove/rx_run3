d92cc148bbd2a0df086d123ae320cf2c5bd5582f
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-26 00:03:58 +0800

    Better test, simple -> stats + assert dataframe is larger than zero

--- tests/test_efficiency_calculator.py
+++ tests/test_efficiency_calculator.py
@@ -12,14 +12,14 @@ def _initialize():
     LogStore.set_level('rx_efficiencies:efficiency_calculator', 10)
 #-------------------------------------------------
 @pytest.mark.parametrize('q2bin', ['low', 'central', 'high'])
-def test_simple(q2bin : str):
+def test_stats(q2bin : str):
     '''
     Tests calculation of total efficiency (acceptance x reco x selection)
     for multiple MC samples
     '''
     obj         = EfficiencyCalculator(q2bin=q2bin)
-    obj.out_dir = '/tmp/tests/rx_efficiencies/efficiency_calculator/simple'
+    obj.out_dir = '/tmp/tests/rx_efficiencies/efficiency_calculator/stats'
     df          = obj.get_stats()
 
-    print(df)
+    assert len(df) > 0
 #-------------------------------------------------
