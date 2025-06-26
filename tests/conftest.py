460d4db6fdfa4f4d1df16394ec4c71fe0890e553
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-26 21:06:26 +0800

    Adding file

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+'''
+This module is needed to control how pytest will run tests
+'''
+
+import pytest
+from rx_data.rdf_getter import RDFGetter
+
+# -----------------------------------------------
+@pytest.fixture(autouse=True)
+def rdf_getter_configuration():
+    '''
+    This will configure RDFGetter for tests
+    '''
+    with RDFGetter.max_entries(1000):
+        yield
+# -----------------------------------------------
