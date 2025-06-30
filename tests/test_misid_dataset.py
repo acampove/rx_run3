d67537dbb871d1374f3d57342ba711f752b66699
Angel Campoverde <angel.campoverde@cern.ch>
2025-06-30 15:51:58 +0800

    Updating

--- tests/test_misid_dataset.py
+++ tests/test_misid_dataset.py
@@ -2,11 +2,15 @@
 Module meant to test MisIDDataset class
 '''
 
+from rx_data.rdf_getter     import RDFGetter
 from rx_misid.misid_dataset import MisIDDataset
 
+# -----------------------------------------------
 def test_simple():
     '''
     Simplest test
     '''
-    dst = MisIDDataset(version='v1', q2bin='central')
-    d_df= dst.get_data()
+    with RDFGetter.max_entries(value = 1000):
+        dst = MisIDDataset(q2bin='central')
+        d_df= dst.get_data()
+# -----------------------------------------------
