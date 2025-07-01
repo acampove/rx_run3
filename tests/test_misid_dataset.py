c2b97b7a462064890ff6771ffb17868e0913eb1d
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-02 00:56:22 +0800

    Parametrizing

--- tests/test_misid_dataset.py
+++ tests/test_misid_dataset.py
@@ -2,14 +2,17 @@
 Module meant to test MisIDDataset class
 '''
 
+import pytest
+
 from rx_data.rdf_getter     import RDFGetter
 from rx_misid.misid_dataset import MisIDDataset
 
 # -----------------------------------------------
-def test_simple():
+@pytest.mark.parametrize('q2bin', ['low', 'central', 'high'])
+def test_simple(q2bin : str):
     '''
     Simplest test
     '''
-    dst = MisIDDataset(q2bin='central')
+    dst = MisIDDataset(q2bin=q2bin)
     d_df= dst.get_data()
 # -----------------------------------------------
