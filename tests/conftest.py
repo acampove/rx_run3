3a940510db6ab09c5665605acf7d9ef9eac50aff
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-02 20:53:14 +0800

    Set cache directory for Cache class tests and set git to warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 from _pytest.config import Config
 # Needed to make sure zfit gets imported properly
 # before any test runs
-from dmu.stats.zfit import zfit
+from dmu.stats.zfit     import zfit
+from dmu.workflow.cache import Cache as Wcache
 
 # ------------------------------
 def pytest_configure(config : Config):
@@ -22,6 +23,8 @@ def pytest_configure(config : Config):
 
     logging.getLogger('PIL').setLevel(logging.WARNING)
     logging.getLogger('matplotlib').setLevel(logging.WARNING)
+    logging.getLogger('git.cmd').setLevel(logging.WARNING)
 
     plt.style.use(mplhep.style.LHCb2)
+    Wcache.set_cache_root(root='/tmp/tests/dmu/cache/cache_dir')
 # ------------------------------
