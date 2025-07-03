f51332dfffde85b579165138dd6415ab8bbe68bd
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-03 22:06:49 +0800

    Renaming files and updating content
    Modules won't reweight, this is already done by existing code.
    These modules need to fit

---
+++ src/rx_misid/pdf_maker.py
@@ -0,0 +1,67 @@
+'''
+Module holding PIDWeighter class
+'''
+import pandas as pnd
+
+from zfit.core.interfaces      import ZfitData   as zobs
+from zfit.core.interfaces      import ZfitPDF    as zpdf
+from dmu.generic               import utilities  as gut
+from dmu.logging.log_store     import LogStore
+from rx_misid.misid_calculator import MisIDCalculator
+
+log=LogStore.add_logger('rx_misid:pdf_maker')
+# ------------------------------------------------
+class PDFMaker:
+    '''
+    Class meant to:
+
+    - For a given sample and q2bin provide the corresponding fitted PDF
+    '''
+    # -----------------------------------------
+    def __init__(
+            self,
+            sample : str,
+            q2bin  : str):
+        '''
+        Parameters
+        ------------------
+        sample: e.g. Bu_KplKplKmn_eq_sqDalitz_DPC
+        q2bin : e.g. central
+        '''
+        self._sample = sample
+        self._q2bin  = q2bin
+    # -----------------------------------------
+    def _pdf_from_df(
+            self,
+            df : pnd.DataFrame,
+            obs : zobs) -> zpdf:
+        '''
+        Parameters
+        ---------------
+        df : Pandas dataframe with observable and weights
+        obs: Observable used for the PDF
+        '''
+
+        print(df)
+    # -----------------------------------------
+    def get_pdf(self, obs : zobs) -> zpdf:
+        '''
+        Parameters
+        ---------------
+        obs: Obserbable used in PDF
+
+        Returns
+        ---------------
+        pdf: Fitted PDF
+        '''
+        cfg = gut.load_data(package='rx_misid_data', fpath = 'misid.yaml')
+
+        cfg['input']['sample'] = self._sample
+        cfg['input']['q2bin' ] = self._q2bin
+
+        obj = MisIDCalculator(cfg=cfg)
+        df  = obj.get_misid()
+        pdf = self._pdf_from_df(df=df, obs=obs)
+
+        return pdf
+# ------------------------------------------------
--- tests/test_pdf_maker.py
+++ tests/test_pdf_maker.py
@@ -4,9 +4,10 @@ File with tests for PIDWeigher class
 import pytest
 
 from dmu.stats.zfit        import zfit
-from rx_data.rdf_getter    import RDFGetter
-from rx_misid.pid_weighter import PIDWeighter
+from dmu.logging.log_store import LogStore
+from rx_misid.pdf_maker    import PDFMaker
 
+log=LogStore.add_logger('rx_misid:test_pdf_maker')
 # ------------------------------------
 class Data:
     '''
@@ -15,13 +16,17 @@ class Data:
     obs     = zfit.Space('B_Mass_smr', limits=(4500, 7000))
     trigger = 'Hlt2RD_BuToKpEE_MVA_noPID'
 # ------------------------------------
+@pytest.fixture(scope='session', autouse=True)
+def _initialize():
+    LogStore.set_level('rx_data:rdf_getter', 10)
+# ------------------------------------
 @pytest.mark.parametrize('sample', ['Bu_KplKplKmn_eq_sqDalitz_DPC'])
 def test_simple(sample : str):
     '''
     Simplest test
     '''
-    gtr = RDFGetter(sample=sample, trigger=Data.trigger, analysis='nopid')
-    rdf = gtr.get_rdf()
+    q2bin = 'central'
 
-    wtr = PIDWeighter(rdf=rdf)
-    pdf = wtr.get_pdf(obs=Data.obs)
+    mkr = PDFMaker(sample=sample, q2bin=q2bin)
+    mkr.get_pdf(obs=Data.obs)
+# ------------------------------------
