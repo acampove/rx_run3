7b0de7b88ef3d737934744ae1b6ef1ec6adc8a61
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-04 23:08:44 +0800

    Can drop up to 6% of candidates
    Add explanation

--- src/rx_misid/misid_calculator.py
+++ src/rx_misid/misid_calculator.py
@@ -98,7 +98,10 @@ class MisIDCalculator:
         df['hadron'] = hadron_id
         df['bmeson'] = 'bplus' if is_bplus else 'bminus'
 
-        df = put.dropna(df, max_frac=0.04)
+        # TODO: Use replace_nan to replace nans with 1s 
+        # This should drop up to 6% of the dataset
+        # Due to NaNs in the PID maps.
+        df = put.dropna(df, max_frac=0.06)
 
         return df
     # -----------------------------
