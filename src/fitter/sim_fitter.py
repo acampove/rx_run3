a45ce3c2824a3ffdae6a99036e414e1176d4161f
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-08 00:49:12 +0800

    Update interface

--- src/fitter/sim_fitter.py
+++ src/fitter/sim_fitter.py
@@ -2,14 +2,35 @@
 Module with SimFitter class
 '''
 
-from fitter.base_fitter import BaseFitter
-from fitter.sim_model   import SimModel
+from omegaconf              import DictConfig
+from zfit.core.interfaces   import ZfitPDF    as zpdf
+from zfit.core.interfaces   import ZfitSpace  as zobs
+from fitter.base_fitter     import BaseFitter
+from fitter.sim_model       import SimModel
 
+# ------------------------
 class SimFitter(BaseFitter):
     '''
     Fitter for simulation samples
     '''
-    def __init__(self):
+    # ------------------------
+    def __init__(
+        self,
+        cfg : DictConfig,
+        obs : zobs):
         '''
-
+        Parameters
+        --------------------
+        obs : Observable
+        cfg : Object storing configuration for fit
         '''
+        self._cfg = cfg
+        self._obs = obs
+    # ------------------------
+    def get_model(self) -> zpdf:
+        '''
+        Returns
+        ------------
+        zfit PDF, not extended yet
+        '''
+# ------------------------
