a5560df693b1cea80adb3f1ac929d7749bd0eeac
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-07 21:48:00 +0800

    Adding basic implementation

--- src/fitter/data_model.py
+++ src/fitter/data_model.py
@@ -2,14 +2,28 @@
 Module containing DataModel class
 '''
 
+from zfit.core.interfaces   import ZfitPDF    as zpdf
+
+from dmu.stats         import utilities as sut
 from fitter.base_model import BaseModel
 from fitter.sim_fitter import SimFitter
 
+# ------------------------
 class DataModel(BaseModel):
     '''
     Model for fitting data samples
     '''
+    # ------------------------
     def __init__(self):
         '''
 
         '''
+    # ------------------------
+    def get_model(self) -> zpdf:
+        '''
+        Returns fitting model for data fit
+        '''
+        pdf = sut.get_model(name='s+b')
+
+        return pdf
+# ------------------------
