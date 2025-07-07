e620c36ac1eda6ac0868fcd86b8044738736ddf1
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-08 02:15:39 +0800

    Remove trailing spaces

--- src/fitter/base_fitter.py
+++ src/fitter/base_fitter.py
@@ -4,12 +4,11 @@ This module contains BaseFitter
 
 class BaseFitter:
     '''
-    Fitting base class, meant to 
+    Fitting base class, meant to
 
     - Provide basic functionality to fiters for data and simulation
     - Behave as a dependency sink, avoiding circular imports
     '''
     def __init__(self):
         '''
-        
         '''
