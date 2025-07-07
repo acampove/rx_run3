f0cb6efb032000dac9a2c603034b76d307ddba2d
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-07 22:29:54 +0800

    Adding log section and using pytest_configure

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,23 @@
 This file is needed by pytest
 '''
 
-import pytest
+from _pytest.config import Config
 
-from dmu.workflow.cache import Cache
+from dmu.workflow.cache    import Cache
+from dmu.logging.log_store import LogStore
 
-@pytest.fixture(scope='session', autouse=True)
-def setup_session():
+# ----------------------------------------
+def _set_logs() -> None:
+    LogStore.set_level('dmu:workflow:cache', 10)
+# ----------------------------------------
+def pytest_configure(config : Config):
     '''
     This function is called before any test run
     '''
+    _config = config
+
     # Use this as the root directory where everything gets
     # cached
     Cache.set_cache_root(root='/tmp/tests/fitter')
+
+    _set_logs()
