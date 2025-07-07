7b820f06658c0de2e3f570800ffddd767c65dcdf
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-08 00:12:12 +0800

    Remove toy model

--- src/fitter/data_model.py
+++ src/fitter/data_model.py
@@ -2,10 +2,11 @@
 Module containing DataModel class
 '''
 
-from dmu.stats.zfit         import zfit
+
 from zfit.core.interfaces   import ZfitPDF    as zpdf
 from zfit.core.interfaces   import ZfitSpace  as zobs
 
+from dmu.generic            import utilities  as gut
 from fitter.base_model      import BaseModel
 from fitter.sim_fitter      import SimFitter
 
@@ -19,15 +20,13 @@ class DataModel(BaseModel):
         '''
 
         '''
+
     # ------------------------
     def get_model(self, obs : zobs) -> zpdf:
         '''
         Returns fitting model for data fit
         '''
 
-        mu  = zfit.Parameter('mu', 5200, 4500, 6000)
-        sg  = zfit.Parameter('sg',   10,   10, 200)
-        pdf = zfit.pdf.Gauss(obs=obs, mu=mu, sigma=sg)
 
         return pdf
 # ------------------------
