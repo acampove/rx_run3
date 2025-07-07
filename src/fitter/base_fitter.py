da17673690cfe4ab9deae668d52481ae11d2abcd
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-08 02:16:38 +0800

    Implement fitting in base class

--- src/fitter/base_fitter.py
+++ src/fitter/base_fitter.py
@@ -17,6 +17,33 @@ class BaseFitter:
     - Provide basic functionality to fiters for data and simulation
     - Behave as a dependency sink, avoiding circular imports
     '''
+    # ------------------------
     def __init__(self):
         '''
+
         '''
+    # ------------------------
+    def _fit(
+            self,
+            cfg   : DictConfig,
+            data  : zdata,
+            model : zpdf) -> zres:
+        '''
+        Parameters
+        --------------------
+        cfg  : Fitting configuration
+        data : Zfit data object
+        model: Zfit PDF
+
+        Returns
+        --------------------
+        DictConfig object with parameters names, values and errors
+        '''
+        fit_cfg = OmegaConf.to_container(cfg, resolve=True)
+        fit_cfg = cast(dict, fit_cfg)
+
+        ftr = Fitter(pdf=model, data=data)
+        res = ftr.fit(cfg=fit_cfg)
+
+        return res
+# ------------------------
