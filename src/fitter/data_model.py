1f95d59f39b86061779ba39c6d9a3ae00e66d8ba
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-08 00:48:12 +0800

    Update imports

--- src/fitter/data_model.py
+++ src/fitter/data_model.py
@@ -2,11 +2,13 @@
 Module containing DataModel class
 '''
 
+from dmu.stats.zfit         import zfit
+from dmu.generic            import utilities  as gut
 
+from omegaconf              import DictConfig
 from zfit.core.interfaces   import ZfitPDF    as zpdf
 from zfit.core.interfaces   import ZfitSpace  as zobs
 
-from dmu.generic            import utilities  as gut
 from fitter.base_model      import BaseModel
 from fitter.sim_fitter      import SimFitter
 
