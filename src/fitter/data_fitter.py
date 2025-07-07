0cdb78edfd652a03c8db776768da4b487be45615
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-07 19:42:21 +0800

    Add basic implementation

--- src/fitter/data_fitter.py
+++ src/fitter/data_fitter.py
@@ -2,14 +2,33 @@
 Module containing DataFitter class
 '''
 
+from omegaconf          import DictConfig, OmegaConf
 from fitter.base_fitter import BaseFitter
-from fitter.data_model  import DataModel 
+from fitter.data_model  import DataModel
 
+# ------------------------
 class DataFitter(BaseFitter):
     '''
     Fitter for data
     '''
-    def __init__(self):
+    # ------------------------
+    def __init__(self, cfg : DictConfig):
         '''
+        cfg : configuration for the fit as a DictConfig object
+        '''
+        self._cfg = cfg
+    # ------------------------
+    def run(self) -> DictConfig:
+        '''
+        Runs fit
 
+        Returns
+        ------------
+        DictConfig object with fitting results
         '''
+        res = OmegaConf.create({
+            'mu' : {'value' : 1.0,
+                    'error' : 0.1}})
+
+        return res
+# ------------------------
