4e82badd981bc703dea3bf56d04322f63a9d3459
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-08 17:17:52 +0800

    Adding test for muon fit

--- tests/test_data_fitter.py
+++ tests/test_data_fitter.py
@@ -23,3 +23,20 @@ def test_simple():
                 cfg    = cfg)
         res = ftr.run()
 # -------------------------------------------
+def test_muon():
+    '''
+    Test using toy data
+    '''
+    cfg = gut.load_conf(
+        package='fitter_data',
+        fpath  ='tests/data_muon.yaml')
+
+    with Cache.turn_off_cache(val=True) and RDFGetter.max_entries(value=100_000):
+        ftr = DataFitter(
+                sample = 'DATA_24_MagDown_24c2',
+                trigger= 'Hlt2RD_BuToKpMuMu_MVA',
+                project= 'rx',
+                q2bin  = 'jpsi',
+                cfg    = cfg)
+        res = ftr.run()
+# -------------------------------------------
