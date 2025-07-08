abce7e9846e9e57926fb9360986b9412ac8448d0
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-08 17:40:21 +0800

    Better handling of context managers

--- tests/test_data_fitter.py
+++ tests/test_data_fitter.py
@@ -5,6 +5,7 @@ Module meant to test DataFitter class
 from dmu.workflow.cache import Cache
 from dmu.generic        import utilities  as gut
 from rx_data.rdf_getter import RDFGetter
+from rx_selection       import selection  as sel
 from fitter.data_fitter import DataFitter
 
 # -------------------------------------------
@@ -32,7 +33,10 @@ def test_muon():
         package='fitter_data',
         fpath  ='tests/data_muon.yaml')
 
-    with Cache.turn_off_cache(val=True) and RDFGetter.max_entries(value=100_000):
+    with Cache.turn_off_cache(val=True), \
+         sel.custom_selection(d_sel = {'bdt' : '(1)'}), \
+         RDFGetter.max_entries(value=300_000):
+
         ftr = DataFitter(
                 sample = 'DATA_24_MagDown_24c2',
                 trigger= 'Hlt2RD_BuToKpMuMu_MVA',
