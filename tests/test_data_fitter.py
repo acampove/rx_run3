5cbff72bc6341967db27f6b44d75bbcfe7d6c7fb
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-08 17:18:02 +0800

    Updating naming for tests with toys

--- tests/test_data_fitter.py
+++ tests/test_data_fitter.py
@@ -4,16 +4,17 @@ Module meant to test DataFitter class
 
 from dmu.workflow.cache import Cache
 from dmu.generic        import utilities  as gut
+from rx_data.rdf_getter import RDFGetter
 from fitter.data_fitter import DataFitter
 
 # -------------------------------------------
-def test_simple():
+def test_toy():
     '''
-    Simplest test
+    Test using toy data
     '''
     cfg = gut.load_conf(
-            package='fitter_data',
-            fpath  ='tests/data_fit.yaml')
+        package='fitter_data',
+        fpath  ='tests/data_toy.yaml')
     with Cache.turn_off_cache(val=True):
         ftr = DataFitter(
                 sample = 'data_toy',
