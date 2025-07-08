4363b4243c21dea74b4988cf09f34fd50dce74e9
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-08 21:09:58 +0800

    Adding test for nomc components

--- tests/test_sim_fitter.py
+++ tests/test_sim_fitter.py
@@ -8,6 +8,22 @@ from dmu.generic       import utilities as gut
 from fitter.sim_fitter import SimFitter
 
 # ---------------------------------------------------
+def test_nomc():
+    '''
+    Test for components that have no MC associated
+    '''
+    obs = zfit.Space('B_Mass', limits=(4500, 7000))
+
+    cfg = gut.load_conf(package='fitter_data', fpath='tests/combinatorial.yaml')
+    ftr = SimFitter(
+        name    = 'combinatorial',
+        obs     = obs,
+        cfg     = cfg,
+        trigger = '',
+        project = '',
+        q2bin   = '')
+    pdf = ftr.get_model()
+# ---------------------------------------------------
 def test_toy():
     '''
     Simplest test of fitter with toy data
