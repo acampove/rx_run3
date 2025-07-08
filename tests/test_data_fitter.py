3a56af5eb6ba6338bd162d6d9bb7d81e356c5467
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-09 02:28:43 +0800

    Adding test for resonant electron

--- tests/test_data_fitter.py
+++ tests/test_data_fitter.py
@@ -64,3 +64,23 @@ def test_rare_muon(q2bin : str):
             cfg    = cfg)
         ftr.run()
 # -------------------------------------------
+def test_reso_electron():
+    '''
+    Test using toy data
+    '''
+    cfg = gut.load_conf(
+        package='fitter_data',
+        fpath  ='reso/electron/data.yaml')
+
+    with Cache.turn_off_cache(val=False), \
+         sel.custom_selection(d_sel = {'bdt' : '(1)'}), \
+         RDFGetter.max_entries(value=100_000):
+
+        ftr = DataFitter(
+                sample = 'DATA_24_MagDown_24c2',
+                trigger= 'Hlt2RD_BuToKpEE_MVA',
+                project= 'rx',
+                q2bin  = 'jpsi',
+                cfg    = cfg)
+        ftr.run()
+# -------------------------------------------
