83f0bfcd11b008bf449255b4a835ee01a80d87f1
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-09 19:40:34 +0800

    Do not just declare, initialize to something

--- src/fitter/prec.py
+++ src/fitter/prec.py
@@ -56,7 +56,7 @@ class PRec(Cache):
         self._d_wg     = copy.deepcopy(d_weight)
 
         self._name     : str
-        self._df       : pnd.DataFrame
+        self._d_df     : dict[str,pnd.DataFrame] = {}
         self._d_fstat  = {}
         d_rdf, uid     = self._get_samples_rdf()
         self._d_rdf    = d_rdf
