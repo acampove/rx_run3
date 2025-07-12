5a6c69c84b76a960449903a112646418ee6e245d
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-13 00:34:38 +0800

    Put prec_scales in debug

--- tests/conftest.py
+++ tests/conftest.py
@@ -104,6 +104,7 @@ def _set_logs() -> None:
     LogStore.set_level('fitter:sim_fitter'          , 10)
     LogStore.set_level('fitter:data_preprocessor'   , 10)
     LogStore.set_level('fitter:prec'                , 10)
+    LogStore.set_level('fitter:prec_scales'         , 10)
 
     # Silence what is below
     LogStore.set_level('rx_selection:selection'     , 30)
