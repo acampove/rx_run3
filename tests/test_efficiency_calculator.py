29dc36e7c81bcd49c948de6894bc8b9841a94eee
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-12 23:05:53 +0800

    Overriding selection to check for hash invalidation

--- tests/test_efficiency_calculator.py
+++ tests/test_efficiency_calculator.py
@@ -3,6 +3,7 @@ Module with functions needed to test EfficiencyCalculator class
 '''
 import pytest
 from dmu.logging.log_store                 import LogStore
+from rx_selection                          import selection as sel
 from rx_efficiencies.efficiency_calculator import EfficiencyCalculator
 
 log = LogStore.add_logger('rx_efficiencies:test_efficiency_calculator')
@@ -30,8 +31,9 @@ def test_efficiency(q2bin : str, sample : str):
     Tests retrieval of total efficiency (acceptance x reco x selection)
     for given sample
     '''
-    obj         = EfficiencyCalculator(q2bin=q2bin)
-    eff         = obj.get_efficiency(sample=sample)
+    with sel.custom_selection(d_sel={'bdt' : '1'}):
+        obj         = EfficiencyCalculator(q2bin=q2bin)
+        eff         = obj.get_efficiency(sample=sample)
 
     assert 0 <= eff < 1
 #-------------------------------------------------
