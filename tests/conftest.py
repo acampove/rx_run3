72f4e1c16ef62b997ac3c41587a3ad9336df4591
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-13 20:49:08 +0800

    Put base_fitter in debug and hide efficiency_calculator logs

--- tests/conftest.py
+++ tests/conftest.py
@@ -103,19 +103,22 @@ def _set_logs() -> None:
     LogStore.set_level('fitter:data_model'          , 10)
     LogStore.set_level('fitter:sim_fitter'          , 10)
     LogStore.set_level('fitter:data_fitter'         , 10)
+    LogStore.set_level('fitter:base_fitter'         , 10)
     LogStore.set_level('fitter:data_preprocessor'   , 10)
     LogStore.set_level('fitter:prec'                , 10)
     LogStore.set_level('fitter:prec_scales'         , 10)
     LogStore.set_level('rx_fitter:constraint_reader', 10)
 
     # Silence what is below
-    LogStore.set_level('rx_selection:selection'     , 30)
-    LogStore.set_level('rx_selection:truth_matching', 30)
-    LogStore.set_level('rx_data:path_splitter'      , 30)
-    LogStore.set_level('rx_data:rdf_getter'         , 30)
-    LogStore.set_level('dmu:workflow:cache'         , 30)
-    LogStore.set_level('dmu:stats:model_factory'    , 30)
-    LogStore.set_level('dmu:zfit_plotter'           , 30)
+
+    LogStore.set_level('rx_efficiencies:efficiency_calculator', 30)
+    LogStore.set_level('rx_selection:selection'               , 30)
+    LogStore.set_level('rx_selection:truth_matching'          , 30)
+    LogStore.set_level('rx_data:path_splitter'                , 30)
+    LogStore.set_level('rx_data:rdf_getter'                   , 30)
+    LogStore.set_level('dmu:workflow:cache'                   , 30)
+    LogStore.set_level('dmu:stats:model_factory'              , 30)
+    LogStore.set_level('dmu:zfit_plotter'                     , 30)
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
     os.environ['GRPC_VERBOSITY'] = 'ERROR'
