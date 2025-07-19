9a503b5a2c58e926abf95efbc5bc7f014fecea55
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-20 07:19:14 +0800

    Add docstring

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -4,9 +4,10 @@ This script tests uses of typehinting with ROOT
 from ROOT import RDataFrame, RDF
 
 def test_rdf_type() -> RDF.RNode|None:
+    '''
+    This method tests using RDF.RNode as return type
+    '''
     rdf = RDataFrame(10)
     print('testing')
 
     return rdf
-
-test_rdf_type()
