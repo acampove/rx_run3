31c4b28353c94fd198556882357f7d9b287269c6
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-22 05:19:35 +0800

    Adding pytest configure

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,20 @@ This module is needed to control how pytest will run tests
 '''
 
 import pytest
-from dmu.generic        import utilities as gut
-from rx_data.rdf_getter import RDFGetter
+from dmu.generic           import utilities as gut
+from dmu.logging.log_store import LogStore
+from _pytest.config        import Config
+from rx_data.rdf_getter    import RDFGetter
 
+# ------------------------------
+def pytest_configure(config : Config):
+    '''
+    This will run before any test by pytest
+    '''
+    _config = config
+
+    LogStore.set_level('rx_data:rdf_getter12', 10)
+    LogStore.set_level('rx_data:rdf_getter'  , 10)
 # -----------------------------------------------
 @pytest.fixture(autouse=True)
 def rdf_getter_configuration():
