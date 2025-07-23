0ecf245bae8d38dcb0c7c378bb02596f089efbd9
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-23 08:31:23 +0800

    Delete get_stats test
    This will be made private. We only need efficiencies through
    get_efficiency

--- tests/test_efficiency_calculator.py
+++ tests/test_efficiency_calculator.py
@@ -12,18 +12,6 @@ log = LogStore.add_logger('rx_efficiencies:test_efficiency_calculator')
 def _initialize():
     LogStore.set_level('rx_efficiencies:efficiency_calculator', 10)
 #-------------------------------------------------
-@pytest.mark.parametrize('q2bin', ['low', 'central', 'high'])
-def test_stats(q2bin : str):
-    '''
-    Tests calculation of total efficiency (acceptance x reco x selection)
-    for multiple MC samples
-    '''
-    obj         = EfficiencyCalculator(q2bin=q2bin)
-    obj.out_dir = '/tmp/tests/rx_efficiencies/efficiency_calculator/stats'
-    df          = obj.get_stats()
-
-    assert len(df) > 0
-#-------------------------------------------------
 @pytest.mark.parametrize('sample', ['Bu_JpsiK_ee_eq_DPC'])
 @pytest.mark.parametrize('q2bin' , ['low', 'central', 'high'])
 def test_efficiency(q2bin : str, sample : str):
