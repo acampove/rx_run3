46578556ed2cf0c4280b7a09606b7a1e89cd2734
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-26 06:45:53 +0800

    Can add bool too

--- src/dmu/generic/typing_utilities.py
+++ src/dmu/generic/typing_utilities.py
@@ -2,14 +2,14 @@
 This module contains utility functions related to typing
 '''
 
-from typing import TypeVar, Type
+from typing import TypeVar, Type, cast
 import pandas as pnd
 import numpy
 
 from dmu.logging.log_store import LogStore
 
 log = LogStore.add_logger('dmu:generic:typing')
-Num = TypeVar('Num', int, float)
+Num = TypeVar('Num', int, float, bool)
 # ----------------------
 def numeric_from_series(
     row     : pnd.Series,
@@ -23,19 +23,28 @@ def numeric_from_series(
     --------------
     row    : Pandas dataframe row
     name   : Name of column whose value to extract
-    numeric: A numeric type, supported int, float or numpy number
+    numeric: A numeric type, supported int, float, bool
 
     Returns
     --------------
-    Either an int or a float with the value of the column
+    Either an int a float or a bool with the value of the column
     '''
     if name not in row:
         raise ValueError(f'Cannot find {name} in row: {row}')
 
     val = row[name]
 
-    if not isinstance(val, (int, float, numpy.number)):
+    if not isinstance(val, (int, float, bool, numpy.integer, numpy.floating, numpy.bool_)):
         raise ValueError(f'Value {name}={val} is not numeric')
 
-    return numeric(val)
+    if numeric is int:
+        return cast(Num, int(val))
+
+    if numeric is float:
+        return cast(Num, float(val))
+
+    if numeric is bool:
+        return cast(Num, bool(val))
+
+    raise TypeError(f'Invalid type {numeric}, only int or float are supported')
 # ----------------------
