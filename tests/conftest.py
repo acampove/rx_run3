f9b5aedd04b297ca08aa24e4373fdf7c60fe2d39
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-25 19:57:23 +0800

    Managing logs in single function

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,20 @@ import mplhep
 import matplotlib.pyplot as plt
 
 from _pytest.config import Config
+from dmu.logging.log_store import LogStore
+
+# ----------------------
+def _set_logs() -> None:
+    '''
+    Used to silence some loggers and put others in debug
+    '''
+    LogStore.set_level('rx_selection:selection'      , 30)
+    LogStore.set_level('rx_data:rdf_getter'          , 30)
+    LogStore.set_level('dmu:ml:cv_predict'           , 30)
+    LogStore.set_level('rx_classifier:mva_calculator', 10)
+
+    logging.getLogger('PIL').setLevel(logging.WARNING)
+    logging.getLogger('matplotlib').setLevel(logging.WARNING)
 # ------------------------------
 def pytest_configure(config : Config):
     '''
@@ -15,9 +29,7 @@ def pytest_configure(config : Config):
     '''
     _config = config
     numpy.random.seed(42)
-
-    logging.getLogger('PIL').setLevel(logging.WARNING)
-    logging.getLogger('matplotlib').setLevel(logging.WARNING)
+    _set_logs()
 
     plt.style.use(mplhep.style.LHCb2)
 # ------------------------------
