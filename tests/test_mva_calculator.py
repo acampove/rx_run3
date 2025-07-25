adffb1cdbe2592b1b56bd073e1856a56e9c39d36
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-25 20:08:48 +0800

    Parametrize tests  over triggers

--- tests/test_mva_calculator.py
+++ tests/test_mva_calculator.py
@@ -2,12 +2,20 @@
 Module containing tests for MVACalculator class
 '''
 
+import pytest
 from ROOT                         import RDF, RDataFrame
 from dmu.logging.log_store        import LogStore
 from rx_data.rdf_getter           import RDFGetter
 from rx_classifier.mva_calculator import MVACalculator
 
 log=LogStore.add_logger('rx_classifier:test_mva_calculator')
+
+# ----------------------
+class Data:
+    '''
+    Class meant to be used to share attributes
+    '''
+    l_trigger= ['Hlt2RD_BuToKpMuMu_MVA', 'Hlt2RD_BuToKpEE_MVA']
 # ----------------------
 def _validate_rdf(rdf : RDF.RNode) -> None:
     '''
@@ -20,12 +28,12 @@ def _validate_rdf(rdf : RDF.RNode) -> None:
     nentries = rdf.Count().GetValue()
     assert nentries > 0
 # ----------------------
-def test_simple() -> None:
+@pytest.mark.parametrize('trigger', Data.l_trigger)
+def test_simple(trigger : str) -> None:
     '''
     Simplest test validating MVACalculator
     '''
     sample = 'DATA_24*'
-    trigger= 'Hlt2RD_BuToKpMuMu_MVA'
     version= 'v7p7'
 
     with RDFGetter.max_entries(value=100):
