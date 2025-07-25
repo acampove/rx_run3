2d8e913487d54d76162e36f0902133932d07e242
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-26 04:46:10 +0800

    Improving logic of version comparison

--- src/dmu/generic/version_management.py
+++ src/dmu/generic/version_management.py
@@ -10,61 +10,84 @@ from dmu.logging.log_store import LogStore
 
 log=LogStore.add_logger('dmu:version_management')
 #---------------------------------------
-def _get_numeric_version(version : str) -> int:
-    '''
-    Takes string with numbers at the end (padded or not)
-    Returns integer version of numbers
-    '''
-    #Skip these directories
-    if version in ['__pycache__']:
-        return -1
+def _get_numeric_version(version : str) -> tuple[int,int]|None:
+    r'''
+    Parameters
+    -------------
+    version: Version name, e.g v1.0, v3, v7p3.
 
-    regex=r'[a-z]+(\d+)'
-    mtch =re.match(regex, version)
+    Returns
+    -------------
+    Tuple with major and minor version, e.g. (1,0), (3,0) (7,3)
+
+    Raises
+    -------------
+    If ANY file or directory that does not follow the regex is found
+    which is not among: README.md, comparison.
+    '''
+    if version in ['README.md', 'comparison']:
+        return None
+
+    version = version.replace('p', '.')
+    mtch    =re.match(r'^v(\d+)(\.\d+)?$', version)
     if not mtch:
-        log.debug(f'Cannot extract numeric version from: {version}')
-        return -1
+        raise ValueError(f'Cannot extract numeric version from: {version}')
 
-    str_val = mtch.group(1)
-    val     = int(str_val)
+    major, minor= mtch.groups()
+    major = int(major)
 
-    return val
+    if minor is None:
+        return major, 0
+
+    minor = minor.replace('.', '')
+    minor = int(minor)
+
+    return major, minor
 #---------------------------------------
-def get_last_version(dir_path : str, version_only : bool = True, main_only : bool = False):
-    '''Returns path or just version associated to latest version found in given path
-
+def get_last_version(dir_path : str, version_only : bool = True) -> str:
+    r'''
     Parameters
     ---------------------
-    dir_path (str) : Path to directory where versioned subdirectories exist
-    version_only (bool): Returns only vxxxx if True, otherwise, full path to directory
-    main_only (bool): Returns vX where X is a number. Otherwise it will return vx.y in case version has subversion
-    '''
-    l_obj = glob.glob(f'{dir_path}/*')
+    dir_path: Path to directory where versioned subdirectories exist
+              Anything matching `\d+`, e.g. v7, v1.2, v3p2.
+              It will make these into tuples like (7,0), (1,2) and (3,2)
+              and compare them
+    version_only : Controls type of returned string, see below
 
-    if len(l_obj) == 0:
+    Returns
+    ---------------------
+    Either:
+        Latest version, e.g v7, v1.2, v3p2, if version_only is True (default)
+        Or path to latest version, e.g. /path/to/latest/version/v7
+    '''
+    l_path = glob.glob(f'{dir_path}/*')
+
+    if len(l_path) == 0:
         raise ValueError(f'Nothing found in {dir_path}')
 
-    d_dir_org = { os.path.basename(obj).replace('.', '') : obj for obj in l_obj if os.path.isdir(obj) }
-    d_dir_num = { _get_numeric_version(name) : dir_path for name, dir_path in d_dir_org.items() }
+    d_dir_num = {}
+    for path in l_path:
+        fname = os.path.basename(path)
+        value = _get_numeric_version(fname)
+        if value is None:
+            continue
+
+        if value in d_dir_num:
+            raise ValueError(f'Found version for {fname} which collides with existing version')
+
+        d_dir_num[value] = path
 
     c_dir = sorted(d_dir_num.items())
+    if not c_dir:
+        raise ValueError(f'Cannot find path in: {dir_path}')
 
-    try:
-        _, path = c_dir[-1]
-    except Exception as exc:
-        raise ValueError(f'Cannot find path in: {dir_path}') from exc
+    _, path = c_dir[-1]
+    if not version_only:
+        return path
 
-    name = os.path.basename(path)
-    dirn = os.path.dirname(path)
+    version = os.path.basename(path)
 
-    if main_only and '.' in name:
-        ind = name.index('.')
-        name= name[:ind]
-
-    if version_only:
-        return name
-
-    return f'{dirn}/{name}'
+    return version
 #---------------------------------------
 def get_latest_file(dir_path : str, wc : str) -> str:
     '''Will find latest file in a given directory
