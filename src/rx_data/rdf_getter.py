393cb3698459a7eb1edbd9ca8ff79ffd5a9e6943
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-26 20:04:28 +0800

    add_truem moved closer to other methods that do the same

--- src/rx_data/rdf_getter.py
+++ src/rx_data/rdf_getter.py
@@ -621,6 +621,55 @@ class RDFGetter:
 
         return rdf
     # ---------------------------------------------------
+    @staticmethod
+    def add_truem(rdf : RDF.RNode) -> RDF.RNode:
+        '''
+        Takes ROOT dataframe associated to MC sample:
+
+        - Adds TRUEM branches missing
+
+        Returns dataframe
+        '''
+        log.info('Adding TRUEM branches')
+
+        tv_tp   = 'ROOT::Math::XYZVector'
+        fv_tp   = 'ROOT::Math::PtEtaPhiM4D<double>'
+
+        par_3d  =f'{tv_tp} PAR_3D(PAR_TRUEPX, PAR_TRUEPY, PAR_TRUEPZ); auto PAR_truept=PAR_3D.Rho(); auto PAR_trueeta=PAR_3D.Eta(); auto PAR_truephi=PAR_3D.Phi()'
+        l1_3d   = par_3d.replace('PAR', 'L1')
+        l2_3d   = par_3d.replace('PAR', 'L2')
+        kp_3d   = par_3d.replace('PAR',  'H')
+
+        lep_4d  =f'{fv_tp} PAR_4D(PAR_truept, PAR_trueeta, PAR_truephi, 0.511)'
+        kpl_4d  =f'{fv_tp} PAR_4D(PAR_truept, PAR_trueeta, PAR_truephi, 493.7)'
+        l1_4d   = lep_4d.replace('PAR', 'L1')
+        l2_4d   = lep_4d.replace('PAR', 'L2')
+        kp_4d   = kpl_4d.replace('PAR',  'H')
+
+        lv      =f'ROOT::Math::LorentzVector<{fv_tp}>(PAR_4D)'
+        lv1     = lv.replace('PAR', 'L1')
+        lv2     = lv.replace('PAR', 'L2')
+        lv3     = lv.replace('PAR',  'H')
+
+        jps_4d  =f'auto jpsi_4d = {lv1} + {lv2};'
+        bpl_4d  =f'auto bpls_4d = {lv1} + {lv2} + {lv3};'
+
+        expr_jp =f'{l1_3d}; {l2_3d}         ; {l1_4d}; {l2_4d}         ; {jps_4d}; auto val = jpsi_4d.M(); return val!=val ? {RDFGetter._JPSI_PDG_MASS} : val'
+        expr_bp =f'{l1_3d}; {l2_3d}; {kp_3d}; {l1_4d}; {l2_4d}; {kp_4d}; {bpl_4d}; auto val = bpls_4d.M(); return val!=val ? {RDFGetter._BPLS_PDG_MASS} : val'
+
+        log.debug('Jpsi_TRUEM')
+        log.debug('-->')
+        log.debug(expr_jp)
+
+        log.debug('B_TRUEM')
+        log.debug('-->')
+        log.debug(expr_bp)
+
+        rdf = rdf.Define('Jpsi_TRUEM', expr_jp)
+        rdf = rdf.Define(   'B_TRUEM', expr_bp)
+
+        return rdf
+    # ---------------------------------------------------
     # ---------------------------------------------------
     def _rdf_from_conf(self, conf_path : str) -> RDF.RNode:
         '''
@@ -732,55 +781,6 @@ class RDFGetter:
         val = val[:10]
 
         return val
-    # ---------------------------------------------------
-    @staticmethod
-    def add_truem(rdf : RDF.RNode) -> RDF.RNode:
-        '''
-        Takes ROOT dataframe associated to MC sample:
-
-        - Adds TRUEM branches missing
-
-        Returns dataframe
-        '''
-        log.info('Adding TRUEM branches')
-
-        tv_tp   = 'ROOT::Math::XYZVector'
-        fv_tp   = 'ROOT::Math::PtEtaPhiM4D<double>'
-
-        par_3d  =f'{tv_tp} PAR_3D(PAR_TRUEPX, PAR_TRUEPY, PAR_TRUEPZ); auto PAR_truept=PAR_3D.Rho(); auto PAR_trueeta=PAR_3D.Eta(); auto PAR_truephi=PAR_3D.Phi()'
-        l1_3d   = par_3d.replace('PAR', 'L1')
-        l2_3d   = par_3d.replace('PAR', 'L2')
-        kp_3d   = par_3d.replace('PAR',  'H')
-
-        lep_4d  =f'{fv_tp} PAR_4D(PAR_truept, PAR_trueeta, PAR_truephi, 0.511)'
-        kpl_4d  =f'{fv_tp} PAR_4D(PAR_truept, PAR_trueeta, PAR_truephi, 493.7)'
-        l1_4d   = lep_4d.replace('PAR', 'L1')
-        l2_4d   = lep_4d.replace('PAR', 'L2')
-        kp_4d   = kpl_4d.replace('PAR',  'H')
-
-        lv      =f'ROOT::Math::LorentzVector<{fv_tp}>(PAR_4D)'
-        lv1     = lv.replace('PAR', 'L1')
-        lv2     = lv.replace('PAR', 'L2')
-        lv3     = lv.replace('PAR',  'H')
-
-        jps_4d  =f'auto jpsi_4d = {lv1} + {lv2};'
-        bpl_4d  =f'auto bpls_4d = {lv1} + {lv2} + {lv3};'
-
-        expr_jp =f'{l1_3d}; {l2_3d}         ; {l1_4d}; {l2_4d}         ; {jps_4d}; auto val = jpsi_4d.M(); return val!=val ? {RDFGetter._JPSI_PDG_MASS} : val'
-        expr_bp =f'{l1_3d}; {l2_3d}; {kp_3d}; {l1_4d}; {l2_4d}; {kp_4d}; {bpl_4d}; auto val = bpls_4d.M(); return val!=val ? {RDFGetter._BPLS_PDG_MASS} : val'
-
-        log.debug('Jpsi_TRUEM')
-        log.debug('-->')
-        log.debug(expr_jp)
-
-        log.debug('B_TRUEM')
-        log.debug('-->')
-        log.debug(expr_bp)
-
-        rdf = rdf.Define('Jpsi_TRUEM', expr_jp)
-        rdf = rdf.Define(   'B_TRUEM', expr_bp)
-
-        return rdf
     # ---------------------------------------------------
     @staticmethod
     def split_per_file(
