04091c096d5107e54934898a54bbdeb46c8a9cff
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-29 22:09:10 +0800

    Add resonant bins to q2bin plots

--- src/rx_plotter_scripts/efficiency_vs_q2.py
+++ src/rx_plotter_scripts/efficiency_vs_q2.py
@@ -340,7 +340,7 @@ def main():
     Data.cfg = gut.load_conf(package='rx_plotter_data', fpath='efficiency/vs_q2.yaml')
 
     df = _get_data()
-    for q2bin in ['none', 'low', 'central', 'high']:
+    for q2bin in ['none', 'low', 'central', 'jpsi', 'psi2', 'high']:
         _plot(df=df, var='q2'     , q2bin=q2bin)
         _plot(df=df, var='q2_true', q2bin=q2bin)
 # ----------------------
