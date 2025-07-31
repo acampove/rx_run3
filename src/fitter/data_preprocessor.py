d48d22fe096e6211675e61861b0d627393ba2bbd
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-31 21:51:48 +0800

    Fix logic of selection application
    - Better message
    - If doing data, cut will be None, manager will do nothing, i.e. keep
      already overriden selection.
    - If doing mc, cut might exist and be associated to category (e.g. brem).
      Then need to allow for overriding of selection.

--- src/fitter/data_preprocessor.py
+++ src/fitter/data_preprocessor.py
@@ -86,9 +86,10 @@ class DataPreprocessor(Cache):
         rdf = gtr.get_rdf(per_file=False)
         uid = gtr.get_uid()
 
-        log.debug('Applying selection')
-        cut = {} if cut is None else cut
-        with sel.custom_selection(d_sel=cut):
+        log.debug(f'Applying selection to {self._sample}/{self._trigger}')
+        
+        # overriding only happens for simulation samples
+        with sel.custom_selection(d_sel=cut, force_override=True):
             rdf = sel.apply_full_selection(
                 rdf     = rdf,
                 uid     = uid,
