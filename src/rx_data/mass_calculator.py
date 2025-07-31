65cfcaff8922b7c2d045c27b09ac6e198f0c63c4
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-01 06:18:43 +0800

    Adding helper functions to do 4D math

--- src/rx_data/mass_calculator.py
+++ src/rx_data/mass_calculator.py
@@ -41,6 +41,108 @@ class MassCalculator:
 
         return out
     # ----------------------
+    def _get_hxy_mass(
+        self,
+        row : pnd.Series,
+        x   : int,
+        y   : int) -> float:
+        '''
+        Parameters
+        -------------
+        row: Series with event information
+        x/y: PDG ID to replace L1/L2 lepton with
+
+        Returns
+        -------------
+        Value of mass when leptons get pion, hadron, etc mass hypothesis
+        '''
+        had_4d = self._get_hadronic_system_4d(row=row)
+        par_1  = self._build_particle(row=row, name='L1', pid=x)
+        par_2  = self._build_particle(row=row, name='L2', pid=y)
+
+        candidate = had_4d + par_1 + par_2
+        candidate = cast(v4d, candidate)
+
+        return candidate.mass
+    # ----------------------
+    def _build_particle(
+        self,
+        row  : pnd.Series,
+        name : str,
+        pid  : int) -> v4d:
+        '''
+        Parameters
+        -------------
+        row  : Pandas series with event information
+        name : Name of particle in original ROOT dataframe, e.g. L1
+        pid  : PDG ID of particle that needs to be built, e.g. 11 for electron
+              Will be used to get mass.
+
+        Returns
+        -------------
+        Particle with the kinematics in the original dataframe
+        but with the mass hypothesis corresponding to
+        '''
+        mass     = self._mass_from_pid(pid=pid)
+        particle = self._get_particle(row=row, name=name)
+
+        return v4d(pt=particle.pt, eta=particle.eta, phi=particle.phi, mass=mass)
+    # ----------------------
+    def _get_hadronic_system_4d(self, row : pnd.Series) -> v4d:
+        '''
+        Parameters
+        -------------
+        row: Pandas series with event information
+
+        Returns
+        -------------
+        Four momentum vector of hadronic system
+        '''
+        b_4d  = self._get_particle(row=row, name='B')
+        l1_4d = self._get_particle(row=row, name='L1')
+        l2_4d = self._get_particle(row=row, name='L2')
+
+        res   = b_4d - l1_4d - l2_4d
+        res   = cast(v4d, res)
+
+        return res
+    # ----------------------
+    def _get_particle(self, row : pnd.Series, name : str) -> v4d:
+        '''
+        Parameters
+        -------------
+        row: Pandas series with event information
+        name: Name of particle whose 4D vector to extract
+
+        Returns
+        -------------
+        4D vector for particle
+        '''
+        particle_id = tut.numeric_from_series(row, f'{name}_ID' ,   int)
+        pt = tut.numeric_from_series(row, f'{name}_PT' , float)
+        et = tut.numeric_from_series(row, f'{name}_ETA', float)
+        ph = tut.numeric_from_series(row, f'{name}_PHI', float)
+        mass = self._mass_from_pid(pid=particle_id)
+
+        return v4d(pt=pt, eta=et, phi=ph, mass=mass)
+    # ----------------------
+    def _mass_from_pid(self, pid : int) -> float:
+        '''
+        Parameters
+        -------------
+        pid: Particle PDG ID
+
+        Returns
+        -------------
+        Mass of particle
+        '''
+        particle = part.from_pdgid(pid)
+        mass     = particle.mass
+        if mass is None:
+            raise ValueError(f'Cannot find mass of particle with ID: {pid}')
+
+        return mass
+    # ----------------------
     def _is_valid_column(self, name : str) -> bool:
         '''
         Parameters
