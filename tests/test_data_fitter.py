83e66fe493fcd5cf10c1f475acfff024dc973c5a
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-04 06:10:17 +0800

    Add test for simultaneous fits

--- tests/test_data_fitter.py
+++ tests/test_data_fitter.py
@@ -28,3 +28,29 @@ def test_single_region() -> None:
     ftr = DataFitter(d_nll=d_nll, cfg=cfg)
     ftr.run()
 # ----------------------
+def test_two_regions() -> None:
+    '''
+    Test simultaneous fit with two regions
+    '''
+    obs     = None#zfit.Space('mass', limits=(4500, 7000))
+
+    pdf_001 = sut.get_model(obs=obs, kind='s+b', suffix='001')
+    dat_001 = pdf_001.create_sampler(10_000)
+    nll_001 = zfit.loss.ExtendedUnbinnedNLL(data=dat_001, model=pdf_001)
+
+    pdf_002 = sut.get_model(obs=obs, kind='s+b', suffix='002')
+    dat_002 = pdf_002.create_sampler(10_000)
+    nll_002 = zfit.loss.ExtendedUnbinnedNLL(data=dat_002, model=pdf_002)
+
+    sel_cfg = {'default' : {}, 'fit' : {}}
+    sel_cfg = OmegaConf.create(obj=sel_cfg)
+    d_nll   = {
+        'region_001' : (nll_001, sel_cfg),
+        'region_002' : (nll_002, sel_cfg),
+    }
+
+    with goc.GofCalculator.disabled(True):
+        cfg = gut.load_conf(package='fitter_data', fpath='tests/single_region.yaml')
+        ftr = DataFitter(d_nll=d_nll, cfg=cfg)
+        ftr.run()
+# ----------------------
