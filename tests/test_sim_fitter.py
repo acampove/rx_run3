a3ff7dd36f7bfa847092878c3a2309fb6c9e2794
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-04 04:44:16 +0800

    Remove test_toy
    Won't test with toys for now

--- tests/test_sim_fitter.py
+++ tests/test_sim_fitter.py
@@ -28,22 +28,6 @@ def test_nomc():
         q2bin   = 'low')
     pdf = ftr.get_model()
 # ---------------------------------------------------
-def test_toy():
-    '''
-    Simplest test of fitter with toy data
-    '''
-    obs = zfit.Space('B_Mass', limits=(4500, 7000))
-
-    cfg = gut.load_conf(package='fitter_data', fpath='tests/signal_toy.yaml')
-    ftr = SimFitter(
-        component= 'gauss_toy',
-        obs     = obs,
-        cfg     = cfg,
-        trigger = '',
-        project = '',
-        q2bin   = '')
-    ftr.get_model()
-# ---------------------------------------------------
 def test_nocat():
     '''
     Test for components without categories, e.g. muon
