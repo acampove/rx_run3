5a00544ebd7d661ec2a0ce36f656dd962c0f1c1d
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-09 14:52:07 +0800

    Changes to misID test
    - Use correct config, data_misid.yaml instead of data.yaml
    - Need name parameter in order to pick up the correct yield from the
      schema

--- tests/test_data_model.py
+++ tests/test_data_model.py
@@ -83,7 +83,7 @@ def test_misid_rare(tag_cut : str, observable : str):
     obs = zfit.Space(f'B_Mass_{observable}', limits=(4500, 7000))
     cfg = gut.load_conf(
         package='fitter_data',
-        fpath  ='misid/electron/data.yaml')
+        fpath  ='misid/electron/data_misid.yaml')
 
     l1_in_cr = f'((L1_PROBNN_E < 0.2) || (L1_PID_E < 3.0)) && L1_{tag_cut}'
     l2_in_cr = f'((L2_PROBNN_E < 0.2) || (L2_PID_E < 3.0)) && L2_{tag_cut}'
@@ -98,6 +98,7 @@ def test_misid_rare(tag_cut : str, observable : str):
         'mass'  : '(1)',
         'bdt'   : 'mva_cmb > 0.80 && mva_prc > 0.60'}):
         dmd = DataModel(
+            name    = observable,
             cfg     = cfg,
             obs     = obs,
             trigger = 'Hlt2RD_BuToKpEE_MVA_noPID',
