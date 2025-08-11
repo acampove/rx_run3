c5a752da633da80ee087df53a2ce2f9d7081a8c7
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-12 07:38:50 +0800

    Remove ParameterHolder, moved to conftest

--- tests/test_constraint_reader.py
+++ tests/test_constraint_reader.py
@@ -5,11 +5,9 @@ Module with functions needed to test ConstraintReader class
 import pytest
 from dmu.stats.zfit           import zfit
 from dmu.logging.log_store    import LogStore
-from zfit.interface           import ZfitParameter as zpar
 from fitter.constraint_reader import ConstraintReader
 
 log=LogStore.add_logger('fitter:test_constraint_reader')
-
 # ----------------------
 class Data:
     '''
@@ -27,79 +25,6 @@ def initialize():
     This runs before any test
     '''
     LogStore.set_level('fitter:constraint_reader', 10)
-# --------------------------------------------------------------
-class ParametersHolder:
-    '''
-    Class used to instantiate objects needed to test ConstraintReader
-    '''
-    # ----------------------
-    def __init__(self, kind : str) -> None:
-        '''
-        Parameters
-        -------------
-        kind: Defines what parameters will be returning depending on test
-        '''
-        self._s_par = self._get_pars(kind=kind)
-    # ----------------------
-    def _get_pars(self, kind : str) -> set[zpar]:
-        '''
-        Parameters
-        -------------
-        kind: Type of parameters
-
-        Returns
-        -------------
-        Set of zfit parameters
-        '''
-        if   kind == 'rare_prec':
-            l_par_name = [
-                'pscale_yld_Bd_Kstee_eq_btosllball05_DPC',
-                'pscale_yld_Bu_Kstee_Kpi0_eq_btosllball05_DPC',
-                'pscale_yld_Bs_phiee_eq_Ball_DPC']
-        elif kind == 'rare_misid':
-            l_par_name = [
-                'yld_kpipi',
-                'yld_kkk']
-        elif kind == 'brem_frac':
-            l_par_name = [
-                'frac_brem_000',
-                'frac_brem_001',
-                'frac_brem_002']
-        elif kind == 'sig_par':
-            l_par_name = [
-                'ar_dscb_Signal_002_1_reso_flt',
-                'mu_Signal_000_scale_flt',
-                'mu_Signal_001_scale_flt',
-                'mu_Signal_002_scale_flt',
-                'nl_dscb_Signal_001_1_reso_flt',
-                'nr_dscb_Signal_002_1_reso_flt',
-                'sg_Signal_000_reso_flt',
-                'sg_Signal_001_reso_flt',
-                'sg_Signal_002_reso_flt',
-            ]
-        elif kind == 'invalid':
-            l_par_name = [
-                'ap_hypexp',
-                'bt_hypexp',
-                'mu_hypexp',
-                'ncmb',
-                'nsig']
-        else:
-            raise ValueError(f'Invalid kind of parameters: {kind}')
-
-        return { zfit.Parameter(name, 0, 0, 1) for name in l_par_name }
-    # ----------------------
-    def get_params(self, floating : bool) -> set[zpar]:
-        '''
-        Parameters
-        -------------
-        floating: Bool, meant to be True
-
-        Returns
-        -------------
-        Set of zfit parameters
-        '''
-        return self._s_par 
 # --------------------------------------------------------------
 def _print_constraints(d_cns : dict[str, tuple[float,float]]) -> None:
     for name, (value, error) in d_cns.items():
