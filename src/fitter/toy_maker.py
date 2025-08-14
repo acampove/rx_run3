c883a0b5e6cbb1889900fc9cf47c50fb2ae51644
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-14 08:53:43 +0800

    Update imports

--- src/fitter/toy_maker.py
+++ src/fitter/toy_maker.py
@@ -1,9 +1,14 @@
 '''
 Module holding ToyMaker class
 '''
+import tqdm
+import pandas as pnd
 
-from zfit.interface        import ZfitLoss
-from dmu.logging.log_store import LogStore
+from zfit.interface             import ZfitLoss   as zlos
+from zfit.minimizers.interface  import ZfitResult as zres
+from dmu.stats                  import utilities  as sut
+from dmu.logging.log_store      import LogStore
+from dmu.stats.fitter           import Fitter
 
 log=LogStore.add_logger('fitter:zfit_maker')
 # ----------------------
