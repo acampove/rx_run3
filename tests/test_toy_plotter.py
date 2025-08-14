83677e79e94145830c1f563f97853e44c3008638
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-14 09:49:34 +0800

    Adding file

---
+++ tests/test_toy_plotter.py
@@ -0,0 +1,54 @@
+'''
+Module with functions meant to test the ToyPlotter class
+'''
+from functools import lru_cache
+
+import numpy
+import pandas as pnd
+
+from dmu.logging.log_store import LogStore
+
+log=LogStore.add_logger('fitter:test_toy_plotter')
+# ----------------------
+@lru_cache(maxsize=3)
+def _get_df(ntoys : int, l_var : tuple[str]) -> pnd.DataFrame:
+    '''
+    Returns 
+    ------------------
+    dataframe with fake fit parameter information
+    '''
+    l_df_var = []
+    for var in l_var:
+        df_var              = pnd.DataFrame(columns=['Parameter', 'Value', 'Error', 'Toy', 'GOF', 'Converged'])
+        df_var['Parameter'] = ntoys * [var]
+        df_var['Value'    ] = numpy.random.normal(loc=0.0, scale=1.0, size=ntoys)
+        df_var['Error'    ] = numpy.random.normal(loc=1.0, scale=0.1, size=ntoys)
+
+        l_df_var.append(df_var)
+
+    df = pnd.concat(l_df_var)
+    df['Toy'] = df.index 
+    df = df.reset_index(drop=True)
+
+    arr_gof = numpy.random.uniform(low=0, high=1, size=ntoys)
+    arr_cnv = numpy.random.choice(a=[True, False], p=[0.95, 0.05], size=ntoys)
+    for itoy in range(ntoys):
+        df.loc[df.Toy == itoy,       'GOF'] = arr_gof[itoy]
+        df.loc[df.Toy == itoy, 'Converged'] = arr_cnv[itoy] 
+
+    return df
+# ----------------------
+def test_simple() -> None:
+    '''
+    This is the simplest test of ToyPlotter
+    '''
+    df = _get_df(ntoys=10_000, l_var=('a', 'b'))
+    print('')
+    print(df)
+    print(df.shape)
+
+    cfg= _get_cfg()
+
+    ptr= ToyPlotter(df=df, cfg=cfg)
+    ptr.plot()
+    
