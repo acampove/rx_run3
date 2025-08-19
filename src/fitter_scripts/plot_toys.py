52715303238ffa21633cf6f9cfbc6c99c526fbc4
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-19 12:19:56 +0800

    Simplify for local inputs
    - Remove source arg and variable
    - Remove update_config, which was giving output path, that now is trival

--- src/fitter_scripts/plot_toys.py
+++ src/fitter_scripts/plot_toys.py
@@ -8,7 +8,6 @@ from pathlib import Path
 from typing  import Iterable
 
 import pandas as pnd
-from omegaconf             import DictConfig
 from dmu.generic           import utilities as gut
 from dmu.logging.log_store import LogStore
 from fitter.toy_plotter    import ToyPlotter
@@ -20,7 +19,6 @@ class Data:
     Class meant to be used to share attributes
     '''
     log_lvl    : int
-    source     : str
     version    : str
     identifier : str
     dry_run    : bool
@@ -36,14 +34,12 @@ def _set_logs() -> None:
 def _parse_args() -> None:
     parser = argparse.ArgumentParser(description='Script used to make plots from outputs of toy fits')
     parser.add_argument('-v', '--version'    , type=str, help='Version of toy fits, e.g. v2', required=True)
-    parser.add_argument('-s', '--source'     , type=str, help='Identifier of place where parquet files are', required=True)
     parser.add_argument('-i', '--identifier' , type=str, help='Identifier of toy, if not passed, will do all toys')
     parser.add_argument('-l', '--log_level'  , type=int, help='Log level', default=20)
     parser.add_argument('-d', '--dry_run'    , help='If used, will skip plotting step', action='store_true')
     args = parser.parse_args()
 
     Data.version    = args.version
-    Data.source     = args.source
     Data.identifier = args.identifier
     Data.log_lvl    = args.log_level
     Data.dry_run    = args.dry_run
@@ -80,29 +76,6 @@ def _get_dataframes(source_path : str) -> dict[str, pnd.DataFrame]:
     d_df = { path : pnd.read_parquet(path) for path in l_path }
 
     return d_df
-# ----------------------
-def _update_config(cfg : DictConfig, path : str) -> DictConfig:
-    '''
-    Parameters
-    -------------
-    cfg: Config as loaded from YAML
-    path: Path to parquet file with dataframe data
-
-    Returns
-    -------------
-    Config with values overriden:
-    - Path to output has to be same as path to input, but in local machine
-    '''
-    source_path = cfg.paths[Data.source]
-    target_path = os.environ['ANADIR']
-    path        = path.replace(source_path, target_path)
-    path        = f'{path}/{Data.version}'
-
-    log.debug(f'Saving to: {path}')
-
-    cfg.paths.saving.plt_dir = path
-
-    return cfg
 # ----------------------
 def main():
     '''
