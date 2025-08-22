0bbda9543cd30b281c0e3eb7a1fed85e5cbf476f
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-22 12:25:16 +0800

    Remove logzero and add verbose level lines

--- tests/logging/test_log_store.py
+++ tests/logging/test_log_store.py
@@ -14,8 +14,8 @@ class Data:
     '''
     Class used to store shared data
     '''
-    l_backend = ['logging', 'logzero']
-    l_level   = [10, 20, 30, 40, 50]
+    l_backend = ['logging']
+    l_level   = [5, 10, 20, 30, 40, 50]
 # --------------------------------
 @pytest.mark.parametrize('backend', Data.l_backend)
 def test_show(backend : str):
@@ -34,32 +34,28 @@ def test_show(backend : str):
 
     LogStore.show_loggers()
 # --------------------------------
-@pytest.mark.parametrize('backend', Data.l_backend)
-def test_messages(backend : str):
+def test_messages():
     '''
     Tests each level
     '''
-    LogStore.backend = backend
 
-    name = f'messages_{backend}'
-    log = LogStore.add_logger(name)
-    LogStore.set_level(name, 10)
+    log = LogStore.add_logger('messages')
+    LogStore.set_level('messages', 5)
 
+    log.info('\n')
+    log.verbose('verbose')
     log.debug('debug')
     log.info('info')
     log.warning('warning')
     log.error('error')
     log.critical('critical')
 # --------------------------------
-@pytest.mark.parametrize('backend', Data.l_backend)
 @pytest.mark.parametrize('level'  , Data.l_level)
-def test_level(backend : str, level : int):
+def test_level(level : int):
     '''
     Test for level setting
     '''
-    LogStore.backend = backend
-
-    name = f'level_{backend}_{level}'
+    name = f'level_{level}'
 
     log=LogStore.add_logger(name)
     LogStore.set_level(name, level)
@@ -67,27 +63,6 @@ def test_level(backend : str, level : int):
     assert log.getEffectiveLevel() == level
 
     LogStore.show_loggers()
-# --------------------------------
-@pytest.mark.parametrize('level', Data.l_level)
-def test_logzero(level : int):
-    '''
-    Tests logzero
-    '''
-    LogStore.backend = 'logzero'
-
-    name = f'logzero_{level}'
-    log  = LogStore.add_logger(name)
-    LogStore.set_level(name, level)
-
-    print(30 * '-')
-    print(f'Level: {level}')
-    print(30 * '-')
-    log.debug('debug')
-    log.info('info')
-    log.warning('warning')
-    log.error('error')
-    log.critical('critical')
-    print(30 * '-')
 # --------------------------------
 def test_exists_ok_true():
     '''
@@ -113,23 +88,27 @@ def test_context_logger():
     log = LogStore.add_logger('context')
 
     print(20 * '-')
+    log.verbose('Verbose message')
     log.debug('Debug message')
     log.info('Info message')
     log.warning('Warning message')
     log.error('Error message')
     print(20 * '-')
-    with LogStore.level('context', 10):
+    with LogStore.level('context', 1):
+        log.verbose('Verbose message')
         log.debug('Debug message')
         log.info('Info message')
         log.warning('Warning message')
         log.error('Error message')
     print(20 * '-')
     with LogStore.level('context', 30):
+        log.verbose('Verbose message')
         log.debug('Debug message')
         log.info('Info message')
         log.warning('Warning message')
         log.error('Error message')
     print(20 * '-')
+    log.verbose('Verbose message')
     log.debug('Debug message')
     log.info('Info message')
     log.warning('Warning message')
