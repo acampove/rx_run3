748a40c556d75429c956edfb209a6c93d70fb842
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-23 16:13:47 +0800

    Ignore ROOT import pyright errors

--- src/rx_plotter_scripts/data_mc.py
+++ src/rx_plotter_scripts/data_mc.py
@@ -10,7 +10,7 @@ from dataclasses         import dataclass
 import yaml
 import mplhep
 import dmu.generic.utilities as gut
-from ROOT                    import RDataFrame
+from ROOT                    import RDataFrame # type: ignore
 from dmu.plotting.plotter_1d import Plotter1D
 from dmu.logging.log_store   import LogStore
 from rx_data.rdf_getter      import RDFGetter
