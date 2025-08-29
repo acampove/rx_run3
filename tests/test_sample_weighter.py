94a68916b4b582f1ebc02d80d1d8e885dfe1c9b8
Angel Campoverde <angel.campoverde@cern.ch>
2025-08-29 22:49:29 +0800

    Adding functions for closure test

--- tests/test_sample_weighter.py
+++ tests/test_sample_weighter.py
@@ -114,7 +114,133 @@ def _check_blocks(
     # Check that no shuffling of entries happened, using
     # array of block numbers
     assert numpy.array_equal(arr_block_inp, arr_block_out)
+# ----------------------
+def _closure_check(
+    df     : pnd.DataFrame,
+    sample : str,
+    block  : int,
+    is_sig : bool) -> None:
+    '''
+    Parameters
+    -------------
+    df    : Dataframe with weighted data
+    sample: MC sample
+    is_sig: If true, will read the signal region maps
+    '''
+    for has_brem in [0, 1]:
+        pidc = _get_pidc_map(has_brem=has_brem, block=block, sample=sample, is_sig=is_sig)
+        if pidc is None:
+            continue
+
+        hist = _build_map(df=df, has_brem=has_brem, block=block)
+
+        _check_maps(hist_calc=hist, hist_pidc=pidc)
+# ----------------------
+def _get_pidc_map(
+    has_brem : int, 
+    block    : int, 
+    sample   : str, 
+    is_sig   : bool) -> numpy.ndarray|None:
+    '''
+    Parameters
+    -------------
+    has_brem: If 1 will build map from tracks with brem only
+    sample  : MC sample
+    is_sig  : If true, will read the signal region maps
+
+    Returns
+    -------------
+    Calibration map from PIDCalib as numpy array 
+    '''
+    if   sample == 'Bu_KplKplKmn_eq_sqDalitz_DPC': 
+        particle = 'K'
+        cut      = '(PROBNN_E>0.2)&(DLLe>3.0)' if is_sig else '((PROBNN_E<0.2)|(DLLe<3.0))&(DLLe>-1.0)&(K_PROBNN_K>0.1)'
+    elif sample == 'Bu_piplpimnKpl_eq_sqDalitz_DPC':
+        particle = 'Pi'
+        cut      = '(PROBNN_E>0.2)&(DLLe>3.0)' if is_sig else '((PROBNN_E<0.2)|(DLLe<3.0))&(DLLe>-1.0)&(pi_PROBNN_K<0.1)'
+    else:
+        return
+
+    particle_cut = particle.lower() if particle == 'Pi' else particle
+    polarity     = 'down' if block in [6, 7] else 'up'
+
+    brem_name = {1 : 'brem', 0 : 'nobrem'}[has_brem]
+    ana_dir   = os.environ['ANADIR']
+    map_name  = f'effhists-2024_WithUT_block{block}_v2-{polarity}-{particle}-{cut}-log10({particle_cut}_PT).{particle_cut}_ETA.pkl'
+    fpath     = f'{ana_dir}/Calibration/mis_id/v14/{brem_name}/{map_name}'
+    with open(fpath, 'rb') as ifile:
+        map = pickle.load(ifile)
+
+    arr = map.values()
+    arr = numpy.where(numpy.isnan(arr), 0, arr)
+    arr = numpy.where(arr          > 1, 1, arr)
+
+    return arr
+# ----------------------
+def _check_maps(
+    hist_calc : numpy.ndarray, 
+    hist_pidc : numpy.ndarray) -> None: 
+    '''
+    Parameters
+    -------------
+    hist_calc: Efficiency map calculated from weighted data
+    hist_pidc: Efficiency map from PIDCalib 
+    '''
+    hist_diff = hist_pidc - hist_calc
+    hist_diff = numpy.where(hist_diff < 1e-7, 0, hist_diff)
+
+    assert numpy.isclose(0, hist_diff, rtol=1e-6).all()
+# ----------------------
+def _build_map(df : pnd.DataFrame, has_brem : int, block : int) -> numpy.ndarray:
+    '''
+    Parameters
+    -------------
+    df      : DataFrame with weights
+    has_brem: If 1 will build map from tracks with brem only
+    block   : Block from which map comes
+
+    Returns
+    -------------
+    Efficiency map
+    '''
+    df           = df[ df['block'     ] == block    ]
+    df_1         = df[ df['L1_HASBREM'] == has_brem ]
+    df_2         = df[ df['L2_HASBREM'] == has_brem ]
+
+    arr_pid_eff1 = df_1['pid_eff_l1'  ].to_numpy()
+    arr_pid_eff2 = df_2['pid_eff_l2'  ].to_numpy()
+    arr_pt_l1    = df_1['L1_TRACK_PT' ].to_numpy()
+    arr_pt_l2    = df_2['L2_TRACK_PT' ].to_numpy()
+    arr_et_l1    = df_1['L1_TRACK_ETA'].to_numpy()
+    arr_et_l2    = df_2['L2_TRACK_ETA'].to_numpy()
+
+    arr_eff      = numpy.concatenate((arr_pid_eff1, arr_pid_eff2))
+    arr_et       = numpy.concatenate((arr_et_l1   , arr_et_l2   ))
+    arr_pt       = numpy.concatenate((arr_pt_l1   , arr_pt_l2   ))
+    arr_pt       = numpy.log10(arr_pt)
+
+    l_xedge, l_yedge = _get_binning()
+
+    eff_wgt, _, _ = numpy.histogram2d(arr_pt, arr_et, bins=[l_xedge, l_yedge], weights=arr_eff)
+    eff_raw, _, _ = numpy.histogram2d(arr_pt, arr_et, bins=[l_xedge, l_yedge], weights=None   )
+
+    return numpy.where(eff_raw > 0, eff_wgt / eff_raw, 0)
+# ----------------------
+@cache
+def _get_binning() -> tuple[list[float], list[float]]:
+    '''
+    Returns
+    -------------
+    Tuple with two lists of floats, representing boundaries used to make
+    calibration maps
+    '''
+    data    = gut.load_conf(package='rx_pid_data', fpath='config/binning.yaml')
+    l_xedge = data['bin']['log10(PARTICLE_TRACK_PT)']
+    l_yedge = data['bin']['PARTICLE_TRACK_ETA']
+
+    return l_xedge, l_yedge
 # ----------------------------
+@pytest.mark.parametrize('block' , [1, 2, 3, 5, 6, 7, 8])
 @pytest.mark.parametrize('is_sig', [True, False])
 @pytest.mark.parametrize('sample', [
     'DATA_24_MagUp_24c2',
