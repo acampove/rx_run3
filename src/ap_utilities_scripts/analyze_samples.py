803aec8888e244a163481636ecf0ee317cbcf2b2
Angel Campoverde <angel.campoverde@cern.ch>
2025-09-03 18:16:26 +0800

    Adding file

---
+++ src/ap_utilities_scripts/analyze_samples.py
@@ -0,0 +1,95 @@
+'''
+Script in charge of:
+
+- Loading YAML file with event types
+- Checking which classified event types do not belong to the all section
+- Checking which event types are in the all section but not in the classified one
+- Save summary to YAML
+'''
+
+import argparse
+
+from dmu.generic           import utilities as gut
+from dmu.logging.log_store import LogStore
+from omegaconf import DictConfig
+
+log=LogStore.add_logger('ap_utilities:analyze_samples')
+# ----------------------
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Script used to update config with event types')
+    parser.add_argument('-c', '--config' , type=str, help='Name of config, e.g. by_priority', required=True)
+    args = parser.parse_args()
+
+    return args
+# ----------------------
+def _analyze_conf(cfg : DictConfig) -> None:
+    '''
+    This method creates YAML file with information mentioned in scripts main docstring
+
+    Parameters
+    -------------
+    cfg: Object storing event types, etc
+    '''
+    d_new = {}
+    for section, d_evt_type in cfg.items():
+        if section == 'all':
+            continue
+
+        tmp = { key : val for key, val in d_evt_type.items() if key not in cfg.all }
+        d_new.update(tmp)
+
+    d_missing = {}
+    for evt_type in cfg.all:
+        if _found_in_any_section(evt_type=evt_type, cfg=cfg):
+            continue
+
+        d_missing[evt_type] = _decay_from_evttype(evt_type=evt_type)
+
+    d_summary = { 'new' : d_new, 'missing' : d_missing }
+    gut.dump_json(data=d_summary, path='./summary.yaml')
+# ----------------------
+def _found_in_any_section(evt_type : int, cfg : DictConfig) -> bool:
+    '''
+    Parameters
+    -------------
+    evt_type: Event type
+    cfg: Config loaded from YAML file
+
+    Returns
+    -------------
+    True if evt_type is found in any of the sections, excluding `all`
+    '''
+    for section in cfg:
+        if section == 'all':
+            continue
+
+        data = cfg[section]
+
+        if evt_type in data:
+            return True
+
+    return False
+# ----------------------
+def _decay_from_evttype(evt_type : int) -> str:
+    '''
+    Parameters
+    -------------
+    evt_type: Event type
+
+    Returns
+    -------------
+    Latex string associated to event
+    '''
+    return str(evt_type)
+# ----------------------
+def main():
+    '''
+    Entry point
+    '''
+    args = _parse_args()
+    cfg  = gut.load_conf(package='ap_utilities_data', fpath=f'analyses/{args.config}.yaml')
+
+    _analyze_conf(cfg=cfg)
+# ----------------------
+if __name__ == '__main__':
+    main()
