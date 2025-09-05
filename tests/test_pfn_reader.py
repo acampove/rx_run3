2a4103635c1bded66aef2b15189829f486d1b5d8
Angel Campoverde <angel.campoverde@cern.ch>
2025-09-05 12:12:32 +0800

    Simplify test

--- tests/test_pfn_reader.py
+++ tests/test_pfn_reader.py
@@ -11,42 +11,20 @@ class Data:
     '''
     Class used to store shared data
     '''
-    gut.TIMER_ON=True
-    l_arg_rd_ap_2024 : list[tuple[str,str,int]] = [
-        ('rd_ap_2024', 'test_w31_34_v1r2266_ee', 1),
-        ('rd_ap_2024', 'test_w35_37_v1r2266_ee', 1),
-        ('rd_ap_2024', 'test_w37_39_v1r2266_ee', 1),
-        ('rd_ap_2024', 'test_w40_42_v1r2266_ee', 1),
-        ('rd_ap_2024', 'test_w31_34_v1r2266_mm', 1),
-        ('rd_ap_2024', 'test_w35_37_v1r2266_mm', 1),
-        ('rd_ap_2024', 'test_w37_39_v1r2266_mm', 1),
-        ('rd_ap_2024', 'test_w40_42_v1r2266_mm', 1),
-        ('rd_ap_2024', 'test_w31_34_v1r2437_ee', 1),
-        ('rd_ap_2024', 'test_w35_37_v1r2437_ee', 1),
-        ('rd_ap_2024', 'test_w37_39_v1r2437_ee', 1),
-        ('rd_ap_2024', 'test_w40_42_v1r2437_ee', 1),
-        ('rd_ap_2024', 'test_w31_34_v1r2437_mm', 1),
-        ('rd_ap_2024', 'test_w35_37_v1r2437_mm', 1),
-        ('rd_ap_2024', 'test_w37_39_v1r2437_mm', 1),
-        ('rd_ap_2024', 'test_w40_42_v1r2437_mm', 1)]
-
-    l_arg_nopid      : list[tuple[str,str,int]] = [
-        ('btoxll_mva_2024_nopid', 'w31_34_v1r3399', 1),
-        ('btoxll_mva_2024_nopid', 'w35_37_v1r3399', 1),
-        ('btoxll_mva_2024_nopid', 'w37_39_v1r3399', 1),
-        ('btoxll_mva_2024_nopid', 'w40_42_v1r3399', 2),
-        ('btoxll_mva_2024_nopid', 'w31_34_v1r3411', 1),
-        ('btoxll_mva_2024_nopid', 'w35_37_v1r3411', 1),
-        ('btoxll_mva_2024_nopid', 'w37_39_v1r3411', 1),
-        ('btoxll_mva_2024_nopid', 'w40_42_v1r3411', 2)]
+    l_arg_nopid : list[tuple[str,int]] = [
+        ('w31_34_v1r3411', 1),
+        ('w35_37_v1r3411', 1),
+        ('w37_39_v1r3411', 1),
+        ('w40_42_v1r3411', 2)]
 # -----------------------------
-@gut.timeit
-@pytest.mark.parametrize('production, nickname, expected', Data.l_arg_nopid)
-def test_simple(production : str, nickname : str, expected : int):
+@pytest.mark.parametrize('nickname, expected', Data.l_arg_nopid)
+def test_simple(nickname : str, expected : int):
     '''
     Test simple reading
     '''
-    cfg    = gut.load_conf(package='post_ap_data', fpath='post_ap/nopid/v1.yaml')
+    production = 'btoxll_mva_2024_nopid'
+
+    cfg    = gut.load_conf(package='post_ap_data', fpath='tests/pfn_reader/simple.yaml')
 
     reader = PFNReader(cfg=cfg)
     d_pfn  = reader.get_pfns(production=production, nickname=nickname)
