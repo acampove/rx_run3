641c239bfe6c4471805df6f89259d132c9101b9b
Angel Campoverde <angel.campoverde@cern.ch>
2025-09-08 21:20:35 +0800

    Add pandas options

--- tests/test_filtered_stats.py
+++ tests/test_filtered_stats.py
@@ -3,6 +3,7 @@ This script tests FilteredStats class
 '''
 
 import pytest
+import pandas as pnd
 from rx_data.filtered_stats import FilteredStats
 from dmu.logging.log_store  import LogStore
 
@@ -14,10 +15,15 @@ def initialize():
     This will run before any test
     '''
     LogStore.set_level('rx_data:filtered_stats', 10)
+    pnd.set_option('display.max_columns' , None)
+    pnd.set_option('display.max_colwidth', None)
+    pnd.set_option('display.width'       ,    0)
 # ----------------------
 def test_simple():
     '''
     Simplest test of class
     '''
-    fst = FilteredStats(analysis='rx', min_vers=8)
+    fst = FilteredStats(analysis='rx', min_vers=8, max_lfns=3)
     df  = fst.get_df()
+
+    print(df)
