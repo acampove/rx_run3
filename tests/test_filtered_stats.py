10d19bfa58b55159c322dbc39cf03497a0a2f105
Angel Campoverde <angel.campoverde@cern.ch>
2025-09-08 18:00:05 +0800

    Adding implementation

--- tests/test_filtered_stats.py
+++ tests/test_filtered_stats.py
@@ -2,14 +2,22 @@
 This script tests FilteredStats class
 '''
 
+import pytest
 from rx_data.filtered_stats import FilteredStats
 from dmu.logging.log_store  import LogStore
 
-log=LogStore.add_logger('rx_data:filtered_stats')
+log=LogStore.add_logger('rx_data:test_filtered_stats')
+# ----------------------
+@pytest.fixture(scope='session', autouse=True)
+def initialize():
+    '''
+    This will run before any test
+    '''
+    LogStore.set_level('rx_data:filtered_stats', 10)
 # ----------------------
 def test_simple():
     '''
     Simplest test of class
     '''
-    fst = FilteredStats(analysis='rx')
+    fst = FilteredStats(analysis='rx', min_vers=8)
     df  = fst.get_df()
