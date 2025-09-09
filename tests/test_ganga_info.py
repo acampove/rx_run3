b7d95d61e981141206de06ee52bc6c2d79f36da8
Angel Campoverde <angel.campoverde@cern.ch>
2025-09-09 17:49:49 +0800

    Add test for full job

--- tests/test_ganga_info.py
+++ tests/test_ganga_info.py
@@ -15,7 +15,10 @@ def initialize():
     '''
     LogStore.set_level('rx_data:ganga_info', 10)
 # ----------------------
-def test_ganga_info():
+def test_small():
+    '''
+    Tests with small inputs
+    '''
     block = 'w40_42'
     fname = 'mc_magup_11264001_bd_dmnpipl_eq_dpc_Hlt2RD_BuToKpEE_MVA_bd46a6ff81.root'
     dir   = Path('/home/acampove/Tests/filtering')
@@ -23,3 +26,9 @@ def test_ganga_info():
     with GangaInfo.set_ganga_dir(dir=dir):
         inf = GangaInfo(job_ids=[257])
         assert block == inf.block_from_fname(fname=fname)
+# ----------------------
+def test_full():
+    '''
+    Tests with all inputs
+    '''
+    inf = GangaInfo(job_ids=[257])
