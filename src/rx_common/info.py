abee23998a788a4eeb7956095ed5b1567a49fde7
Angel Campoverde <angel.campoverde@cern.ch>
2025-09-16 17:14:57 +0800

    Transferring files from rx_data

---
+++ src/rx_common/info.py
@@ -0,0 +1,80 @@
+'''
+This script contains functions needed to get information on samples
+'''
+
+from dmu.generic            import utilities  as gut
+
+_triggers = gut.load_data(package='rx_common_data', fpath='triggers.yaml')
+
+# ---------------------------------
+def is_mc(sample : str) -> bool:
+    '''
+    Given a sample name, it will check if it is MC or data
+    '''
+
+    if sample.startswith('DATA'):
+        return False
+
+    return True
+# ---------------------------------
+def channel_from_trigger(trigger : str) -> str:
+    '''
+    Parameters
+    ----------------
+    trigger: Hlt2 trigger name, e.g. HLT2_BuKp...
+
+    Returns
+    ----------------
+    Channel, i.e. EE, MM, EM
+    '''
+    for project in _triggers:
+        for channel in _triggers[project]:
+            if trigger not in _triggers[project][channel]:
+                continue
+
+            return channel
+
+    raise ValueError(f'Trigger {trigger} not found')
+# ---------------------------------
+def is_ee(trigger : str) -> bool:
+    '''
+    Given Hlt2 trigger name, it will tell if it belongs to
+    muon or electron channel
+    '''
+
+    return channel_from_trigger(trigger) == 'EE'
+# ---------------------------------
+def project_from_trigger(trigger : str) -> str:
+    '''
+    Parameters
+    -------------------
+    trigger: HLT2 trigger
+
+    Returns
+    -------------------
+    Project, e.g RK, RKst
+    '''
+    for project in _triggers:
+        for channel in _triggers[project]:
+            if trigger not in _triggers[project][channel]:
+                continue
+
+            return project
+
+    raise ValueError(f'Trigger {trigger} not found')
+# ---------------------------------
+def is_reso(q2bin : str) -> bool:
+    '''
+    Takes q2bin name, returns true if it has associated
+    charmonium component. If not, returns false.
+    '''
+    reso = ['jpsi', 'psi2']
+    rare = ['low', 'cen_low', 'central', 'cen_high', 'high']
+
+    if q2bin in reso:
+        return True
+
+    if q2bin in rare:
+        return False
+
+    raise ValueError(f'Invalid q2bin: {q2bin}')
---
+++ tests/test_info.py
@@ -0,0 +1,84 @@
+'''
+This module tests the functions in the info.py module
+'''
+
+import pytest
+from rx_common import info
+
+# -----------------------------------------
+def test_is_ee():
+    '''
+    Test that trigger is recognized as electron trigger
+    '''
+    assert info.is_ee(trigger='Hlt2RD_BuToKpEE_MVA')
+    assert info.is_ee(trigger='Hlt2RD_BuToKpEE_MVA_noPID')
+    assert info.is_ee(trigger='Hlt2RD_BuToKpEE_SameSign_MVA')
+    assert info.is_ee(trigger='Hlt2RD_BuToKpEE_MVA_misid')
+
+    assert not info.is_ee(trigger='Hlt2RD_BuToKpMuMu_MVA')
+    assert not info.is_ee(trigger='Hlt2RD_BuToKpMuMu_MVA_noPID')
+    assert not info.is_ee(trigger='Hlt2RD_BuToKpMuMu_SameSign_MVA')
+    assert not info.is_ee(trigger='Hlt2RD_B0ToKpPimMuMu_MVA')
+    assert not info.is_ee(trigger='Hlt2RD_B0ToKpPimMuMu_SameSign_MVA')
+# -----------------------------------------
+def test_is_reso():
+    '''
+    Tests is_reso
+    '''
+    assert not info.is_reso(q2bin='low')
+    assert not info.is_reso(q2bin='central')
+    assert not info.is_reso(q2bin='high')
+
+    assert info.is_reso(q2bin='jpsi')
+    assert info.is_reso(q2bin='psi2')
+# -----------------------------------------
+@pytest.mark.parametrize('channel,trigger', [
+                             ('EE', 'Hlt2RD_BuToKpEE_MVA'),
+                             ('EE', 'Hlt2RD_BuToKpEE_MVA_noPID'),
+                             ('EE', 'Hlt2RD_BuToKpEE_SameSign_MVA'),
+                             ('EE', 'Hlt2RD_BuToKpEE_MVA_misid'),
+                             ('EE', 'Hlt2RD_BuToKpEE_MVA_ext'),
+                             ('EE', 'Hlt2RD_BuToKpEE_MVA_cal'),
+                             ('MM', 'Hlt2RD_BuToKpMuMu_MVA'),
+                             ('MM', 'Hlt2RD_BuToKpMuMu_MVA_noPID'),
+                             ('MM', 'Hlt2RD_BuToKpMuMu_SameSign_MVA'),
+                             ('EE', 'Hlt2RD_B0ToKpPimEE_MVA'),
+                             ('EE', 'Hlt2RD_B0ToKpPimEE_MVA_noPID'),
+                             ('EE', 'Hlt2RD_B0ToKpPimEE_SameSign_MVA'),
+                             ('EE', 'Hlt2RD_B0ToKpPimEE_MVA_misid'),
+                             ('EE', 'Hlt2RD_B0ToKpPimEE_MVA_ext'),
+                             ('EE', 'Hlt2RD_B0ToKpPimEE_MVA_cal'),
+                             ('MM', 'Hlt2RD_B0ToKpPimMuMu_MVA'),
+                             ('MM', 'Hlt2RD_B0ToKpPimMuMu_MVA_noPID'),
+                             ('MM', 'Hlt2RD_B0ToKpPimMuMu_SameSign_MVA')])
+def test_channel_from_trigger(channel : str, trigger : str):
+    '''
+    Test for function providing EE/MM for a given HLT2 trigger
+    '''
+    assert channel == info.channel_from_trigger(trigger=trigger)
+# -----------------------------------------
+@pytest.mark.parametrize('project,trigger', [
+                             ('RK', 'Hlt2RD_BuToKpEE_MVA'),
+                             ('RK', 'Hlt2RD_BuToKpEE_MVA_noPID'),
+                             ('RK', 'Hlt2RD_BuToKpEE_SameSign_MVA'),
+                             ('RK', 'Hlt2RD_BuToKpEE_MVA_misid'),
+                             ('RK', 'Hlt2RD_BuToKpEE_MVA_ext'),
+                             ('RK', 'Hlt2RD_BuToKpEE_MVA_cal'),
+                             ('RK', 'Hlt2RD_BuToKpMuMu_MVA'),
+                             ('RK', 'Hlt2RD_BuToKpMuMu_MVA_noPID'),
+                             ('RK', 'Hlt2RD_BuToKpMuMu_SameSign_MVA'),
+                             ('RKst', 'Hlt2RD_B0ToKpPimEE_MVA'),
+                             ('RKst', 'Hlt2RD_B0ToKpPimEE_MVA_noPID'),
+                             ('RKst', 'Hlt2RD_B0ToKpPimEE_SameSign_MVA'),
+                             ('RKst', 'Hlt2RD_B0ToKpPimEE_MVA_misid'),
+                             ('RKst', 'Hlt2RD_B0ToKpPimEE_MVA_ext'),
+                             ('RKst', 'Hlt2RD_B0ToKpPimEE_MVA_cal'),
+                             ('RKst', 'Hlt2RD_B0ToKpPimMuMu_MVA'),
+                             ('RKst', 'Hlt2RD_B0ToKpPimMuMu_MVA_noPID'),
+                             ('RKst', 'Hlt2RD_B0ToKpPimMuMu_SameSign_MVA')])
+def test_project_from_trigger(project : str, trigger : str):
+    '''
+    Test for function providing RK/RKst for a given HLT2 trigger
+    '''
+    assert project == info.project_from_trigger(trigger=trigger)
+# -----------------------------------------
