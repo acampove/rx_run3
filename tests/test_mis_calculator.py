7880f47d63dc375ef9543ed5074fd40df506bdb0
Angel Campoverde <angel.campoverde@cern.ch>
2025-09-17 21:10:57 +0800

    Updated to work with Rkst decays

--- tests/test_mis_calculator.py
+++ tests/test_mis_calculator.py
@@ -1,45 +1,48 @@
 '''
 Module with functions testing MisCalculator
 '''
+import pytest
 
-from ROOT                   import RDataFrame
+from ROOT                   import RDataFrame # type: ignore
 from dmu.logging.log_store  import LogStore
 from rx_data.mis_calculator import MisCalculator
+from rx_data                import testing       as tst
 
 log=LogStore.add_logger('rx_data:test_mis_calculator')
-
+# ------------------------
 class Data:
     '''
     Class used to share attributes
     '''
-    l_must_have = [
-            'L1_PE',
-            'L2_PE',
-            'H_PE']
+    l_must_have_bplus = ['L1_PE', 'L2_PE', 'H_PE']
+    l_must_have_bzero = ['L1_PE', 'L2_PE', 'H1_PE', 'H2_PE']
 # ------------------------
-def _get_rdf() -> RDataFrame:
-    path  = '/home/acampove/external_ssd/Data/main/v5/data_24_magup_24c1_Hlt2RD_BuToKpEE_MVA_0000000000.root'
-    rdf   = RDataFrame('DecayTree', path)
-
-    return rdf
-# ------------------------
-def _check_rdf(rdf : RDataFrame) -> None:
+def _check_rdf(rdf : RDataFrame, l_must_have : list[str]) -> None:
     l_name = [ name.c_str() for name in rdf.GetColumnNames() ]
 
     success = True
-    for must_have in Data.l_must_have:
+    for must_have in l_must_have:
         if must_have not in l_name:
             success = False
             log.warning(f'Missing {must_have}')
 
     assert success
 # ------------------------
-def test_simple():
+@pytest.mark.parametrize('prefix, kind', tst.l_prefix_kind)
+def test_simple(kind : str, prefix : str):
     '''
     Simplest test of class
     '''
-    rdf = _get_rdf()
-    cal = MisCalculator(rdf=rdf, trigger='Hlt2RD_BuToKpEE_MVA')
+    trigger = tst.get_trigger(kind=kind, prefix=prefix)
+    rdf = tst.get_rdf(kind=kind, prefix=prefix)
+    cal = MisCalculator(rdf=rdf, trigger=trigger)
     rdf = cal.get_rdf()
 
-    _check_rdf(rdf)
+    if 'B0ToKpPim' in prefix:
+        l_must_have = Data.l_must_have_bzero
+    elif 'BuToKp' in prefix:
+        l_must_have = Data.l_must_have_bplus
+    else:
+        raise ValueError(f'Invalid preffix: {prefix}')
+
+    _check_rdf(rdf=rdf, l_must_have=l_must_have)
