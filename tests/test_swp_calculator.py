f4e9ed9db18333de2d82e9bfc998b7092bc033f1
Angel Campoverde <angel.campoverde@cern.ch>
2025-09-17 19:55:44 +0800

    Update plotting function
    - Take test argument and preffix
    - Save diagnostic plots in different directories
    - Fix type hinting issue with range

--- tests/test_swp_calculator.py
+++ tests/test_swp_calculator.py
@@ -202,29 +202,31 @@ def test_jpsi_misid(prefix : str, kind : str):
 
     _plot(rdf, test='jpsi_misid', kind=kind, prefix=prefix)
 # ----------------------------------
-def _plot(rdf : RDataFrame, preffix : str, kind : str):
-    d_data = rdf.AsNumpy([f'{preffix}_mass_swp', f'{preffix}_mass_org'])
-    arr_swp= d_data[f'{preffix}_mass_swp']
-    arr_org= d_data[f'{preffix}_mass_org']
+def _plot(rdf : RDataFrame, test : str, kind : str, prefix : str):
+    d_data = rdf.AsNumpy([f'{test}_mass_swp', f'{test}_mass_org'])
+    arr_swp= d_data[f'{test}_mass_swp']
+    arr_org= d_data[f'{test}_mass_org']
 
     mass_rng = {
-            'jpsi_misid' : [2700, 3300],
-            'dzero_misid': [1700, 2000],
-            'phi_misid'  : [ 980, 1100],
-            }[preffix]
+        'jpsi_misid' : (2700, 3300),
+        'dzero_misid': (1700, 2000),
+        'phi_misid'  : ( 980, 1100)}[test]
 
     plt.hist(arr_org, bins=80, range=mass_rng, alpha=0.5, label='Original', color='gray')
     plt.hist(arr_swp, bins=80, range=mass_rng, histtype='step', label='Swapped', color='blue')
     plt.grid(False)
 
-    if preffix == 'phi_misid':
+    if test == 'phi_misid':
         plt.axvline(x=1020, color='r', label=r'$\phi$', linestyle=':')
-    elif preffix == 'jpsi_misid':
+    elif test == 'jpsi_misid':
         plt.axvline(x=3100, color='r', label=r'$J/\psi$', linestyle=':')
     else:
         plt.axvline(x=1864, color='r', label='$D_0$', linestyle=':')
 
+    out_dir = Data.out_dir/test
+    out_dir.mkdir(parents=True, exist_ok=True)
+
     plt.legend()
-    plt.savefig(f'{Data.out_dir}/{kind}_{preffix}.png')
+    plt.savefig(out_dir/f'{kind}_{prefix}.png')
     plt.close('all')
 # ----------------------------------
