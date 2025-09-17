26aa305d83017be02c99ab8b47729f7f5865cd35
Angel Campoverde <angel.campoverde@cern.ch>
2025-09-17 21:17:00 +0800

    Generalize code to add Hx_PE columns for Rkst

--- src/rx_data/mis_calculator.py
+++ src/rx_data/mis_calculator.py
@@ -6,6 +6,7 @@ import fnmatch
 
 from ROOT import RDataFrame # type: ignore
 from dmu.logging.log_store  import LogStore
+from rx_common              import info
 
 log = LogStore.add_logger('rx_data:mis_calculator')
 # ----------------------------------------------
@@ -18,7 +19,8 @@ class MisCalculator:
         '''
         Initializer taking dataframe and trigger, the latter is needed to know mass hypotheses of leptons
         '''
-        self._rdf   = rdf
+        self._rdf    = rdf
+        self._trigger= trigger
 
         self._emass = 0.511 # electron mass
         self._mmass = 105.6 # muon mass
@@ -48,8 +50,18 @@ class MisCalculator:
             name = 'L2_P'
             rdf  = rdf.Define(f'{name}E', f'TMath::Sqrt({name}X * {name}X + {name}Y * {name}Y + {name}Z * {name}Z + {self._lmass} * {self._lmass})')
 
-        if 'H_PE' not in l_col:
+        project = info.project_from_trigger(trigger=self._trigger, lower_case=True)
+
+        if project == 'rk' and 'H_PE' not in l_col:
             name = 'H_P'
+            rdf  = rdf.Define(f'{name}E', f'TMath::Sqrt({name}X * {name}X + {name}Y * {name}Y + {name}Z * {name}Z + {self._kmass} * {self._kmass})')
+
+        if project == 'rkst' and 'H1_PE' not in l_col:
+            name = 'H1_P'
+            rdf  = rdf.Define(f'{name}E', f'TMath::Sqrt({name}X * {name}X + {name}Y * {name}Y + {name}Z * {name}Z + {self._kmass} * {self._kmass})')
+
+        if project == 'rkst' and 'H2_PE' not in l_col:
+            name = 'H2_P'
             rdf  = rdf.Define(f'{name}E', f'TMath::Sqrt({name}X * {name}X + {name}Y * {name}Y + {name}Z * {name}Z + {self._kmass} * {self._kmass})')
 
         return rdf
