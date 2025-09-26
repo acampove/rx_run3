c9c290404e2ba026a40209475ac467dbc07cbc4f
Angel Campoverde <angel.campoverde@cern.ch>
2025-09-27 07:55:06 +0800

    Version working with law

--- workflows/plots/compare.py
+++ workflows/plots/compare.py
@@ -1,69 +1,99 @@
 
 import os
+# Force TensorFlow to use single thread to avoid multiprocessing conflicts
+os.environ['TF_NUM_INTRAOP_THREADS'] = '1'
+os.environ['TF_NUM_INTEROP_THREADS'] = '1'
+
+import ROOT
+
+import json
 from pathlib                    import Path
-from prefect                    import flow, task
-from prefect.task_runners       import ProcessPoolTaskRunner 
 
-from rx_plotter_scripts.compare import main      as compare 
-from dmu.generic                import utilities as gut
+import law
+from law.parameter              import Parameter
 from omegaconf                  import DictConfig, OmegaConf
+from dmu.generic                import utilities as gut
 
-ANADIR = os.environ['ANADIR']
-PLTDIR = Path(ANADIR) / 'plots/comparison/brem_track_2'
 
-def _get_settings() -> list[DictConfig]:
-    """Return all expected plot paths."""
-    config = gut.load_conf(package='configs', fpath='rx_plots/compare.yaml')
+class CompareTask(law.Task):
+    config_string : str = Parameter()
 
-    l_cfg = [] 
-    for args in config['args']:
-        sample  = args['sample' ]
-        trigger = args['trigger']
-        q2bin   = args['q2_bin' ]
+    def _get_config(self) -> DictConfig:
+        if hasattr(self, '_cfg'):
+            return self._cfg
 
-        for block in [12, 5, 6, 78]:
-            if block == 78 and sample == 'Bd_JpsiKst_ee_eq_DPC':
-                continue
+        if not isinstance(self.config_string, str):
+            raise ValueError('Configuration string not a string')
 
-            for brem in [1, 2]:
-                cfg            = {}
-                cfg['sample' ] = sample
-                cfg['trigger'] = trigger
-                cfg['q2_bin' ] = q2bin 
-                cfg['config' ] = 'resolution'
-                cfg['substr' ] = None 
-                cfg['brem'   ] = brem 
-                cfg['block'  ] = block
-                cfg['nomva'  ] = True
-                cfg['emulate'] = False
+        cfg = OmegaConf.create(self.config_string)
+        if not isinstance(cfg, DictConfig):
+            raise ValueError('cfg not a DictConfig')
 
-                cfg = OmegaConf.create(cfg)
+        self._cfg = cfg
 
-                l_cfg.append(cfg)
+        return self._cfg
 
-    return l_cfg 
+    def output(self) -> law.LocalFileTarget:
+        cfg    = self._get_config()
+        ANADIR = os.environ['ANADIR']
+        PLTDIR = Path(ANADIR) / 'plots/comparison/brem_track_2'
+        plot   = PLTDIR / f'{cfg.sample}/{cfg.trigger}/{cfg.q2_bin}/{cfg.brem}_{cfg.block}/drop_mva/npv.png'
 
-@task
-def run_compare(cfg : DictConfig) -> Path:
-    """Run the `compare` command for one set of wildcards."""
+        return law.LocalFileTarget(plot)
 
-    plot = PLTDIR / f'{cfg.sample}/{cfg.trigger}/{cfg.q2_bin}/{cfg.brem}_{cfg.block}/drop_mva/npv.png'
-    if plot.exists():
-        return plot 
+    def run(self):
+        """Run the `compare` command for one set of wildcards."""
+        with gut.silent_import():
+            import tensorflow as tf
 
-    settings = OmegaConf.to_container(cfg, resolve=True)
-    if not isinstance(settings, dict):
-        raise ValueError('Invalid settings type')
+            tf.config.threading.set_inter_op_parallelism_threads(1)
+            tf.config.threading.set_intra_op_parallelism_threads(1)
 
-    compare(settings=settings)
+        from rx_plotter_scripts.compare import main      as compare 
 
-    return plot
+        cfg = self._get_config() 
+    
+        compare(settings=cfg)
 
-@flow(task_runner=ProcessPoolTaskRunner(max_workers=10))
+class WrapCompare(law.WrapperTask):
+    def _get_settings(self) -> list[str]:
+        """Return all expected plot paths."""
+        config = gut.load_conf(package='configs', fpath='rx_plots/compare.yaml')
+    
+        l_cfg = [] 
+        for args in config['args']:
+            sample  = args['sample' ]
+            trigger = args['trigger']
+            q2bin   = args['q2_bin' ]
+    
+            for block in [12, 5, 6, 78]:
+                if block == 78 and sample == 'Bd_JpsiKst_ee_eq_DPC':
+                    continue
+    
+                for brem in [1, 2]:
+                    cfg            = {}
+                    cfg['sample' ] = sample
+                    cfg['trigger'] = trigger
+                    cfg['q2_bin' ] = q2bin 
+                    cfg['config' ] = 'resolution'
+                    cfg['substr' ] = None 
+                    cfg['brem'   ] = brem 
+                    cfg['block'  ] = block
+                    cfg['nomva'  ] = True
+                    cfg['emulate'] = False
+    
+                    json_str = json.dumps(cfg) 
+                    l_cfg.append(json_str)
+    
+        return l_cfg 
+    
+    def requires(self):
+        l_settings = self._get_settings()
+        for settings in l_settings:
+            yield CompareTask(config_string = settings)
+
 def main_task():
-    l_settings = _get_settings()
-    futures    = [run_compare.submit(cfg=settings) for settings in l_settings]
-    _          = [future.result() for future in futures]
+    law.run(argv=['WrapCompare', '--workers', '8'])
 
 if __name__ == "__main__":
     main_task()
