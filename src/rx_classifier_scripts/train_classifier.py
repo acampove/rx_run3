2ab6b624956b26d6246cc142c5d98e4764130ee2
Angel Campoverde <angel.campoverde@cern.ch>
2025-10-01 13:39:30 +0800

    exists_ok = Data.plot_only

--- src/rx_classifier_scripts/train_classifier.py
+++ src/rx_classifier_scripts/train_classifier.py
@@ -232,7 +232,9 @@ def _save_selection(
     out_dir = f'{out_dir}/input'
     os.makedirs(out_dir, exist_ok=True)
 
-    gut.dump_json(cuts, f'{out_dir}/selection_{kind}_{sample}.yaml')
+    gut.dump_json(data      = cuts, 
+                  exists_ok = Data.plot_only, # If not training, just plotting, allow overriding this
+                  path      = f'{out_dir}/selection_{kind}_{sample}.yaml')
 #---------------------------------
 def _get_overriding_selection(kind : str) -> dict[str,str]:
     '''
