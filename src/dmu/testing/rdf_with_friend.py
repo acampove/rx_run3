3911f7494f18bc1d20d9c972c86da70a1bceeda1
Angel Campoverde <angel.campoverde@cern.ch>
2025-10-14 20:25:38 +0800

    Adding file

---
+++ src/dmu/testing/rdf_with_friend.py
@@ -0,0 +1,53 @@
+import pandas as pnd
+
+from pydantic import BaseModel
+from pathlib  import Path
+from ROOT     import RDF
+
+# ----------------------------------
+class Sample(BaseModel):
+    trees : list[Path] = []
+    files : list[Path] = []
+
+class Spec(BaseModel):
+    samples : dict[str, Sample] = {}
+    friends : dict[str, Sample] = {}
+# ----------------------------------
+def _make_files(df : pnd.DataFrame, branch : str) -> tuple[Path, Path]:
+    df_1 = df.drop(columns=branch)
+    df_2 = df[[branch]]
+
+    path_main  = _make_file(df=df_1, name='main'  )
+    path_friend= _make_file(df=df_2, name='friend')
+
+    return path_main, path_friend
+# ----------------------------------
+def _make_file(df : pnd.DataFrame, name : str) -> Path:
+    path = Path(f'/tmp/{name}.root')
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    rdf = RDF.FromPandas(df)
+    rdf.Snapshot(treename='tree', filename=str(path))
+
+    return path
+# ----------------------------------
+def _get_json_path(main : str, friend : str) -> Path:
+    spc = Spec()
+    spc.samples['main'] = Sample(trees = ['tree'], files = [main  ])
+    spc.friends['bfrn'] = Sample(trees = ['tree'], files = [friend])
+
+    json_path= Path('/tmp/spec.json')
+    with open(json_path, 'w', encoding='utf-8') as ofile:
+        data = spc.json()
+        ofile.write(data)
+
+    return json_path
+# ----------------------------------
+def rdf_with_friend(df : pnd.DataFrame, branch : str) -> RDF.RNode:
+    path_main, path_friend = _make_files(df=df, branch=branch)
+    json_path  = _get_json_path(main=path_main, friend=path_friend)
+
+    rdf = RDF.Experimental.FromSpec(str(json_path))
+
+    return rdf
+# ----------------------------------
