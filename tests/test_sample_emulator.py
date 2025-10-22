81feabb4a6711452c6f4862627ee5f1b46c8104a
Angel Campoverde <angel.campoverde@cern.ch>
2025-10-22 22:54:58 +0800

    Fix names, etc

--- tests/test_sample_emulator.py
+++ tests/test_sample_emulator.py
@@ -5,18 +5,21 @@ Module holding tests for SampleEmulator class
 import pytest
 from rx_data.sample_emulator import SampleEmulator
 
-@pytest.mark.parametrize('old_name, expected_name',
+@pytest.mark.parametrize('old_name, new_name',
     [
-       ('Bd_JpsiKst_ee_eq_DPC', 'Bs_JpsiKst_ee_eq_DPC'),
-       ('Bd_JpsiKst_mm_eq_DPC', 'Bs_JpsiKst_mm_eq_DPC'),
+       ('Bs_JpsiKst_ee_eq_DPC', 'Bd_JpsiKst_ee_eq_DPC'),
+       ('Bs_JpsiKst_mm_eq_DPC', 'Bd_JpsiKst_mm_eq_DPC'),
     ])
 def test_rename(
-    old_name      : str, 
-    expected_name : str):
+    old_name : str, 
+    new_name : str):
     '''
     Check renaming of samples
+
+    old_name: Sample requested
+    new_name: Expected sample to use
     '''
     emu = SampleEmulator(sample=old_name)
-    new_name = emu.get_sample_name()
+    val = emu.get_sample_name()
 
-    assert new_name == expected_name
+    assert new_name == val 
