5957fa7382efe14c4deb1935221d371c2cf4127e
Angel Campoverde <angel.campoverde@cern.ch>
2025-10-22 23:25:46 +0800

    Test emulation

--- tests/test_sample_emulator.py
+++ tests/test_sample_emulator.py
@@ -2,9 +2,26 @@
 Module holding tests for SampleEmulator class
 '''
 
+import numpy
 import pytest
+from ROOT                    import RDataFrame, RDF # type: ignore
 from rx_data.sample_emulator import SampleEmulator
 
+# ----------------------
+def _get_rdf() -> RDF.RNode:
+    '''
+    Returns
+    -------------
+    Dataframe with zeroed columns 
+    '''
+    rdf = RDataFrame(100)
+    rdf = rdf.Define('B_M'                 , '0')
+    rdf = rdf.Define('B_Mass'              , '0')
+    rdf = rdf.Define('B_M_brem_track_2'    , '0')
+    rdf = rdf.Define('B_const_mass_psi2S_M', '0')
+
+    return rdf
+# --------------------------------------------
 @pytest.mark.parametrize('old_name, new_name',
     [
        ('Bs_JpsiKst_ee_eq_DPC', 'Bd_JpsiKst_ee_eq_DPC'),
@@ -23,3 +40,17 @@ def test_rename(
     val = emu.get_sample_name()
 
     assert new_name == val 
+# --------------------------------------------
+@pytest.mark.parametrize('sample', ['Bs_JpsiKst_ee_eq_DPC'])
+def test_post_process(sample : str):
+    '''
+    sample: Name of sample to emulate
+    '''
+    rdf_old = _get_rdf()
+    emu     = SampleEmulator(sample=sample)
+    rdf_new = emu.post_process(rdf = rdf_old)
+
+    data    = rdf_new.AsNumpy()
+    expected= 87.23
+    for arr_val in data.values():
+        assert numpy.isclose(arr_val, expected).all()
