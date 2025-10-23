e91a2e22e19435c39b6e57cd3a129515a27c1c10
Angel Campoverde <angel.campoverde@cern.ch>
2025-10-23 22:41:35 +0800

    Add logger and initializer

--- tests/test_scale_combiner.py
+++ tests/test_scale_combiner.py
@@ -1,8 +1,19 @@
 '''
 This module contains functions to test ScaleCombiner
 '''
-from rx_q2 import ScaleCombiner
+import pytest
 
+from rx_q2                 import ScaleCombiner
+from dmu.logging.log_store import LogStore
+
+# ----------------------
+@pytest.fixture(scope='session', autouse=True)
+def initialize():
+    '''
+    This will run before any test
+    '''
+    LogStore.set_level('rx_q2:scale_combiner', 10)
+# ----------------------
 def test_simple():
     '''
     Simplest test
@@ -10,3 +21,4 @@ def test_simple():
     cmb = ScaleCombiner(version = 'v2')
     cmb.combine(name='parameters_ee.json', measurements=['rk_ee', 'rkst_ee'])
     cmb.combine(name='parameters_mm.json', measurements=['rk_mm', 'rkst_mm'])
+# ----------------------
