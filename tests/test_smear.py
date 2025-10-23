94feae7f12e5263784818a8daad597bc5d480eb4
Angel Campoverde <angel.campoverde@cern.ch>
2025-10-23 19:38:14 +0800

    Use apply to apply corrections to data
    Easier to read

--- tests/test_smear.py
+++ tests/test_smear.py
@@ -53,22 +53,33 @@ def _get_df(uniform : bool = True) -> pnd.DataFrame:
         df['reco_mass'] = numpy.random.normal(loc=3000, scale=100, size=Data.nentries)
 
     return df
+# ----------------------
+def _correct_q2(row : pnd.Series, corrector : Q2SmearCorrector) -> float:
+    '''
+    Parameters
+    -------------
+    row: Pandas series with entry
+
+    Returns
+    -------------
+    Instance of Q2SmearCorrector
+    '''
+    args                   = dict()
+    args['nbrem']          = tut.numeric_from_series(row, 'nbrem'    ,   int)
+    args['block']          = tut.numeric_from_series(row, 'block'    ,   int)
+    args['jpsi_mass_true'] = tut.numeric_from_series(row, 'true_mass', float)
+    args['jpsi_mass_reco'] = tut.numeric_from_series(row, 'reco_mass', float)
+
+    return corrector.get_mass(**args)
 # -------------------------------------------
 @pytest.mark.parametrize('is_uniform', [True, False])
 def test_simple(is_uniform : bool):
     '''
     Checks if the input is wrong
     '''
-    obj = Q2SmearCorrector()
-    df  = _get_df(uniform = is_uniform)
-
-    l_val = []
-    itr   = df.itertuples(index=False)
-    for nbrem, block, reco_mass, true_mass in tqdm.tqdm(itr, total=len(df), ascii=' -'):
-        val    = obj.get_mass(nbrem=nbrem, block=block, jpsi_mass_reco=reco_mass, jpsi_mass_true=true_mass)
-        l_val += [val]
-
-    df['mass_smr'] = l_val
+    obj            = Q2SmearCorrector()
+    df             = _get_df(uniform = is_uniform)
+    df['mass_smr'] = df.apply(_correct_q2, args=(obj,), axis=1)
 
     _plot_masses(df=df, name = f'simple_{is_uniform}')
 # -------------------------------------------
