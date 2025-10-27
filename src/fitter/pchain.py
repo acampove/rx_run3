19f019487ff9ab4780327bbffd02431018498eec
Angel Campoverde <angel.campoverde@cern.ch>
2025-10-27 18:45:08 +0800

    Improve code
    - Add type annotations
    - Add docstrings

--- src/fitter/pchain.py
+++ src/fitter/pchain.py
@@ -11,9 +11,18 @@ log=LogStore.add_logger('fitter:pchain')
 class PChain:
     '''
     Class meant to represent a decay chain
+    for a given particle
     '''
     #----------------------------------
-    def __init__(self, pid, mid, gmid, ggmid):
+    def __init__(
+        self, 
+        pid   : int, 
+        mid   : int, 
+        gmid  : int,
+        ggmid : int):
+        '''
+        Takes particle ID for particle and parents
+        '''
         self._TID          = pid
         self._MOTHER_TID   = mid
         self._GMOTHER_TID  = gmid
@@ -35,50 +44,71 @@ class PChain:
     def ggmother_id(self) -> int:
         return self._GGMOTHER_TID
     #----------------------------------------------------
-    def match_decay(self, l_dec_id):
+    def match_decay(self, l_dec_id : list[int]) -> bool:
+        '''
+        Parameters
+        -----------------
+        l_dec_id: List of event numbers for particles in decay
+
+        Return
+        -----------------
+        True if particles match this decay
+        '''
         if len(l_dec_id) == 1:
             return self._TID ==      l_dec_id[0]
+
         if len(l_dec_id) == 2:
             return  self._TID == abs(l_dec_id[0]) and self._MOTHER_TID == abs(l_dec_id[1])
+
         if len(l_dec_id) == 3:
             return  self._TID == abs(l_dec_id[0]) and self._MOTHER_TID == abs(l_dec_id[1]) and self._GMOTHER_TID == abs(l_dec_id[2])
+
         if len(l_dec_id) == 4:
             return  self._TID == abs(l_dec_id[0]) and self._MOTHER_TID == abs(l_dec_id[1]) and self._GMOTHER_TID == abs(l_dec_id[2]) and self._GGMOTHER_TID == abs(l_dec_id[3])
 
         return False
     #----------------------------------------------------
-    def has_in_chain(self, ID):
-        _return = False
+    def has_in_chain(self, particle_id : int) -> bool:
+        '''
+        Parameters
+        -----------------
+        particle_id: PDG ID of particle
 
-        if self._MOTHER_TID   == ID:
-            _return = True
-        if self._GMOTHER_TID  == ID:
-            _return = True
-        if self._GGMOTHER_TID == ID:
-            _return = True
+        Returns
+        -----------------
+        True if this particle is in the current decay chain, mother, grand mother, etc
+        '''
+        flag = False
 
-        return _return
+        if self._MOTHER_TID   == particle_id:
+            flag = True
+        if self._GMOTHER_TID  == particle_id:
+            flag = True
+        if self._GGMOTHER_TID == particle_id:
+            flag = True
+
+        return flag
     #----------------------------------------------------
-    def match_upstream(self, IDFirstDau, HeadPart):
-        _return = False
+    def match_upstream(self, IDFirstDau, HeadPart) -> bool:
+        flag = False
         if  self._MOTHER_TID   == IDFirstDau and self._GMOTHER_TID  == HeadPart:
-            _return = True
+            flag = True
         if  self._GMOTHER_TID  == IDFirstDau and self._GGMOTHER_TID == HeadPart:
-            _return = True
+            flag = True
         if  self._GGMOTHER_TID == IDFirstDau:
-            _return = True
+            flag = True
 
-        return _return
+        return flag
     #----------------------------------------------------
-    def match_id(self, iD):
+    def match_id(self, iD : int) -> bool:
         return self._TID == abs(iD)
 
-    def match_mother(self, iD):
+    def match_mother(self, iD : int) -> bool:
         return self._MOTHER_TID == abs(iD)
 
-    def match_gmother(self, iD):
+    def match_gmother(self, iD : int) -> bool:
         return self._GMOTHER_TID == abs(iD)
 
-    def match_ggmother(self, iD):
+    def match_ggmother(self, iD : int) -> bool:
         return self._GGMOTHER_TID == abs(iD)
 #----------------------------------------------------
