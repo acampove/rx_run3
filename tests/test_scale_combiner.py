c63362f4011ab6d945fbd62ceab55897467e5a04
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-01 10:08:00 +0800

    Add check for dataframe with combined measurements

--- tests/test_scale_combiner.py
+++ tests/test_scale_combiner.py
@@ -2,6 +2,7 @@
 This module contains functions to test ScaleCombiner
 '''
 import pytest
+import pandas as pnd
 
 from rx_q2                 import ScaleCombiner
 from dmu.logging.log_store import LogStore
@@ -14,11 +15,27 @@ def initialize():
     '''
     LogStore.set_level('rx_q2:scale_combiner', 10)
 # ----------------------
+def _check_df(df : pnd.DataFrame) -> None:
+    '''
+    Parameters
+    -------------
+    df: Dataframe with scales
+    '''
+    l_block = df['block'].tolist()
+    s_block = set(l_block)
+
+    assert s_block == {1, 2, 3, 4, 5, 6, 7, 8}
+
+    assert not df.isna().any().any()
+# ----------------------
 def test_simple():
     '''
     Simplest test
     '''
-    cmb = ScaleCombiner(version = 'v2')
-    cmb.combine(name='parameters_ee.json', measurements=['rk_ee', 'rkst_ee'])
-    cmb.combine(name='parameters_mm.json', measurements=['rk_mm', 'rkst_mm'])
+    cmb   = ScaleCombiner(version = 'v3')
+    df_ee = cmb.combine(name='parameters_ee.json', measurements=['rk_ee', 'rkst_ee'])
+    _check_df(df=df_ee)
+
+    df_mm = cmb.combine(name='parameters_mm.json', measurements=['rk_mm', 'rkst_mm'])
+    _check_df(df=df_mm)
 # ----------------------
