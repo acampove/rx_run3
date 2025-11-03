bf69a55565a908416ac7fc3bf261a2a111755ae1
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-03 08:39:59 +0800

    Update custom mva version to v8

--- tests/test_rdf_getter.py
+++ tests/test_rdf_getter.py
@@ -835,7 +835,7 @@ def test_custom_friend(sample : str, trigger : str):
     '''
     Tests getting data with a custom version for a given tree, either friend or main
     '''
-    with RDFGetter.custom_friends(versions={'mva' : 'v7.7'}):
+    with RDFGetter.custom_friends(versions={'mva' : 'v8'}):
         gtr = RDFGetter(sample=sample, trigger=trigger)
         rdf = gtr.get_rdf(per_file=False)
 
