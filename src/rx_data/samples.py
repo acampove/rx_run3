a4793e04820a86c565b6bf42b2d7ffd1e6ca7ee6
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-04 18:49:51 +0800

    WIP: Full implementation but actual measurement of block statistics

--- src/rx_data/samples.py
+++ src/rx_data/samples.py
@@ -2,11 +2,19 @@
 This module is meant to hold SamplesPrinter
 '''
 
-#    ana_dir = Path(os.environ['ANADIR'])
-#    ana_dir / f'Data/{project}/main/v11'
+import os
+import tqdm
+import pandas as pnd 
 
-from rx_common.types import Project
+from pathlib                import Path
+from ROOT                   import RDF # type: ignore
+from rx_common.types        import Project
+from rx_data                import utilities as dut
+from rx_data.rdf_getter     import RDFGetter
+from dmu.generic            import version_management as vm
+from dmu.logging.log_store  import LogStore
 
+log=LogStore.add_logger('rx_data:samples')
 # ----------------------
 class SamplesPrinter:
     '''
@@ -20,10 +28,78 @@ class SamplesPrinter:
         project: Project, e.g. rk, rkst, rk_no_refit
         '''
         self._project = project
+
+        LogStore.set_level('rx_data:rdf_getter', 40)
+    # ----------------------
+    def _get_input_samples(self) -> list[tuple[str,str]]:
+        '''
+        Returns
+        -------------
+        List of tuples with sample name and trigger name
+        '''
+        ana_dir   = Path(os.environ['ANADIR'])
+        main_path = ana_dir / f'Data/{self._project}/main'
+        vers_path = vm.get_last_version(dir_path = main_path, version_only=False)
+        paths     = vers_path.glob('*.root')
+        if not paths:
+            raise ValueError(f'No ROOT files found in: {vers_path}')
+
+        s_sample_trigger = { dut.info_from_path(path, sample_lowercase=False) for path in paths }
+        nsamples         = len(s_sample_trigger)
+
+        log.info(f'Found {nsamples} samples')
+
+        return list(s_sample_trigger)
+    # ----------------------
+    def _get_rdf(self) -> dict[str, RDF.RNode]:
+        '''
+        Returns
+        -------------
+        Dictionary mapping sample name to corresponding dataframe built from only main trees
+        '''
+        input_samples : list[tuple[str,str]] = self._get_input_samples()
+        d_rdf : dict[str, RDF.RNode]         = dict()
+
+        with RDFGetter.only_friends(s_friend=set()),\
+             RDFGetter.project(name = self._project):
+            for sample, trigger in tqdm.tqdm(input_samples, ascii=' -'):
+                gtr = RDFGetter(sample =sample, trigger=trigger)
+                try:
+                    d_rdf[sample] = gtr.get_rdf(per_file=False)
+                except ValueError:
+                    log.warning(f'Cannot read {sample}, skipping')
+
+        return d_rdf
+    # ----------------------
+    def _get_block_stats(self, rdf : RDF.RNode) -> dict[str,str]:
+        '''
+        Parameters
+        -------------
+        rdf : ROOT dataframe with main sample 
+
+        Returns
+        -------------
+        dictionary where:
+
+        Key  : Block number
+        Value: Percentage of events in block
+        '''
+        return {'1' : '20', '2' : '20', '3' : '20'}
     # ----------------------
     def print_by_block(self) -> None:
         '''
         Prints a table with block column and samples as the rows
         '''
+        d_rdf = self._get_rdf()
+
+        l_d_block_stats : list[dict[str,str]] = []
+        for rdf in d_rdf.values():
+            d_block_stats : dict[str,str] = self._get_block_stats(rdf=rdf)
+            l_d_block_stats.append(d_block_stats)
+
+        df = pnd.DataFrame(l_d_block_stats, index=list(d_rdf))
+        df = df.sort_index()
+
+        print(df)
 # ----------------------
 
