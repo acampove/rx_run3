83379b2aeabebd80171d28cef865e9d343313969
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-06 08:39:15 +0800

    Parametrize test

--- tests/test_spec_maker.py
+++ tests/test_spec_maker.py
@@ -2,6 +2,8 @@
 Module with tests for SpecMaker class
 '''
 
+import pytest
+
 from rx_common.types    import Trigger
 from rx_data.spec_maker import SpecMaker
 
@@ -9,6 +11,7 @@ _SAMPLES=[
     ('Bu_JpsiK_ee_eq_DPC', 'Hlt2RD_BuToKpEE_MVA'),
 ]
 # ------------------------------------------------------
+@pytest.mark.parametrize('sample, trigger', _SAMPLES)
 def test_simple(sample : str, trigger : Trigger) -> None:
     '''
     Simplest test of SpecMaker
