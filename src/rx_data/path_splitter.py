8eb2ee3b5aeb4ad4ea3c674348ffc2de1d552fdf
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-06 12:05:28 +0800

    Modernize and simplify
    - Use Path instead of strings
    - Improve docstrings
    - Use type aliases to improve readability

--- src/rx_data/path_splitter.py
+++ src/rx_data/path_splitter.py
@@ -1,11 +1,15 @@
 '''
 Module containing PathSplitter class
 '''
-# pylint: disable=line-too-long, import-error, too-few-public-methods
+from pathlib import Path
+from typing  import TypeAlias
 
 import ap_utilities.decays.utilities as aput
 from dmu.logging.log_store  import LogStore
-from rx_data import utilities as ut
+from rx_data                import utilities as ut
+
+Samples       : TypeAlias = dict[tuple[str, str], list[Path]]
+NestedSamples : TypeAlias = dict[str, dict[str, list[Path]]]
 
 log   = LogStore.add_logger('rx_data:path_splitter')
 # ------------------------------------------
@@ -15,7 +19,11 @@ class PathSplitter:
     Samples and HLT2 trigger categories
     '''
     # ------------------------------------------
-    def __init__(self, paths : list[str], max_files : int = -1, sample_naming : str = 'new'):
+    def __init__(
+        self, 
+        paths         : list[Path], 
+        max_files     : int = -1, 
+        sample_naming : str = 'new'):
         '''
         paths: List of LFNs/PFNs/Local paths
         max_files: If doing tests, the output lists will be limited to this number, default not truncate
@@ -25,25 +33,34 @@ class PathSplitter:
         self._max_files    = max_files
         self._sample_naming= sample_naming
     # ------------------------------------------
-    def _truncate_paths(self, d_path):
+    def _truncate_paths(self, samples : Samples) -> Samples: 
         '''
         Will limit the number of paths in the values if Data.Max is larger than zero
         '''
-
         if self._max_files < 0:
-            return d_path
+            return samples
 
         log.warning(f'Truncating to {self._max_files} paths')
 
-        d_path_trunc = { key : val[:self._max_files] for key, val in d_path.items() }
+        d_path_trunc = { key : val[:self._max_files] for key, val in samples.items() }
 
         return d_path_trunc
     # ------------------------------------------
-    def _rename_sample(self, d_info_path : dict[tuple[str,str],list[str]]) -> dict[tuple[str,str],list[str]]:
+    def _rename_sample(self, samples : Samples) -> Samples:
+        '''
+        Parameters
+        ---------------
+        samples : Dictionary mapping sample, trigger tuple to list of ROOT file paths
+
+        Returns
+        ---------------
+        Same as input, with sample names changed to go from lower case (extracted from paths) to 
+        mixed case (needed by downstream code)
+        '''
         log.debug('Renaming samples from lower-case only')
         d_renamed = {}
 
-        for (sample, line_name), l_fpath in d_info_path.items():
+        for (sample, line_name), l_fpath in samples.items():
             try:
                 sample = aput.name_from_lower_case(sample)
             except ValueError as exc:
@@ -67,13 +84,17 @@ class PathSplitter:
 
         return d_renamed
     # ------------------------------------------
-    def _nest_structure(self, d_data : dict) -> dict:
+    def _nest_samples(self, samples : Samples ) -> NestedSamples:
         '''
-        Takes a dictionary of the form:
+        Parameters
+        ----------------
+        samples: Dictionary mapping sample name and trigger tuple to list of ROOT file paths
 
         (sample, trigger) : path_1
 
-        And nests it as:
+        Returns
+        ----------------
+        Nested structure like:
 
         sample:
             trigger:
@@ -81,8 +102,8 @@ class PathSplitter:
                 - path 2
                 - path 3
         '''
-        d_struc = {}
-        for (sample, line), l_path in d_data.items():
+        d_struc : NestedSamples = {}
+        for (sample, line), l_path in samples.items():
             if sample not in d_struc:
                 d_struc[sample] = {}
 
@@ -90,9 +111,7 @@ class PathSplitter:
 
         return d_struc
     # ------------------------------------------
-    def split(
-            self,
-            nested : bool = False) -> dict[tuple[str,str],list[str]]:
+    def split(self, nested : bool = False) -> Samples | NestedSamples:
         '''
         Takes list of paths to ROOT files and splits them in an easier to read structure
 
@@ -113,24 +132,24 @@ class PathSplitter:
         npath = len(self._l_path)
         log.info(f'Splitting {npath} paths into categories')
 
-        d_info_path = {}
+        samples : Samples = {}
         for path in self._l_path:
-            info = ut.info_from_path(path)
-            if info not in d_info_path:
-                d_info_path[info] = []
+            info = ut.info_from_path(path=path)
+            if info not in samples:
+                samples[info] = []
 
-            d_info_path[info].append(path)
+            samples[info].append(path)
 
-        d_info_path = self._truncate_paths(d_info_path)
-        d_info_path = self._rename_sample(d_info_path)
+        samples = self._truncate_paths(samples = samples)
+        samples = self._rename_sample(samples = samples)
 
         log.debug('Found samples:')
-        d_info_path = dict(sorted(d_info_path.items()))
-        for sample, line in sorted(d_info_path):
+        samples = dict(sorted(samples.items()))
+        for sample, line in sorted(samples):
             log.debug(f'{sample:<50}{line:<30}')
 
-        if nested:
-            d_info_path = self._nest_structure(d_data = d_info_path)
+        if not nested:
+            return samples
 
-        return d_info_path
+        return self._nest_samples(samples = samples)
 # ------------------------------------------
