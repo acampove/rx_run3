e0172b4368d772061421fab57b283cc0a3483a44
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-07 14:34:38 +0800

    Implement test for unpatched

--- tests/test_sample_patcher.py
+++ tests/test_sample_patcher.py
@@ -8,17 +8,20 @@ from rx_common.types import Trigger
 from rx_data.sample_patcher import SamplePatcher
 from rx_data.spec_maker import SpecMaker
 
-_SAMPLES = [
+_UNPATCHED_SAMPLES = [
     ('Bu_JpsiK_ee_eq_DPC', Trigger.rk_ee_os),
 ]
 # ----------------------
-@pytest.mark.parametrize('sample, trigger', _SAMPLES)
-def test_sample_patcher(sample : str, trigger : Trigger) -> None:
+@pytest.mark.parametrize('sample, trigger', _UNPATCHED_SAMPLES)
+def test_unpatched(sample : str, trigger : Trigger) -> None:
     '''
-    Simplest test of sample patcher
+    Tests that patching does not affect samples that are not
+    meant to be patched
     '''
     spk  = SpecMaker(sample=sample, trigger=trigger)
 
     ptr  = SamplePatcher(sample = sample, spec = spk.spec)
     spec = ptr.get_patched_specification()
-    
+
+    assert spec == spk.spec
+    assert len(ptr.redefinitions) == 0
