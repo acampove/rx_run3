e0b6a62cec5320406f0b3c91e0ca4f7f9bc314b1
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-07 14:38:40 +0800

    Improve test
    - Turn on logging at debug for tested class and turn it at Warning for
      other classes
    - Add more samples to tests

--- tests/test_sample_patcher.py
+++ tests/test_sample_patcher.py
@@ -3,14 +3,27 @@ Module with tests for SamplePatcher class
 '''
 
 import pytest
-from rx_common.types import Trigger
-
+from dmu.logging.log_store  import LogStore
+from rx_common.types        import Trigger
 from rx_data.sample_patcher import SamplePatcher
-from rx_data.spec_maker import SpecMaker
+from rx_data.spec_maker     import SpecMaker
 
 _UNPATCHED_SAMPLES = [
-    ('Bu_JpsiK_ee_eq_DPC', Trigger.rk_ee_os),
+    ('Bu_JpsiK_ee_eq_DPC'            , Trigger.rk_ee_os),
+    ('Bu_JpsiK_mm_eq_DPC'            , Trigger.rk_mm_os),
+    ('Bd_Kstee_eq_btosllball05_DPC'  , Trigger.rkst_ee_os),
+    ('Bd_Kstmumu_eq_btosllball05_DPC', Trigger.rkst_mm_os),
 ]
+# ----------------------
+@pytest.fixture(scope='session', autouse=True)
+def initialize():
+    '''
+    This will run before any test
+    '''
+    LogStore.set_level('rx_data:sample_patcher', 10)
+    LogStore.set_level('rx_data:spec_maker'    , 30)
+    LogStore.set_level('rx_data:path_splitter' , 30)
+    LogStore.set_level('rx_data:rdf_getter'    , 30)
 # ----------------------
 @pytest.mark.parametrize('sample, trigger', _UNPATCHED_SAMPLES)
 def test_unpatched(sample : str, trigger : Trigger) -> None:
