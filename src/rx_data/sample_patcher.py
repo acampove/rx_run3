a03e48cec53177dadecc271d9aab2ec00baa3f8b
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-07 13:35:20 +0800

    Add redefinitions property and _redefinitions attribute

--- src/rx_data/sample_patcher.py
+++ src/rx_data/sample_patcher.py
@@ -17,6 +17,19 @@ class SamplePatcher:
         sample : Name of sample, e.g. Bs_JpsiX_ee_eq_JpsiInAcc
         '''
         self._sample = sample 
+        self._redefinitions : None | dict[str,str] = None
+    # ----------------------
+    @property
+    def redefinitions(self) -> dict[str,str]:
+        '''
+        Returns
+        --------------
+        Dictionary between column name and string with new definition
+        '''
+        if self._redefinitions is None:
+            raise ValueError(f'No redefinitions available for sample {self._sample}')
+
+        return self._redefinitions
     # ----------------------
     def patch(self, spec : Specification) -> Specification:
         '''
