19bb0b1ce28efdcdaaabeca2ace14f781f2c4b27
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-10 18:46:17 +0800

    Improvements to test
    - Use fixture to disable caching. If outputs are cached, this won't
      really test the code.
    - Limit to 100K entries to speed up tests
    - Turn off misid tests. These tests need smear friend tree, which will
      need to be added later

--- tests/test_sim_fitter.py
+++ tests/test_sim_fitter.py
@@ -13,6 +13,15 @@ from rx_selection           import selection as sel
 from fitter.sim_fitter      import SimFitter
 
 # ---------------------------------------------------
+@pytest.fixture(autouse=True)
+def disable_caching():
+    '''
+    This will disable caching for all tests in this module
+    '''
+    with RDFGetter.max_entries(value = 100_000),\
+         Cache.turn_off_cache(val=None):
+        yield
+# ---------------------------------------------------
 def test_nomc():
     '''
     Test for components that have no MC associated
@@ -33,97 +42,75 @@ def test_nocat():
     '''
     Test for components without categories, e.g. muon
     '''
-    block = 1
     obs   = zfit.Space('B_Mass', limits=(5000, 5800))
     cfg   = gut.load_conf(package='fitter_data', fpath='rare/rk/muon/signal.yaml')
 
-    with RDFGetter.max_entries(value=-1),\
-        RDFGetter.multithreading(nthreads=8),\
-        sel.custom_selection(d_sel = {'block' : f'block == {block}'}):
-
-        ftr = SimFitter(
-            name     = 'test_nocat',
-            component= 'signal_muon',
-            obs     = obs,
-            cfg     = cfg,
-            trigger = Trigger.rk_mm_os,
-            q2bin   = 'jpsi')
-        ftr.get_model()
+    ftr = SimFitter(
+        name     = 'test_nocat',
+        component= 'signal_muon',
+        obs     = obs,
+        cfg     = cfg,
+        trigger = Trigger.rk_mm_os,
+        q2bin   = 'jpsi')
+    ftr.get_model()
 # ---------------------------------------------------
 def test_with_cat():
     '''
     Test for components with brem categories
     '''
-    block = 1
     obs   = zfit.Space('B_Mass', limits=(4500, 7000))
     cfg   = gut.load_conf(package='fitter_data', fpath='rare/rk/electron/signal_parametric.yaml')
 
-    with RDFGetter.max_entries(value=-1),\
-        RDFGetter.multithreading(nthreads=8),\
-        sel.custom_selection(d_sel = {'block' : f'block == {block}'}):
-
-        ftr = SimFitter(
-            name     = 'test_with_cat',
-            component= 'signal_electron',
-            obs     = obs,
-            cfg     = cfg,
-            trigger = Trigger.rk_ee_os,
-            q2bin   = 'jpsi')
-        _ = ftr.get_model()
+    ftr = SimFitter(
+        name     = 'test_with_cat',
+        component= 'signal_electron',
+        obs     = obs,
+        cfg     = cfg,
+        trigger = Trigger.rk_ee_os,
+        q2bin   = 'jpsi')
+    _ = ftr.get_model()
 # ---------------------------------------------------
 @pytest.mark.parametrize('component', ['bdkstee', 'bukstee', 'bsphiee'])
 def test_kde(component : str):
     '''
     Test fitting with KDE
     '''
-    block = 1
-    obs   = zfit.Space('B_Mass_smr', limits=(4500, 7000))
-    cfg   = gut.load_conf(package='fitter_data', fpath=f'rare/rk/electron/{component}.yaml')
+    obs = zfit.Space('B_Mass_smr', limits=(4500, 7000))
+    cfg = gut.load_conf(package='fitter_data', fpath=f'rare/rk/electron/{component}.yaml')
 
-    with Cache.turn_off_cache(val = ['SimFitter', 'DataPreprocessor']),\
-        RDFGetter.multithreading(nthreads=8),\
-        sel.custom_selection(d_sel = {'block' : f'block == {block}'}):
-
-        ftr = SimFitter(
-            name     = 'test_kde',
-            component= component,
-            obs      = obs,
-            cfg      = cfg,
-            trigger  = Trigger.rk_ee_os,
-            q2bin    = 'central')
-        ftr.get_model()
+    ftr = SimFitter(
+        name     = 'test_kde',
+        component= component,
+        obs      = obs,
+        cfg      = cfg,
+        trigger  = Trigger.rk_ee_os,
+        q2bin    = 'central')
+    ftr.get_model()
 # ---------------------------------------------------
+@pytest.mark.skip(reason='These tests require smear friend trees for noPID samples')
 @pytest.mark.parametrize('component', ['kkk', 'kpipi'])
 @pytest.mark.parametrize('q2bin'    , ['low', 'central', 'high'])
 def test_misid(component : str, q2bin : str):
     '''
     Test fitting misID simulation 
     '''
-    block = 1
-    obs   = zfit.Space('B_Mass_smr', limits=(4500, 7000))
-    cfg   = gut.load_conf(package='fitter_data', fpath=f'rare/rk/electron/{component}.yaml')
+    obs = zfit.Space('B_Mass_smr', limits=(4500, 7000))
+    cfg = gut.load_conf(package='fitter_data', fpath=f'rare/rk/electron/{component}.yaml')
 
-    # For no PID triggers, take unsmeared simulation. This simulation will be B -> 3/4 H and
-    # smearing is not needed
-    with RDFGetter.multithreading(nthreads=8),\
-        RDFGetter.custom_columns(columns={'B_Mass_smr' : 'B_M', 'q2_smr' : 'q2'}),\
-        sel.custom_selection(d_sel = {'block' : f'block == {block}'}):
-
-        ftr = SimFitter(
-            name     = 'test_misid',
-            component= component,
-            obs      = obs,
-            cfg      = cfg,
-            trigger  = Trigger.rk_ee_nopid,
-            q2bin    = q2bin)
-        ftr.get_model()
+    ftr = SimFitter(
+        name     = 'test_misid',
+        component= component,
+        obs      = obs,
+        cfg      = cfg,
+        trigger  = Trigger.rk_ee_nopid,
+        q2bin    = q2bin)
+    ftr.get_model()
 # ---------------------------------------------------
 @pytest.mark.parametrize('limits', ['wide', 'narrow'])
 def test_ccbar_reso(limits : str):
     '''
     Tests retriveval of PDF associated to ccbar inclusive decays
     '''
-    block     = 1
     tp_limits = {'wide' : (4500, 6000), 'narrow' : (5000, 6000)}[limits]
     component = 'ccbar'
     obs       = zfit.Space('B_const_mass_M', limits=tp_limits)
@@ -131,18 +118,15 @@ def test_ccbar_reso(limits : str):
 
     out_dir   = f'{cfg.output_directory}/{limits}'
     cfg.output_directory = out_dir
-    with RDFGetter.max_entries(value=-1),\
-        RDFGetter.multithreading(nthreads=8),\
-        sel.custom_selection(d_sel = {'block' : f'block == {block}'}):
 
-        ftr = SimFitter(
-            name     = 'test_ccbar_reso',
-            component= component,
-            obs      = obs,
-            cfg      = cfg,
-            trigger  = Trigger.rk_ee_os,
-            q2bin    = 'jpsi')
-        pdf = ftr.get_model()
+    ftr = SimFitter(
+        name     = 'test_ccbar_reso',
+        component= component,
+        obs      = obs,
+        cfg      = cfg,
+        trigger  = Trigger.rk_ee_os,
+        q2bin    = 'jpsi')
+    pdf = ftr.get_model()
 
     assert pdf is not None
 
@@ -153,16 +137,13 @@ def test_ccbar_rare():
     Tests retriveval of PDF associated to ccbar inclusive decays
     for rare modes, i.e. without jpsi mass constraint
     '''
-    block     = 1
     component = 'ccbar'
     mass      = 'B_Mass'
     q2bin     = 'high'
     obs       = zfit.Space(mass, limits=(4500, 6000))
     cfg       = gut.load_conf(package='fitter_data', fpath=f'rare/rk/electron/{component}.yaml')
 
-    with Cache.turn_off_cache(val=['SimFitter']),\
-        sel.custom_selection(d_sel={
-            'block' :f'block == {block}',
+    with sel.custom_selection(d_sel={
             'nobr0' : 'nbrem != 0',
             'bdt'   : 'mva_cmb > 0.8 && mva_prc > 0.8'}):
         ftr = SimFitter(
@@ -180,15 +161,10 @@ def test_mc_reso(component : str, brem : int):
     '''
     Tests retriveval of PDF associated to ccbar inclusive decays
     '''
-    block = 1
     obs   = zfit.Space('B_const_mass_M', limits=(5000, 6900))
     cfg   = gut.load_conf(package='fitter_data', fpath=f'reso/rk/electron/{component}.yaml')
 
-    with RDFGetter.max_entries(value=-1),\
-        RDFGetter.multithreading(nthreads=8),\
-        Cache.turn_off_cache(val=None), \
-        sel.custom_selection(d_sel={
-            'block' :f'block == {block}',
+    with sel.custom_selection(d_sel={
             'mass'  : '(1)',
             'nbrem' :f'nbrem == {brem}'}):
         ftr = SimFitter(
@@ -205,41 +181,34 @@ def test_name(name : str):
     '''
     Will run test and specify the name argument
     '''
-    block     = 1
     component = 'ccbar'
     obs       = zfit.Space('B_const_mass_M', limits=(4500, 6000))
     cfg       = gut.load_conf(package='fitter_data', fpath=f'reso/rk/electron/{component}.yaml')
 
-    with RDFGetter.max_entries(value=-1),\
-        RDFGetter.multithreading(nthreads=8),\
-        sel.custom_selection(d_sel = {'block' : f'block == {block}'}):
-        ftr = SimFitter(
-            name     = name,
-            component= component,
-            obs      = obs,
-            cfg      = cfg,
-            trigger  = Trigger.rk_ee_os,
-            q2bin    = 'jpsi')
-        ftr.get_model()
+    ftr = SimFitter(
+        name     = name,
+        component= component,
+        obs      = obs,
+        cfg      = cfg,
+        trigger  = Trigger.rk_ee_os,
+        q2bin    = 'jpsi')
+    ftr.get_model()
 # ---------------------------------------------------
+@pytest.mark.skip(reason='These tests require smear friend trees for noPID samples')
 @pytest.mark.parametrize('component', ['kpipi', 'kkk'])
 def test_weights(component : str):
     '''
     Test fitting weighted MC sample
     '''
-    block = 1
-    obs   = zfit.Space('B_Mass_smr', limits=(4500, 7000))
-    cfg   = gut.load_conf(package='fitter_data', fpath=f'misid/rk/electron/{component}.yaml')
+    obs = zfit.Space('B_Mass_smr', limits=(4500, 7000))
+    cfg = gut.load_conf(package='fitter_data', fpath=f'misid/rk/electron/{component}.yaml')
 
-    with RDFGetter.max_entries(value=-1),\
-        RDFGetter.custom_columns(columns={'B_Mass_smr' : 'B_M', 'q2_smr' : 'q2'}),\
-        RDFGetter.multithreading(nthreads=8),\
-        sel.custom_selection(d_sel = {'block' : f'block == {block}'}):
-        ftr = SimFitter(
-            name     = 'test_weights',
-            component= component,
-            obs     = obs,
-            cfg     = cfg,
-            trigger = Trigger.rk_ee_nopid,
-            q2bin   = 'central')
-        ftr.get_model()
+    ftr = SimFitter(
+        name     = 'test_weights',
+        component= component,
+        obs     = obs,
+        cfg     = cfg,
+        trigger = Trigger.rk_ee_nopid,
+        q2bin   = 'central')
+    ftr.get_model()
+# ---------------------------------------------------
