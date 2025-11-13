d3af109d6d26143e1c09074d31e5927f6cb92238
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-13 14:18:00 +0800

    Simpler fit_summary function

--- src/fitter_scripts/cli.py
+++ src/fitter_scripts/cli.py
@@ -6,20 +6,18 @@ provided by this project
 import typer
 
 from dmu.logging.log_store import LogStore
-from rx_common.types       import Project 
 
 app = typer.Typer(help=__doc__)
 log = LogStore.add_logger('rx_fitter:cli')
 # ----------------------
 @app.command()
 def fit_summary(
-    log_lvl : int     = typer.Option(20 , '--log_lvl', '-l', help='Logging level'),
-    project : str     = typer.Option(..., '--project', '-p', help='E.g. rk, rkst')) -> None:
+    log_lvl : int     = typer.Option(20 , '--log_lvl', '-l', help='Logging level'))-> None:
     '''
     This will compare refitted and non-refitted data
     '''
-    if project not in [Project.rk, Project.rkst]:
-        raise ValueError(f'Invalid project: {project}')
+    smr = FitSummary()
+    smr.save()
 
     LogStore.set_level('rx_fitter:refitting', log_lvl)
 # ----------------------
