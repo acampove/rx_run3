c43e42e59f9cf494f6a681a0a850b0a8da0e0ffb
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-14 14:58:38 +0800

    Can save parquet and markdown file with fit parameters

--- src/fitter/fit_summary.py
+++ src/fitter/fit_summary.py
@@ -2,6 +2,7 @@
 Module holding FitSummary class
 '''
 import os
+import re
 import tqdm
 import pandas as pnd
 
@@ -9,6 +10,7 @@ from pathlib import Path
 from typing  import Final
 
 from dmu.logging.log_store import LogStore
+from dmu.generic           import utilities as gut
 
 log=LogStore.add_logger('fitter:fit_summary')
 # -------------------------------
@@ -29,6 +31,7 @@ class FitSummary:
         '''
         self._fit_dir = self._get_fit_dir(name=name)
 
+        # This captures seven fields
         self._regex   : Final[str] = r'(\d{3})_(\d{3})_b(\d)/reso/(rk|rkst)/(muon|electron)/data/(psi2|jpsi)/brem_(\d{3})/parameters.json'
         self._pattern : Final[str] = '*_*_b*/reso/r*/*/data/*/brem_*/parameters.json'
     # ----------------------
@@ -66,6 +69,57 @@ class FitSummary:
             log.info(f'Found {nfile} files')
 
         return files
+    # ----------------------
+    def _attach_information(self, data : dict[str, float | str], path : Path) -> dict[str, float | str]:
+        '''
+        Parameters
+        -------------
+        data: Dictionary with fitting parameter information
+        path: Path to JSON file with this information
+
+        Returns
+        -------------
+        Input dictionary with extra information added, taken from path
+        '''
+        mtch = re.match(f'.*/{self._regex}', str(path))
+        if not mtch:
+            raise ValueError(f'Cannot match pattern \"{self._regex}\" to \"{path}\"')
+
+        groups : list[str] = list(mtch.groups())
+
+        data['mva_cmb'] = groups[0]
+        data['mva_prc'] = groups[1]
+        data['block'  ] = groups[2]
+        data['project'] = groups[3]
+        data['channel'] = groups[4]
+        data['q2bin'  ] = groups[5]
+        data['brem'   ] = groups[6]
+
+        return data
+    # ----------------------
+    def _get_dataframe(self, path : Path) -> pnd.DataFrame:
+        '''
+        Parameters
+        -------------
+        path: Path to JSON file with fit parameters
+
+        Returns
+        -------------
+        DataFrame
+        '''
+        data       = gut.load_json(path=path)
+        parameters = dict()
+
+        for name, [value, error] in data.items():
+            parameters[f'{name}_value'] = value
+            parameters[f'{name}_error'] = error 
+
+        parameters = self._attach_information(data=parameters, path=path)
+        values     = { key : [value] for key, value in parameters.items() }
+        df         = pnd.DataFrame(values)
+
+        return df
+    # ----------------------
     def save(self) -> None:
         '''
         Saves summary to _fit_dir/summary
@@ -74,10 +128,14 @@ class FitSummary:
 
         l_df : list[pnd.DataFrame] = []
         for path in tqdm.tqdm(paths, ascii=' -'):
-            df = _get_dataframe(path=path)
+            df = self._get_dataframe(path=path)
             l_df.append(df)
 
-        df = pnd.concat(objs=l_df, axis=1)
+        df = pnd.concat(objs=l_df, axis=0)
+        df = df.reset_index(drop=True)
+
+        df.to_parquet(path = self._fit_dir / 'parameters.parquet')
+        df.to_markdown(buf = self._fit_dir / 'parameters.md')
 
         log.info(f'Saving summary to: {self._fit_dir}')
 # -------------------------------
