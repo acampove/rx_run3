0eda6e01010e51a5e5c001f93c1a1ed671609874
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-14 19:10:30 +0800

    Better test

--- tests/test_fit_summary.py
+++ tests/test_fit_summary.py
@@ -2,13 +2,17 @@
 Module with tests for FitSummary class 
 '''
 
+import pytest
 from fitter import FitSummary
 
 # ----------------------
-def test_simple() -> None:
+@pytest.mark.parametrize('force_update', [True, False])
+def test_update(force_update : bool) -> None:
     '''
     Simplest test
     '''
     smr = FitSummary(name='mid_window')
-    smr.save()
+    df  = smr.get_df(force_update = force_update)
+
+    print(df)
     
