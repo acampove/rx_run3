23b7698563e5d3c756c60d800fc2ac0aa75126aa
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-17 14:42:26 +0800

    Adding SS misid and calibration triggers

--- src/rx_common/types.py
+++ src/rx_common/types.py
@@ -9,12 +9,20 @@ class Trigger(str, Enum):
     Class meant to represent MVA triggers
     '''
     rk_ee_os     = 'Hlt2RD_BuToKpEE_MVA'
+    rk_ee_ss     = 'Hlt2RD_BuToKpEE_SameSign_MVA'
     rk_ee_nopid  = 'Hlt2RD_BuToKpEE_MVA_noPID'
+    rk_ee_cal    = 'Hlt2RD_BuToKpEE_MVA_cal'
+    rk_ee_misid  = 'Hlt2RD_BuToKpEE_MVA_misid'
     rk_mm_os     = 'Hlt2RD_BuToKpMuMu_MVA'
+    rk_mm_ss     = 'Hlt2RD_BuToKpMuMu_SameSign_MVA'
     # -----------
     rkst_ee_os   = 'Hlt2RD_B0ToKpPimEE_MVA'
+    rkst_ee_ss   = 'Hlt2RD_B0ToKpPimEE_SameSign_MVA'
     rkst_ee_nopid= 'Hlt2RD_B0ToKpPimEE_MVA_noPID'
+    rkst_ee_cal  = 'Hlt2RD_B0ToKpPimEE_MVA_cal'
+    rkst_ee_misid= 'Hlt2RD_B0ToKpPimEE_MVA_misid'
     rkst_mm_os   = 'Hlt2RD_B0ToKpPimMuMu_MVA'
+    rkst_mm_ss   = 'Hlt2RD_B0ToKpPimMuMu_SameSign_MVA'
     # -----------
     uninitialized= ''
 
