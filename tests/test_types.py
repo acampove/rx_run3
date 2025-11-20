8234fb4c02b718f5787804e0924257a9f0de914e
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-20 20:41:37 +0800

    Add file

---
+++ tests/test_types.py
@@ -0,0 +1,19 @@
+'''
+Script with functions meant to test Enums
+'''
+
+from rx_common import Component
+
+def test_component():
+    '''
+    Test Enum representing fit component 
+    '''
+    assert Component.data          == 'data'
+    assert Component.jpsi          == 'jpsi'
+    assert Component.psi2          == 'psi2'
+    assert Component.ccbar         == 'ccbar'
+    assert Component.cabibbo       == 'cabibbo'
+    assert Component.lbjpsipk      == 'lbjpsipk'
+    assert Component.bsjpsiphi     == 'bsjpsiphi'
+    assert Component.bsjpsikst     == 'bsjpsikst'
+    assert Component.bdjpsikst_swp == 'bdjpsikst_swp'
