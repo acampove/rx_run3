98657bebdb155c8f16693e4947f556645e9b885c
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-20 21:32:28 +0800

    Update test

--- tests/test_parameter_reader.py
+++ tests/test_parameter_reader.py
@@ -1,14 +1,17 @@
 from fitter    import ParameterReader
-from rx_common import Component, Qsq
+from rx_common import Project, Qsq
 from rx_common import Trigger 
 
 def test_simple():
     '''
     Test simplest use of reader
     '''
-    rdr    = ParameterReader(name = 'mid_window')
-    ms_sim = rdr(component=Component.jpsi, brem=1, block=3, trigger=Trigger.rk_ee_os, q2bin = Qsq.jpsi)
-    ms_dat = rdr(component=Component.data, brem=1, block=3, trigger=Trigger.rk_ee_os, q2bin = Qsq.jpsi)
+    rdr = ParameterReader(name = 'mid_window')
+    ms_sim = rdr(
+        brem     = 1, 
+        block    = 3, 
+        trigger  = Trigger.rk_ee_os, 
+        project  = Project.rk,
+        q2bin    = Qsq.jpsi)
 
-    _, _ = ms_sim.candidates
-    _, _ = ms_dat.candidates
+    print(ms_sim)
