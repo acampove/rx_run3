2e5a170d634d1d58e1e1596e407ad71cda100924
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-20 20:30:06 +0800

    Adding q2bin argument

--- tests/test_parameter_reader.py
+++ tests/test_parameter_reader.py
@@ -1,5 +1,5 @@
 from fitter    import ParameterReader
-from rx_common import Component
+from rx_common import Component, Qsq
 from rx_common import Trigger 
 
 def test_simple():
@@ -7,8 +7,8 @@ def test_simple():
     Test simplest use of reader
     '''
     rdr    = ParameterReader(name = 'mid_window')
-    ms_sim = rdr(component=Component.jpsi, brem=1, block=3, trigger=Trigger.rk_ee_os)
-    ms_dat = rdr(component=Component.data, brem=1, block=3, trigger=Trigger.rk_ee_os)
+    ms_sim = rdr(component=Component.jpsi, brem=1, block=3, trigger=Trigger.rk_ee_os, q2bin = Qsq.jpsi)
+    ms_dat = rdr(component=Component.data, brem=1, block=3, trigger=Trigger.rk_ee_os, q2bin = Qsq.jpsi)
 
     _, _ = ms_sim.candidates
     _, _ = ms_dat.candidates
