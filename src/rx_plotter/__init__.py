cbe67fbf3a2eb807ec30b3a3d12cf78ca3ef9c7f
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-21 13:16:40 +0800

    Add FitParametersConf

--- src/rx_plotter/__init__.py
+++ src/rx_plotter/__init__.py
@@ -1,4 +1,4 @@
-from .fit_parameters import FitParameters
+from .fit_parameters      import FitParameters
+from .fit_parameters_conf import FitParametersConf
 
-
-__all__ = ['FitParameters']
+__all__ = ['FitParameters', 'FitParametersConf']
