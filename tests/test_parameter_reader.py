c36642cc0c3c328d0c7dcfd7088e5965d5e2f735
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-21 11:24:21 +0800

    Add test for manager and improve tests

--- tests/test_parameter_reader.py
+++ tests/test_parameter_reader.py
@@ -1,7 +1,18 @@
+import os
+import pytest
+from pathlib import Path
+
 from fitter    import ParameterReader
 from rx_common import Project, Qsq
 from rx_common import Trigger 
+from dmu       import LogStore
 
+log=LogStore.add_logger('fitter:test_parameter_reader')
+# ----------------------
+@pytest.fixture(scope='session', autouse=True)
+def initialize() -> None:
+    LogStore.set_level('fitter:parameter_reader', 10)
+# -----------------------
 def test_simple():
     '''
     Test simplest use of reader
@@ -15,3 +26,22 @@ def test_simple():
         q2bin    = Qsq.jpsi)
 
     print(ms_sim)
+# -----------------------
+def test_pars_path():
+    '''
+    Test context manager user to set path to parameters 
+    '''
+    ana_dir = Path(os.environ['ANADIR'])
+    pars_path = ana_dir / 'fits/data/mid_window/parameters.parquet'
+
+    with ParameterReader.inputs_from(pars_path = pars_path):
+        rdr = ParameterReader(name = 'mid_window')
+        ms_sim = rdr(
+            brem     = 1, 
+            block    = 3, 
+            trigger  = Trigger.rk_ee_os, 
+            project  = Project.rk,
+            q2bin    = Qsq.jpsi)
+
+        print(ms_sim)
+
