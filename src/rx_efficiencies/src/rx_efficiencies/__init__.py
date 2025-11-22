87e8a339567248e0ced336879c6194b249487565
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-22 16:54:46 +0800

    Add EfficiencyScanner

--- src/rx_efficiencies/src/rx_efficiencies/__init__.py
+++ src/rx_efficiencies/src/rx_efficiencies/__init__.py
@@ -1,3 +1,4 @@
 from .efficiency_calculator import EfficiencyCalculator
+from .efficiency_scanner    import EfficiencyScanner
 
-__all__ = ['EfficiencyCalculator']
+__all__ = ['EfficiencyCalculator', 'EfficiencyScanner']
