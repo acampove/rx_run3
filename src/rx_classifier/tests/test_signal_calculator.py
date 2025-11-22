a630b84a2f4d2f13ced0fe4231df08a23787641e
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-22 19:14:19 +0800

    Improve test
    - Use path from pytest
    - Set caching directory
    - Use tuples when needed, not lists

--- src/rx_classifier/tests/test_signal_calculator.py
+++ src/rx_classifier/tests/test_signal_calculator.py
@@ -1,29 +1,20 @@
 '''
 This module contains tests for SignalCalculator class
 '''
-import os
-
 import pandas            as pnd
 import matplotlib.pyplot as plt
 import pytest
 
+from dmu.workflow                    import Cache
+from pathlib                         import Path
 from dmu.generic                     import utilities as gut
 from rx_classifier.signal_calculator import SignalCalculator
 
 # -----------------------------------
-class Data:
-    '''
-    Stores shared data
-    '''
-    out_dir = '/tmp/tests/rx_classifier/signal_calculator'
-# -----------------------------------
-@pytest.fixture(scope='session', autouse=True)
-def _initialize():
-    os.makedirs(Data.out_dir, exist_ok=True)
-# -----------------------------------
 def _plot_values(
-        df  : pnd.DataFrame,
-        name:  str) -> None:
+    df      : pnd.DataFrame,
+    out_dir : Path,
+    name    : str) -> None:
     '''
     Plot dataframe as an interpolated mesh
     '''
@@ -35,9 +26,9 @@ def _plot_values(
     plt.imshow(
         pivoted.values,
         origin='lower',
-        extent=[
+        extent=(
             pivoted.columns.min(), pivoted.columns.max(),
-            pivoted.index.min()  , pivoted.index.max()],
+            pivoted.index.min()  , pivoted.index.max()),
         cmap='viridis',
         interpolation='bilinear',
     )
@@ -47,18 +38,20 @@ def _plot_values(
     plt.ylabel('MVA${}_{prec}$')
     plt.tight_layout()
 
-    out_path = f'{Data.out_dir}/{name}.png'
+    out_path = out_dir / f'{name}.png'
     plt.savefig(out_path)
     plt.close()
 # --------------------------------
 @pytest.mark.parametrize('q2bin', ['low', 'central', 'high'])
-def test_simple(q2bin : str):
+def test_simple(q2bin : str, tmp_path : Path):
     '''
     Simplest test
     '''
     cfg = gut.load_data(package='rx_classifier_data', fpath='optimization/scanning.yaml')
-    cal = SignalCalculator(cfg=cfg, q2bin=q2bin)
-    df  = cal.get_signal(control=100_000)
 
-    _plot_values(df, name = f'simple_{q2bin}')
+    with Cache.cache_root(path = tmp_path):
+        cal = SignalCalculator(cfg=cfg, q2bin=q2bin)
+        df  = cal.get_signal(control=100_000)
+
+    _plot_values(df = df, name = f'simple_{q2bin}', out_dir = tmp_path)
 # --------------------------------
