b6ce84441b89c2f5f9cbcfc829eddddb2e9556fd
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-22 17:07:18 +0800

    Add ecal_calibration

--- import_packages.sh
+++ import_packages.sh
@@ -7,6 +7,7 @@ set -euo pipefail
 
 # Array of packages: "name|git_url|branch"
 PACKAGES=(
+"ecal_calibration|ssh://git@gitlab.cern.ch:7999/rx_run3/ecal_calibration.git|master"
 "ap_utilities|ssh://git@gitlab.cern.ch:7999/rx_run3/ap_utilities.git|master"
 "dmu|ssh://git@gitlab.cern.ch:7999/rx_run3/dmu.git|master"
 "rx_data|ssh://git@gitlab.cern.ch:7999/rx_run3/rx_data.git|master"
