cdcf5debceb991c5f64b4a2268af43ea59eed609
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-22 16:59:07 +0800

    Add classes

--- src/rx_efficiencies/src/rx_efficiencies/__init__.py
+++ src/rx_efficiencies/src/rx_efficiencies/__init__.py
@@ -1,4 +1,12 @@
 from .efficiency_calculator import EfficiencyCalculator
 from .efficiency_scanner    import EfficiencyScanner
+from .acceptance_reader     import AcceptanceReader
+from .acceptance_calculator import AcceptanceCalculator
+from .decay_names           import DecayNames
 
-__all__ = ['EfficiencyCalculator', 'EfficiencyScanner']
+__all__ = [
+    'EfficiencyCalculator', 
+    'EfficiencyScanner', 
+    'AcceptanceReader', 
+    'AcceptanceCalculator', 
+    'DecayNames']
