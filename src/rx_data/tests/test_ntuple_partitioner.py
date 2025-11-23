501d2802dae9a3ccd269005090855fada5557796
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-23 16:23:12 +0800

    Improve test

--- src/rx_data/tests/test_ntuple_partitioner.py
+++ src/rx_data/tests/test_ntuple_partitioner.py
@@ -1,12 +1,32 @@
 '''
 This module has functions to test the NtuplePartitioner class
 '''
+import pytest
 from rx_common import Project
 from rx_data   import NtuplePartitioner
 
-def test_simple():
-    prt   = NtuplePartitioner(kind = 'main', project = Project.rk_no_pid)
-    paths = prt.partition(index=3, total=10)
+_FRIENDS=[
+    'main', 
+    'mva', 
+    'hop', 
+    'mass',
+    'brem_track_2', 
+    'swp_jpsi_misid', 
+    'swp_cascade']
 
-    assert isinstance(paths, set)
+_TOTAL=3
+
+@pytest.mark.parametrize('friend', _FRIENDS)
+@pytest.mark.parametrize('index' , range(_TOTAL))
+def test_all_friends(friend : str, index : int):
+    '''
+    Test that can access all paths
+    '''
+    prt   = NtuplePartitioner(kind = friend, project = Project.rk)
+    paths = prt.get_paths(index=index, total=_TOTAL)
+
+    assert isinstance(paths, list)
     assert paths
+
+    for path in paths:
+        assert path.exists()
