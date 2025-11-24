132573980700c80cd628f679422cc07b3fc09e70
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-24 13:35:10 +0800

    Add Stats

--- src/rx_data/src/rx_data/__init__.py
+++ src/rx_data/src/rx_data/__init__.py
@@ -1,4 +1,5 @@
 from .rdf_getter         import RDFGetter
 from .ntuple_partitioner import NtuplePartitioner 
+from .stats              import Stats
 
-__all__ = ['RDFGetter', 'NtuplePartitioner']
+__all__ = ['RDFGetter', 'NtuplePartitioner', 'Stats']
