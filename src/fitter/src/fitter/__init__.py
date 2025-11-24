e1ac590872f4cb01f5b81568c65e44a542781433
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-24 14:13:44 +0800

    Add LikelihoodFactory to namespace

--- src/fitter/src/fitter/__init__.py
+++ src/fitter/src/fitter/__init__.py
@@ -1,4 +1,5 @@
-from .fit_summary      import FitSummary
-from .parameter_reader import ParameterReader
+from .fit_summary        import FitSummary
+from .parameter_reader   import ParameterReader
+from .likelihood_factory import LikelihoodFactory
 
-__all__ = ['FitSummary', 'ParameterReader']
+__all__ = ['FitSummary', 'ParameterReader', 'LikelihoodFactory']
