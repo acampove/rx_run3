63f72d0a88f7729304b9f9577e042fbc0f179f94
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-24 16:45:16 +0800

    Add more classes in fitter namespace

--- src/fitter/src/fitter/__init__.py
+++ src/fitter/src/fitter/__init__.py
@@ -1,5 +1,14 @@
 from .fit_summary        import FitSummary
 from .parameter_reader   import ParameterReader
 from .likelihood_factory import LikelihoodFactory
+from .toy_maker          import ToyMaker
+from .toy_plotter        import ToyPlotter
+from .data_fitter        import DataFitter 
 
-__all__ = ['FitSummary', 'ParameterReader', 'LikelihoodFactory']
+__all__ = [
+    'FitSummary', 
+    'ParameterReader', 
+    'DataFitter',
+    'ToyMaker',
+    'ToyPlotter',
+    'LikelihoodFactory']
