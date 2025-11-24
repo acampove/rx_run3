7d579452d5b0314377a3356cec7ffc2ad2508b65
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-24 13:37:44 +0800

    Update test
    - Pick classes from top namespaces
    - Remove d_samples dictionary
    - Use trigger enum instead of string

--- src/rx_data/tests/test_stats.py
+++ src/rx_data/tests/test_stats.py
@@ -3,23 +3,24 @@ Module with testing functions for the Stats class
 '''
 
 import pytest
-from dmu.logging.log_store import LogStore
-from rx_data.stats         import Stats
+from dmu        import LogStore
+from rx_common  import Trigger
+from rx_data    import Stats
 
 log=LogStore.add_logger('rx_data:test_stats')
 # ----------------------------------------
 @pytest.fixture(scope='session', autouse=True)
-def _initialize():
+def initialize():
+    '''
+    This runs before the tests
+    '''
     LogStore.set_level('rx_data:stats', 10)
-    Stats.d_sample = {
-            'main' : '/home/acampove/external_ssd/Data/samples/main.yaml',
-            }
 # ----------------------------------------
 def test_mcdt():
     '''
     Tests MCDecayTree statistics retrieval
     '''
-    obj = Stats(sample='Bu_JpsiK_ee_eq_DPC', trigger='Hlt2RD_BuToKpEE_MVA')
+    obj = Stats(sample='Bu_JpsiK_ee_eq_DPC', trigger=Trigger.rk_ee_os)
     val = obj.get_entries(tree='MCDecayTree')
 
     assert val > 0
