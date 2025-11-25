68be1a2d9b3128a0209ce16260142d270a1fc999
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-25 21:48:44 +0800

    Adding extra fields
    - YRangeConf
    - Yields
    - Observable
    ...

--- src/fitter/src/fitter_schema/data_config.py
+++ src/fitter/src/fitter_schema/data_config.py
@@ -13,17 +13,24 @@ class ObservableConfig:
     range: list[float] = field(default_factory=list)
 
 @dataclass
+class YRangeConf:
+    log    : list[Any] = field(default_factory=list[Any]) 
+    linear : list[Any] = field(default_factory=list[Any]) 
+
+@dataclass
 class PlotConfig:
     nbins  : int            = 30
     stacked: bool           = False
+    yrange : YRangeConf     = field(default_factory=YRangeConf)
     d_leg  : dict[str, str] = field(default_factory=dict)
+    plot_components : str   = field(default_factory=str)
 
 @dataclass
 class ModelConfig:
-    yields     : str                  = MISSING
-    observable : ObservableConfig     = MISSING
-    components : dict[str, str]       = field(default_factory=dict)
-    constraints: dict[str, list[str]] = field(default_factory=dict)
+    yields     : str                         = MISSING
+    observable : dict[str, ObservableConfig] = MISSING
+    components : dict[str, str]              = field(default_factory=dict)
+    constraints: dict[str, Any]              = field(default_factory=dict)
 
 @dataclass
 class ConfigSchema:
@@ -31,3 +38,5 @@ class ConfigSchema:
     model           : ModelConfig    = MISSING
     fit             : dict[str, Any] = field(default_factory=dict)   # If unspecified now
     plots           : PlotConfig     = field(default_factory=PlotConfig)
+    trigger         : str            = field(default_factory=str)
+    selection       : dict[str, str] = field(default_factory=dict)
