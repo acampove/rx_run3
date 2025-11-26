e363b5b0fe82771748bb238cac97fe413c62eb7d
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-26 16:07:06 +0800

    Drop rel_dir, define it in the test itself

--- src/fitter/tests/test_misid_constraints.py
+++ src/fitter/tests/test_misid_constraints.py
@@ -15,12 +15,6 @@ from dmu.generic              import utilities     as gut
 from fitter.misid_constraints import MisIDConstraints 
 
 log=LogStore.add_logger('fitter:test_misid_constraints')
-# ----------------------
-class Data:
-    '''
-    Class meant to be used to share attributes
-    '''
-    rel_dir = 'misid_constraints'
 # --------------------------------------------------------------
 @pytest.fixture(scope='session', autouse=True)
 def initialize():
@@ -32,8 +26,6 @@ def initialize():
     LogStore.set_level('fitter:base_fitter'      , 30)
     LogStore.set_level('fitter:data_preprocessor', 10)
     LogStore.set_level('rx_misid:sample_weighter', 20)
-
-    os.makedirs(Data.rel_dir, exist_ok=True)
 # ----------------------
 @pytest.mark.parametrize('q2bin', ['low', 'central', 'high'])
 def test_simple(q2bin : str, tmp_path : Path) -> None:
@@ -42,7 +34,7 @@ def test_simple(q2bin : str, tmp_path : Path) -> None:
     '''
     obs = zfit.Space('B_Mass_smr', limits=(4500, 6000))
     cfg = gut.load_conf(package='fitter_data', fpath='misid/rk/electron/data_misid.yaml')
-    cfg.output_directory = Data.rel_dir
+    cfg.output_directory = 'misid_constraints' 
 
     with sel.custom_selection(d_sel={'nobrm0' : 'nbrem != 0'}),\
          RDFGetter.max_entries(value = -1),\
