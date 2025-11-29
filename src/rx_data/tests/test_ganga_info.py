d6ad8277d773609a774102fa6031c71caf24fccd
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-29 19:25:48 +0800

    Do not run these tests
    Need Ganga sandbox or mock of it

--- src/rx_data/tests/test_ganga_info.py
+++ src/rx_data/tests/test_ganga_info.py
@@ -2,10 +2,10 @@
 This module has tests for the GangaInfo class
 '''
 import pytest
-from pathlib            import Path
 
+from pathlib               import Path
 from dmu.generic.utilities import LogStore
-from rx_data.ganga_info import GangaInfo
+from rx_data.ganga_info    import GangaInfo
 
 # ----------------------
 @pytest.fixture(scope='session', autouse=True)
@@ -15,21 +15,22 @@ def initialize():
     '''
     LogStore.set_level('rx_data:ganga_info', 10)
 # ----------------------
-def test_small():
+@pytest.mark.skip(reason='Need Ganga sandbox, this does not exist in REANA, CI/CD, etc')
+def test_small(tmp_path : Path):
     '''
     Tests with small inputs
     '''
     block = 'w40_42'
     fname = 'mc_magup_11264001_bd_dmnpipl_eq_dpc_Hlt2RD_BuToKpEE_MVA_bd46a6ff81.root'
-    dir   = Path('/home/acampove/Tests/filtering')
 
-    with GangaInfo.set_ganga_dir(dir=dir):
+    with GangaInfo.set_ganga_dir(dir=tmp_path):
         inf = GangaInfo(job_ids=[257])
         assert block == inf.block_from_fname(fname=fname)
 # ----------------------
+@pytest.mark.skip(reason='Need Ganga sandbox, this does not exist in REANA, CI/CD, etc')
 @pytest.mark.parametrize('job_id', [257, 258])
 def test_full(job_id : int):
     '''
     Tests with all inputs
     '''
-    inf = GangaInfo(job_ids=[job_id])
+    _ = GangaInfo(job_ids=[job_id])
