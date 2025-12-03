9e48353f12544657722449bb7d3cd59941f11670
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-03 17:55:15 +0800

    Update

--- hooks/replace_with_sha.sh
+++ hooks/replace_with_sha.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+# This hook is meant to swap xxx in:
+#
+# gitlab-registry.cern.ch/lhcb-rd/cal-rx-run3:XXX
+#
+# with the SHA of the current commit. This is meant to be done in the Snakefile
+
 set -euo pipefail
 
 echo "Running SHA replacement"
 
+if [[ ! -f Snakefile ]];then
+    echo "Missing Snakefile"
+    exit 1
+fi
