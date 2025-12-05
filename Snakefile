ca8d60c9d57209e676b9f04009aee0639cb016e5
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-05 14:47:20 +0800

    Improvements and fixes due to new zfit behavior?
    - Use tmp_path instead of custom
    - Do not use values at the edge of parameter, but just before or in the
      middle

--- Snakefile
+++ Snakefile
@@ -24,7 +24,7 @@ rule test:
         path   = ' '.join(PATHS),
         ngroups= NJOBS
     container:
-        'gitlab-registry.cern.ch/lhcb-rd/cal-rx-run3:993d423ce'
+        'gitlab-registry.cern.ch/lhcb-rd/cal-rx-run3:4df4972cb'
     resources:
         kubernetes_memory_limit="4000Mi"
     shell:
--- src/dmu/tests/stats/test_zfit_models.py
+++ src/dmu/tests/stats/test_zfit_models.py
@@ -42,66 +42,69 @@ def _plot_pdf(pdf : zpdf, par_val : float) -> None:
 
     plt.plot(arr_x, arr_y, label=par_str)
 # -------------------------------
-def test_modexp_mu_scan():
+def test_modexp_mu_scan(tmp_path : Path):
     '''
     Test ModExp PDF scan over mu parameter
     '''
     min_mu = Data.l_mu_mod_exp[ 0]
     max_mu = Data.l_mu_mod_exp[-1]
+    mid_mu = 0.5 * (min_mu + max_mu) 
 
-    mu = zfit.Parameter('mu', min_mu, min_mu, max_mu)
+    mu = zfit.Parameter('mu', mid_mu, min_mu, max_mu)
     ap = zfit.Parameter('ap',   0.01,   0.01,   0.02)
     bt = zfit.Parameter('bt',  0.002,  0.001,  0.005)
     pdf= ModExp(obs=Data.obs, mu=mu, alpha=ap, beta=bt)
 
-    for mu_val in Data.l_mu_mod_exp:
+    for mu_val in Data.l_mu_mod_exp[1:-1]:
         mu.set_value(mu_val)
         _plot_pdf(pdf, par_val=mu_val)
 
     plt.title(r'$\mu$')
     plt.legend()
-    plt.savefig(f'{Data.out_dir}/mu_scan.png')
+    plt.savefig(tmp_path / 'mu_scan.png')
     plt.close()
 # -------------------------------
-def test_modexp_ap_scan():
+def test_modexp_ap_scan(tmp_path : Path):
     '''
     Test ModExp PDF scan over alpha parameter
     '''
     min_ap = Data.l_ap_mod_exp[ 0]
     max_ap = Data.l_ap_mod_exp[-1]
+    mid_ap = 0.5 * (min_ap + max_ap)
 
     mu = zfit.Parameter('mu',   4500,   4000,   5000)
-    ap = zfit.Parameter('ap', min_ap, min_ap, max_ap)
+    ap = zfit.Parameter('ap', mid_ap, min_ap, max_ap)
     bt = zfit.Parameter('bt',  0.002,  0.001,  0.005)
     pdf= ModExp(obs=Data.obs, mu=mu, alpha=ap, beta=bt)
 
-    for ap_val in Data.l_ap_mod_exp:
+    for ap_val in Data.l_ap_mod_exp[1:-1]:
         ap.set_value(ap_val)
         _plot_pdf(pdf, par_val=ap_val)
 
     plt.title(r'$\alpha$')
     plt.legend()
-    plt.savefig(f'{Data.out_dir}/ap_scan.png')
+    plt.savefig(tmp_path / 'ap_scan.png')
     plt.close()
 # -------------------------------
-def test_modexp_bt_scan():
+def test_modexp_bt_scan(tmp_path : Path):
     '''
     Test ModExp PDF scan over beta parameter
     '''
     min_bt = Data.l_bt_mod_exp[ 0]
     max_bt = Data.l_bt_mod_exp[-1]
+    mid_bt = 0.5 * (min_bt + max_bt)
 
     mu = zfit.Parameter('mu',   4500,   4000,     5000)
     ap = zfit.Parameter('ap',   0.01,   0.01,     0.02)
-    bt = zfit.Parameter('bt', min_bt, min_bt,   max_bt)
+    bt = zfit.Parameter('bt', mid_bt, min_bt,   max_bt)
     pdf= ModExp(obs=Data.obs, mu=mu, alpha=ap, beta=bt)
 
-    for bt_val in Data.l_bt_mod_exp:
+    for bt_val in Data.l_bt_mod_exp[1:-1]:
         bt.set_value(bt_val)
         _plot_pdf(pdf, par_val=bt_val)
 
     plt.title(r'$\beta$')
     plt.legend()
-    plt.savefig(f'{Data.out_dir}/bt_scan.png')
+    plt.savefig(tmp_path / 'bt_scan.png')
     plt.close()
 # -------------------------------
