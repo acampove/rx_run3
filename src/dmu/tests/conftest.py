fadd5a7f8b5cc79fb81075d51674a40ef2a0f638
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-06 19:18:52 +0800

    Remove no_schema_pkg

--- src/dmu/tests/conftest.py
+++ src/dmu/tests/conftest.py
@@ -3,10 +3,7 @@ File needed by pytest
 '''
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-import sys
 import logging
-
-import pytest
 import numpy
 import mplhep
 import matplotlib.pyplot as plt
@@ -35,24 +32,4 @@ def pytest_configure(config : Config):
     plt.style.use(mplhep.style.LHCb2)
     user = os.environ['USER']
     Wcache.set_cache_root(root=f'/tmp/{user}/tests/dmu/cache/cache_dir')
-# ------------------------------
-@pytest.fixture
-def no_schema_pkg(tmp_path):
-    '''
-    This fixture creates a fake package missing the schema
-    validating data package
-    '''
-    pkg_dir = tmp_path / 'fake_data' 
-    pkg_dir.mkdir()
-    (pkg_dir / '__init__.py').write_text('')
-    (pkg_dir / 'config.yaml').write_text('dummy: data')
-
-    sys.path.insert(0, str(tmp_path))
-    try:
-        yield pkg_dir
-    finally:
-        try:
-            sys.path.remove(str(tmp_path))
-        except ValueError:
-            pass
 # ------------------------------
