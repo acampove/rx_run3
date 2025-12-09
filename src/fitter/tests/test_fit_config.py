e5542eb1da7d263d5b83f36afeb9cf120e20ad64
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-09 12:45:17 +0800

    Add initializer fixture and test for saving

--- src/fitter/tests/test_fit_config.py
+++ src/fitter/tests/test_fit_config.py
@@ -2,11 +2,21 @@
 This module contains tests for the FitConfig class
 '''
 
-from omegaconf             import OmegaConf, DictConfig
-from fitter.fit_config     import FitConfig
-from dmu.logging.log_store import LogStore
+import pytest
+from pathlib           import Path
+from omegaconf         import OmegaConf, DictConfig
+from fitter.fit_config import FitConfig
+from dmu               import LogStore
+from dmu.generic       import utilities as gut
 
 log=LogStore.add_logger('fitter:test_fit_config')
+# ----------------------
+@pytest.fixture(scope='module', autouse=True)
+def initialize():
+    '''
+    This will run before any test
+    '''
+    LogStore.set_level('fitter:fit_config', 10)
 # ----------------------
 def _get_config(name : str) -> DictConfig:
     fit_cfg = {
@@ -24,7 +34,20 @@ def test_replace():
     new     = '7634244'
     fit_cfg = _get_config(name = org)
 
-    cfg = FitConfig(fit_cfg = fit_cfg)
+    cfg = FitConfig(name = 'test_replace', fit_cfg = fit_cfg)
     cfg.replace(substring=org, value=new)
 
     assert cfg.fit_cfg == _get_config(name=new)
+# ----------------------
+def test_save(tmp_path : Path):
+    '''
+    Tests saving config 
+    '''
+    name    = 'test_save'
+    fit_cfg = _get_config(name = name)
+
+    with gut.environment(mapping = {'ANADIR' : str(tmp_path)}):
+        cfg = FitConfig(name = name, fit_cfg = fit_cfg)
+        cfg.save()
+
+    assert cfg.fit_cfg == _get_config(name=name)
