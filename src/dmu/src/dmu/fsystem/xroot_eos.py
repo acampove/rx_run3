cab36aae6212ef4672771ad222f4142fa047f512
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-11 15:50:06 +0800

    Add logger and docstrings

--- src/dmu/src/dmu/fsystem/xroot_eos.py
+++ src/dmu/src/dmu/fsystem/xroot_eos.py
@@ -3,7 +3,9 @@ This module contains the XRootEOS class
 '''
 from XRootD.client        import FileSystem
 from XRootD.client.flags  import DirListFlags, StatInfoFlags
+from dmu                  import LogStore
 
+log=LogStore.add_logger('dmu:xroot_eos')
 # ----------------------
 class XRootEOS:
     '''
@@ -20,12 +22,22 @@ class XRootEOS:
         '''
         self._fs = FileSystem(host)
     # ----------------------
-    def glob(self, root : str, ext : str) -> list[str]:
-        print(f'Logging into: {root}')
+    def glob(self, dir_path : str, ext : str) -> list[str]:
+        '''
+        Parameters
+        ------------
+        dir_path: Full path to directory in EOS where globbing should start
+        ext     : Extension, e.g. root
+
+        Returns
+        ------------
+        List of paths in EOS, i.e. PFNs
+        '''
+        log.debug(f'Logging into: {dir_path}')
     
-        status , dir_list = self._fs.dirlist(root, DirListFlags.STAT)
+        status , dir_list = self._fs.dirlist(dir_path, DirListFlags.STAT)
         if not status.ok:
-            raise ValueError(f'Cannot open {root}')
+            raise ValueError(f'Cannot open {dir_path}')
     
         paths   = []
         objects = dir_list.dirlist
@@ -35,11 +47,14 @@ class XRootEOS:
         for entry in objects:
             is_dir = (entry.statinfo.flags & StatInfoFlags.IS_DIR)
             if is_dir:
-                paths += self.glob(root=f'{root}/{entry.name}', ext = ext)
+                paths += self.glob(dir_path=f'{dir_path}/{entry.name}', ext = ext)
                 continue
     
             if entry.name.endswith(f'.{ext}'):
-                paths.append(f'{root}/{entry.name}')
+                paths.append(f'{dir_path}/{entry.name}')
+
+        npath = len(paths)
+        log.debug(f'Found {npath} paths')
     
         return paths
 # ----------------------
