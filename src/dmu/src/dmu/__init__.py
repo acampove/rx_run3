5332b2c31dec8d429ab0896e6906057ed505f277
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-11 15:42:03 +0800

    Add XRootEOS

--- src/dmu/src/dmu/__init__.py
+++ src/dmu/src/dmu/__init__.py
@@ -2,5 +2,11 @@ from .fsystem.fcopy     import FCopy
 from .stats.measurement import Measurement
 from .logging.log_store import LogStore
 from .testing           import pytest_utilities
+from .fsystem.xroot_eos import XRootEOS
 
-__all__ = ['FCopy', 'Measurement', 'LogStore', 'pytest_utilities']
+__all__ = [
+    'FCopy', 
+    'Measurement', 
+    'LogStore', 
+    'XRootEOS',
+    'pytest_utilities']
