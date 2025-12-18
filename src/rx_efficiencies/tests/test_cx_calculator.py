9ca48e1409054f4951bb0dda89c9d5d2edce52e2
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-18 20:52:01 +0800

    Add q2 argument

--- src/rx_efficiencies/tests/test_cx_calculator.py
+++ src/rx_efficiencies/tests/test_cx_calculator.py
@@ -5,15 +5,16 @@ This module is used to test the CXCalculator class
 import pytest
 
 from rx_efficiencies import CXCalculator
-from rx_common       import Project
+from rx_common       import Project, Qsq
 
 # ----------------------
 @pytest.mark.parametrize('project', [Project.rk, Project.rkst])
-def test_cxcalculator(project : Project) -> None:
+@pytest.mark.parametrize('qsq'    , [Qsq.low, Qsq.central, Qsq.high])
+def test_cxcalculator(project : Project, qsq : Qsq) -> None:
     '''
     Tests calculation for different projects
     '''
-    obj      = CXCalculator(project = project)
+    obj      = CXCalculator(project = project, qsq = qsq)
     val, err = obj.calculate()
 
     assert val > 0
