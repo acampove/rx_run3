af0dbedfe060be63707060e2102f6f091e00d06a
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-19 17:53:09 +0800

    Remove CXCalculator

--- src/rx_efficiencies/src/rx_efficiencies/__init__.py
+++ src/rx_efficiencies/src/rx_efficiencies/__init__.py
@@ -1,4 +1,3 @@
-from .cxcalculator          import CXCalculator
 from .efficiency_calculator import EfficiencyCalculator
 from .efficiency_scanner    import EfficiencyScanner
 from .acceptance_reader     import AcceptanceReader
@@ -6,7 +5,6 @@ from .acceptance_calculator import AcceptanceCalculator
 from .decay_names           import DecayNames
 
 __all__ = [
-    'CXCalculator',
     'EfficiencyCalculator', 
     'EfficiencyScanner', 
     'AcceptanceReader', 
