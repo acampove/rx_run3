3b126acd084c6c072e36572d4d4b7c9a348f5649
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-22 20:52:17 +0800

    Updating tests
    For new design of constraint wrapping classes

--- src/dmu/tests/stats/test_constraint.py
+++ src/dmu/tests/stats/test_constraint.py
@@ -2,6 +2,7 @@
 Module with tests for classes and functions in constraint.py module
 '''
 import zfit
+import numpy
 
 from zfit.constraint import GaussianConstraint as GConstraint 
 from zfit.constraint import PoissonConstraint  as PConstraint 
@@ -11,44 +12,68 @@ from dmu.stats import PoissonConstraint
 from dmu.stats import print_constraints 
 
 # ---------------------------------
-def test_make_gauss():
+def test_gauss():
     '''
     Basic test for GaussianConstraint
     '''
+    numpy.random.seed(42)
+
+    par = zfit.param.Parameter('a', 1, 0, 2)
     cns = GaussianConstraint(
-        name = 'a', 
-        mu   = 1, 
-        sg   = 2)
+        par = par, 
+        mu  = 1, 
+        sg  = 2)
 
-    par = zfit.param.Parameter('a', 0, 0, 2)
-    val = cns.to_zfit(par = par)
+    assert isinstance(cns.zfit_cons, GConstraint)
 
-    assert isinstance(val, GConstraint)
+    values = []
+    for _ in range(100):
+        cns.resample()
+        val = cns.observation.value()
+        values.append(val)
+
+    mu=numpy.mean(values)
+    sg=numpy.std(values)
+
+    assert numpy.isclose(mu, 0.792306965211812, rtol=1e-5)
+    assert numpy.isclose(sg, 1.807232353289259, rtol=1e-5)
 # ---------------------------------
-def test_make_poisson():
+def test_poisson():
     '''
     Basic test for PoissonConstraint
     '''
+    numpy.random.seed(42)
+
+    par = zfit.param.Parameter('a', 1, 0, 20)
     cns = PoissonConstraint(
-        name = 'a', 
-        lam  = 1)
+        par = par, 
+        lam = 10.)
 
-    par = zfit.param.Parameter('a', 0, 0, 2)
-    val = cns.to_zfit(par = par)
+    assert isinstance(cns.zfit_cons, PConstraint)
 
-    assert isinstance(val, PConstraint)
+    values = []
+    for _ in range(100):
+        cns.resample()
+        val = cns.observation.value()
+        values.append(val)
+
+    lam = numpy.mean(values)
+
+    assert numpy.isclose(lam, 10.06, rtol=1e-5)
 # ---------------------------------
 def test_print_constraint():
     '''
     Test printing of constraints
     '''
-    ca = PoissonConstraint(
-        name = 'a', 
-        lam  = 1)
+    par = zfit.param.Parameter('a', 1, 0, 2)
+    ca  = PoissonConstraint(
+        par = par, 
+        lam = 1)
 
-    cb = GaussianConstraint(
-        name = 'b', 
-        mu   = 1, 
-        sg   = 2)
+    par = zfit.param.Parameter('a', 1, 0, 20)
+    cb  = GaussianConstraint(
+        par = par, 
+        mu  = 1, 
+        sg  = 2)
 
     print_constraints(constraints = [ca, cb])
--- toys.smk
+++ toys.smk
@@ -25,7 +25,7 @@ rule collect:
         prc = r'\d{3}', 
         qsq = '[a-z]+', 
     container:
-        'gitlab-registry.cern.ch/lhcb-rd/cal-rx-run3:c4464c6c5'
+        'gitlab-registry.cern.ch/lhcb-rd/cal-rx-run3:138ea1f8a'
     shell:
         '''
         REMOTE=$(echo {output} | sed 's/\.eos/\/eos/g')
@@ -47,7 +47,7 @@ rule toys:
         ntoys = ntoys,
         conf  = conf_val,
     container:
-        'gitlab-registry.cern.ch/lhcb-rd/cal-rx-run3:c4464c6c5'
+        'gitlab-registry.cern.ch/lhcb-rd/cal-rx-run3:138ea1f8a'
     resources:
         kubernetes_memory_limit='5000Mi'
     shell : 
