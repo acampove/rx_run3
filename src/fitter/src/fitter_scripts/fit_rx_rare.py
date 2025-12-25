783a89a5a9687946b2615ed5c1a5de80592e6034
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-25 19:41:04 +0800

    Update imports remove alias to constraint

--- src/fitter/src/fitter_scripts/fit_rx_rare.py
+++ src/fitter/src/fitter_scripts/fit_rx_rare.py
@@ -25,14 +25,13 @@ from fitter.data_fitter        import DataFitter
 from fitter.likelihood_factory import LikelihoodFactory
 from fitter.misid_constraints  import MisIDConstraints 
 from fitter.toy_maker          import ToyMaker
-from fitter.constraint         import GaussianConstraint, PoissonConstraint, print_constraints
+from dmu.stats                 import Constraint
 from rx_data.rdf_getter        import RDFGetter
 from rx_selection              import selection as sel
 from rx_common                 import info
 
 log=LogStore.add_logger('fitter:fit_rx_rare')
 
-Constraint               = GaussianConstraint | PoissonConstraint
 DATA_SAMPLE : Final[str] = 'DATA_24_*'
 # ----------------------
 def _parse_args(args : DictConfig | argparse.Namespace | None = None) -> FitConfig:
