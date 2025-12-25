b619e1c6769bbad2b7867a0bbcd5a1330c9c77a0
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-25 20:29:08 +0800

    Add FitConfig to fitter namespace

--- src/fitter/src/fitter/__init__.py
+++ src/fitter/src/fitter/__init__.py
@@ -1,4 +1,5 @@
 from .fit_summary        import FitSummary
+from .fit_config         import FitConfig
 from .parameter_reader   import ParameterReader
 from .likelihood_factory import LikelihoodFactory
 from .toy_maker          import ToyMaker
@@ -10,6 +11,7 @@ from .constraint_reader  import ConstraintReader
 
 __all__ = [
     'FitSummary', 
+    'FitConfig',
     'ConstraintReader',
     'DataPreprocessor',
     'ParameterReader', 
