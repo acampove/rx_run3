3a316705059f137964ff168eefacf0e975c71364
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-26 16:16:23 +0800

    Adding basic test

--- src/fitter/tests/test_cmb_constraints.py
+++ src/fitter/tests/test_cmb_constraints.py
@@ -2,24 +2,29 @@
 Module used to test CmbConstraints class
 '''
 
+import zfit
+import pytest 
+
 from dmu.stats   import ConstraintND
+from rx_common   import Qsq
 from fitter      import CmbConstraints
 from dmu.generic import utilities as gut
 
 # ----------------------
-def test_simple():
+@pytest.mark.parametrize('q2bin', ['low'])
+def test_simple(q2bin : Qsq):
     '''
     Simplest test of CmbConstraints
     '''
-    cfg = gut.load_conf(package='fitter_data', fpath = 'tests/fits/constraint_reader.yaml')
+    fit_cfg = gut.load_conf(package='fitter_data', fpath = 'tests/fits/constraint_reader.yaml')
+    obs     = zfit.Space('B_Mass', 4500, 7000)
 
     calc      = CmbConstraints(
-        obs   = cfg.observable,
-        cfg   = cfg.fit_cfg.model.components.combinatorial,
-        q2bin = cfg.q2bin)
+        obs   = obs,
+        cfg   = fit_cfg.model.components.combinatorial,
+        q2bin = q2bin)
 
-    constraints = calc.get_constraints()
+    constraint = calc.get_constraint()
 
-    assert isinstance(constraints, list)
-    assert all( isinstance(const, ConstraintND) for const in constraints )
+    assert isinstance(constraint, ConstraintND)
 
