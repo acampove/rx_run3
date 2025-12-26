25d3a4f1a5447d0309f6de35bb8cd788b7e39c42
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-26 17:32:05 +0800

    Add implementation

--- src/fitter/src/fitter/cmb_constraints.py
+++ src/fitter/src/fitter/cmb_constraints.py
@@ -32,9 +32,76 @@ class CmbConstraints:
         cfg  : configuration for combinatorial component
         q2bin: E.g. central
         '''
-        self._obs   = obs
-        self._cfg   = cfg
-        self._q2bin = q2bin
+        self._obs    = obs
+        self._cfg    = cfg
+        self._cmb_cfg= cfg.model.combinatorial
+        self._q2bin  = q2bin
+    # ----------------------
+    def _get_data(self) -> zdat:
+        '''
+        Returns
+        -------------
+        1D numpy array with masses to fit
+        '''
+        cmb_cfg= self._cfg.model.combinatorial
+
+        cons   = cmb_cfg[self._q2bin]['constraints']
+        sample = Sample(cons.sample)
+        trigger= Trigger(cons.trigger)
+
+        gtr = RDFGetter(sample = sample, trigger = trigger)
+        rdf = gtr.get_rdf(per_file = False)
+
+        rdf = sel.apply_full_selection(
+            rdf     = rdf,
+            trigger = trigger,
+            process = sample,
+            q2bin   = self._q2bin,
+        )
+
+        array = rdf.AsNumpy([self._obs.name])[self._obs.name]
+        data  = zfit.Data.from_numpy(obs = self._obs, array = array)
+
+        if not isinstance(data, zdat):
+            raise ValueError('Data is not a zfit.Data instance')
+
+        return data
+    # ----------------------
+    def _get_model(self) -> zpdf:
+        '''
+        Returns
+        -------------
+        PDF used to fit combinatorial
+        '''
+
+        mod         = ModelFactory(
+            preffix = 'ss' ,
+            obs     = self._obs,
+            l_pdf   = self._cmb_cfg['categories'].main.models[self._q2bin],
+            l_shared= self._cmb_cfg[self._q2bin ].shared,
+            l_float = self._cmb_cfg[self._q2bin ].float,
+        )
+
+        pdf = mod.get_pdf()
+
+        return pdf
+    # ----------------------
+    def _fit(self, data : zdat) -> zres:
+        '''
+        Parameters
+        -------------
+        data: 1D array with masses
+
+        Returns
+        -------------
+        Result of fit
+        '''
+        pdf = self._get_model()
+
+        ftr = Fitter(pdf = pdf, data = data)
+        res = ftr.fit()
+
+        return res
     # ----------------------
     def get_constraint(self) -> ConstraintND:
         '''
@@ -42,14 +109,19 @@ class CmbConstraints:
         ----------------------
         N dimensional Gaussian constraint
         '''
+        data = self._get_data()
+        res  = self._fit(data=data)
+
+        all_pars   = self._cmb_cfg[self._q2bin].constraints.parameters
+        values     = [ float(par.value().numpy()) for par in res.params if par.name in all_pars ]
+        parameters = [ par.name                   for par in res.params if par.name in all_pars ]
+        cov        = res.covariance(params=parameters)
+
         cns = ConstraintND(
             kind       = 'GaussianConstraint',
-            parameters = ['a', 'b', 'c'],
-            values     = [1.0, 2.0, 3.0],
-            cov        = [
-                [1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9]]
+            parameters = parameters, 
+            values     = values, 
+            cov        = cov,
         )
 
         return cns
