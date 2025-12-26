5d611e217e2f96afd59f183c47ccfe2445d85835
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-26 21:40:45 +0800

    Do not put Measurement in dmu namespace
    Was already in stats namespace

--- src/dmu/src/dmu/__init__.py
+++ src/dmu/src/dmu/__init__.py
@@ -1,12 +1,10 @@
 from .fsystem.fcopy     import FCopy
-from .stats.measurement import Measurement
 from .logging.log_store import LogStore
 from .testing           import pytest_utilities
 from .fsystem.xroot_eos import XRootEOS
 
 __all__ = [
     'FCopy', 
-    'Measurement', 
     'LogStore', 
     'XRootEOS',
     'pytest_utilities']
