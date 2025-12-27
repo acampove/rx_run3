1a2788b2d539c8cbd3ca7a17989bc7db125474c8
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-27 22:05:04 +0800

    Test with custom selection

--- src/fitter/tests/test_cmb_constraints.py
+++ src/fitter/tests/test_cmb_constraints.py
@@ -14,6 +14,7 @@ from dmu          import LogStore
 from dmu.generic  import utilities           as gut
 from dmu.stats    import ModelFactory
 from zfit.loss    import ExtendedUnbinnedNLL as zlos
+from rx_selection import selection           as sel
 
 log=LogStore.add_logger('test_cmb_constraints')
 # ----------------------
@@ -51,7 +52,8 @@ def test_simple(q2bin : Qsq, tmp_path : Path):
     fit_cfg   = gut.load_conf(package='fitter_data', fpath = 'tests/fits/constraint_reader.yaml')
     nll       = _get_nll()
 
-    with Cache.cache_root(path = tmp_path):
+    with Cache.cache_root(path = tmp_path),\
+        sel.custom_selection(d_sel = {'bdt' : 'mva_cmb > 0.8'}):
         calc      = CmbConstraints(
             name  = 'test',
             nll   = nll,
