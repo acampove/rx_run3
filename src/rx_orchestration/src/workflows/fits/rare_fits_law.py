c1bd743bd1ae24ed628638696c2f07c74ab21d53
Angel Campoverde <angel.campoverde@cern.ch>
2025-12-29 00:05:32 +0800

    Remove _get_mva_cut
    - Do not multiply WP by 100, they are already scaled
    - Only pad with 3 zeros

--- src/rx_orchestration/src/workflows/fits/rare_fits_law.py
+++ src/rx_orchestration/src/workflows/fits/rare_fits_law.py
@@ -50,21 +50,6 @@ class Fit(law.Task):
 
         return self._cfg
     # ----------------------
-    def _get_mva_cut(self, args : DictConfig) -> str:
-        '''
-        Parameters
-        -------------
-        args: Configuration needed for fit
-
-        Returns
-        -------------
-        string identifying MVA WP, e.g. 050_050
-        '''
-        cmb_wp = args.mva_cmb * 100
-        prc_wp = args.mva_prc * 100
-
-        return f'{cmb_wp:03.0f}_{prc_wp:03.0f}'
-    # -------------------------------------
     def output(self) -> list[law.LocalFileTarget]:
         '''
         Returns
@@ -75,7 +60,7 @@ class Fit(law.Task):
         args    = cfg.args
 
         ana_dir = Path(os.environ['ANADIR'])
-        mva_cut = self._get_mva_cut(args=args)
+        mva_cut = f'{args.mva_cmb:03}_{args.mva_prc:03}'
 
         if args.fit_cfg.endswith('muon'):
             l_brem = ['brem_0xx']
