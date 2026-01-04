0dff2632917154d1e77c028fe0f1dfe0f1d7e76a
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-04 22:02:55 +0800

    Add SWPCalculator to rx_data namespace

--- src/rx_data/src/rx_data/__init__.py
+++ src/rx_data/src/rx_data/__init__.py
@@ -1,6 +1,12 @@
 from .rdf_getter         import RDFGetter
+from .swp_calculator     import SWPCalculator
 from .spec_maker         import SpecMaker
 from .ntuple_partitioner import NtuplePartitioner 
 from .stats              import Stats
 
-__all__ = ['RDFGetter', 'NtuplePartitioner', 'Stats', 'SpecMaker']
+__all__ = [
+    'NtuplePartitioner', 
+    'SWPCalculator',
+    'RDFGetter', 
+    'Stats', 
+    'SpecMaker']
