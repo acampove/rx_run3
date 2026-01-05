96d3fd6fcf4731cc9c47033b92cc6a45f6b275e5
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-05 09:30:53 +0800

    Add MisCalculator to rx_data namespace

--- snakemake/rules/test_reana.smk
+++ snakemake/rules/test_reana.smk
@@ -6,7 +6,7 @@ rule test:
     output:
         'results/file_{index}.txt'
     container:
-        'gitlab-registry.cern.ch/lhcb-rd/cal-rx-run3:61c0d55d5'
+        'gitlab-registry.cern.ch/lhcb-rd/cal-rx-run3:50b5fb43e'
     resources:
         kubernetes_memory_limit="4000Mi"
     shell:
--- src/rx_data/src/rx_data/__init__.py
+++ src/rx_data/src/rx_data/__init__.py
@@ -3,9 +3,11 @@ from .swp_calculator     import SWPCalculator
 from .spec_maker         import SpecMaker
 from .ntuple_partitioner import NtuplePartitioner 
 from .stats              import Stats
+from .mis_calculator     import MisCalculator
 
 __all__ = [
     'NtuplePartitioner', 
+    'MisCalculator',
     'SWPCalculator',
     'RDFGetter', 
     'Stats', 
