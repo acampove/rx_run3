0f68281f67db60bb776a513f083c1222031f9b42
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-07 11:16:37 +0800

    Update fit_cfg argument
    Needs full path to config for data fit.
    Needed to switch models from DTF to non-DTF fits

--- src/fitter/src/fitter_scripts/fit_rx_reso.py
+++ src/fitter/src/fitter_scripts/fit_rx_reso.py
@@ -53,19 +53,19 @@ def _parse_args(args : DictConfig | argparse.Namespace | None) -> FitConfig:
     '''
     if args is None:
         parser = argparse.ArgumentParser(description='Script used to fit RX data')
-        parser.add_argument('-b', '--block'  , type=int  , help='Block number, if not passed will do all data'    , choices =[1,2,3,4,5,6,7,8], default=-1)
-        parser.add_argument('-g', '--group'  , type=str  , help='Name of group to which fit belongs, e.g. toys'   , required= True)
-        parser.add_argument('-c', '--fit_cfg', type=str  , help='Name of configuration, e.g. reso/rkst/electron'  , required=True)
-        parser.add_argument('-t', '--toy_cfg', type=str  , help='Name of toy config, e.g. toys/maker.yaml'        , default =  '')
-        parser.add_argument('-N', '--ntoys'  , type=int  , help='If specified, this will override ntoys in config', default =0)
-        parser.add_argument('-n', '--nthread', type=int  , help='Number of threads'                               , default =1)
-        parser.add_argument('-l', '--log_lvl', type=int  , help='Logging level', choices=[5, 10, 20, 30]          , default =20)
-        parser.add_argument('-q', '--q2bin'  , type=str  , help='q2 bin'      , choices=['jpsi', 'psi2']          , required=True)
-        parser.add_argument('-C', '--mva_cmb', type=float, help='Cut on combinatorial MVA working point'          , required=True)
-        parser.add_argument('-P', '--mva_prc', type=float, help='Cut on part reco MVA working point'              , required=True)
+        parser.add_argument('-b', '--block'  , type=int  , help='Block number, if not passed will do all data'            , choices =[1,2,3,4,5,6,7,8], default=-1)
+        parser.add_argument('-g', '--group'  , type=str  , help='Name of group to which fit belongs, e.g. toys'           , required= True)
+        parser.add_argument('-c', '--fit_cfg', type=str  , help='Name of configuration, e.g. reso/rkst/electron/data.yaml', required=True)
+        parser.add_argument('-t', '--toy_cfg', type=str  , help='Name of toy config, e.g. toys/maker.yaml'                , default =  '')
+        parser.add_argument('-N', '--ntoys'  , type=int  , help='If specified, this will override ntoys in config'        , default =0)
+        parser.add_argument('-n', '--nthread', type=int  , help='Number of threads'                                       , default =1)
+        parser.add_argument('-l', '--log_lvl', type=int  , help='Logging level', choices=[5, 10, 20, 30]                  , default =20)
+        parser.add_argument('-q', '--q2bin'  , type=str  , help='q2 bin'      , choices=['jpsi', 'psi2']                  , required=True)
+        parser.add_argument('-C', '--mva_cmb', type=float, help='Cut on combinatorial MVA working point'                  , required=True)
+        parser.add_argument('-P', '--mva_prc', type=float, help='Cut on part reco MVA working point'                      , required=True)
         args = parser.parse_args()
 
-    fit_cfg = gut.load_conf(package='fitter_data', fpath=f'{args.fit_cfg}/data.yaml')
+    fit_cfg = gut.load_conf(package='fitter_data', fpath=args.fit_cfg)
     toy_cfg = gut.load_conf(package='fitter_data', fpath=args.toy_cfg) if args.toy_cfg else None
 
     cfg         = FitConfig(
