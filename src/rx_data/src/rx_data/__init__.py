c097485f85f7555eb7f5eb1c41859b5247554886
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-11 12:42:48 +0800

    Put patcher and emulator in rx_data namespace

--- src/rx_data/src/rx_data/__init__.py
+++ src/rx_data/src/rx_data/__init__.py
@@ -2,11 +2,15 @@ from .rdf_getter         import RDFGetter
 from .rdf_loader         import RDFLoader
 from .swp_calculator     import SWPCalculator
 from .spec_maker         import SpecMaker
+from .spec_maker         import SamplePatcher 
+from .sample_emulator    import SampleEmulator
 from .ntuple_partitioner import NtuplePartitioner 
 from .stats              import Stats
 from .mis_calculator     import MisCalculator
 
 __all__ = [
+    'SampleEmulator',
+    'SamplePatcher',
     'NtuplePartitioner', 
     'MisCalculator',
     'SWPCalculator',
