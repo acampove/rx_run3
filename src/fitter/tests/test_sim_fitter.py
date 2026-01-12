3880bbd597b391540f29bf191a6f6626123c130b
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-12 14:17:46 +0800

    Update test_with_cat
    Do both brems and update paths

--- src/fitter/tests/test_sim_fitter.py
+++ src/fitter/tests/test_sim_fitter.py
@@ -62,22 +62,22 @@ def test_nocat(tmp_path : Path):
             q2bin    = 'jpsi')
         ftr.get_model()
 # ---------------------------------------------------
-def test_with_cat(tmp_path : Path):
+@pytest.mark.parametrize('brem', [1, 2])
+def test_with_cat(tmp_path : Path, brem : int):
     '''
     Test for components with brem categories
     '''
-    sample= Sample.bpkpjpsiee
-    obs   = zfit.Space('B_Mass', limits=(4500, 6000))
-    cfg   = gut.load_conf(package='fitter_data', fpath=f'reso/rk/electron/{sample}.yaml')
+    obs   = zfit.Space('B_Mass', limits=(4500, 7000))
+    cfg   = gut.load_conf(package='fitter_data', fpath='reso/rk/electron/jpsi_non_dtf.yaml')
 
     cuts  = {
-        'q2' : '(1)',
-        'cmb': 'mva_cmb > 0.85',
-        'prc': 'mva_prc > 0.50',
-        'mass': 'B_Mass > 4500 && B_Mass < 6000',
-        'block': 'block == 1',
-        'nobrm0': 'nbrem != 0',
-        'brem_cat': 'nbrem == 2',
+        'q2'       : '(1)',
+        'cmb'      : 'mva_cmb > 0.50',
+        'prc'      : 'mva_prc > 0.50',
+        'mass'     : 'B_Mass > 4500 && B_Mass < 7000',
+        'block'    : 'block == 1',
+        'nobrm0'   : 'nbrem != 0',
+        'brem_cat' : f'nbrem == {brem}',
     }
 
     with ExitStack() as stack:
