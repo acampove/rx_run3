da9f4c4b22a3591bf1b4953326cdce9b03ffb834
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-14 11:19:55 +0800

    Add files

---
+++ src/rx_q2/src/rx_q2/parameter_reader.py
@@ -0,0 +1,39 @@
+'''
+Module holding the ParameterReader class
+'''
+import pandas as pnd
+
+from pathlib           import Path
+from rx_q2.scales_conf import ScalesConf
+
+from dmu import LogStore
+
+log=LogStore.add_logger('rx_q2:parameter_reader')
+# ----------------------
+class ParameterReader:
+    '''
+    Class meant to extract information from JSON files
+    created from fits to the B and Jpsi mass distributions
+    '''
+    # ----------------------
+    def __init__(self, cfg : ScalesConf):
+        '''
+        Parameters
+        -------------
+        cfg: Object holding configuration
+        '''
+        self._cfg = cfg
+
+    # ----------------------
+    def read(self, path : Path) -> pnd.Series:
+        '''
+        Parameters
+        -------------
+        path: Path to JSON file with fitting parameters
+
+        Returns
+        -------------
+        Pandas series with fit parameters information
+        '''
+        return pnd.Series()
+# ----------------------
---
+++ src/rx_q2/tests/test_parameter_reader.py
@@ -0,0 +1,30 @@
+'''
+This module contains functions to test the ParameterReader class
+'''
+import pandas as pnd
+
+from pathlib import Path
+from rx_q2   import ParameterReader
+from rx_q2   import ScalesConf
+from dmu     import LogStore
+from dmu.generic import utilities as gut
+
+log=LogStore.add_logger('rx_q2:test_parameter_reader')
+# ----------------------
+def test_simple() -> None:
+    '''
+    Simplest test of tool
+    '''
+    path = Path('/home/acampove/external_02/q2/fits/v6/dat/rk_ee_2024/1_1_nom/parameters.json')
+    data = gut.load_data(package='rx_q2_data', fpath='plots/scales.yaml')
+    data['kind'] = 'q2'
+    data['vers'] = 'dummy'
+    data['year'] = 'dummy'
+    data['proj'] = 'dummy'
+
+    cfg  = ScalesConf(**data)
+    rdr  = ParameterReader(cfg = cfg)
+    srs  = rdr.read(path = path)
+
+    assert isinstance(srs, pnd.Series)
+    
