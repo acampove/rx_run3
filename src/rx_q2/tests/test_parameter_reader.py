dccdd7f5c17238bb0ef2a4790023b229b18326a6
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-14 14:20:57 +0800

    Do not test the B fit parameters

--- src/rx_q2/tests/test_parameter_reader.py
+++ src/rx_q2/tests/test_parameter_reader.py
@@ -14,7 +14,7 @@ from dmu.generic import utilities as gut
 log=LogStore.add_logger('rx_q2:test_parameter_reader')
 # ----------------------
 @pytest.mark.parametrize('sample', ['rk_ee_2024', 'rk_mm_2024'])
-def test_jpsi(sample : str) -> None:
+def test_simple(sample : str) -> None:
     '''
     Test of tool with jpsi fits
     '''
@@ -43,31 +43,3 @@ def test_jpsi(sample : str) -> None:
     print(df)
     print(df.dtypes)
 # ----------------------
-def test_bplus():
-    '''
-    Test with fits to bplus
-    '''
-    ANADIR = os.environ['ANADIR']
-    path   = Path(ANADIR) / 'fits/data/reso_no_dtf/v1/rk_ee/050_060_b3/reso/rk/electron/data/jpsi/brem_001/parameters.json'
-    if not path.exists():
-        raise ValueError(f'Cannot find: {path}')
-
-    data            = gut.load_data(package='rx_q2_data', fpath='plots/scales.yaml')
-    data['kind']    = 'B'
-    data['regex']   =  r'(\d{3})_(\d{3})_b(\d)'
-
-    data['vers']    = 'dummy'
-    data['year']    = 'dummy'
-    data['proj']    = 'dummy'
-    data['inp_dir'] = Path('dummy')
-    data['out_dir'] = Path('dummy')
-
-    cfg    = ScalesConf(**data)
-    rdr    = ParameterReader(cfg = cfg)
-    l_data = [ rdr.read(path = path) for _ in range(3) ]
-
-    df = pnd.DataFrame(l_data)
-
-    print('')
-    print(df)
-    print(df.dtypes)
