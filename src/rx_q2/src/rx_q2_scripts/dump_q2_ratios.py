e5009f6d6a3469ffa16737bf499c1f3dad300259
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-14 11:18:44 +0800

    Move ScalesConf to its own module

--- src/rx_q2/src/rx_q2_scripts/dump_q2_ratios.py
+++ src/rx_q2/src/rx_q2_scripts/dump_q2_ratios.py
@@ -26,49 +26,6 @@ log=LogStore.add_logger('rx_q2:dump_q2_ratios')
 ARGS     : DictConfig | argparse.Namespace | None = None
 PROJECTS : list[str] = ['rk_ee', 'rk_mm', 'rkst_ee', 'rkst_mm']
 #-------------------------------------
-class ScalesConf(BaseModel):
-    '''
-    Class storing configuration
-    '''
-    model_config = ConfigDict(frozen=True)
-    jpsi_mass : float
-
-    mm      : dict[str,list[float]]
-    ee      : dict[str,list[float]]
-
-    kind    : str
-    vers    : str
-    year    : str
-    proj    : str
-
-    inp_dir : Path
-    out_dir : Path
-    regex   : str
-    # ------------------------------
-    def get_range(self, var : str) -> tuple[float,float]:
-        '''
-        Parameters
-        -----------------
-        var: Variable name, e.g. mu, sg, dm, ss
-
-        Returns
-        -----------------
-        Y axis range for plot
-        '''
-        if   self.proj.endswith('_mm'):
-            data = self.mm
-        elif self.proj.endswith('_ee'):
-            data = self.ee
-        else:
-            raise ValueError(f'Invalid project: {self.proj}')
-
-        if var not in data:
-            raise ValueError(f'Invalid variable: {var}')
-
-        [low, high] = data[var]
-
-        return low, high
-#-------------------------------------
 @cache
 def _load_config() -> ScalesConf:
     '''
