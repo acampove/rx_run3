8ee95df96eef57ea9b5735c14444a0a405edc685
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-14 13:40:08 +0800

    Add simple test and print columns

--- src/fitter/tests/test_fit_summary.py
+++ src/fitter/tests/test_fit_summary.py
@@ -5,13 +5,31 @@ import pytest
 from fitter   import FitSummary
 
 # ----------------------
+@pytest.mark.parametrize('kind', ['reso_no_dtf'])
+def test_simple(kind : str) -> None:
+    '''
+    Simplest test
+    '''
+    smr = FitSummary(name = kind)
+    df  = smr.get_df(force_update = False)
+
+    assert len(df) > 0
+
+    print('')
+    print(df)
+    for column in df.columns:
+        print(column)
+# ----------------------
 @pytest.mark.parametrize('force_update', [True, False])
 def test_update(force_update : bool) -> None:
     '''
-    Simplest test
+    Test updating the parquet file
     '''
     smr = FitSummary(name='mid_window')
     df  = smr.get_df(force_update = force_update)
 
     assert len(df) > 0
+
+    print('')
+    print(df)
 # ----------------------
