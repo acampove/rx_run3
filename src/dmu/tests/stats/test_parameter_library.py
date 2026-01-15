e092ff05595d4ede04c8a6755cbcdc148d0b68d7
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-15 12:42:34 +0800

    Bug: CBL tails are al and nl, not ac and nc + Fix default value

--- src/dmu/tests/stats/test_parameter_library.py
+++ src/dmu/tests/stats/test_parameter_library.py
@@ -69,7 +69,7 @@ def test_print(kind : str):
     '''
     PL.print_parameters(kind=kind)
 # ------------------------------------
-@pytest.mark.parametrize('parameter', ['mu', 'sg', 'ac', 'nc'])
+@pytest.mark.parametrize('parameter', ['mu', 'sg', 'al', 'nl'])
 def test_get_values(parameter : str):
     '''
     Will test retrieving values for different parameters of cbl
@@ -80,28 +80,28 @@ def test_get_values(parameter : str):
 
     log.info(f'{parameter:<20}{x:<10.0f}{y:<10.0f}{z:<10.0f}')
 # ------------------------------------
-@pytest.mark.parametrize('parameter', ['mu', 'sg', 'ac', 'nc'])
+@pytest.mark.parametrize('parameter', ['mu', 'sg', 'al', 'nl'])
 def test_values(parameter : str):
     '''
     Will test retrieving values for different parameters of cbl
     '''
     kind = 'cbl'
-    x_in = 11111
-    y_in = 22222
-    z_in = 33333
+    l_in = 11111 
+    v_in = 22222 
+    h_in = 33333
 
-    with PL.values(kind=kind, parameter=parameter, val=x_in, low=y_in, high=z_in):
-        x_out, y_out, z_out = PL.get_values(kind=kind, parameter=parameter)
+    with PL.values(kind=kind, parameter=parameter, val=v_in, low=l_in, high=h_in):
+        v_out, l_out, h_out = PL.get_values(kind=kind, parameter=parameter)
 
-    assert x_in == x_out
-    assert y_in == y_out
-    assert z_in == z_out
+    assert v_in == v_out
+    assert l_in == l_out
+    assert h_in == h_out
 
-    x_out, y_out, z_out = PL.get_values(kind=kind, parameter=parameter)
+    v_out, l_out, h_out = PL.get_values(kind=kind, parameter=parameter)
 
-    assert x_in != x_out
-    assert y_in != y_out
-    assert z_in != z_out
+    assert v_in != v_out
+    assert l_in != l_out
+    assert h_in != h_out
 # ------------------------------------
 @pytest.mark.parametrize('config', ['scales', 'simultaneous'])
 def test_get_yield(config : str) -> None:
