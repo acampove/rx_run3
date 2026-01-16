219b33fecaac52c6ffc4cb9d02a58e4d3b077b6f
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-16 13:20:06 +0800

    Use Value and Error instead of mu and sg...

--- src/fitter/src/fitter/signal_constraints.py
+++ src/fitter/src/fitter/signal_constraints.py
@@ -61,8 +61,8 @@ class SignalConstraints:
         val, err = self._srd.get_scale(corr = kind, block = block, brem = brem)
 
         log.info(f'Constraining: {par.name}')
-        log.debug(f'{"mu":<20}{val:20.3f}')
-        log.debug(f'{"sg":<20}{err:20.3f}')
+        log.debug(f'{"Value":<20}{val:20.3f}')
+        log.debug(f'{"Error":<20}{err:20.3f}')
 
         return Constraint1D(
             name = par.name,
