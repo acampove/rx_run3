d1ad9452a3eecbc81f2317b20793c983c23e9831
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-16 08:52:43 +0800

    First minimal working version

--- src/fitter/tests/test_signal_constraints.py
+++ src/fitter/tests/test_signal_constraints.py
@@ -1,13 +1,22 @@
 '''
 Module holding functions needed to test SignalConstraints class
 '''
-
+import pytest
+from dmu       import LogStore
 from dmu.stats import zfit
 from dmu.stats import ModelFactory 
 from fitter    import SignalConstraints
 
 Loss = zfit.loss.ExtendedUnbinnedNLL
 zobs = zfit.Space
+log  = LogStore.add_logger('fitter:test_signal_constraints')
+# ----------------------
+@pytest.fixture(scope='session', autouse=True)
+def initialize():
+    '''
+    This will run before any test
+    '''
+    LogStore.set_level('fitter:signal_constraints', 10)
 # ----------------------
 def _get_nll(obs : zobs) -> Loss:
     '''
@@ -21,25 +30,30 @@ def _get_nll(obs : zobs) -> Loss:
 
     - PDF used to fit signal 
     '''
-    pdf_names = ['dscb'] 
+    signal_name = 'signal_brem_001_b1'
 
-    mu   = zfit.Parameter('mu', 5200, 4500, 6000)
-    sg   = zfit.Parameter('sg',  150,   10, 200)
-    gaus = zfit.pdf.Gauss(obs=obs, mu=mu, sigma=sg)
+    fct = ModelFactory(
+        obs     = obs,
+        l_pdf   = ['dscb'],
+        l_shared= [],
+        l_float = [],
+        d_rep   = {'mu' : 'scale', 'sg' : 'reso'},
+        preffix = signal_name)
+    dscb = fct.get_pdf()
 
     fct  = ModelFactory(
         obs     = obs,
-        l_pdf   = pdf_names,
+        l_pdf   = ['exp'],
         l_shared= [],
         l_float = [],
-        preffix = 'signal')
+        preffix = 'bkg')
     expo = fct.get_pdf()
 
-    nexpo = zfit.param.Parameter('nbkg', 1000, 0, 1000_000)
-    ngaus = zfit.param.Parameter('nsig', 1000, 0, 1000_000)
+    nbkg  = zfit.param.Parameter('nbkg', 1000, 0, 1000_000)
+    nsig  = zfit.param.Parameter('nsig', 1000, 0, 1000_000)
 
-    bkg   = expo.create_extended(nexpo)
-    sig   = gaus.create_extended(ngaus)
+    bkg   = expo.create_extended(nbkg)
+    sig   = dscb.create_extended(nsig)
     pdf   = zfit.pdf.SumPDF([bkg, sig])
     dat   = pdf.create_sampler()
 
@@ -51,10 +65,8 @@ def test_simple() -> None:
     '''
     obs = zfit.Space('dummy', limits=(4500, 6000))
     nll = _get_nll(obs = obs)    
-    calc= SignalConstraints(
-        name = 'signal',
-        nll  = nll)
 
+    calc= SignalConstraints(nll = nll)
     constraints = calc.get_constraints()
 
     assert isinstance(constraints, list)
