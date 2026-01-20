52456ff0f3ce2803b0846c51df777e64a2241aeb
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-20 13:21:20 +0800

    Category is a dataclass
    Eliminates boilerplate

--- src/fitter/src/fitter/category.py
+++ src/fitter/src/fitter/category.py
@@ -2,50 +2,24 @@
 Module holding the Category class
 '''
 
-from omegaconf import DictConfig
-from dmu.stats import zfit
-from dmu       import LogStore
+from dataclasses import dataclass
+from omegaconf   import DictConfig
+from dmu.stats   import zfit
+from dmu         import LogStore
 
 zpdf = zfit.pdf.BasePDF
 log  = LogStore.add_logger('fitter:category')
 # -----------------------------------
+@dataclass
 class Category:
     '''
     Class meant to represent a fit category, i.e. a fit for a given
     dataset when splitting in e.g. blocks and/or Brem categories
     '''
-    # ----------------------
-    def __init__(
-        self, 
-        name : str, 
-        model: zpdf, 
-        sumw : float,
-        cres : DictConfig):
-        '''
-        Parameters
-        -------------
-        name: Name of category, e.g. brem_001_b1
-        model: Zfit PDF representing component after fit to MC
-        sumw : Sum of weights in MC sample
-        cres : Dictionary mapping variable name to value and error for parameter
-        '''
-        self._name = name
-        self._model= model
-        self._sumw = sumw
-        self._cres = cres
-    # ----------------------
-    @property
-    def cres(self) -> DictConfig:
-        """
-        Object holding parameternames
-        mapped to values and error tuples
-        """
-        return self._cres
-    # ----------------------
-    @property
-    def model(self) -> zpdf:
-        """
-        Zfit PDF
-        """
-        return self._model
+    name      : str 
+    pdf       : zpdf 
+    sumw      : float
+    cres      : DictConfig
+    selection : dict[str,str]
+    model     : list[str]
 # -----------------------------------
