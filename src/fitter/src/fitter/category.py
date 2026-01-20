25b526f6596e0226631be591a8ec2337952b3bdc
Angel Campoverde <angel.campoverde@cern.ch>
2026-01-20 13:29:03 +0800

    Document attributes

--- src/fitter/src/fitter/category.py
+++ src/fitter/src/fitter/category.py
@@ -15,11 +15,20 @@ class Category:
     '''
     Class meant to represent a fit category, i.e. a fit for a given
     dataset when splitting in e.g. blocks and/or Brem categories
+
+    Attributes
+
+    name     : Name of component, e.g. brem_001_b1
+    pdf      : Zfit PDF associated to category
+    sumw     : Yield in MC sample associated to category
+    cres     : Dictionary mapping name of variable to value and error
+    model    : List of model names used for category
+    selection: Selection that was used to get category, on top of nominal
     '''
     name      : str 
     pdf       : zpdf 
     sumw      : float
     cres      : DictConfig
-    selection : dict[str,str]
     model     : list[str]
+    selection : dict[str,str]
 # -----------------------------------
