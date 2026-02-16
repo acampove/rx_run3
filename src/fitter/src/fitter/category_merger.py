4c435e48cd468629fba848c3efad826bd06ce9e5
Angel Campoverde <angel.campoverde@cern.ch>
2026-02-16 17:55:04 +0800

    Add enforcer and make it work for all_same and all_different

--- src/fitter/src/fitter/category_merger.py
+++ src/fitter/src/fitter/category_merger.py
@@ -3,6 +3,7 @@ Module holding CategoryMerger class
 '''
 import zfit
 
+from typing      import Literal
 from omegaconf   import OmegaConf
 from dmu         import LogStore
 from rx_common   import Brem, Block, Correction
@@ -11,7 +12,8 @@ from .category   import Category
 
 log=LogStore.add_logger('fitter:category_merger')
 
-zpar = zfit.param.Parameter
+zpar      = zfit.param.Parameter
+Condition = Literal['all_same', 'all_different']
 # ----------------------
 class CategoryMerger:
     '''
@@ -29,8 +31,40 @@ class CategoryMerger:
         '''
         self._categories = categories
     # ----------------------
-    def _merge_brem_categories(self) -> list[Category]:
+    def _enforce(
+        self, 
+        categories : list[Category], 
+        kind       : type, 
+        condition  : Condition) -> None:
         '''
+        Parameters
+        -------------
+        categories: List of categories
+        kind      : Used to enforce condition on given attribute of categories
+        condition : Condition that has to be satisfied by categories
+        '''
+        objs : set[Brem|Block]
+
+        if   kind is Brem:
+            objs = { cat.brem  for cat in categories }
+        elif kind is Block:
+            objs = { cat.block for cat in categories }
+        else:
+            raise ValueError(f'Invalid type: {type}')
+
+        ntotal  = len(categories)
+        nunique = len(objs)
+
+        if condition == 'all_same'      and nunique == 1: 
+            return
+        
+        if condition == 'all_different' and ntotal  == nunique:
+            return
+
+        for obj in objs:
+            log.error(obj)
+
+        raise ValueError(f'Condition {condition} failed')
         Returns
         -------------
         List of categories, one per block
