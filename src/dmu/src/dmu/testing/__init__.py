b1332ca34829b8cca755a9f844d8d583465bed8f
Angel Campoverde <angel.campoverde@cern.ch>
2026-02-17 13:46:56 +0800

    Add file

---
+++ src/dmu/src/dmu/testing/__init__.py
@@ -0,0 +1,10 @@
+from .stats import get_nll
+from .stats import get_model
+from .stats import placeholder_fit
+from .stats import SumLiteral 
+
+__all__ = [
+    'placeholder_fit',
+    'get_nll', 
+    'get_model',
+    'SumLiteral']
