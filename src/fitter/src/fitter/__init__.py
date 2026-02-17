aed3325903c4cf0ede05e8d88c751d1b70a82e19
Angel Campoverde <angel.campoverde@cern.ch>
2026-02-17 16:42:04 +0800

    Add classes to namespace

--- src/fitter/src/fitter/__init__.py
+++ src/fitter/src/fitter/__init__.py
@@ -5,14 +5,24 @@ from .likelihood_factory import LikelihoodFactory
 from .toy_maker          import ToyMaker
 from .toy_plotter        import ToyPlotter
 from .data_fitter        import DataFitter 
+from .data_model         import DataModel
 from .sim_fitter         import SimFitter
 from .data_preprocessor  import DataPreprocessor
 from .constraint_reader  import ConstraintReader
 from .cmb_constraints    import CmbConstraints
+from .pars_loader        import ParsLoader
+from .holders            import ChannelHolder
+from .holders            import ComponentHolder 
+from .misid_constraints  import MisIDConstraints
 
 __all__ = [
+    'ChannelHolder',
+    'ComponentHolder',
+    'DataModel',
     'FitSummary', 
+    'ParsLoader',
     'FitConfig',
+    'MisIDConstraints',
     'ConstraintReader',
     'CmbConstraints',
     'DataPreprocessor',
