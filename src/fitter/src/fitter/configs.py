c09f6f17467b088630a5eb6603d35cf6592f3633
Angel Campoverde <angel.campoverde@cern.ch>
2026-02-23 12:05:23 +0800

    Add file

---
+++ src/fitter/src/fitter/configs.py
@@ -0,0 +1,207 @@
+'''
+Module containing classes representing configurations used for fitting
+'''
+
+from pathlib     import Path
+from typing      import Literal
+from dmu.generic import UnpackerModel
+from rx_common   import Brem, Mass, Project, Qsq, Particle
+from rx_common   import Component, Trigger, CCbarComponent
+from pydantic    import BaseModel, ConfigDict
+from dmu.stats   import ModelFactoryConf
+from dmu.stats   import Model
+from dmu.stats   import KDEConf
+from dmu.stats   import FitConf
+from dmu.stats   import ZFitPlotterConf 
+from dmu.stats   import Model as FModel
+from .types      import CCbarWeight
+
+# ------------------------------
+# Component elements
+# ------------------------------
+class CmbCategoryConf(BaseModel):
+    '''
+    Class meant to configure fitting categories for combinatorial component
+    '''
+    model_config = ConfigDict(frozen=True)
+
+    models : dict[Qsq, list[Model]]
+# ------------------------------
+# Components
+# ------------------------------
+class ComponentConf(BaseModel):         # Tested
+    '''
+    Class meant to configure the fit for different components
+    '''
+    model_config = ConfigDict(frozen=True)
+
+    output_directory : Path
+# ------------------------------
+class CombinatorialConf(ComponentConf, UnpackerModel): # Tested
+    '''
+    Class mean to control configuration of fit to combinatorial
+    '''
+    model_config = ConfigDict(frozen=True)
+
+    bins       : dict[Qsq, ModelFactoryConf]
+    categories : dict[str,  CmbCategoryConf]
+# ------------------------------
+class NonParametricConf(ComponentConf): # Tested
+    '''
+    Class mean to control configuration of fit to non parametric models 
+    '''
+    sample : Component
+    fit    : KDEConf
+    plots  : ZFitPlotterConf 
+# ------------------------------
+class BremCatConf(BaseModel):  # Tested
+    '''
+    Class meant to configure a brem category fit
+    '''
+    selection : dict[str,str]
+    model     : list[FModel ]
+# ------------------------------
+class ParametricConf(ComponentConf):  # Tested
+    '''
+    Class mean to control configuration of fit to parametric models 
+    '''
+    sample     : Component
+    fit        : FitConf 
+    categories : dict[Brem, BremCatConf]
+    plots      : ZFitPlotterConf 
+# ------------------------------
+class CCbarConf(ComponentConf):         # Tested
+    '''
+    Class mean to control configuration to fit ccbar components
+    '''
+    model_config = ConfigDict(frozen=True)
+
+    ccbar_samples : list[CCbarComponent]
+    weights       : dict[CCbarWeight,bool]
+    fitting       : KDEConf
+# ------------------------------
+# MisID
+# ------------------------------
+class MisIDSampleWeights(UnpackerModel):       # Tested
+    '''
+    Configuration controlling how to pick weights for misID samples
+    '''
+    maps_path : Path
+    regions   : dict[str,str]
+    pars      : tuple[str,str]
+# ------------------------------
+class MisIDSampleSplitting(UnpackerModel):    # Tested
+    '''
+    Configuration controlling how samples are split into control and
+    signal region
+    '''
+    branches      : list[str]
+    tracks        : dict[str,str]
+    hadron_tagging: dict[Particle,str]
+    lepton_tagging: dict[str,str]
+# ------------------------------
+class MisIDCategory(UnpackerModel):          # Tested
+    selection  : dict[str,str]
+    weights    : MisIDSampleWeights 
+    splitting  : MisIDSampleSplitting 
+# ------------------------------
+class MisIDConf(UnpackerModel, NonParametricConf): # Tested
+    '''
+    Configuration needed to build MisID components
+    '''
+    trigger    : Trigger
+    project    : Project
+    categories : dict[str,MisIDCategory]
+# ------------------------------
+# Yields, observables, etc
+# ------------------------------
+class YieldConf(BaseModel):            # Tested
+    '''
+    Class representing configuration for yields
+    '''
+    val    : float
+    min    : float
+    max    : float
+    scl    : list[str]         | None = None
+    prefix : Literal['pscale'] | None = None
+# ------------------------------
+class ObservableConf(BaseModel):
+    '''
+    Class representing configuration for observable
+    '''
+    model_config = ConfigDict(frozen=True)
+
+    name : Mass
+    range: tuple[int,int]
+# ------------------------------
+class MisIDFitComponents(UnpackerModel):
+    '''
+    Class describing fitting components for fits to misID control region
+    '''
+    model_config = ConfigDict(frozen=True)
+
+    cominatorial : CmbCategoryConf
+    hdpipi       : NonParametricConf
+    hdkk         : NonParametricConf
+# ------------------------------
+class MisIDFitModel(UnpackerModel):
+    '''
+    Class meant to configure how the fit model for misID control region
+    fits are done
+    '''
+    model_config = ConfigDict(frozen=True)
+
+    yields     : dict[str,YieldConf]
+    observable : dict[str,ObservableConf]
+    components : MisIDFitComponents
+# ------------------------------
+class MisIDConstraintConf(UnpackerModel):
+    '''
+    Configuration meant to control how fits used to get misID constraints
+    are used to get the constraints
+    '''
+    model_config = ConfigDict(frozen=True)
+
+    output_directory : Path
+    trigger          : Trigger
+    selection        : dict[str,str]
+    fit              : FitConf
+    plots            : ZFitPlotterConf
+    model            : MisIDFitModel 
+# ------------------------------
+class ConstraintsCfg(UnpackerModel):
+    '''
+    Class meant to configure constraints
+    '''
+    model_config = ConfigDict(frozen=True)
+
+    misid   : MisIDConstraintConf | None = None
+    pre_rare: list[Component]     | None = None
+# ------------------------------
+AnyModel = CombinatorialConf | ParametricConf
+# ------------------------------
+# Fits
+# ------------------------------
+class FitModelConf(UnpackerModel):
+    '''
+    Class representing fitting model
+    '''
+    model_config = ConfigDict(frozen = True)
+
+    yields     : dict[str,YieldConf]
+    observable : dict[Qsq, ObservableConf]
+    components : dict[Component, CombinatorialConf]
+    constraints: ConstraintsCfg
+    plots      : ZFitPlotterConf
+    fit        : FitConf
+# ------------------------------
+class DataModelConf(UnpackerModel):
+    '''
+    Configuration needed to do a fit to data
+    '''
+    model_config = ConfigDict(frozen=True)
+    
+    trigger          : Trigger
+    output_directory : Path
+    model            : FitModelConf
+# ------------------------------
