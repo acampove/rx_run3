2690bdcff161443d9fe316150111b5ccb7c2639d
Angel Campoverde <angel.campoverde@cern.ch>
2026-02-23 12:38:29 +0800

    Do not put pytest_utilities to dmu namespace

--- src/dmu/src/dmu/__init__.py
+++ src/dmu/src/dmu/__init__.py
@@ -1,8 +1,6 @@
 from .fsystem.fcopy     import FCopy
 from .logging.log_store import LogStore
-from .testing           import pytest_utilities
 
 __all__ = [
     'FCopy', 
-    'LogStore', 
-    'pytest_utilities']
+    'LogStore']
