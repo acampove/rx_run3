ed23aad1e39d40e44d6ed9ff3f311d0012f6dfd9
Angel Campoverde <angel.campoverde@cern.ch>
2026-02-23 15:44:03 +0800

    Add LogLevels to namespace

--- src/dmu/src/dmu/__init__.py
+++ src/dmu/src/dmu/__init__.py
@@ -1,6 +1,8 @@
 from .fsystem.fcopy     import FCopy
 from .logging.log_store import LogStore
+from .logging.log_store import LogLevels
 
 __all__ = [
     'FCopy', 
+    'LogLevels',
     'LogStore']
