7f3fc6134b777efb6093285f6f565dd543f564f7
Angel Campoverde <angel.campoverde@cern.ch>
2026-02-26 19:35:25 +0800

    Freeze classes

--- src/rx_misid/src/rx_misid/types.py
+++ src/rx_misid/src/rx_misid/types.py
@@ -3,6 +3,7 @@ Module meant to hold pydantic models, data classes and enums
 '''
 from pathlib     import Path
 from rx_common   import Particle
+from pydantic    import ConfigDict
 from dmu.generic import UnpackerModel
 
 # ------------------------------
@@ -11,6 +12,8 @@ class MisIDSampleSplitting(UnpackerModel):    # Tested
     Configuration controlling how samples are created and
     split into control and signal region
     '''
+    model_config = ConfigDict(frozen=True)
+
     branches      : list[str]
     tracks        : dict[str,str]
     hadron_tagging: dict[Particle,str]
@@ -20,6 +23,8 @@ class MisIDSampleWeights(UnpackerModel):       # Tested
     '''
     Configuration controlling how to pick weights for misID samples
     '''
+    model_config = ConfigDict(frozen=True)
+
     maps_path : Path
     regions   : dict[str,str]
     pars      : tuple[str,str]
