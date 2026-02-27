1ae6651b09b7779ddcef0357b92f290a6d397465
Angel Campoverde <angel.campoverde@cern.ch>
2026-02-27 18:38:08 +0800

    Update yield configurations

--- src/fitter/src/fitter/configs.py
+++ src/fitter/src/fitter/configs.py
@@ -5,7 +5,7 @@ import os
 import dataclasses
 
 from functools   import cached_property
-from pydantic    import BaseModel, ConfigDict, TypeAdapter, model_validator
+from pydantic    import BaseModel, ConfigDict, model_validator
 from pathlib     import Path
 from typing      import Literal, Self
 
@@ -26,11 +26,49 @@ from .toy_maker  import ToyConf
 
 log=LogStore.add_logger('fitter:configs')
 # ------------------------------
-# Components
+# Yields, observables, etc
 # ------------------------------
-class ComponentConf(BaseModel):         # Tested
+class SimpleYieldConf(BaseModel):            # Tested
     '''
-    Class meant to configure the fit for different components
+    Class representing configuration for yields
+    '''
+    val    : float
+    min    : float
+    max    : float
+    scl    : list[str]         | None = None
+    prefix : Literal['pscale'] | None = None
+# ------------------------------
+class CompositeYieldConf(BaseModel):
+    '''
+    Class meant to represent composite yield
+    '''
+    kind : Literal['mul', 'dif']
+    pars : list[str]
+    # ----------------------
+    @model_validator(mode = 'after')
+    def check_size(self) -> Self:
+        if len(self.pars) < 2:
+            raise ValueError(f'At least two parameters are needed, found: {self.pars}')
+
+        if self.kind == 'dif' and len(self.pars) != 2:
+            raise ValueError(f'Expected two parameters found: {self.pars}')
+
+        return self
+# ------------------------------
+class YieldConf(BaseModel):
+    '''
+    Class meant to wrap SimpleYieldConf and CompositeYieldConf
+    pydantic models
+    '''
+    root : SimpleYieldConf | CompositeYieldConf
+
+    @model_validator(mode='before')
+    @classmethod
+    def _pack_data(cls, data):
+        return {'root' :  data}
+
+    def __getattr__(self, item):
+        return getattr(self.root, item)
     '''
     model_config = ConfigDict(frozen=True)
 
