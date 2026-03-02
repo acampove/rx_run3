65b624b305c477fd16531f9eaab89a673afc5b90
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-02 15:25:13 +0800

    Use Component instead of string

--- src/rx_data/tests/test_spec_maker.py
+++ src/rx_data/tests/test_spec_maker.py
@@ -47,52 +47,57 @@ def initialize():
     LogStore.set_level('rx_data:sample_emulator', 10)
     LogStore.set_level('rx_data:sample_patcher' , 10)
 # ------------------------------------------------------
-@pytest.mark.parametrize('sample, trigger', _PATCHING_SAMPLES)
-def test_patching(sample : str, trigger : Trigger) -> None:
+@pytest.mark.parametrize('component, trigger', _PATCHING_SAMPLES)
+def test_patching(
+    component : Component, 
+    trigger   : Trigger) -> None:
     '''
     Return path to specification of combined sample
     '''
-    mkr  = SpecMaker(sample=sample, trigger=trigger)
+    mkr  = SpecMaker(
+        component =component, 
+        trigger=trigger)
+
     path = mkr.get_spec_path(per_file=False)
 
     assert path.exists()
 # ------------------------------------------------------
 @pytest.mark.parametrize('sample, trigger', _INCLUSIVE_SAMPLES)
-def test_inclusive(sample : str, trigger : Trigger) -> None:
+def test_inclusive(sample : Component, trigger : Trigger) -> None:
     '''
     Disable patching
     '''
-    mkr  = SpecMaker(sample=sample, trigger=trigger, skip_patch=True)
+    mkr  = SpecMaker(component=sample, trigger=trigger, skip_patch=True)
     path = mkr.get_spec_path(per_file=False)
 
     assert path.exists()
 # ------------------------------------------------------
 @pytest.mark.parametrize('sample, trigger', _PATCHING_SAMPLES)
-def test_skip_patching(sample : str, trigger : Trigger) -> None:
+def test_skip_patching(sample : Component, trigger : Trigger) -> None:
     '''
     Disable patching
     '''
-    mkr  = SpecMaker(sample=sample, trigger=trigger, skip_patch=True)
+    mkr  = SpecMaker(component=sample, trigger=trigger, skip_patch=True)
     path = mkr.get_spec_path(per_file=False)
 
     assert path.exists()
 # ------------------------------------------------------
 @pytest.mark.parametrize('sample, trigger', _GOODSAMPLES)
-def test_combined(sample : str, trigger : Trigger) -> None:
+def test_combined(sample : Component, trigger : Trigger) -> None:
     '''
     Return path to specification of combined sample
     '''
-    mkr  = SpecMaker(sample=sample, trigger=trigger)
+    mkr  = SpecMaker(component=sample, trigger=trigger)
     path = mkr.get_spec_path(per_file=False)
 
     assert path.exists()
 # ------------------------------------------------------
 @pytest.mark.parametrize('sample, trigger', _GOODSAMPLES)
-def test_per_file(sample : str, trigger : Trigger) -> None:
+def test_per_file(sample : Component, trigger : Trigger) -> None:
     '''
     Return path to specification of combined sample
     '''
-    mkr  = SpecMaker(sample=sample, trigger=trigger)
+    mkr  = SpecMaker(component=sample, trigger=trigger)
     paths= mkr.get_spec_path(per_file=True)
 
     for root_path, config_path in paths.items():
@@ -100,14 +105,14 @@ def test_per_file(sample : str, trigger : Trigger) -> None:
         assert config_path.exists()
 # ------------------------------------------------
 @pytest.mark.parametrize('friend' , ['mva', 'brem_track_2'])
-@pytest.mark.parametrize('sample' , ['DATA_24_MagDown_24c2'])
+@pytest.mark.parametrize('sample' , [Component.data_24_md_c2])
 @pytest.mark.parametrize('trigger', [Trigger.rk_ee_os, Trigger.rk_mm_os])
-def test_exclude_friends(friend : str, sample : str, trigger : Trigger):
+def test_exclude_friends(friend : str, sample : Component, trigger : Trigger):
     '''
     Tests excluding friend trees through a context manager
     '''
     with SpecMaker.exclude_friends(names=[friend]):
-        gtr = SpecMaker(sample=sample, trigger=trigger)
+        gtr = SpecMaker(component=sample, trigger=trigger)
         path= gtr.get_spec_path(per_file=False)
 
     assert path.exists()
@@ -117,50 +122,50 @@ def test_exclude_friends(friend : str, sample : str, trigger : Trigger):
 
     assert friend not in spec.friends
 # ------------------------------------------------
-@pytest.mark.parametrize('sample' , ['DATA_24_MagDown_24c2'])
+@pytest.mark.parametrize('sample' , [Component.data_24_md_c2])
 @pytest.mark.parametrize('trigger', [Trigger.rk_ee_os, Trigger.rk_mm_os])
-def test_custom_friend(sample : str, trigger : Trigger):
+def test_custom_friend(sample : Component, trigger : Trigger):
     '''
     Tests getting data with a custom version for a given tree, either friend or main
     '''
     with SpecMaker.custom_friends(versions={'mva' : 'v8'}):
-        gtr = SpecMaker(sample=sample, trigger=trigger)
+        gtr = SpecMaker(component=sample, trigger=trigger)
         path= gtr.get_spec_path(per_file=False)
 
     assert path.exists()
 # ------------------------------------------------
-@pytest.mark.parametrize('sample' , ['DATA_24_MagDown_24c2'])
+@pytest.mark.parametrize('sample' , [Component.data_24_md_c2])
 @pytest.mark.parametrize('trigger', [Trigger.rk_ee_os, Trigger.rk_mm_os])
-def test_only_friends(sample : str, trigger : Trigger):
+def test_only_friends(sample : Component, trigger : Trigger):
     '''
     Tests getting data with a custom version for a given tree, either friend or main
     '''
     s_friend = {'mva', 'hop'}
     with SpecMaker.only_friends(s_friend=s_friend):
-        gtr = SpecMaker(sample=sample, trigger=trigger)
+        gtr = SpecMaker(component=sample, trigger=trigger)
         path= gtr.get_spec_path(per_file=False)
 
     assert path.exists()
 # ------------------------------------------------
 @pytest.mark.parametrize('project', [Project.rk, Project.rk_no_refit])
-@pytest.mark.parametrize('sample' , ['DATA_24_MagDown_24c2'])
+@pytest.mark.parametrize('sample' , [Component.data_24_md_c2])
 @pytest.mark.parametrize('trigger', [Trigger.rk_ee_os, Trigger.rk_mm_os])
-def test_project(sample : str, trigger : Trigger, project : Project):
+def test_project(sample : Component, trigger : Trigger, project : Project):
     '''
     Tests getting data with a custom version for a given tree, either friend or main
     '''
     with SpecMaker.project(name=project):
-        gtr = SpecMaker(sample=sample, trigger=trigger)
+        gtr = SpecMaker(component=sample, trigger=trigger)
         path= gtr.get_spec_path(per_file=False)
 
     assert path.exists()
 # ----------------------
 @pytest.mark.parametrize('sample', _NOPIDSAMPLES)
-def test_nopid(sample : str):
+def test_nopid(sample : Component):
     '''
     Test specification building for noPID samples
     '''
-    gtr = SpecMaker(sample=sample, trigger=Trigger.rk_ee_nopid)
+    gtr = SpecMaker(component=sample, trigger=Trigger.rk_ee_nopid)
     path= gtr.get_spec_path(per_file=False)
 
     assert path.exists()
