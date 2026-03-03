74be7f4c431ba7b257c3f7b99864b8b254f325c9
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-03 16:13:41 +0800

    Update Project class
    - Move it before Trigger
    - Add with_pid property

--- src/rx_common/src/rx_common/types.py
+++ src/rx_common/src/rx_common/types.py
@@ -54,6 +54,37 @@ class Channel(StrEnum):
     def __str__(self):
         return self.value
 # ---------------------------------------
+class Project(StrEnum):
+    '''
+    This class represents the projects
+    '''
+    rk            = 'rk'
+    rk_no_pid     = 'rk_nopid'
+    rk_no_refit   = 'rk_no_refit'
+    rk_sim10d     = 'rk_sim10d'
+    # ---------------
+    rkst          = 'rkst'
+    rkst_no_pid   = 'rkst_nopid'
+    rkst_no_refit = 'rkst_no_refit'
+    rkst_sim10d   = 'rkst_sim10d'
+
+    @property
+    def with_pid(self) -> 'Project':
+        '''
+        Return PID version of project
+        Only makes sense for noPID projects
+        '''
+        match self:
+            case Project.rk_no_pid:
+                return Project.rk
+            case Project.rkst_no_pid:
+                return Project.rkst
+            case _:
+                return self
+
+    def __str__(self):
+        return self.value
+# ---------------------------------------
 class Trigger(StrEnum):
     '''
     Class meant to represent MVA HLT2 triggers
@@ -123,23 +154,6 @@ class Trigger(StrEnum):
             return Project.rkst
 
         raise ValueError(f'Cannot assign trigger {self} to any project')
-# ---------------------------------------
-class Project(StrEnum):
-    '''
-    This class represents the projects
-    '''
-    rk            = 'rk'
-    rk_no_pid     = 'rk_nopid'
-    rk_no_refit   = 'rk_no_refit'
-    rk_sim10d     = 'rk_sim10d'
-    # ---------------
-    rkst          = 'rkst'
-    rkst_no_pid   = 'rkst_nopid'
-    rkst_no_refit = 'rkst_no_refit'
-    rkst_sim10d   = 'rkst_sim10d'
-
-    def __str__(self):
-        return self.value
 # ---------------------------------------
 class Qsq(StrEnum):
     '''
