6e5d52cbb5089f3626fcf72f9ddf67def67b670f
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-05 13:02:23 +0800

    Use Component instead of string

--- src/rx_data/tests/test_stats.py
+++ src/rx_data/tests/test_stats.py
@@ -4,7 +4,7 @@ Module with testing functions for the Stats class
 
 import pytest
 from dmu        import LogStore
-from rx_common  import Trigger
+from rx_common  import Component, Trigger
 from rx_data    import Stats
 
 log=LogStore.add_logger('rx_data:test_stats')
@@ -20,7 +20,7 @@ def test_mcdt():
     '''
     Tests MCDecayTree statistics retrieval
     '''
-    obj = Stats(sample='Bu_JpsiK_ee_eq_DPC', trigger=Trigger.rk_ee_os)
+    obj = Stats(sample=Component.bpkpjpsiee, trigger=Trigger.rk_ee_os)
     val = obj.get_entries(tree='MCDecayTree')
 
     assert val > 0
