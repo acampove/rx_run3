49be0d90a5878a152c4f0f566509d6b666ada740
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-05 11:43:40 +0800

    Add classes

--- src/rx_data/src/rx_data/__init__.py
+++ src/rx_data/src/rx_data/__init__.py
@@ -6,11 +6,21 @@ from .ntuple_partitioner      import NtuplePartitioner
 from .stats                   import Stats
 from .utilities               import df_from_rdf
 from .electron_bias_corrector import ElectronBiasCorrector
+from .filtered_stats          import FilteredStats
+from .ganga_info              import GangaInfo
+from .hop_calculator          import HOPCalculator
+from .mis_calculator          import MisCalculator
+from .mass_bias_corrector     import MassBiasCorrector
 
 __all__ = [
+    'MassBiasCorrector',
+    'HOPCalculator',
+    'MisCalculator',
     'ElectronBiasCorrector',
     'NtuplePartitioner', 
     'SWPCalculator',
+    'FilteredStats',
+    'GangaInfo',
     'RDFGetter', 
     'Stats', 
     'df_from_rdf',
