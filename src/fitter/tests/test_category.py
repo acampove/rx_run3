2eeecc46e57f0cf54fbb5a4fd60208f9e3f9589f
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-07 15:10:20 +0800

    Categories cannot be added, remove test

--- src/fitter/tests/test_category.py
+++ src/fitter/tests/test_category.py
@@ -5,8 +5,8 @@ import pytest
 
 from fitter      import Category
 from omegaconf   import OmegaConf
-from dmu.testing import get_model
 from dmu         import LogStore
+from dmu.testing import get_model
 from rx_common   import Block, Brem
 
 log=LogStore.add_logger('fitter:test_category')
@@ -50,33 +50,3 @@ def test_properties(brem : Brem, block : Block):
     assert cat.brem == brem
     assert cat.block== block 
 # ----------------------
-def test_brem_sum():
-    '''
-    Test adding brem categories in same block
-    '''
-    cres= OmegaConf.create(obj = {'mu' : [10., 1.]})
-
-    obs     = zfit.Space('mass', limits=(4500, 7000))
-    pdf_001 = sut.get_model(kind = 'signal', suffix = '001', obs = obs)
-    cat_001 = Category(
-        name      = 'brem_001_b1',
-        pdf       = pdf_001,
-        sumw      = 1000.,
-        cres      = cres,
-        model     = ['gauss'],
-        selection = {'mass' : '(1)'})
-
-    pdf_002 = sut.get_model(kind = 'signal', suffix = '002', obs = obs)
-    cat_002 = Category(
-        name      = 'brem_002_b1',
-        pdf       = pdf_002,
-        sumw      = 1000.,
-        cres      = cres,
-        model     = ['gauss'],
-        selection = {'mass' : '(1)'})
-
-    cat = cat_001 + cat_002
-
-    assert isinstance(cat, Category)
-
-    print(cat)
