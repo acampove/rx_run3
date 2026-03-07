21a0c1c31f7b62ad8835863e598a62b73071cc85
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-07 19:58:42 +0800

    Test only over mass corrections

--- src/fitter/tests/test_scale_reader.py
+++ src/fitter/tests/test_scale_reader.py
@@ -10,8 +10,13 @@ from rx_common import Brem
 from rx_common import Block 
 
 log=LogStore.add_logger('fitter:test_scale_reader')
+
+CORRS = [
+    Correction.mass_scale,
+    Correction.mass_resolution,
+]
 # ----------------------
-@pytest.mark.parametrize('kind' , Correction)
+@pytest.mark.parametrize('kind' , CORRS)
 @pytest.mark.parametrize('block', Block.blocks())
 @pytest.mark.parametrize('brem' , [Brem.one, Brem.two])
 def test_simple(
