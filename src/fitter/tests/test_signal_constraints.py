c0732b0b61a03428847a37f17393adc8d064947f
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-10 20:10:08 +0800

    Add extra methods to test fractions for all datasets

--- src/fitter/tests/test_signal_constraints.py
+++ src/fitter/tests/test_signal_constraints.py
@@ -3,16 +3,19 @@ Module holding functions needed to test SignalConstraints class
 '''
 import pytest
 from dmu       import LogStore
-from dmu.stats import CorrectionImplementation, Model, zfit
+from dmu.stats import CorrectionImplementation, Model, print_pdf, zfit
 from dmu.stats import ModelFactory
 from rx_common import Block, Brem
 from fitter    import SignalConstraints
+from fitter    import Category 
+from fitter    import CategoryMerger
 
 Loss = zfit.loss.ExtendedUnbinnedNLL
 zobs = zfit.Space
+zpdf = zfit.pdf.BasePDF
 log  = LogStore.add_logger('fitter:test_signal_constraints')
 
-brem_cats = [Brem.one, Brem.two]
+_BREM_CATS = [Brem.one, Brem.two]
 # ----------------------
 @pytest.fixture(scope='session', autouse=True)
 def initialize():
@@ -20,6 +23,50 @@ def initialize():
     This will run before any test
     '''
     LogStore.set_level('fitter:signal_constraints', 10)
+# ----------------------
+def _fix_tails(pdf : zpdf) -> zpdf:
+    '''
+    Parameters
+    -------------
+    pdf: Zfit PDF
+
+    Returns
+    -------------
+    Input PDF with tails fixed
+    '''
+    pars = pdf.get_params()
+    for par in pars:
+        if par.name.startswith('mu_'):
+            continue
+
+        if par.name.startswith('sg_'):
+            continue
+
+        par.floating = False
+   
+    return pdf
+# ----------------------
+def _get_signal(
+    obs  : zobs,
+    brem : Brem, 
+    block: Block):
+
+    signal_name = f'signal_brem_{brem}_b{block}'
+    scale       = CorrectionImplementation.scale
+    reso        = CorrectionImplementation.reso
+
+    with ModelFactory.correction(fixed = False):
+        fct = ModelFactory(
+            obs     = obs,
+            l_pdf   = [Model.dscb],
+            l_shared= [],
+            l_float = [],
+            d_rep   = {'mu' : scale, 'sg' : reso},
+            preffix = signal_name)
+        dscb = fct.get_pdf()
+        dscb = _fix_tails(pdf = dscb)
+
+    return dscb
 # ----------------------
 def _get_nll(
     obs   : zobs,
@@ -36,19 +83,10 @@ def _get_nll(
 
     - PDF used to fit signal
     '''
-    signal_name = f'signal_brem_{brem}_b{block}'
-    scale       = CorrectionImplementation.scale
-    reso        = CorrectionImplementation.reso
-
-    with ModelFactory.correction(fixed = False):
-        fct = ModelFactory(
-            obs     = obs,
-            l_pdf   = [Model.dscb],
-            l_shared= [],
-            l_float = [],
-            d_rep   = {'mu' : scale, 'sg' : reso},
-            preffix = signal_name)
-        dscb = fct.get_pdf()
+    dscb= _get_signal(
+        obs  = obs,
+        brem = brem, 
+        block= block)
 
     fct  = ModelFactory(
         obs     = obs,
@@ -67,9 +105,36 @@ def _get_nll(
     dat   = pdf.create_sampler()
 
     return zfit.loss.ExtendedUnbinnedNLL(model=pdf, data=dat)
+# ----------------------------------------
+def _get_category(
+    block: Block,
+    brem : Brem,
+    obs  : zobs) -> Category:
+    '''
+    Parameters
+    -------------
+
+    Returns
+    -------------
+    Category object
+    '''
+
+    pdf  = _get_signal(
+        obs  = obs,
+        brem = brem, 
+        block= block)
+
+    cat  = Category(
+        name      = f'brem_{brem}_b{block}', 
+        pdf       = pdf, 
+        sumw      = 1000., 
+        selection = {'mass' : '(1)'},
+        model     = ['gauss'])
+
+    return cat
 # ----------------------
 @pytest.mark.parametrize('block', Block.blocks())
-@pytest.mark.parametrize('brem' , brem_cats)
+@pytest.mark.parametrize('brem' , _BREM_CATS)
 def test_simple(block : Block, brem : Brem) -> None:
     '''
     Simplest test
@@ -86,6 +151,18 @@ def test_simple(block : Block, brem : Brem) -> None:
     for cns in constraints:
         log.info(cns)
 
-    assert len(constraints) == len(brem_cats)
+    assert len(constraints) == len(_BREM_CATS)
     assert isinstance(constraints, list)
+# ----------------------
+def test_add_brem_block():
+    '''
+    Add brems and then blocks
+    '''
+    obs        = zfit.Space('dummy', limits=(4500, 6000))
+    categories = [ _get_category(block = block, brem = brem, obs = obs) for block in Block.blocks() for brem in _BREM_CATS ]
+
+    mgr = CategoryMerger(categories = categories)
+    cat = mgr.get_category()
+
+    print_pdf(cat.pdf)
 # ----------------------
