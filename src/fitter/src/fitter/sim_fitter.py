8c811d4698271c3c8233dd10ac097a6281b93f67
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-12 13:36:52 +0800

    Use Category classes to offload merging and handling of PDFs

--- src/fitter/src/fitter/sim_fitter.py
+++ src/fitter/src/fitter/sim_fitter.py
@@ -19,6 +19,8 @@ from zfit.pdf            import BasePDF          as zpdf
 from zfit                import Space            as zobs
 from zfit.param          import Parameter
 
+from .category           import Category, FitCategory
+from .category_merger    import CategoryMerger, FitCategoryMerger
 from .base_fitter        import BaseFitter
 from .configs            import CombinatorialConf, MisIDConf, NonParametricConf, ParametricConf, CCbarConf 
 from .data_preprocessor  import DataPreprocessor
@@ -273,40 +275,55 @@ class SimFitter(BaseFitter, Cache):
     # ------------------------
     # Fitting section
     # ------------------------
-    def _fit_category(
+    @overload
+    def _get_fit_category(
+        self,
+        cfg      : ParametricConf,
+        skip_fit : Literal[False],
+        name     : str) -> FitCategory | None : ...
+    @overload
+    def _get_fit_category(
+        self,
+        cfg      : ParametricConf,
+        skip_fit : Literal[True],
+        name     : str) -> Category: ...
+    def _get_fit_category(
         self,
         cfg      : ParametricConf,
         skip_fit : bool,
-        category : str) -> tuple[zpdf|None,float|None,FitResult|None]:
+        name     : str) -> FitCategory | Category | None:
         '''
         Parameters
         ----------------
-        skip_fit     : If true, it will only return model, used if fit parameters were already found
-        category     : Name of fitting category
+        skip_fit : If true, it will only return model, used if fit parameters were already found
+        name     : Name of fitting category
 
         Returns
         ----------------
-        Tuple with:
-            - Fitted PDF, None if problems were found building it, e.g. too few entries
-            - Size (sum of weights) of dataset in given category.
-              If fit is skipped, returns None, because this is used to set
-              the value of the fit fraction, which should already be in the cached data.
-            - Fit result object, if fit is skipped, returns None
+        FitCategory: Fit was requested and fit worked
+        Category   : Fit was not requested
+        None       : Fit was requested but it failed
         '''
-        log.info(f'Fitting category {category}')
+        log.info(f'Fitting category {name}')
 
-        cat_cfg = cfg.categories[category]
+        cat_cfg = cfg.categories[name]
         model = self._get_pdf(
-            category= category,
+            category= name,
             cfg     = cat_cfg.model)
 
-        data  = self._d_data[category]
+        data  = self._d_data[name]
 
         sumw  = sut.yield_from_zdata(data=data)
-        if skip_fit:
-            return model, sumw, None
 
-        cut_cfg = self._get_cut_config(cfg = cfg, category = category)
+        if skip_fit:
+            return Category(
+                name      = name, 
+                pdf       = model, 
+                sumw      = sumw,
+                model     = cat_cfg.model.pdfs,
+                selection = cat_cfg.selection)
+
+        cut_cfg = self._get_cut_config(cfg = cfg, category = name)
 
         if sumw < MIN_FIT_ENTRIES:
             log.warning(f'Found to few entries {sumw:.1f} < {MIN_FIT_ENTRIES}, skipping {self._component} component')
@@ -316,9 +333,9 @@ class SimFitter(BaseFitter, Cache):
                 data     = data,
                 model    = None,
                 res      = None,
-                out_path = self._out_path / category )
+                out_path = self._out_path / name )
 
-            return None, 0, None
+            return None 
 
         res = self._fit(
             data = data, 
@@ -331,11 +348,17 @@ class SimFitter(BaseFitter, Cache):
             data     = data,
             model    = model,
             res      = res,
-            out_path = self._out_path / category)
+            out_path = self._out_path / name)
 
         model = self._fix_tails(pdf=model, res=res)
 
-        return model, sumw, res
+        return FitCategory(
+            name      = name,
+            pdf       = model,
+            sumw      = sumw,
+            res       = res,
+            model     = cat_cfg.model.pdfs,
+            selection = cat_cfg.selection)
     # ----------------------
     def _get_cut_config(
         self, 
@@ -397,10 +420,19 @@ class SimFitter(BaseFitter, Cache):
         return par
     # ------------------------
     @overload
-    def _get_full_model(self, cfg : ParametricConf, skip_fit : Literal[True]) -> zpdf: ...
+    def _get_merged_category(
+        self, 
+        cfg      : ParametricConf, 
+        skip_fit : Literal[True]) -> Category: ...
     @overload
-    def _get_full_model(self, cfg : ParametricConf, skip_fit : Literal[False]) -> tuple[zpdf,FitResult] | None: ...
-    def _get_full_model(self, cfg : ParametricConf, skip_fit : bool) -> tuple[zpdf,FitResult] | zpdf | None:
+    def _get_merged_category(
+        self, 
+        cfg      : ParametricConf, 
+        skip_fit : Literal[False]) -> FitCategory | None: ...
+    def _get_merged_category(
+        self, 
+        cfg      : ParametricConf, 
+        skip_fit : bool) -> FitCategory | Category | None:
         '''
         Parameters
         ---------------
@@ -409,76 +441,43 @@ class SimFitter(BaseFitter, Cache):
 
         Returns
         ---------------
-        Tuple with:
-
-        - PDF for the combined categories with the parameters set
-        to the fitted values
-        - Instance of FitResult
+        Category    If fit is not requested
+        FitCategory If fit was requested and succeeded
+        None        If fit was requested and failed for all categories
         '''
-        l_pdf   : list[zpdf      ] = []
-        l_yield : list[float     ] = []
-        l_res   : list[FitResult ] = []
-        for category in cfg.categories:
-            model, sumw, res = self._fit_category(
-                cfg          = cfg,
-                skip_fit     = skip_fit,
-                category     = category)
+        cats_fit : list[FitCategory] = []
+        cats_raw : list[Category]    = []
+        for name in cfg.categories:
+            cat = self._get_fit_category(
+                cfg      = cfg,
+                skip_fit = skip_fit,
+                name     = name)
 
-            if model is None or res is None or sumw is None:
-                log.warning(f'Skipping category {category}')
+            if cat is None:
+                log.warning(f'Skipping category {name}')
                 continue
 
-            l_pdf.append(model)
-            l_yield.append(sumw)
-            l_res.append(res)
+            if isinstance(cat, FitCategory):
+                cats_fit.append(cat)
+            else:
+                cats_raw.append(cat)
 
-        if len(l_pdf) == 0:
+        cats = cats_fit + cats_raw
+        if len(cats) == 1:
+            return cats[0]
+
+        if len(cats) == 0:
+            log.warning('No categories found, dropping component')
             return None
 
-        return self._merge_categories(
-            cfg    = cfg,
-            l_pdf  = l_pdf,
-            l_yield= l_yield,
-            l_res  = l_res)
-    # ------------------------
-    def _merge_categories(
-        self,
-        cfg     : ParametricConf,
-        l_pdf   : list[zpdf],
-        l_yield : list[float],
-        l_res   : list[FitResult]) -> tuple[zpdf,FitResult]:
-        '''
-        Parameters
-        -----------------
-        l_pdf  : List of zfit PDFs from fit, one per category
-        l_yield: List of yields from MC sample, not the fitted one
-        l_res  : List of fit result objects holding parameter values from fits
+        if skip_fit:
+            mgr = CategoryMerger(categories = cats_raw)
+        else:
+            mgr = FitCategoryMerger(categories = cats_fit)
 
-        Returns
-        -----------------
-        Tuple with:
+        cat = mgr.get_category()
 
-        - Full PDF, i.e. sum of components
-        - Merged dictionary of parameters
-        '''
-        if len(l_pdf) == 1:
-            return l_pdf[0], l_res[0]
-
-        log.debug(60 * '-')
-        log.debug(f'{"Fraction":<50}{"Value":<10}')
-        log.debug(60 * '-')
-        l_frac = [
-            self._get_fraction(
-                sumw,
-                total   = sum(l_yield),
-                category= category)
-            for sumw, category in zip(l_yield, cfg.categories, strict=True) ]
-        log.debug(60 * '-')
-
-        full_model = zfit.pdf.SumPDF(l_pdf, l_frac)
-        full_res   = FitResult.merge(results = l_res)
-
-        return full_model, full_res
+        return cat
     # ------------------------
     def _get_kde(self, cfg : NonParametricConf) -> zpdf | None:
         '''
@@ -576,21 +575,17 @@ class SimFitter(BaseFitter, Cache):
 
             # If caching, need only model, second return value
             # Is None because no fit happened 
-            model = self._get_full_model(skip_fit=True, cfg = self._cfg)
-            model = self._fix_tails(pdf=model, res=res)
-
-            return model
+            cat = self._get_merged_category(skip_fit=True, cfg = self._cfg)
+            return self._fix_tails(pdf=cat.pdf, res=res)
 
         log.info(f'Fitting, could not find cached parameters in {result_path}')
 
-        val = self._get_full_model(skip_fit=False, cfg = self._cfg)
-        if val is None:
+        cat = self._get_merged_category(skip_fit=False, cfg = self._cfg)
+        if cat is None:
             return None
 
-        full_model, fres = val
-
-        fres.to_json(path = result_path)
+        cat.res.to_json(path = result_path)
 
         self._cache()
-        return full_model
+        return cat.pdf 
 # ------------------------
