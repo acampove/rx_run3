c85382460454d0187f7929aa5f323fc97b961ec4
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-18 16:52:37 +0800

    Add name to likelihood factory

--- src/fitter/src/fitter_scripts/fit_rx_rare.py
+++ src/fitter/src/fitter_scripts/fit_rx_rare.py
@@ -185,6 +185,7 @@ def _fit(cfg : RXFitConfig) -> None:
     This is where DataFitter is used
     '''
     ftr = LikelihoodFactory(
+        name   = 'rare_ee',
         obs    = cfg.observable,
         q2bin  = cfg.q2bin,
         sample = DATA_SAMPLE,
