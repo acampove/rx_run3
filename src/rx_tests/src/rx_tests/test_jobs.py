6f107912701a921a6fc697703fc309a8cc2ffcfd
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-25 19:10:25 +0800

    Add implementation

--- src/rx_tests/src/rx_tests/test_jobs.py
+++ src/rx_tests/src/rx_tests/test_jobs.py
@@ -1,8 +1,9 @@
 '''
-
+Module holding TestJobs class
 '''
 
 from rx_tests import TestConfig
+from rx_tests import PackageConf
 
 # --------------------
 class TestJobs:
@@ -11,4 +12,40 @@ class TestJobs:
     job names mapped to list of commands
     '''
     # --------------------
-    def __init__(self, cfg : TestConfig)
+    def __init__(self, cfg : TestConfig):
+        '''
+        Parameters
+        ---------------
+        cfg: Configuration
+        '''
+        self._cfg = cfg
+    # ----------------------
+    def _cmd_from_cfg(
+        self, 
+        group: int,
+        cfg  : PackageConf) -> str:
+        '''
+        Parameters
+        -------------
+        cfg  : Config for package tests
+        group: Index of group
+
+        Returns
+        -------------
+        String representing basic pytest command
+        '''
+        return f'pytest {cfg.path} --splits {cfg.splits} --group {group}'
+    # --------------------
+    def get_jobs(self) -> dict[str,list[str]]:
+        '''
+        Returns
+        -----------------
+        dictionary mapping job names to list of
+        commands
+        '''
+        data : dict[str, list[str]] = dict()
+        for test, cfg in self._cfg.items():
+            data[test] = [ self._cmd_from_cfg(cfg = cfg, group = group)  for group in range(cfg.splits) ]
+
+        return data
+# --------------------
