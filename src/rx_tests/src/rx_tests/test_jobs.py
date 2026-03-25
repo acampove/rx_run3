9df0502e1b10456d709660144299d83e766c74b2
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-25 21:23:57 +0800

    Send outputs to XML

--- src/rx_tests/src/rx_tests/test_jobs.py
+++ src/rx_tests/src/rx_tests/test_jobs.py
@@ -2,9 +2,12 @@
 Module holding TestJobs class
 '''
 
+import os
+from pathlib  import Path
 from rx_tests import TestConfig
 from rx_tests import PackageConf
 
+ANADIR = Path(os.environ['ANADIR'])
 # --------------------
 class TestJobs:
     '''
@@ -23,18 +26,24 @@ class TestJobs:
     def _cmd_from_cfg(
         self, 
         group: int,
+        test : str,
         cfg  : PackageConf) -> str:
         '''
         Parameters
         -------------
         cfg  : Config for package tests
+        test : Name of test
         group: Index of group
 
         Returns
         -------------
         String representing basic pytest command
         '''
-        return f'pytest {cfg.path} --splits {cfg.splits} --group {group}'
+        out_path = ANADIR / self._cfg.output / f'{test}_{cfg.splits:03}_{group:03}.xml'
+
+        val = f'pytest {cfg.path} --splits {cfg.splits} --group {group} --junitxml={out_path}'
+
+        return val
     # --------------------
     def get_jobs(self) -> dict[str,list[str]]:
         '''
@@ -44,8 +53,8 @@ class TestJobs:
         commands
         '''
         data : dict[str, list[str]] = dict()
-        for test, cfg in self._cfg.items():
-            data[test] = [ self._cmd_from_cfg(cfg = cfg, group = group)  for group in range(1, cfg.splits + 1) ]
+        for test, cfg in self._cfg.projects.items():
+            data[test] = [ self._cmd_from_cfg(cfg = cfg, group = group, test = test) for group in range(1, cfg.splits + 1) ]
 
         return data
 # --------------------
