3d9cbfef30f067ee73b6536e5828f06a22768faf
Angel Campoverde <angel.campoverde@cern.ch>
2026-03-25 20:54:03 +0800

    Add find_path validator
    Need to replace package name with path to tests

--- src/rx_tests/src/rx_tests/test_config.py
+++ src/rx_tests/src/rx_tests/test_config.py
@@ -14,8 +14,38 @@ class PackageConf(BaseModel):
     Class meant to represent testing configuration
     for single project
     '''
-    path   : str
+    path   : str 
     splits : int = Field(gt = 0, lt = 50)
+    # --------------------------------
+    @field_validator('path', mode = 'before')
+    @classmethod
+    def find_path(cls, path : str) -> str:
+        '''
+        Parameters
+        ------------
+        path: Package name 
+
+        Returns
+        ------------
+        Full path to tests/ directory
+        '''
+        spec = iut.find_spec(path)
+        if spec is None:
+            raise ValueError(f'Cannot find path for package: {path}')
+
+        origin = spec.origin
+        if origin is None:
+            raise ValueError(f'Path to init not found in: {spec}')
+
+        fpath = Path(origin)
+        if not fpath.exists():
+            raise ValueError(f'Cannot find: {fpath}')
+
+        test_path = fpath.parent.parent.parent / 'tests'
+        if not test_path.exists():
+            raise ValueError(f'Cannot find: {test_path}')
+
+        return str(test_path)
 # ---------------------------------------------------
 class TestConfig(RootModel[dict[str,PackageConf]]):
     '''
